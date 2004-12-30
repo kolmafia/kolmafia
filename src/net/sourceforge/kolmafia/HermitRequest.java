7e731914243b703f7f5649a62b59a24ddebf105c
shwei <shwei@users.sourceforge.net>
2004-12-30 00:59:01 +0000

    handling error states

--- src/net/sourceforge/kolmafia/HermitRequest.java
+++ src/net/sourceforge/kolmafia/HermitRequest.java
@@ -68,6 +68,12 @@ public class HermitRequest extends KoLRequest
 
 	public void run()
 	{
+		// If an error state occurred, return from this
+		// request, since there's no content to parse
+
+		if ( isErrorState || responseCode != 200 )
+			return;
+
 		String item = client.getSettings().getProperty( "hermitTrade" );
 
 		if ( item == null )
