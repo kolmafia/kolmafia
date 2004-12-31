973239d565f8120e5b806faacb9cf49178cc3b27
shwei <shwei@users.sourceforge.net>
2004-12-31 05:14:27 +0000

    password deletion

--- src/net/sourceforge/kolmafia/KoLRequest.java
+++ src/net/sourceforge/kolmafia/KoLRequest.java
@@ -470,7 +470,7 @@ public class KoLRequest implements Runnable
 
 					logStream.println(
 						"\n ==== HYPERTEXT ====\n" +
-						replyContent.replaceAll( " pwd=\'[\\d]*\'", "" ) +
+						replyContent.replaceAll( " pwd=[\'\"][\\d]*[\'\"]", "" ) +
 						"\n ==== PLAINTEXT ====\n" +
 						replyContent.replaceAll( "><", "" ).replaceAll( "<.*?>", "\n" ) +
 						"\n ==================="
