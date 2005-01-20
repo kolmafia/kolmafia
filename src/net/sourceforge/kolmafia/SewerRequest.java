b62bd2062b9d22ad4dc30d05fbabb172c2607e5c
shwei <shwei@users.sourceforge.net>
2005-01-20 06:17:35 +0000

    error check occurs in subroutines, not here

--- src/net/sourceforge/kolmafia/SewerRequest.java
+++ src/net/sourceforge/kolmafia/SewerRequest.java
@@ -71,12 +71,6 @@ public class SewerRequest extends KoLRequest
 
 	public void run()
 	{
-		// If an error state occurred, return from this
-		// request, since there's no content to parse
-
-		if ( isErrorState || responseCode != 200 )
-			return;
-
 		if ( isLuckySewer )
 			runLuckySewer();
 		else
