fbf5e813630e4d9073a92c88b9373ef6720efb2a
shwei <shwei@users.sourceforge.net>
2005-02-04 00:25:40 +0000

    fixed problem in removing last element

--- src/net/java/dev/spellcast/utilities/LockableListModel.java
+++ src/net/java/dev/spellcast/utilities/LockableListModel.java
@@ -763,7 +763,7 @@ public class LockableListModel extends javax.swing.AbstractListModel
 
 		private synchronized void intervalRemoved( LockableListModel source, int index0, int index1 )
 		{
-			if ( mirrorImage == null || source == null || index1 < 0 || index1 >= size() )
+			if ( mirrorImage == null || source == null || index1 < 0 || index1 >= mirrorImage.size() )
 				return;
 
 			for ( int i = index1; i >= index0; --i )
