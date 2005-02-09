f6561efa993764638386afc265983df94597931f
shwei <shwei@users.sourceforge.net>
2005-02-09 07:48:52 +0000

    green message request capabilities

---
+++ src/net/sourceforge/kolmafia/GreenMessageRequest.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2005, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia development team" nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia;
+import java.util.List;
+
+/**
+ * An extension of a <code>KoLRequest</code> which specifically handles
+ * donating to the Hall of the Legends of the Times of Old.
+ */
+
+public class GreenMessageRequest extends KoLRequest
+{
+	private String recipient, message;
+	private Object [] attachments;
+
+	public GreenMessageRequest( KoLmafia client, String recipient, String message, Object [] attachments )
+	{
+		super( client, "sendmessage.php" );
+
+		addFormField( "action", "send" );
+		addFormField( "pwd", client.getPasswordHash() );
+		addFormField( "towho", recipient );
+		addFormField( "savecopy", "on" );
+		addFormField( "message", message );
+
+		this.recipient = recipient;
+		this.message = message;
+		this.attachments = attachments;
+	}
+
+	/**
+	 * Runs the request.  Note that this does not report an error if it fails;
+	 * it merely parses the results to see if any gains were made.
+	 */
+
+	public void run()
+	{
+		addFormField( "sendmeat", "" );
+
+		// First, check to see how many attachments are to be
+		// placed in the closet - if there's too many,
+		// then you'll need to break up the request
+
+		if ( attachments == null || attachments.length == 0 )
+			return;
+
+		if ( attachments.length > 11 )
+		{
+			int currentBaseIndex = 0;
+			int remainingItems = attachments.length;
+
+			Object [] itemHolder = null;
+
+			while ( remainingItems > 0 )
+			{
+				itemHolder = new Object[ remainingItems < 11 ? remainingItems : 11 ];
+
+				for ( int i = 0; i < itemHolder.length; ++i )
+					itemHolder[i] = attachments[ currentBaseIndex + i ];
+
+				// For each broken-up request, you create a new ItemStorage request
+				// which will create the appropriate data to post.
+
+				(new GreenMessageRequest( client, recipient, message, itemHolder )).run();
+
+				currentBaseIndex += 11;
+				remainingItems -= 11;
+			}
+
+			// Since all the sub-requests were run, there's nothing left
+			// to do - simply return from this method.
+
+			return;
+		}
+
+		boolean attachedMeat = false;
+
+		for ( int i = 0; i < attachments.length; ++i )
+		{
+			AdventureResult result = (AdventureResult) attachments[i];
+			int itemID = TradeableItemDatabase.getItemID( result.getName() );
+
+			if ( itemID != -1 )
+			{
+				int index = attachedMeat ? i : i + 1;
+				addFormField( "whichitem" + index, "" + itemID );
+				addFormField( "howmany" + index, "" + result.getCount() );
+			}
+			else if ( result.getName().equals( AdventureResult.MEAT ) )
+			{
+				addFormField( "sendmeat", "" + result.getCount() );
+				attachedMeat = true;
+			}
+		}
+
+		// Once all the form fields are broken up, this
+		// just calls the normal run method from KoLRequest
+		// to execute the request.
+
+		super.run();
+
+		// If an error state occurred, return from this
+		// request, since there's no content to parse
+
+		if ( isErrorState || responseCode != 200 )
+			return;
+
+		// With that done, the client needs to be updated
+		// to note that the items were sent.
+
+		AdventureResult currentResult, negatedResult;
+		for ( int i = 0; i < attachments.length; ++i )
+		{
+			currentResult = (AdventureResult) attachments[i];
+			if ( currentResult.isItem() )
+			{
+				negatedResult = new AdventureResult( currentResult.getName(), 0 - currentResult.getCount() );
+				client.addToResultTally( negatedResult );
+			}
+		}
+	}
+}
