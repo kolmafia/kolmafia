1c55d5a88748a6ac6b2d7c101025035ab18e10c1
shwei <shwei@users.sourceforge.net>
2005-02-25 04:58:39 +0000

    standard delay reduced to 0.8 seconds

--- src/net/sourceforge/kolmafia/KoLRequest.java
+++ src/net/sourceforge/kolmafia/KoLRequest.java
@@ -66,7 +66,7 @@ import java.util.regex.Pattern;
 
 public class KoLRequest implements Runnable, KoLConstants
 {
-	protected static final int REFRESH_RATE = 1600;
+	protected static final int REFRESH_RATE = 800;
 
 	private static String KOL_HOST = "www.kingdomofloathing.com";
 	private static String KOL_ROOT = "http://" + KOL_HOST + "/";
