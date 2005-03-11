08e9a1e4faf4ce6c5b968d81fa3baff87e5286df
shwei <shwei@users.sourceforge.net>
2005-03-11 20:44:36 +0000

    fixed item creation request not disabling pane when resuming after explosion

--- src/net/sourceforge/kolmafia/ItemCreationRequest.java
+++ src/net/sourceforge/kolmafia/ItemCreationRequest.java
@@ -200,12 +200,11 @@ public class ItemCreationRequest extends KoLRequest implements Comparable
 		int index = client.getInventory().indexOf( item );
 		int beforeRequestQuantity = (index == -1) ? 0 : ((AdventureResult)client.getInventory().get( index )).getCount();
 
-		updateDisplay( NOCHANGE_STATE, "Creating " + item.getName() + " (" + quantityNeeded + ")..." );
-
 		// Auto-create chef or bartender if one doesn't
 		// exist and the user has opted to repair.
 
 		autoRepairBoxServant();
+		updateDisplay( DISABLED_STATE, "Creating " + item.getName() + " (" + quantityNeeded + ")..." );
 
 		super.run();
 
