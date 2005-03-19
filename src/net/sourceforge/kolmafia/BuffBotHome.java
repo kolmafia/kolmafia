0ddb0dd0b8b6167e96a9b356825be44d3835c0e7
shwei <shwei@users.sourceforge.net>
2005-03-19 23:31:32 +0000

    removing adventure tabs access, streamlining code phase 1

--- src/net/sourceforge/kolmafia/BuffBotHome.java
+++ src/net/sourceforge/kolmafia/BuffBotHome.java
@@ -47,68 +47,58 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 
-
-
 /**
  * Holder for the BuffBot log (which should survive outside of
  * the BuffBot Frame
- *
  */
-public class BuffBotHome {
-	private BuffBotLog BBLog;
-	private DateFormat LogDF;
+
+public class BuffBotHome{
+
+	private static final DateFormat logDF =
+		DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+
+	private static final SimpleDateFormat logSDF = new SimpleDateFormat("D");
+
 	private KoLmafia client;
+	private BuffBotLog BBLog;
 	private PrintStream BBLogFile;
 	private JTabbedPane advTabs;
-	private String LogFileName;
-	
-	/** Creates a new instance of BuffBotHome */
-	public BuffBotHome(KoLmafia client) {
-		this.client = client;
-		LogDF = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
-		LogFileName = "BuffBot??.log";
-		
-	}
-	
-	/** create the BuffBotLog (<code>BBLog</code>) and the
-	 *      BuffBotLogFile (<code>BBLogFile</code>)
-	 *      if they don't already exist
+
+	/**
+	 * Creates a new instance of <code>BuffBotHome</code>.
 	 */
-	public void initializeBBLogs(){
-		if (!(BBLog instanceof BuffBotLog)){
-			BBLog = new BuffBotLog();
-		}
-		initializeBBLogFile();
-		BBLog.append("Today's Log: " + LogFileName + " \n");
+
+	public BuffBotHome(KoLmafia client){
+		this.client = client;
 	}
-	
+
+	/**
+	 * Create the <code>BuffBotLog and its associated file, if
+	 * they don't already exist.
+	 */
+
+	public void initializeBBLogs(){
+		if (!(BBLog instanceof BuffBotLog))
+			BBLog = new BuffBotLog();
+		initializeBBLogFile();
+	}
+
 	public BuffBotLog getBBLog(){
 		return BBLog;
 	}
-	
-	public void setAdventureTabs(JTabbedPane tabs){
-		advTabs = tabs;
-	}
-	
-	public JTabbedPane getAdventureTabs(){
-		return advTabs;
-	}
-	
-	
+
 	/**
 	 * For now, this will be a simple JTextArea.
 	 * Future upgrades may support formatted text .
 	 */
 	public class BuffBotLog extends JTextArea {
-		
+
 		private BuffBotLog(){
 			this.setEditable( false );
-			
 		}
-		
+
 		public void BBLogEntry(String logEntry){
-			Date now = new Date();
-			String ExtendedLogEntry = LogDF.format(now) + " " + logEntry + "\n";
+			String ExtendedLogEntry = logDF.format(new Date()) + " " + logEntry + "\n";
 			this.append(ExtendedLogEntry);
 			BBLogFile.println(ExtendedLogEntry);
 //            Not currently adding to system log. Remove comments to do this.
@@ -116,7 +106,7 @@ public class BuffBotHome {
 //                client.getLogStream().println("BuffBot:" + ExtendedLogEntry );
 //            }
 		}
-		
+
 		public void ClearLogDisplay(){
 			BBLog.selectAll();
 			BBLog.replaceSelection("");
@@ -128,26 +118,25 @@ public class BuffBotHome {
 	 * data directory if one does not exist, or appends to the existing
 	 * log. A new file is opened each day.
 	 */
-	
+
 	public void initializeBBLogFile() {
 		// First, ensure that a BBlog stream has not already been
 		// initialized - this can be checked by observing what
 		// class the current log stream is.
-		
-		if ( BBLogFile instanceof BBLogFileClass )
+
+		if ( BBLogFile instanceof BuffBotLogStream )
 			return;
-		
+
 		try {
-			String DayOfYear = (new SimpleDateFormat("D")).format(new Date());
+			String DayOfYear = logSDF.format(new Date());
 			String characterName = client.getLoginName();
-			String noExtensionName = characterName.replaceAll( "\\p{Punct}", "" ).replaceAll( " ", "_" );
+			String noExtensionName = characterName.replaceAll( "\\p{Punct}", "" ).replaceAll( " ", "_" ).toLowerCase();
 			File f = new File( KoLmafia.DATA_DIRECTORY + noExtensionName + "_BuffBot" + DayOfYear + ".log" );
-			
-			LogFileName = f.getName();
+
 			if ( !f.exists() )
 				f.createNewFile();
-			
-			BBLogFile = new BBLogFileClass( f );
+
+			BBLogFile = new BuffBotLogStream( f );
 		} catch ( IOException e ) {
 			// This should not happen, unless the user
 			// security settings are too high to allow
@@ -155,45 +144,41 @@ public class BuffBotHome {
 			// pretend for now that everything works.
 		}
 	}
-	
+
 	/**
 	 * De-initializes the BBlog stream.  This method is not currently
 	 * ever called.
 	 * TODO Change to call when BuffBotPanel is closed.
 	 */
-	
+
 	public void deinitializeBBLogFile() {
 		if ( BBLogFile != null )
 			BBLogFile.close();
 		BBLogFile = new NullStream();
 	}
-	
-	
+
+
 	/**
 	 * An extension of {@link java.io.PrintStream} which handles BuffBot
 	 * logging message. All logs generated
 	 * by this class will be appended to provided files.
 	 */
-	public class BBLogFileClass extends java.io.PrintStream{
-		
-		public BBLogFileClass( String fileName ) throws FileNotFoundException{
+
+	private class BuffBotLogStream extends java.io.PrintStream{
+
+		public BuffBotLogStream( String fileName ) throws FileNotFoundException{
 			this( new File(fileName ) );
 		}
-		public BBLogFileClass( File file ) throws FileNotFoundException{
+		public BuffBotLogStream( File file ) throws FileNotFoundException{
 			super( new FileOutputStream( file, true ) );
 			println();
 			println();
-			println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" );
-			println( "                  Beginning New BuffBot Logging Session          " );
-			println( "                          " + LogDF.format(new Date()) + "       " );
-			println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" );
+			println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" );
+			println( "          Beginning New BuffBot Logging Session          " );
+			println( "                  " + logDF.format(new Date()) + "       " );
+			println( "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" );
 			println();
 		}
 	}
-	
+
 }
-
-
-
-
-
