fd570568e08d5abad9774f1d6a3f64c8c4229e7f
shwei <shwei@users.sourceforge.net>
2005-06-01 07:09:46 +0000

    familiar kills added

--- src/net/sourceforge/kolmafia/FamiliarRequest.java
+++ src/net/sourceforge/kolmafia/FamiliarRequest.java
@@ -96,15 +96,15 @@ public class FamiliarRequest extends KoLRequest
 		}
 		else
 		{
-			Matcher currentMatcher = Pattern.compile( "Current Familiar.*?</b><br>(\\d+) pound (.*?)<table><tr><td valign=center>Equipment:.*?<td valign=center>(.*?)</td>" ).matcher( responseText );
+			Matcher currentMatcher = Pattern.compile( "Current Familiar.*?</b><br>(\\d+) pound (.*?) \\(\\d+ kills\\)<table><tr><td valign=center>Equipment:.*?<td valign=center>(.*?)</td>" ).matcher( responseText );
 			if ( currentMatcher.find() )
 			{
-				characterData.setFamiliarDescription( currentMatcher.group(2), Integer.parseInt( currentMatcher.group(1) ) );
+				characterData.setFamiliarDescription( currentMatcher.group(2).trim(), Integer.parseInt( currentMatcher.group(1) ) );
 				characterData.setFamiliarItem( currentMatcher.group(3) );
 			}
 			else
 			{
-				currentMatcher = Pattern.compile( "Current Familiar.*?</b><br>(\\d+) pound (.*?)<p>" ).matcher( responseText );
+				currentMatcher = Pattern.compile( "Current Familiar.*?</b><br>(\\d+) pound (.*?) \\(\\d+ kills\\)<p>" ).matcher( responseText );
 				if ( currentMatcher.find() )
 					characterData.setFamiliarDescription( currentMatcher.group(2), Integer.parseInt( currentMatcher.group(1) ) );
 			}
