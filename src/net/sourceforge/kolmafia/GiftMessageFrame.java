0677dbe916c6c92f89797b518f899748ee015ff5
shwei <shwei@users.sourceforge.net>
2005-06-03 04:00:54 +0000

    adding gift sending

---
+++ src/net/sourceforge/kolmafia/GiftMessageFrame.java
@@ -0,0 +1,275 @@
+/**
+ * Copyright (c) 2005, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia development team" nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia;
+
+// layout
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+// event listeners
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+// containers
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
+import javax.swing.JOptionPane;
+
+// other imports
+import net.java.dev.spellcast.utilities.SortedListModel;
+import net.java.dev.spellcast.utilities.JComponentUtilities;
+
+public class GiftMessageFrame extends KoLFrame
+{
+	private static final int COLS = 32;
+	private static final int ROWS = 8;
+
+	private JTextField recipientEntry;
+	private JTextField meatEntry;
+	private JTextArea insideEntry, outsideEntry;
+	private JComboBox attachSelect;
+	private JComboBox packageSelect;
+	private JButton attachButton;
+
+	private SortedListModel attachedItems;
+	private JLabel sendMessageStatus;
+
+	public GiftMessageFrame( KoLmafia client )
+	{	this( client, "" );
+	}
+
+	public GiftMessageFrame( KoLmafia client, String recipient )
+	{	this( client, recipient, "" );
+	}
+
+	public GiftMessageFrame( KoLmafia client, String recipient, String quotedMessage )
+	{
+		super( "KoLmafia: Send a Green Message", client );
+
+		this.attachedItems = new SortedListModel();
+		this.contentPanel = new GiftMessagePanel( recipient, quotedMessage );
+
+		this.getContentPane().setLayout( new CardLayout( 10, 10 ) );
+		this.getContentPane().add( contentPanel, "" );
+
+		setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+		addWindowListener( new ReturnFocusAdapter() );
+		setResizable( false );
+	}
+
+	public void setEnabled( boolean isEnabled )
+	{
+	}
+
+	private class GiftMessagePanel extends NonContentPanel
+	{
+		public GiftMessagePanel( String recipient, String quotedMessage )
+		{
+			super( "send", "clear", new Dimension( 80, 20 ), new Dimension( 320, 20 ) );
+
+			recipientEntry = new JTextField( recipient );
+			meatEntry = new JTextField();
+
+			JPanel attachPanel = new JPanel();
+			attachPanel.setLayout( new BorderLayout( 0, 0 ) );
+			attachSelect = new JComboBox( attachedItems );
+			attachPanel.add( attachSelect, BorderLayout.CENTER );
+			attachButton = new JButton( JComponentUtilities.getSharedImage( "icon_plus.gif" ) );
+			JComponentUtilities.setComponentSize( attachButton, 20, 20 );
+			attachButton.addActionListener( new AttachItemListener() );
+			attachPanel.add( attachButton, BorderLayout.EAST );
+
+			packageSelect = new JComboBox( GiftMessageRequest.PACKAGES );
+
+			VerifiableElement [] elements = new VerifiableElement[4];
+			elements[0] = new VerifiableElement( "Target:  ", recipientEntry );
+			elements[1] = new VerifiableElement( "Package:  ", packageSelect );
+			elements[2] = new VerifiableElement( "Items:  ", attachPanel );
+			elements[3] = new VerifiableElement( "Meat: ", meatEntry );
+
+			setContent( elements );
+
+			outsideEntry = new JTextArea( ROWS, COLS );
+			outsideEntry.setLineWrap( true );
+			outsideEntry.setWrapStyleWord( true );
+			outsideEntry.setText( quotedMessage );
+
+			JScrollPane outsideScroll = new JScrollPane( outsideEntry, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER );
+
+			JPanel outsidePanel = new JPanel();
+			outsidePanel.setLayout( new BorderLayout( 0, 0 ) );
+			outsidePanel.add( JComponentUtilities.createLabel( "Outside Package", JLabel.CENTER, Color.black, Color.white ), BorderLayout.NORTH );
+			outsidePanel.add( outsideScroll, BorderLayout.CENTER );
+
+			insideEntry = new JTextArea( ROWS, COLS );
+			insideEntry.setLineWrap( true );
+			insideEntry.setWrapStyleWord( true );
+			insideEntry.setText( quotedMessage );
+
+			JScrollPane insideScroll = new JScrollPane( insideEntry, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER );
+
+			JPanel insidePanel = new JPanel();
+			insidePanel.setLayout( new BorderLayout( 0, 0 ) );
+			insidePanel.add( JComponentUtilities.createLabel( "Inside Package", JLabel.CENTER, Color.black, Color.white ), BorderLayout.NORTH );
+			insidePanel.add( insideScroll, BorderLayout.CENTER );
+
+			JPanel centerPanel = new JPanel();
+			centerPanel.setLayout( new GridLayout( 2, 1, 10, 10 ) );
+			centerPanel.add( outsidePanel );
+			centerPanel.add( insidePanel );
+
+			add( centerPanel, BorderLayout.CENTER );
+		}
+
+		public void actionConfirmed()
+		{	(new SendGiftMessageThread()).start();
+		}
+
+		public void actionCancelled()
+		{
+			recipientEntry.setText( "" );
+			meatEntry.setText( "" );
+			insideEntry.setText( "" );
+			attachedItems.clear();
+		}
+
+		public void setEnabled( boolean isEnabled )
+		{
+			super.setEnabled( isEnabled );
+			recipientEntry.setEnabled( isEnabled );
+			insideEntry.setEnabled( isEnabled );
+			attachSelect.setEnabled( isEnabled );
+			attachButton.setEnabled( isEnabled );
+		}
+
+		private class SendGiftMessageThread extends Thread
+		{
+			public SendGiftMessageThread()
+			{
+				super( "Green-Message-Thread" );
+				setDaemon( true );
+			}
+
+			public void run()
+			{
+				if ( client == null )
+					return;
+
+				GiftMessagePanel.this.setEnabled( false );
+				(new GiftMessageRequest( client, recipientEntry.getText(), insideEntry.getText(), outsideEntry.getText(),
+					packageSelect.getSelectedItem(), attachedItems.toArray(), getValue( meatEntry ) )).run();
+				GiftMessagePanel.this.setEnabled( true );
+
+				if ( client.permitsContinue() )
+				{
+					client.updateDisplay( ENABLED_STATE, "Gift sent to " + recipientEntry.getText() );
+					String closeWindowSetting = client.getSettings().getProperty( "closeSending" );
+					if ( closeWindowSetting != null && closeWindowSetting.equals( "true" ) )
+						GiftMessageFrame.this.dispose();
+				}
+				else
+					client.updateDisplay( ERROR_STATE, "Failed to send gift to " + recipientEntry.getText() );
+
+			}
+		}
+	}
+
+	/**
+	 * Internal class used to handle attaching items to the message.  This
+	 * is done by prompting the user with a dialog box where they choose
+	 * the item they wish to attach.  Note that only one of the item will
+	 * be attached at a time.  This item cannot be removed after attaching.
+	 */
+
+	private class AttachItemListener implements ActionListener
+	{
+		public void actionPerformed( ActionEvent e )
+		{
+			try
+			{
+				AdventureResult [] possibleValues =
+					client == null ? new AdventureResult[0] : new AdventureResult[ client.getInventory().size() ];
+
+				Object [] items = client.getInventory().toArray();
+
+				AdventureResult attachment = (AdventureResult) JOptionPane.showInputDialog(
+					null, "Add to gift...", "Input", JOptionPane.INFORMATION_MESSAGE, null,
+					possibleValues, possibleValues[0] );
+
+				int existingIndex = attachedItems.indexOf( attachment );
+				if ( existingIndex == -1 )
+					return;
+
+				int defaultCount = ((AdventureResult)client.getInventory().get( client.getInventory().indexOf( attachment ) )).getCount();
+
+				int attachmentCount = df.parse( JOptionPane.showInputDialog(
+					"Adding " + attachment.getName() + "...", String.valueOf( defaultCount ) ) ).intValue();
+
+				if ( existingIndex != -1 )
+					attachedItems.remove( existingIndex );
+
+				AdventureResult.addResultToList( attachedItems, new AdventureResult( attachment.getItemID(), attachmentCount ) );
+				attachedItems.setSelectedIndex( attachedItems.size() - 1 );
+			}
+			catch ( Exception e1 )
+			{
+				// If an exception happened, the attachment should not occur.
+				// Which means, if nothing is done, everything works great.
+			}
+		}
+	}
+
+	/**
+	 * Main class used to view the user interface without having to actually
+	 * start the program.  Used primarily for user interface testing.
+	 */
+
+	public static void main( String [] args )
+	{
+		KoLFrame test = new GiftMessageFrame( null );
+		test.pack();  test.setVisible( true );  test.requestFocus();
+	}
+}
---
+++ src/net/sourceforge/kolmafia/GiftMessageRequest.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2005, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia development team" nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia;
+
+import java.util.List;
+import net.java.dev.spellcast.utilities.LockableListModel;
+
+/**
+ * An extension of a <code>KoLRequest</code> which specifically handles
+ * donating to the Hall of the Legends of the Times of Old.
+ */
+
+public class GiftMessageRequest extends KoLRequest
+{
+	private String recipient, outsideMessage, insideMessage;
+	private Object [] attachments;
+	private GiftWrapper wrappingType;
+	private int maxCapacity, materialCost;
+
+	public static final LockableListModel PACKAGES = new LockableListModel();
+	static {
+		PACKAGES.add( new GiftWrapper( "plain brown wrapper", 1, 1, 0 ) );
+		PACKAGES.add( new GiftWrapper( "less-than-three-shaped box", 2, 2, 100 ) );
+	}
+
+	private static class GiftWrapper
+	{
+		private String name;
+		private int radio, maxCapacity, materialCost;
+
+		public GiftWrapper( String name, int radio, int maxCapacity, int materialCost )
+		{
+			this.name = name;
+			this.radio = radio;
+			this.maxCapacity = maxCapacity;
+			this.materialCost = materialCost;
+		}
+
+		public String toString()
+		{	return name;
+		}
+	}
+
+	public static final int PLAIN_BROWN_WRAPPER = 1;
+	public static final int LESS_THAN_THREE_SHAPED_BOX = 2;
+
+	private static final int [] CAPACITIES = { 0, 1, 2 };
+	private static final int [] MATERIAL_COST = { 0, 0, 100 };
+
+	public GiftMessageRequest( KoLmafia client, String recipient, String outsideMessage, String insideMessage,
+		Object wrappingType, Object [] attachments, int meatAttachment )
+	{
+		super( client, "town_sendgift.php" );
+		addFormField( "pwd", client.getPasswordHash() );
+		addFormField( "action", "Yep." );
+		addFormField( "towho", recipient );
+		addFormField( "note", outsideMessage );
+		addFormField( "insidenote", insideMessage );
+
+		this.recipient = client.getMessenger() == null ? recipient : client.getPlayerID( recipient );
+		this.outsideMessage = outsideMessage;
+		this.insideMessage = insideMessage;
+		this.attachments = attachments;
+
+		this.wrappingType = (GiftWrapper) wrappingType;
+		this.maxCapacity = this.wrappingType.maxCapacity;
+		this.materialCost = this.wrappingType.materialCost;
+
+		addFormField( "whichpackage", String.valueOf( this.wrappingType.radio ) );
+		addFormField( "sendmeat", String.valueOf( meatAttachment ) );
+		client.processResult( new AdventureResult( AdventureResult.MEAT, 0 - meatAttachment ) );
+	}
+
+	/**
+	 * Runs the request.  Note that this does not report an error if it fails;
+	 * it merely parses the results to see if any gains were made.
+	 */
+
+	public void run()
+	{
+		// First, check to see how many attachments are to be
+		// placed in the closet - if there's too many,
+		// then you'll need to break up the request
+
+		if ( attachments != null && attachments.length != 0 )
+		{
+			if ( attachments.length > maxCapacity )
+			{
+				int currentBaseIndex = 0;
+				int remainingItems = attachments.length;
+
+				Object [] itemHolder = null;
+
+				while ( remainingItems > 0 )
+				{
+					itemHolder = new Object[ remainingItems < maxCapacity ? remainingItems : maxCapacity ];
+
+					for ( int i = 0; i < itemHolder.length; ++i )
+						itemHolder[i] = attachments[ currentBaseIndex + i ];
+
+					// For each broken-up request, you create a new ItemStorage request
+					// which will create the appropriate data to post.
+
+					if ( client.permitsContinue() )
+						(new GiftMessageRequest( client, recipient, outsideMessage, insideMessage, wrappingType, itemHolder, 0 )).run();
+
+					currentBaseIndex += maxCapacity;
+					remainingItems -= maxCapacity;
+				}
+
+				// Since all the sub-requests were run, there's nothing left
+				// to do - simply return from this method.
+
+				return;
+			}
+
+			for ( int i = 0; i < attachments.length; ++i )
+			{
+				AdventureResult result = (AdventureResult) attachments[i];
+				addFormField( "whichitem" + i, String.valueOf( result.getItemID() ) );
+				addFormField( "howmany" + i, String.valueOf( result.getCount() ) );
+			}
+		}
+
+		// Once all the form fields are broken up, this
+		// just calls the normal run method from KoLRequest
+		// to execute the request.
+
+		client.resetContinueState();
+		client.processResult( new AdventureResult( AdventureResult.MEAT, 0 - materialCost ) );
+
+		super.run();
+
+		// If an error state occurred, return from this
+		// request, since there's no content to parse
+
+		if ( isErrorState || responseCode != 200 )
+			return;
+
+		// Make sure that the outsideMessage was actually sent -
+		// the person could have input an invalid player ID
+
+		if ( responseText.indexOf( "<center>Gift Sent.</center>" ) != -1 )
+		{
+			// With that done, the client needs to be updated
+			// to note that the items were sent.
+
+			AdventureResult currentResult, negatedResult;
+			for ( int i = 0; i < attachments.length; ++i )
+			{
+				currentResult = (AdventureResult) attachments[i];
+				negatedResult = new AdventureResult( currentResult.getItemID(), 0 - currentResult.getCount() );
+				client.processResult( negatedResult );
+			}
+		}
+		else
+		{
+			client.cancelRequest();
+			return;
+		}
+	}
+}
