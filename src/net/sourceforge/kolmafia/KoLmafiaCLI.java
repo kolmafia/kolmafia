d660b22fa7df83e86636691b469200b6b89f4be8
shwei <shwei@users.sourceforge.net>
2005-06-13 19:28:20 +0000

    do not default to scripts folder for all CLI loading

--- src/net/sourceforge/kolmafia/KoLmafiaCLI.java
+++ src/net/sourceforge/kolmafia/KoLmafiaCLI.java
@@ -161,7 +161,7 @@ public class KoLmafiaCLI extends KoLmafia
 	 */
 
 	public KoLmafiaCLI( KoLmafia scriptRequestor, String scriptLocation ) throws IOException
-	{	this( scriptRequestor, scriptLocation == null ? System.in : DataUtilities.getFileInputStream( "", "scripts", scriptLocation ) );
+	{	this( scriptRequestor, scriptLocation == null ? System.in : DataUtilities.getFileInputStream( "", "", scriptLocation ) );
 	}
 
 	public KoLmafiaCLI( KoLmafia scriptRequestor, InputStream inputStream ) throws IOException
