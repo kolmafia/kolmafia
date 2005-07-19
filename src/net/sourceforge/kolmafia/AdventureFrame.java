73840a34c22f57e445b9b3e21a9f5a089151ccc0
shwei <shwei@users.sourceforge.net>
2005-07-19 20:41:15 +0000

    more informative combo box names notification of inability to deposit into hagnks

--- src/net/sourceforge/kolmafia/AdventureFrame.java
+++ src/net/sourceforge/kolmafia/AdventureFrame.java
@@ -1,4 +1,3 @@
-
 /**
  * Copyright (c) 2005, KoLmafia development team
  * http://kolmafia.sourceforge.net/
@@ -32,41 +31,6 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-
-/**
- * Copyright (c) 2003, Spellcast development team
- * http://spellcast.dev.java.net/
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *  [1] Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *  [2] Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *  [3] Neither the name "Spellcast development team" nor the names of
- *      its contributors may be used to endorse or promote products
- *      derived from this software without specific prior written
- *      permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 
 package net.sourceforge.kolmafia;
 
@@ -717,9 +681,9 @@ public class AdventureFrame extends KoLFrame
 			super( "Meat Management", "deposit", "withdraw", new Dimension( 80, 20 ), new Dimension( 240, 20 ) );
 
 			fundSource = new JComboBox();
-			fundSource.addItem( "Inventory (On-Hand)" );
-			fundSource.addItem( "Campground (In-Closet)" );
-			fundSource.addItem( "Hagnk's Storage (Limited)" );
+			fundSource.addItem( "Inventory to Closet" );
+			fundSource.addItem( "Closet to Inventory" );
+			fundSource.addItem( "Hagnk's Storage" );
 
 			amountField = new JTextField();
 
@@ -748,6 +712,11 @@ public class AdventureFrame extends KoLFrame
 				case 1:
 					contentPanel = this;
 					(new RequestThread( new ItemStorageRequest( client, getValue( amountField ), ItemStorageRequest.MEAT_TO_INVENTORY ) )).start();
+					return;
+
+				case 2:
+					contentPanel = this;
+					updateDisplay( ERROR_STATE, "You cannot deposit into Hagnk's storage." );
 					return;
 			}
 		}
