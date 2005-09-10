21534e6f0dcefbf4c35794dcda75b45ffd4e77f8
shwei <shwei@users.sourceforge.net>
2005-09-10 20:18:29 +0000

    adding chat frames and character sheet frame to single-instance frames

--- src/net/sourceforge/kolmafia/CreateFrameRunnable.java
+++ src/net/sourceforge/kolmafia/CreateFrameRunnable.java
@@ -48,10 +48,11 @@ public class CreateFrameRunnable implements Runnable, KoLConstants
 {
 	private static final Class [] SINGLE_INSTANCE =
 	{
-		BuffBotFrame.class, CalendarFrame.class, CakeArenaFrame.class,
-		ItemManageFrame.class, GearChangeFrame.class, HagnkStorageFrame.class,
-		MailboxFrame.class, MuseumFrame.class, OptionsFrame.class,
-		PendingTradesFrame.class, StoreManageFrame.class, CommandDisplayFrame.class
+		BuffBotFrame.class, CalendarFrame.class, CakeArenaFrame.class, CharsheetFrame.class,
+		ClanManageFrame.class, CommandDisplayFrame.class, ContactListFrame.class, GearChangeFrame.class,
+		HagnkStorageFrame.class, ItemManageFrame.class, LoginFrame.class, MailboxFrame.class,
+		MuseumFrame.class, OptionsFrame.class, PendingTradesFrame.class, StoreManageFrame.class,
+		TabbedChatFrame.class
 	};
 
 	private KoLmafia client;
