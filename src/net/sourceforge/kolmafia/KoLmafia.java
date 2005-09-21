60b63cd1024a3163cb008171b9432a8489f7f952
shwei <shwei@users.sourceforge.net>
2005-09-21 17:51:27 +0000

    ignore adventure results -- let choice adventure parser do it

--- src/net/sourceforge/kolmafia/KoLmafia.java
+++ src/net/sourceforge/kolmafia/KoLmafia.java
@@ -602,7 +602,7 @@ public abstract class KoLmafia implements KoLConstants
 
 		int conditionsIndex = conditions.indexOf( result );
 
-		if ( conditionsIndex != -1 )
+		if ( !resultName.equals( AdventureResult.ADV ) && conditionsIndex != -1 )
 		{
 			if ( resultName.equals( AdventureResult.SUBSTATS ) )
 			{
