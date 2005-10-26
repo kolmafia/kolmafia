07d2d683efc00cb8a2c156d404281558e4024644
pldw <pldw@users.sourceforge.net>
2005-10-26 14:41:28 +0000

    Fix item # for monocle. Fix item name for pocketwatch. Shillelagh is a weapon.

--- src/net/sourceforge/kolmafia/ClassSkillsDatabase.java
+++ src/net/sourceforge/kolmafia/ClassSkillsDatabase.java
@@ -218,7 +218,7 @@ public class ClassSkillsDatabase extends KoLDatabase
 					manaModifier -= 2;
 					break;
 
-				case 1235: // plexiglass pocket watch
+				case 1235: // plexiglass pocketwatch
 
 					manaModifier -= 3;
 					break;
