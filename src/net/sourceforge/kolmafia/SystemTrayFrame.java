3d602091fb03a29f567394a14643c1e33db624ca
shwei <shwei@users.sourceforge.net>
2005-12-25 10:26:57 +0000

    system tray icon for windows

---
+++ src/net/sourceforge/kolmafia/SystemTrayFrame.java
@@ -0,0 +1,89 @@
+package net.sourceforge.kolmafia;
+
+import javax.swing.JPopupMenu;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+
+import com.gc.systray.SystemTrayIconManager;
+import net.java.dev.spellcast.utilities.DataUtilities;
+
+
+public class SystemTrayFrame extends KoLFrame implements Runnable
+{
+	private SystemTrayIconManager manager;
+
+	public SystemTrayFrame( KoLmafia client )
+	{
+		super( client, "SystemTrayFrame" );
+		(new Thread( this )).start();
+	}
+
+	public void run()
+	{
+		try
+		{
+			// First load the DesktopIndicator library to allow
+			// for system tray usage.
+
+			File library = new File( "data/DesktopIndicator.dll" );
+
+			if ( !library.exists() )
+			{
+				InputStream input = DataUtilities.getFileInputStream( "", "", "DesktopIndicator.dll" );
+				OutputStream output = new FileOutputStream( library );
+
+				byte [] buffer = new byte[ 1024 ];
+				int bufferLength;
+				while ( (bufferLength = input.read( buffer )) != -1 )
+					output.write( buffer, 0, bufferLength );
+
+				output.close();
+			}
+
+			// Next, load the icon which will be used by KoLmafia
+			// in the system tray.  For now, this will be the old
+			// icon used by KoLmelion.
+
+			File trayicon = new File( "images/KoLmelionIcon.ico" );
+
+			if ( !trayicon.exists() )
+			{
+				java.io.InputStream input = DataUtilities.getFileInputStream( "", "", "KoLmelionIcon.ico" );
+				java.io.OutputStream output = new java.io.FileOutputStream( trayicon );
+
+				byte [] buffer = new byte[ 1024 ];
+				int bufferLength;
+				while ( (bufferLength = input.read( buffer )) != -1 )
+					output.write( buffer, 0, bufferLength );
+
+				output.close();
+			}
+
+			// Now, make calls to SystemTrayIconManager in order
+			// to make use of the system tray.
+
+			System.load( library.getAbsolutePath() );
+			this.manager = new SystemTrayIconManager( SystemTrayIconManager.loadImage( trayicon.getAbsolutePath() ), VERSION_NAME );
+
+			JPopupMenu popup = new JPopupMenu();
+			constructMenus( popup );
+			popup.add( new InvocationMenuItem( "End Session", this, "dispose" ) );
+
+			manager.setLeftClickView( popup );
+			manager.setRightClickView( popup );
+			manager.setVisible( true );
+		}
+		catch ( Exception e )
+		{
+		}
+	}
+
+	public void dispose()
+	{
+		super.dispose();
+		manager.setVisible( false );
+	}
+}
