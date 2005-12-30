54d4029a14e3e74dc53b56131ea7240e3d925951
pldw <pldw@users.sourceforge.net>
2005-12-30 22:56:02 +0000

    CLI "use" command forces argument to be an item.

--- src/net/sourceforge/kolmafia/KoLmafiaCLI.java
+++ src/net/sourceforge/kolmafia/KoLmafiaCLI.java
@@ -2397,9 +2397,9 @@ public class KoLmafiaCLI extends KoLmafia
 		consumptionType = TradeableItemDatabase.getConsumptionType( itemName );
 
 		if ( itemCount == 1 || consumptionType == ConsumeItemRequest.CONSUME_MULTIPLE || consumptionType == ConsumeItemRequest.CONSUME_RESTORE )
-			scriptRequestor.makeRequest( new ConsumeItemRequest( scriptRequestor, new AdventureResult( itemName, itemCount ) ), 1 );
+			scriptRequestor.makeRequest( new ConsumeItemRequest( scriptRequestor, new AdventureResult( itemName, itemCount, false ) ), 1 );
 		else
-			scriptRequestor.makeRequest( new ConsumeItemRequest( scriptRequestor, new AdventureResult( itemName, 1 ) ), itemCount );
+			scriptRequestor.makeRequest( new ConsumeItemRequest( scriptRequestor, new AdventureResult( itemName, 1, false ) ), itemCount );
 	}
 
 	/**
