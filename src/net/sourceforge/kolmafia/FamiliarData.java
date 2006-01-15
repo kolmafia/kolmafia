1fcbaeedd1d228d3be2c1c6fa2522f4502010f84
veracity0 <veracity0@users.sourceforge.net>
2006-01-15 19:10:58 +0000

    tiny nose-bone fetish does not add to familiar weight

--- src/net/sourceforge/kolmafia/FamiliarData.java
+++ src/net/sourceforge/kolmafia/FamiliarData.java
@@ -181,6 +181,7 @@ public class FamiliarData implements KoLConstants, Comparable
 		case 1116:	// annoying pitchfork
 		case 1152:	// miniature gravy-covered maypole
 		case 1260:	// wax lips
+		case 1264:	// tiny nose-bone fetish
 		case 1419:	// teddy bear sewing kit
 			return 0;
 
