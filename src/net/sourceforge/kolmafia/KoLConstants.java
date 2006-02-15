d999c41662322dcf59faf9cc8f86b00feef26b16
shwei <shwei@users.sourceforge.net>
2006-02-15 19:35:08 +0000

    release date for v6.7

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -55,8 +55,8 @@ public interface KoLConstants extends UtilityConstants
 	public static final JLabel BLANK_LABEL = new JLabel();
 	public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
 
-	public static final String VERSION_NAME = "KoLmafia v6.6";
-	public static final String VERSION_DATE = "Released February 9, 2006";
+	public static final String VERSION_NAME = "KoLmafia v6.7";
+	public static final String VERSION_DATE = "Released February 17, 2006";
 
 	public static final KoLSettings GLOBAL_SETTINGS = new KoLSettings();
 	public static final KoLmafiaCLI DEFAULT_SHELL = new KoLmafiaCLI( System.in );
