bed80daf1bf3c1350f350e0adf7c8ce377fc0cf8
holatuwol <shwei@users.sourceforge.net>
2006-04-28 06:46:03 +0000

    Handle new layout for non-compact mode
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@244 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/CharpaneRequest.java
+++ src/net/sourceforge/kolmafia/CharpaneRequest.java
@@ -69,6 +69,12 @@ public class CharpaneRequest extends KoLRequest
 
 		return instance;
 	}
+	
+	public void run()
+	{
+		this.responseText = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"http://images.kingdomofloathing.com/styles.css\"><style type=\"text/css\">body, td {	font-size: 1em;}</style></head><body bgcolor=white text=black link=black alink=black vlink=black><center><table align=center><tr><td><a target=mainpane href=\"charsheet.php\"><img src=\"http://images.kingdomofloathing.com/otherimages/accordionthief_f.gif\" width=60 height=100 border=0></a></td><td valign=center><center><a target=mainpane href=\"charsheet.php\"><b>Playername</b></a><br>Level 10<br>Sub-Sub-Apprentice Accordion Thief</td></tr></table><table align=center><tr><td align=right>Muscle:</td><td align=left><b><font color=blue>150</font>&nbsp;(78)</b></td></tr><tr><td align=right>Mysticality:</td><td align=left><b><font color=blue>124</font>&nbsp;(79)</b></td></tr><tr><td align=right>Moxie:</td><td align=left><b><font color=blue>161</font>&nbsp;(100)</b></td></tr><tr><td align=right>Temulency:</td><td><b>19</b></td></tr></table><center>You'd better keep an eye on your drinking...</center><table cellpadding=3 align=center><tr><td align=center><img src=\"http://images.kingdomofloathing.com/itemimages/hp.gif\" class=hand onclick='doc(\"hp\");'><br><span class=black>161&nbsp;/&nbsp;165</span></td><td align=center><img src=\"http://images.kingdomofloathing.com/itemimages/mp.gif\" class=hand onclick='doc(\"mp\");'><br><span class=black>14&nbsp;/&nbsp;196</span></td></tr><tr><td align=center><img src=\"http://images.kingdomofloathing.com/itemimages/meat.gif\" class=hand onclick='doc(\"meat\");'><br><span class=black>503,480</span></td><td align=center><img src=\"http://images.kingdomofloathing.com/itemimages/hourglass.gif\" class=hand onclick='doc(\"adventures\");'><br><span class=black>324</span></td></tr></table><br><font size=2><b><a class=nounder href=\"mclargehuge.php\" target=mainpane>Last Adventure:</a></b></font><br><center><table cellspacing=0 cellpadding=0><tr><td><font size=2><a target=mainpane href=\"adventure.php?snarfblat=64\">The Icy Peak</a><br></font></td></tr></table></center><center><p><b><font size=2>Effects:</font></b><br></center><center><table><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/happy.gif\" class=hand onClick='eff(\"26\");'></td><td valign=center><font size=2>Mariachi Mood (101)</font><br></td></tr><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/notes.gif\" class=hand onClick='eff(\"60\");'></td><td valign=center><font size=2>Aloysius' Antiphon of Aptitude (189)</font><br></td></tr><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/notes.gif\" class=hand onClick='eff(\"67\");'></td><td valign=center><font size=2>Fat Leon's Phat Loot Lyric (303)</font><br></td></tr><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/notes.gif\" class=hand onClick='eff(\"63\");'></td><td valign=center><font size=2>Polka of Plenty (303)</font><br></td></tr><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/happy.gif\" class=hand onClick='eff(\"50\");'></td><td valign=center><font size=2>Empathy (322)</font><br></td></tr><tr><td><img src=\"http://images.kingdomofloathing.com/itemimages/string.gif\" class=hand onClick='eff(\"16\");'></td><td valign=center><font size=2>Leash of Linguini (322)</font><br></td></tr></table><p><table width=90%><tr><td colspan=2 align=center><font size=2><b>Familiar:</b></font></td></tr><tr><td align=center valign=center><a target=mainpane href=\"familiar.php\"><img src=\"http://images.kingdomofloathing.com/itemimages/hat2.gif\" width=30 height=30 border=0></a></td><td valign=center align=left><a target=mainpane href=\"familiar.php\"><b><font size=2>Cid</a></b>, the  45-pound Hovering Sombrero</font></td></tr></table></center></center></body></html>";
+		processResults();
+	}
 
 	protected void processResults()
 	{	processCharacterPane( this.responseText );
@@ -110,7 +116,7 @@ public class CharpaneRequest extends KoLRequest
 		if ( responseText.indexOf( "<img src=\"http://images.kingdomofloathing.com/otherimages/inf_small.gif\">" ) == -1 )
 		{
 			handleStatPoints( responseText, "Mus", "Mys", "Mox" );
-			handleMiscPoints( responseText, "HP", "MP", "Meat", "Adv", "" );
+			handleMiscPoints( responseText, "HP", "MP", "Adv", "Meat", "", "<b>", "</b>" );
 			handleMindControl( responseText, "MC" );
 		}
 	}
@@ -120,7 +126,7 @@ public class CharpaneRequest extends KoLRequest
 		if ( responseText.indexOf( "<img src=\"http://images.kingdomofloathing.com/otherimages/inf_small.gif\">" ) == -1 )
 		{
 			handleStatPoints( responseText, "Muscle", "Mysticality", "Moxie" );
-			handleMiscPoints( responseText, "hp\\.gif", "mp\\.gif", "meat\\.gif", "hourglass\\.gif", "&nbsp;" );
+			handleMiscPoints( responseText, "hp\\.gif", "mp\\.gif", "meat\\.gif", "hourglass\\.gif", "&nbsp;", "<span.*?>", "</span>" );
 			handleMindControl( responseText, "Mind Control" );
 		}
 	}
@@ -146,22 +152,27 @@ public class CharpaneRequest extends KoLRequest
 				KoLCharacter.getTotalMysticality(), modified[2], KoLCharacter.getTotalMoxie() );
 		}
 	}
-
-	private static void handleMiscPoints( String responseText, String hpString, String mpString, String meatString, String advString, String spacerString ) throws Exception
+	
+	private static void handleMiscPoints( String responseText, String hpString, String mpString, String meatString, String advString, String spacer, String openTag, String closeTag ) throws Exception
 	{
-		Matcher miscMatcher = Pattern.compile( hpString + ".*?(<font.*?>)?<font.*?>(.*?)" + spacerString + "/" + spacerString + "(.*?)</font>.*?" +
-			mpString + ".*?<b>(<font.*?>)?(.*?)" + spacerString + "/" + spacerString + "(.*?)(</font>)?</b>.*?" +
-			meatString + ".*?<b>(<font.*?>)?(.*?)(</font>)?</b>.*?" + advString + ".*?<b>(<font.*?>)?(.*?)(</font>)?</b>" ).matcher( responseText );
+		// On the other hand, health and all that good stuff
+		// is complicated, has nested images, and lots of other
+		// weird stuff.  Handle it in a non-modular fashion.
+		
+		Matcher miscMatcher = Pattern.compile(
+			hpString + ".*?" + openTag + "(.*?)" + spacer + "/" + spacer + "(.*?)" + closeTag + ".*?" +
+			mpString + ".*?" + openTag + "(.*?)" + spacer + "/" + spacer + "(.*?)" + closeTag + ".*?" +
+			meatString + ".*?" + openTag + "(.*?)" + closeTag + ".*?" + advString + ".*?" + openTag + "(.*?)" + closeTag ).matcher( responseText );
 
 		if ( miscMatcher.find() )
 		{
-			KoLCharacter.setHP( df.parse( miscMatcher.group(2) ).intValue(), df.parse( miscMatcher.group(3) ).intValue(), df.parse( miscMatcher.group(3) ).intValue() );
-			KoLCharacter.setMP( df.parse( miscMatcher.group(5) ).intValue(), df.parse( miscMatcher.group(6) ).intValue(), df.parse( miscMatcher.group(6) ).intValue() );
+			KoLCharacter.setHP( df.parse( miscMatcher.group(1).replaceAll( "<.*?>", "" ) ).intValue(), df.parse( miscMatcher.group(2).replaceAll( "<.*?>", "" ) ).intValue(), df.parse( miscMatcher.group(2).replaceAll( "<.*?>", "" ) ).intValue() );
+			KoLCharacter.setMP( df.parse( miscMatcher.group(3) ).intValue(), df.parse( miscMatcher.group(4) ).intValue(), df.parse( miscMatcher.group(4) ).intValue() );
 
-			KoLCharacter.setAvailableMeat( df.parse( miscMatcher.group(9) ).intValue() );
+			KoLCharacter.setAvailableMeat( df.parse( miscMatcher.group(5) ).intValue() );
 
 			int oldAdventures = KoLCharacter.getAdventuresLeft();
-			int newAdventures = df.parse( miscMatcher.group(12) ).intValue();
+			int newAdventures = df.parse( miscMatcher.group(6) ).intValue();
 
 			StaticEntity.getClient().processResult( new AdventureResult( AdventureResult.ADV, newAdventures - oldAdventures ) );
 		}
