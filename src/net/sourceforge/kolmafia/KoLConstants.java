bfb50a337c61bbf891913e22ae528cf5ba94a415
holatuwol <shwei@users.sourceforge.net>
2006-05-16 17:36:31 +0000

    Belated update to version number
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@467 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -57,8 +57,8 @@ public interface KoLConstants extends UtilityConstants
 	public static final JLabel BLANK_LABEL = new JLabel();
 	public static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
 
-	public static final String VERSION_NAME = "KoLmafia v7.5";
-	public static final String VERSION_DATE = "Released May 13, 2006";
+	public static final String VERSION_NAME = "KoLmafia v7.6";
+	public static final String VERSION_DATE = "Released May 16, 2006";
 
 	public static final KoLSettings GLOBAL_SETTINGS = new KoLSettings();
 	public static final KoLmafiaCLI DEFAULT_SHELL = new KoLmafiaCLI( System.in );
