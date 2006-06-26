ef896621af803d253bc46bbd4a9be62fdb815802
holatuwol <shwei@users.sourceforge.net>
2006-06-26 15:56:48 +0000

    Fix thumb twiddling
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@837 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/FightRequest.java
+++ src/net/sourceforge/kolmafia/FightRequest.java
@@ -131,11 +131,15 @@ public class FightRequest extends KoLRequest
 			action1 = CombatSettings.getSetting( encounterLookup, KoLCharacter.getNextAdventure(), roundCount - 2 );
 		}
 
+		// Let the de-level action figure out what
+		// should be done, and then re-process.
+
 		if ( action1.equals( "delevel" ) )
 		{
 			action1 = getMonsterWeakenAction();
 		}
-		else if ( action1.equals( "abort" ) || !KoLmafia.permitsContinue() )
+
+		if ( action1.equals( "abort" ) || !KoLmafia.permitsContinue() )
 		{
 			// If the user has chosen to abort
 			// combat, flag it.
