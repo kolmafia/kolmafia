6d72e8c6cc32626228519e632aa05d02d2725fee
holatuwol <shwei@users.sourceforge.net>
2006-07-26 20:18:39 +0000

    Do not reset last seen on non-error Only add relay browser links to charpane.php
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@1111 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/RequestEditorKit.java
+++ src/net/sourceforge/kolmafia/RequestEditorKit.java
@@ -823,7 +823,7 @@ public class RequestEditorKit extends HTMLEditorKit implements KoLConstants
 		// if there's an item present, and if so, modify
 		// it so that you get a use link.
 
-		if ( text.indexOf( "charsheet.php" ) != -1 )
+		if ( location.indexOf( "charpane.php" ) != -1 )
 			text = addShrugOffLinks( text );
 
 		if ( StaticEntity.getProperty( "relayAddsUseLinks" ).equals( "true" ) )
