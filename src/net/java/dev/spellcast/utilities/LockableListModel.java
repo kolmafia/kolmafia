1c2e119cdf669c9b3574824e6356f5d5ad7f860c
holatuwol <shwei@users.sourceforge.net>
2006-08-06 02:12:39 +0000

    Reduce maximal buffer size for message buffers Remove unnecessary ripple layer on event firing
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@1181 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/java/dev/spellcast/utilities/LockableListModel.java
+++ src/net/java/dev/spellcast/utilities/LockableListModel.java
@@ -84,51 +84,6 @@ public class LockableListModel extends javax.swing.AbstractListModel
 		addAll( c );
 	}
 
-	private class FireListEventRunnable implements Runnable
-	{
-		private int changeType;
-		private Object source;
-		private int index0, index1;
-
-		public FireListEventRunnable( int changeType, Object source, int index0, int index1 )
-		{
-			this.changeType = changeType;
-			this.source = source;
-			this.index0 = index0;
-			this.index1 = index1;
-		}
-
-		public void run()
-		{
-			switch ( changeType )
-			{
-				case ListDataEvent.CONTENTS_CHANGED:
-					LockableListModel.super.fireContentsChanged( source, index0, index1 );
-					break;
-
-				case ListDataEvent.INTERVAL_ADDED:
-					LockableListModel.super.fireIntervalAdded( source, index0, index1 );
-					break;
-
-				case ListDataEvent.INTERVAL_REMOVED:
-					LockableListModel.super.fireIntervalRemoved( source, index0, index1 );
-					break;
-			}
-		}
-	}
-
-	protected void fireContentsChanged( Object source, int index0, int index1 )
-	{	(new FireListEventRunnable( ListDataEvent.CONTENTS_CHANGED, source, index0, index1 )).run();
-	}
-
-	protected void fireIntervalAdded( Object source, int index0, int index1 )
-	{	(new FireListEventRunnable( ListDataEvent.INTERVAL_ADDED, source, index0, index1 )).run();
-	}
-
-	protected void fireIntervalRemoved( Object source, int index0, int index1 )
-	{	(new FireListEventRunnable( ListDataEvent.INTERVAL_REMOVED, source, index0, index1 )).run();
-	}
-
 	public void sort()
 	{
 		Collections.sort( elements );
