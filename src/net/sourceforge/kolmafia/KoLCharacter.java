a6395de5368fcc53d69f49db6188ec93535422b4
holatuwol <shwei@users.sourceforge.net>
2006-10-06 19:37:29 +0000

    Fix maps not resetting based on ascension count
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@1897 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLCharacter.java
+++ src/net/sourceforge/kolmafia/KoLCharacter.java
@@ -427,19 +427,6 @@ public abstract class KoLCharacter extends StaticEntity
 		eventHistory.clear();
 		recentEffects.clear();
 		activeEffects.clear();
-
-		// Clear the violet fog path table and everything
-		// else that changes on the player.
-
-		VioletFog.reset();
-		Louvre.reset();
-		CombatSettings.reset();
-		MoodSettings.reset();
-		KoLMailManager.reset();
-		StoreManager.reset();
-		MuseumManager.reset();
-		ClanManager.reset();
-		MushroomPlot.reset();
 	}
 
 	public static boolean isFallingDown()
