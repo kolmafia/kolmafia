fa39ad380dfbd8eeb0acb72999700ef31d2b79b1
holatuwol <shwei@users.sourceforge.net>
2006-10-06 20:39:25 +0000

    Count from 0 instead of -1
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@1898 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/FightRequest.java
+++ src/net/sourceforge/kolmafia/FightRequest.java
@@ -54,7 +54,7 @@ public class FightRequest extends KoLRequest
 	public static final AdventureResult DICTIONARY1 = new AdventureResult( 536, 1 );
 	public static final AdventureResult DICTIONARY2 = new AdventureResult( 1316, 1 );
 
-	private static int currentRound = -1;
+	private static int currentRound = 0;
 	private static int offenseModifier = 0, defenseModifier = 0;
 
 	private static String action1 = null;
@@ -102,7 +102,7 @@ public class FightRequest extends KoLRequest
 			if ( encounterLookup.indexOf( RARE_MONSTERS[i] ) != -1 )
 				KoLmafia.updateDisplay( ABORT_STATE, "You have encountered the " + encounter );
 
-		if ( currentRound < 1 )
+		if ( currentRound == 0 )
 		{
 			action1 = StaticEntity.getProperty( "defaultAutoAttack" );
 			if ( action1.equals( "" ) || action1.equals( "0" ) )
@@ -110,7 +110,7 @@ public class FightRequest extends KoLRequest
 		}
 		else if ( action1.equals( "custom" ) )
 		{
-			action1 = CombatSettings.getSetting( encounterLookup, currentRound );
+			action1 = CombatSettings.getSetting( encounterLookup, currentRound - 1 );
 		}
 
 		// If the person wants to use their own script,
@@ -138,7 +138,7 @@ public class FightRequest extends KoLRequest
 			// If the user has chosen to abort combat, flag it.
 			action1 = null;
 		}
-		else if ( currentRound == -1 )
+		else if ( currentRound == 0 )
 		{
 			// If this is the first round, you do not
 			// submit extra data.
@@ -306,7 +306,7 @@ public class FightRequest extends KoLRequest
 		// If this is the first round, then register the opponent
 		// you are fighting against.
 
-		if ( currentRound == 0 )
+		if ( currentRound == 1 )
 		{
 			encounterLookup = CombatSettings.encounterKey( encounter );
 			monsterData = MonsterDatabase.findMonster( encounter );
@@ -318,7 +318,7 @@ public class FightRequest extends KoLRequest
 			encounterLookup = "";
 			monsterData = null;
 
-			currentRound = -1;
+			currentRound = 0;
 			offenseModifier = 0;
 			defenseModifier = 0;
 		}
