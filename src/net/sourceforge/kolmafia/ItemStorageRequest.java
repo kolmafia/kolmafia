6c31e1c85b278af8c01dfeef926a80a436bbb2f6
holatuwol <shwei@users.sourceforge.net>
2006-10-07 11:02:43 +0000

    Refresh meat amount when emptying storage
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@1912 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/ItemStorageRequest.java
+++ src/net/sourceforge/kolmafia/ItemStorageRequest.java
@@ -178,6 +178,7 @@ public class ItemStorageRequest extends SendMessageRequest
 				while ( !storage.isEmpty() )
 					StaticEntity.getClient().processResult( (AdventureResult) storage.remove(0) );
 
+				CharpaneRequest.getInstance().run();
 				break;
 
 			case STORAGE_TO_INVENTORY:
