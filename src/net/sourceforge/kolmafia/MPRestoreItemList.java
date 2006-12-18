8baa6b43ca99c5ca3b7de3d10c3620f6bdaac19d
holatuwol <shwei@users.sourceforge.net>
2006-12-18 16:36:54 +0000

    Add sofa to the checkbox list (not just as a phantom option)
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@2580 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/MPRestoreItemList.java
+++ src/net/sourceforge/kolmafia/MPRestoreItemList.java
@@ -51,7 +51,7 @@ public abstract class MPRestoreItemList extends StaticEntity
 
 	public static final MPRestoreItem [] CONFIGURES = new MPRestoreItem []
 	{
-		BEANBAG, CAMPGROUND, GALAKTIK, new MPRestoreItem( "bottle of Vangoghbitussin", 100 ),
+		SOFA, BEANBAG, CAMPGROUND, GALAKTIK, new MPRestoreItem( "bottle of Vangoghbitussin", 100 ),
 		new MPRestoreItem( "bottle of Monsieur Bubble", 56 ), new MPRestoreItem( "unrefined mountain stream syrup", 55 ),
 		new MPRestoreItem( "phonics down", 48 ), new MPRestoreItem( "tonic water", 40 ), new MPRestoreItem( "Marquis de Poivre soda", 35 ),
 		new MPRestoreItem( "blue paisley oyster egg", 33 ), new MPRestoreItem( "blue polka-dot oyster egg", 33 ),
