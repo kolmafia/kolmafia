0c97cb8d35eacd0dccee5152cb6e60c41acdaf37
holatuwol <shwei@users.sourceforge.net>
2007-02-07 01:26:43 +0000

    Fix handling of all-numeric titles for clan snapshot
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@2942 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/ClanMembersRequest.java
+++ src/net/sourceforge/kolmafia/ClanMembersRequest.java
@@ -40,7 +40,7 @@ import java.util.regex.Pattern;
 public class ClanMembersRequest extends KoLRequest
 {
 	private static final Pattern CLANID_PATTERN = Pattern.compile( "showclan\\.php\\?whichclan=(\\d+)\">(.*?)</a>" );
-	private static final Pattern MEMBER_PATTERN = Pattern.compile( "<a class=nounder href=\"showplayer\\.php\\?who=(\\d+)\">(.*?)</a>.*?<td class=small>(\\d+).*?</td>" );
+	private static final Pattern MEMBER_PATTERN = Pattern.compile( "<a class=nounder href=\"showplayer\\.php\\?who=(\\d+)\">([^<]+)</a></b>&nbsp;</td><td class=small>[^<]*?</td><td class=small>(\\d+).*?</td>" );
 
 	private String clanId;
 	private String clanName;
