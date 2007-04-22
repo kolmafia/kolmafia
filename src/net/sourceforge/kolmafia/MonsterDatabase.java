c841aed5459340a75a6fa988cb18ac0abd8d50fb
holatuwol <shwei@users.sourceforge.net>
2007-04-22 13:40:28 +0000

    Pseudo-fuzzy matching for monster names
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@3619 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/MonsterDatabase.java
+++ src/net/sourceforge/kolmafia/MonsterDatabase.java
@@ -143,6 +143,8 @@ public class MonsterDatabase extends KoLDatabase
 
 		if ( realName == null && trySubstrings )
 		{
+			int matchCount = 0;
+
 			if ( MONSTER_STRINGS == null )
 			{
 				MONSTER_STRINGS = new String[ MONSTER_NAMES.size() ];
@@ -150,8 +152,23 @@ public class MonsterDatabase extends KoLDatabase
 			}
 
 			for ( int i = 0; realName == null && i < MONSTER_STRINGS.length; ++i )
-				if ( MONSTER_STRINGS[i].indexOf( keyName ) == 0 && MONSTER_STRINGS[i].indexOf( " (" ) == keyName.length() )
+			{
+				if ( MONSTER_STRINGS[i].indexOf( keyName ) != -1 )
+				{
+					++matchCount;
 					realName = (String) MONSTER_NAMES.get( MONSTER_STRINGS[i] );
+				}
+			}
+
+			if ( matchCount > 1 )
+			{
+				realName = null;
+				keyName = keyName + " (";
+
+				for ( int i = 0; realName == null && i < MONSTER_STRINGS.length; ++i )
+					if ( MONSTER_STRINGS[i].indexOf( keyName ) == 0 )
+						realName = (String) MONSTER_NAMES.get( MONSTER_STRINGS[i] );
+			}
 		}
 
 		return realName == null ? null : (Monster) MONSTER_DATA.get( realName );
