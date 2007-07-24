95d5f9cba9b3fd31f623c7752695bc75ef542729
Veracity <veracity0@users.sourceforge.net>
2007-07-24 16:31:14 +0000

    Arid, Extra-Dry Desert has different adventure number when Ultrahydrated Add power (from Wiki) of various shields black & tan gives 2 drunkenness, not 3
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4421 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/AdventureDatabase.java
+++ src/net/sourceforge/kolmafia/AdventureDatabase.java
@@ -388,7 +388,7 @@ public class AdventureDatabase extends KoLDatabase
 		// Choice 131 is Dr. Awkward
 
 		// Let's Make a Deal!
-		new ChoiceAdventure( "Beach", "choiceAdventure132", "Arid Extra-Dry Desert",
+		new ChoiceAdventure( "Beach", "choiceAdventure132", "Arid, Extra-Dry Desert",
 		  new String [] { "broken carburetor", "Unlock Oasis" },
 		  new String [] { "2316", null } ),
 
