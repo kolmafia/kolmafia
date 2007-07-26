ae025c7f0b15527afd54105263c59505658c1c5c
Veracity <veracity0@users.sourceforge.net>
2007-07-26 00:45:24 +0000

    Modifiers are now table-driven
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4450 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/Modifiers.java
+++ src/net/sourceforge/kolmafia/Modifiers.java
@@ -96,67 +96,237 @@ public class Modifiers extends KoLDatabase
 	public static final int MYS = 19;
 	public static final int MYS_PCT = 20;
 
-	public static final int MODIFIER_COUNT = 21;
-
-	private static final Pattern [] MODIFIER_PATTERNS = new Pattern [] {
-		Pattern.compile( "Weight: ([+-]\\d+)" ),
-		Pattern.compile( "ML: ([+-]\\d+)" ),
-		Pattern.compile( "Combat: ([+-][\\d.]+)" ),
-		Pattern.compile( "Init: ([+-][\\d.]+)" ),
-		Pattern.compile( "Exp: ([+-][\\d.]+)" ),
-		Pattern.compile( "Item: ([+-][\\d.]+)" ),
-		Pattern.compile( "Meat: ([+-][\\d.]+)" ),
-		Pattern.compile( "DA: ([+-]\\d+)" ),
-		Pattern.compile( "DR: (\\d+)" ),
-		Pattern.compile( "Cold: ([+-]\\d+)" ),
-		Pattern.compile( "Hot: ([+-]\\d+)" ),
-		Pattern.compile( "Sleaze: ([+-]\\d+)" ),
-		Pattern.compile( "Spooky: ([+-]\\d+)" ),
-		Pattern.compile( "Stench: ([+-]\\d+)" ),
-		Pattern.compile( "Mana: ([+-]\\d+)" ),
-		Pattern.compile( "Mox: ([+-]\\d+)" ),
-		Pattern.compile( "Mox%: ([+-]\\d+)" ),
-		Pattern.compile( "Mus: ([+-]\\d+)" ),
-		Pattern.compile( "Mus%: ([+-]\\d+)" ),
-		Pattern.compile( "Mys: ([+-]\\d+)" ),
-		Pattern.compile( "Mys%: ([+-]\\d+)" ),
+	private static final Object [][] floatModifiers = {
+		{ "Familiar Weight", "Weight",
+		  Pattern.compile( "(.*) to Familiar Weight" ),
+		  Pattern.compile( "Weight: ([+-]\\d+)" )
+		},
+		{ "Monster Level", "ML",
+		  Pattern.compile( "(.*) to Monster Level" ),
+		  Pattern.compile( "ML: ([+-]\\d+)" )
+		},
+		{ "Combat Rate", "Combat",
+		  null,
+		  Pattern.compile( "Combat: ([+-][\\d.]+)" )
+		}, 
+		{ "Initiative", "Init",
+		  Pattern.compile( "Combat Initiative (.*)%" ),
+		  Pattern.compile( "Init: ([+-][\\d.]+)" )
+		},
+		{ "Experience", "Exp",
+		  Pattern.compile( "(.*) Stat.*Per Fight" ),
+		  Pattern.compile( "Exp: ([+-][\\d.]+)" )
+		},
+		{ "Item Drop", "Item",
+		  Pattern.compile( "(.*)% Item Drops from Monsters" ),
+		  Pattern.compile( "Item: ([+-][\\d.]+)" )
+		},
+		{ "Meat Drop", "Meat",
+		  Pattern.compile( "(.*)% Meat from Monsters" ),
+		  Pattern.compile( "Meat: ([+-][\\d.]+)" )
+		},
+		{ "Damage Absorption", "DA",
+		  Pattern.compile( "Damage Absorption (.*)" ),
+		  Pattern.compile( "DA: ([+-]\\d+)" )
+		},
+		{ "Damage Reduction", "DR",
+		  Pattern.compile( "Damage Reduction: (\\d+)" ),
+		  Pattern.compile( "DR: (\\d+)" )
+		},
+		{ "Cold Resistance", "Cold",
+		  null,
+		  Pattern.compile( "Cold: ([+-]\\d+)" )
+		},
+		{ "Hot Resistance", "Hot",
+		  null,
+		  Pattern.compile( "Hot: ([+-]\\d+)" )
+		},
+		{ "Sleaze Resistance", "Sleaze",
+		  null,
+		  Pattern.compile( "Sleaze: ([+-]\\d+)" )
+		},
+		{ "Spooky Resistance", "Spooky",
+		  null,
+		  Pattern.compile( "Spooky: ([+-]\\d+)" )
+		},
+		{ "Stench Resistance", "Stench",
+		  null,
+		  Pattern.compile( "Stench: ([+-]\\d+)" )
+		},
+		{ "Mana Cost", "Mana",
+		  Pattern.compile( "(.*) MP to use Skills" ),
+		  Pattern.compile( "Mana: ([+-]\\d+)" )
+		},
+		{ "Moxie", "Mox",
+		  Pattern.compile( "Moxie ([+-]\\d+)$" ),
+		  Pattern.compile( "Mox: ([+-]\\d+)" )
+		},
+		{ "Moxie Percent", "Mox%",
+		  Pattern.compile( "Moxie ([+-]\\d+)%" ),
+		  Pattern.compile( "Mox%: ([+-]\\d+)" )
+		},
+		{ "Muscle", "Mus",
+		  Pattern.compile( "Muscle ([+-]\\d+)$" ),
+		  Pattern.compile( "Mus: ([+-]\\d+)" )
+		},
+		{ "Muscle Percent", "Mus%",
+		  Pattern.compile( "Muscle ([+-]\\d+)%" ),
+		  Pattern.compile( "Mus%: ([+-]\\d+)" )
+		},
+		{ "Mysticality", "Mys",
+		  Pattern.compile( "Mysticality ([+-]\\d+)$" ),
+		  Pattern.compile( "Mys: ([+-]\\d+)" )
+		},
+		{ "Mysticality Percent", "Mys%",
+		  Pattern.compile( "Mysticality ([+-]\\d+)%" ),
+		  Pattern.compile( "Mys%: ([+-]\\d+)" )
+		},
 	};
 
-	private static final Pattern [] STRING_PATTERNS = new Pattern [] {
-		Pattern.compile( "Class: (\\w\\w)" ),
-		Pattern.compile( "Intrinsic: [^,]+" ),
+	public static final int FLOAT_MODIFIERS = floatModifiers.length;
+
+	public static final int CLASS = 0;
+	public static final int INTRINSIC_EFFECT = 1;
+
+        private static final Object [][] stringModifiers = {
+                { "Class", "Class",
+                  null,
+                  Pattern.compile( "Class: (\\w\\w)" )
+                },
+                { "Intrinsic Effect", "Intrinsic",
+                  Pattern.compile( "Intrinsic effect: (.*)" ),
+                  Pattern.compile( "Intrinsic: [^,]+" )
+                },
 	};
 
-        private float[] modifiers;
+	public static final int STRING_MODIFIERS = stringModifiers.length;
+
+	private static final String modifierName( Object [][] table, int index )
+	{
+		if ( index < 0 || index >= table.length )
+			return null;
+		return (String)table[index][0];
+	};
+
+	private static final String modifierTag( Object [][] table, int index )
+	{
+		if ( index < 0 || index >= table.length )
+			return null;
+		return (String)table[index][1];
+	};
+
+	private static final Pattern modifierDescPattern( Object [][] table, int index )
+	{
+		if ( index < 0 || index >= table.length )
+			return null;
+		return (Pattern)table[index][2];
+	};
+
+	private static final Pattern modifierTagPattern( Object [][] table, int index )
+	{
+		if ( index < 0 || index >= table.length )
+			return null;
+		return (Pattern)table[index][3];
+	};
+
+	private static final String COLD = modifierTag( floatModifiers, COLD_RESISTANCE ) + ": ";
+	private static final String HOT = modifierTag( floatModifiers, HOT_RESISTANCE ) + ": ";
+	private static final String SLEAZE = modifierTag( floatModifiers, SLEAZE_RESISTANCE ) + ": ";
+	private static final String SPOOKY = modifierTag( floatModifiers, SPOOKY_RESISTANCE ) + ": ";
+	private static final String STENCH = modifierTag( floatModifiers, STENCH_RESISTANCE ) + ": ";
+
+	private static final String MOXIE = modifierTag( floatModifiers, MOX ) + ": ";
+	private static final String MUSCLE = modifierTag( floatModifiers, MUS ) + ": ";
+	private static final String MYSTICALITY = modifierTag( floatModifiers, MYS ) + ": ";
+
+	private static final String MOXIE_PCT = modifierTag( floatModifiers, MOX_PCT ) + ": ";
+	private static final String MUSCLE_PCT = modifierTag( floatModifiers, MUS_PCT ) + ": ";
+	private static final String MYSTICALITY_PCT = modifierTag( floatModifiers, MYS_PCT ) + ": ";
+
+	private static int findName( Object [][] table, String name )
+	{
+		for ( int i = 0; i < table.length; ++i )
+			if ( name.equals( (String)table[i][0] ) )
+				return i;
+		return -1;
+	};
+
+        private float[] floats;
+        private String[] strings;
 
 	public Modifiers()
 	{
-		this.modifiers = new float[ MODIFIER_COUNT ];
+		this.floats = new float[ FLOAT_MODIFIERS ];
+		this.strings = new String[ STRING_MODIFIERS ];
 		reset();
 	};
 
         public void reset()
 	{
-                for ( int i = 0; i < this.modifiers.length; ++i )
-                        this.modifiers[i] = 0.0f;
+		for ( int i = 0; i < this.floats.length; ++i )
+			this.floats[i] = 0.0f;
+		for ( int i = 0; i < this.strings.length; ++i )
+			this.strings[i] = "";
 	};
 
 	public float get( int index )
 	{
-		if ( index < 0 || index >= this.modifiers.length )
+		if ( index < 0 || index >= this.floats.length )
 			return 0.0f;
 
-		return this.modifiers[index];
+		return this.floats[index];
+	};
+
+	public float get( String name )
+	{
+		int index = findName( floatModifiers, name );
+		if ( index < 0 || index >= this.floats.length )
+			return 0.0f;
+
+		return this.floats[index];
+	};
+
+	public String getString( int index )
+	{
+		if ( index < 0 || index >= this.strings.length )
+			return "";
+
+		return this.strings[index];
+	};
+
+	public String getString( String name )
+	{
+		int index = findName( stringModifiers, name );
+		if ( index < 0 || index >= this.strings.length )
+			return "";
+
+		return this.strings[index];
 	};
 
 	public boolean set( int index, double mod )
 	{
-		if ( index < 0 || index >= this.modifiers.length )
+		if ( index < 0 || index >= this.floats.length )
 			return false;
 
-		if ( this.modifiers[index] != mod )
+		if ( this.floats[index] != mod )
 		{
-			this.modifiers[index] = (float)mod;
+			this.floats[index] = (float)mod;
+			return true;
+		}
+		return false;
+	};
+
+	public boolean set( int index, String mod )
+	{
+		if ( index < 0 || index >= this.strings.length )
+			return false;
+
+		if ( mod == null )
+			mod = "";
+
+		if ( !mod.equals( this.strings[index] ) )		      
+		{
+			this.strings[index] = mod;
 			return true;
 		}
 		return false;
@@ -167,13 +337,21 @@ public class Modifiers extends KoLDatabase
 		if ( mods == null )
 			return false;
 
-		float [] copy = mods.modifiers;
 		boolean changed = false;
 
-		for ( int index = 0; index < this.modifiers.length; ++index )
-			if ( this.modifiers[index] != copy[index] )
+		float [] copyFloats = mods.floats;
+		for ( int index = 0; index < this.floats.length; ++index )
+			if ( this.floats[index] != copyFloats[index] )
 			{
-				this.modifiers[index] = copy[index];
+				this.floats[index] = copyFloats[index];
+				changed = true;
+			}
+
+		String [] copyStrings = mods.strings;
+		for ( int index = 0; index < this.strings.length; ++index )
+			if ( !this.strings[index].equals( copyStrings[index] ) )
+			{
+				this.strings[index] = copyStrings[index];
 				changed = true;
 			}
 
@@ -182,9 +360,9 @@ public class Modifiers extends KoLDatabase
 
 	public void add( int index, double mod )
 	{
-		if ( index < 0 || index >= this.modifiers.length )
+		if ( index < 0 || index >= this.floats.length )
 			return;
-		this.modifiers[index] += mod;
+		this.floats[index] += mod;
 	};
 
 	public void add( Modifiers mods )
@@ -192,11 +370,11 @@ public class Modifiers extends KoLDatabase
 		if ( mods == null )
 			return;
 
-		float [] addition = mods.modifiers;
+		float [] addition = mods.floats;
 
-		for ( int i = 0; i < this.modifiers.length; ++i )
+		for ( int i = 0; i < this.floats.length; ++i )
 			if ( addition[i] != 0.0f )
-				this.modifiers[i] += addition[i];
+				this.floats[i] += addition[i];
 	}
 
 	public static final Modifiers getModifiers( String name )
@@ -216,13 +394,38 @@ public class Modifiers extends KoLDatabase
 		if ( !( modifier instanceof String ) )
 			return null;
 
-		Modifiers newMods = new Modifiers();
-		float [] newModifiers = newMods.modifiers;
+		String string = (String) modifier;
 
-		for ( int i = 0; i < newModifiers.length; ++i )
+		Modifiers newMods = new Modifiers();
+		float [] newFloats = newMods.floats;
+		String [] newStrings = newMods.strings;
+
+		for ( int i = 0; i < newFloats.length; ++i )
 		{
-			Matcher effectMatcher = MODIFIER_PATTERNS[ i ].matcher( (String) modifier );
-			newModifiers[i] = effectMatcher.find() ? Float.parseFloat( effectMatcher.group(1) ) : 0.0f;
+			Pattern pattern = modifierTagPattern( floatModifiers, i );
+			if ( pattern != null )
+			{
+				Matcher matcher = pattern.matcher( string );
+				if ( matcher.find() )
+				{
+					newFloats[i] = Float.parseFloat ( matcher.group(1) );
+					continue;
+				}
+			}
+		}
+
+		for ( int i = 0; i < newStrings.length; ++i )
+		{
+			Pattern pattern = modifierTagPattern( stringModifiers, i );
+			if ( pattern != null )
+			{
+				Matcher matcher = pattern.matcher( string );
+				if ( matcher.find() )
+				{
+					newStrings[i] =	 matcher.group(1);
+					continue;
+				}
+			}
 		}
 
 		modifierMap.put( name, newMods );
@@ -273,7 +476,7 @@ public class Modifiers extends KoLDatabase
 	{
 		Matcher matcher = DR_PATTERN.matcher( text );
 		if (matcher.find() )
-			return "DR: " + matcher.group(1);
+			return modifierTag( floatModifiers, DAMAGE_REDUCTION ) + ": " + matcher.group(1);
 
 		return null;
 	}
@@ -282,40 +485,40 @@ public class Modifiers extends KoLDatabase
 	private static final Pattern ALL_ATTR_PCT_PATTERN = Pattern.compile( "^All Attributes ([+-]\\d+)%$" );
 	private static final Pattern CLASS_PATTERN = Pattern.compile( "Bonus for (.*) only" );
 	private static final Pattern COMBAT_PATTERN = Pattern.compile( "Monsters will be (.*) attracted to you." );
-	private static final Pattern DA_PATTERN = Pattern.compile( "Damage Absorption (.*)" );
-	private static final Pattern DR2_PATTERN = Pattern.compile( "Damage Reduction: (\\d+)" );
-	private static final Pattern EXP_PATTERN = Pattern.compile( "(.*) Stat.*Per Fight" );
-	private static final Pattern INIT_PATTERN = Pattern.compile( "Combat Initiative (.*)%" );
-	private static final Pattern INTRINSIC_PATTERN = Pattern.compile( "Intrinsic effect: (.*)" );
-	private static final Pattern ITEM_PATTERN = Pattern.compile( "(.*)% Item Drops from Monsters" );
-	private static final Pattern MEAT_PATTERN = Pattern.compile( "(.*)% Meat from Monsters" );
-	private static final Pattern ML_PATTERN = Pattern.compile( "(.*) to Monster Level" );
-	private static final Pattern MOX_PATTERN = Pattern.compile( "Moxie ([+-]\\d+)$" );
-	private static final Pattern MOX_PCT_PATTERN = Pattern.compile( "Moxie ([+-]\\d+)%" );
-	private static final Pattern MUS_PATTERN = Pattern.compile( "Muscle ([+-]\\d+)$" );
-	private static final Pattern MUS_PCT_PATTERN = Pattern.compile( "Muscle ([+-]\\d+)%" );
-	private static final Pattern MYS_PATTERN = Pattern.compile( "Mysticality ([+-]\\d+)$" );
-	private static final Pattern MYS_PCT_PATTERN = Pattern.compile( "Mysticality ([+-]\\d+)%" );
-	private static final Pattern MP_PATTERN = Pattern.compile( "(.*) MP to use Skills" );
-	private static final Pattern WEIGHT_PATTERN = Pattern.compile( "(.*) to Familiar Weight" );
 
 	public static String parseModifier( String enchantment )
 	{
+		String result;
+
+		// Search the float modifiers first
+
+		result = parseModifier( floatModifiers, enchantment );
+		if (result != null )
+			return result;
+
+		// Then the string modifiers
+
+		result = parseModifier( stringModifiers, enchantment );
+		if (result != null )
+			return result;
+
+		// Special handling needed
+
 		Matcher matcher;
 
 		matcher = ALL_ATTR_PATTERN.matcher( enchantment );
 		if ( matcher.find() )
-                {
-                        String mod = matcher.group(1);
-			return "Mox: " + mod + ", Mus: " + mod + ", Mys: " + mod;
-                }
+		{
+			String mod = matcher.group(1);
+			return MOXIE + mod + ", " + MUSCLE + mod + ", " + MYSTICALITY + mod;
+		}
 
 		matcher = ALL_ATTR_PCT_PATTERN.matcher( enchantment );
 		if ( matcher.find() )
-                {
-                        String mod = matcher.group(1);
-			return "Mox%: " + mod + ", Mus%: " + mod + ", Mys%: " + mod;
-                }
+		{
+			String mod = matcher.group(1);
+			return MOXIE_PCT + mod + ", " + MUSCLE_PCT + mod + ", " + MYSTICALITY_PCT + mod;
+		}
 
 		matcher = CLASS_PATTERN.matcher( enchantment );
 		if ( matcher.find() )
@@ -334,76 +537,12 @@ public class Modifiers extends KoLDatabase
 				cls = "SC";
 			else if ( plural.equals( "Turtle Tamers" ) )
 				cls = "TT";
-			return "Class: " + cls;
+			return modifierTag( stringModifiers, CLASS ) + ": " + cls;
 		}
 
 		matcher = COMBAT_PATTERN.matcher( enchantment );
 		if ( matcher.find() )
-			return "Combat: " + ( matcher.group(1).equals( "more" ) ? "+5" : "-5" );
-
-		matcher = DA_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "DA: " + matcher.group(1);
-
-		matcher = DR2_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "DR: " + matcher.group(1);
-
-		matcher = EXP_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Exp: " + matcher.group(1);
-
-		matcher = INIT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Init: " + matcher.group(1);
-
-		matcher = INTRINSIC_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Intrinsic: " + matcher.group(1);
-
-		matcher = ITEM_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Item: " + matcher.group(1);
-
-		matcher = MEAT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Meat: " + matcher.group(1);
-
-		matcher = MP_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mana: " + matcher.group(1);
-
-		matcher = ML_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "ML: " + matcher.group(1);
-
-		matcher = MOX_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mox: " + matcher.group(1);
-
-		matcher = MOX_PCT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mox%: " + matcher.group(1);
-
-		matcher = MUS_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mus: " + matcher.group(1);
-
-		matcher = MUS_PCT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mus%: " + matcher.group(1);
-
-		matcher = MYS_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mys: " + matcher.group(1);
-
-		matcher = MYS_PCT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Mys%: " + matcher.group(1);
-
-		matcher = WEIGHT_PATTERN.matcher( enchantment );
-		if ( matcher.find() )
-			return "Weight: " + matcher.group(1);
+			return modifierTag( floatModifiers, COMBAT_RATE ) + ": " + ( matcher.group(1).equals( "more" ) ? "+5" : "-5" );
 
 		if ( enchantment.indexOf( "Resistance" ) != -1 )
 			return parseResistance( enchantment );
@@ -411,36 +550,52 @@ public class Modifiers extends KoLDatabase
 		return null;
 	}
 
+	private static String parseModifier( Object [][] table, String enchantment )
+	{
+		for ( int i = 0; i < table.length; ++i )
+		{
+			Pattern pattern = modifierDescPattern( table, i );
+			if ( pattern == null )
+				continue;
+
+			Matcher matcher = pattern.matcher( enchantment );
+			if ( matcher.find() )
+				return modifierTag( table, i ) + ": " + matcher.group(1);
+		}
+
+		return null;
+	}
+
 	private static String parseResistance( String enchantment )
 	{
-		int level = 0;
+		String level = "";
 
 		if ( enchantment.indexOf( "Slight" ) != -1 )
-			level = 10;
+			level = "+10";
 		else if ( enchantment.indexOf( "So-So" ) != -1 )
-			level = 20;
+			level = "+20";
 		else if ( enchantment.indexOf( "Serious" ) != -1 )
-			level = 30;
+			level = "+30";
 		else if ( enchantment.indexOf( "Superhuman" ) != -1 )
-			level = 40;
+			level = "+40";
 
 		if ( enchantment.indexOf( "All Elements" ) != -1 )
-			return "Cold: +" + level + ", Hot: +" + level + ", Sleaze: +" + level + ", Spooky: +" + level + ", Stench: +" + level;
+			return COLD + level + ", " + HOT + level + ", " + SLEAZE + level + ", " + SPOOKY + level + ", " + STENCH + level;
 
 		if ( enchantment.indexOf( "Cold" ) != -1 )
-			return "Cold: +" + level;
+			return COLD + level;
 
 		if ( enchantment.indexOf( "Hot" ) != -1 )
-			return "Hot: +" + level;
+			return HOT + level;
 
 		if ( enchantment.indexOf( "Sleaze" ) != -1 )
-			return "Sleaze: +" + level;
+			return SLEAZE + level;
 
 		if ( enchantment.indexOf( "Spooky" ) != -1 )
-			return "Spooky: +" + level;
+			return SPOOKY + level;
 
 		if ( enchantment.indexOf( "Stench" ) != -1 )
-			return "Stench: +" + level;
+			return STENCH + level;
 
 		return null;
 	}
