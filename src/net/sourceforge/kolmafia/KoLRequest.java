2f069b324a76ba6662156b5e5acf501895ea4702
holatuwol <shwei@users.sourceforge.net>
2007-07-30 17:17:41 +0000

    Reduce number of URL restrictions
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4543 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLRequest.java
+++ src/net/sourceforge/kolmafia/KoLRequest.java
@@ -817,10 +817,7 @@ public class KoLRequest extends Job implements KoLConstants
 	}
 
 	public static boolean shouldIgnore( String formURLString )
-	{
-		return formURLString.indexOf( "mall" ) != -1 || formURLString.indexOf( "chat" ) != -1 ||
-			formURLString.indexOf( "send" ) != -1 || formURLString.indexOf( "bhh" ) != -1 ||
-			formURLString.indexOf( "login" ) != -1 || formURLString.indexOf( "logout" ) != -1;
+	{	return formURLString.indexOf( "mall" ) != -1 || formURLString.indexOf( "chat" ) != -1 || formURLString.startsWith( "bhh" );
 	}
 
 	public static boolean delay()
