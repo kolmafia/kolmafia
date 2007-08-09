c51ca215141e94aa558294e6e5636a375bd794de
holatuwol <shwei@users.sourceforge.net>
2007-08-09 22:03:43 +0000

    Add more result-exempt pages
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4693 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLRequest.java
+++ src/net/sourceforge/kolmafia/KoLRequest.java
@@ -346,7 +346,8 @@ public class KoLRequest extends Job implements KoLConstants
 		this.hasNoResult = this.isChatRequest || !this.formURLString.endsWith( ".php" ) || this.formURLString.startsWith( "http://" ) ||
 			this.formURLString.startsWith( "char" ) || this.formURLString.endsWith( "menu.php" ) || this.formURLString.startsWith( "desc" ) ||
 			this.formURLString.startsWith( "display" ) || this.formURLString.startsWith( "search" ) || this.formURLString.startsWith( "show" ) ||
-			this instanceof LoginRequest || this instanceof LogoutRequest || (this instanceof LocalRelayRequest && this.formURLString.startsWith( "clan" ));
+			this instanceof LoginRequest || this instanceof LogoutRequest || this.formURLString.startsWith( "message" ) ||
+			this.formURLString.startsWith( "makeoffer" ) || (this instanceof LocalRelayRequest && this.formURLString.startsWith( "clan" ));
 
 		return this;
 	}
