74439b928e7254aa0f8c793b88eff86345166cdd
holatuwol <shwei@users.sourceforge.net>
2007-08-17 23:14:30 +0000

    Disable controls as refreshes are happening
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4784 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/relay/basics.js
+++ src/relay/basics.js
@@ -158,27 +158,59 @@ function showObject( id )
 }
 
 
+function basementUpdate( select, command )
+{
+	var httpObject1 = getHttpObject();
+	if ( !httpObject1 )
+	return true;
+
+	isRefreshing = true;
+	httpObject1.onreadystatechange = function()
+	{
+		if ( httpObject1.readyState != 4 )
+			return;
+
+		var bodyBegin = httpObject1.responseText.indexOf( ">", httpObject1.responseText.indexOf( "<body" ) ) + 1;
+		var bodyEnd = httpObject1.responseText.indexOf( "</body>" );
+
+		if ( bodyBegin > 0 && bodyEnd > 0 )
+		{
+			top.charpane.document.getElementsByTagName( "body" )[0].innerHTML =
+				httpObject1.responseText.substring( bodyBegin, bodyEnd );
+		}
+
+		isRefreshing = false;
+
+		var httpObject2 = getHttpObject();
+		if ( !httpObject2 )
+			return true;
+
+		httpObject2.onreadystatechange = function()
+		{
+			if ( httpObject2.readyState != 4 )
+				return;
+
+			getObject( "spoiler" ).innerHTML = httpObject2.responseText;
+			select.selectedIndex = 0;
+			select.disabled = false;
+		}
+
+		httpObject2.open( "POST", "/KoLmafia/basementSpoiler" );
+		httpObject2.send( null );
+		return true;
+	}
+
+	select.disabled = true;
+	httpObject1.open( "POST", "/KoLmafia/sideCommand?cmd=" + command );
+	httpObject1.send( "" );
+}
+
+
 function changeBasementOutfit()
 {
 	var select = document.getElementById( "outfit" );
 	var option = select.options[select.selectedIndex];
-	top.charpane.document.location.href = "/KoLmafia/sideCommand?cmd=outfit+" + option.value;
-
-	var httpObject = getHttpObject();
-	if ( !httpObject )
-		return true;
-
-	httpObject.onreadystatechange = function()
-	{
-		if ( httpObject.readyState != 4 )
-			return;
-
-		getObject( "spoiler" ).innerHTML = httpObject.responseText;
-	}
-
-	httpObject.open( "POST", "/KoLmafia/basementSpoiler" );
-	httpObject.send( null );
-	return true;
+	basementUpdate( select, "outfit+" + option.value );
 }
 
 
@@ -186,26 +218,7 @@ function changeBasementPotion()
 {
 	var select = document.getElementById( "potion" );
 	var option = select.options[select.selectedIndex];
-	top.charpane.document.location.href = "/KoLmafia/sideCommand?cmd=" + option.value;
-
-	var httpObject = getHttpObject();
-	if ( !httpObject )
-		return true;
-
-	httpObject.onreadystatechange = function()
-	{
-		if ( httpObject.readyState != 4 )
-			return;
-
-		getObject( "spoiler" ).innerHTML = httpObject.responseText;
-	}
-
-	select.removeChild( select.options[select.selectedIndex] );
-	select.selectedIndex = 0;
-
-	httpObject.open( "POST", "/KoLmafia/basementSpoiler" );
-	httpObject.send( null );
-	return true;
+	basementUpdate( select, option.value );
 }
 
 
