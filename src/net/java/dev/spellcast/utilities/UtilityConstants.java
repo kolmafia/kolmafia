4cd565f61a6c1e5055e1e80684426b2e9ebfbb38
holatuwol <shwei@users.sourceforge.net>
2007-08-21 05:52:40 +0000

    Only move directories around if app.name property set
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@4831 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/java/dev/spellcast/utilities/UtilityConstants.java
+++ src/net/java/dev/spellcast/utilities/UtilityConstants.java
@@ -49,7 +49,7 @@ public interface UtilityConstants
 	public static final ClassLoader SYSTEM_CLASSLOADER    = ClassLoader.getSystemClassLoader();
 	public static final ClassLoader MAINCLASS_CLASSLOADER = net.java.dev.spellcast.utilities.UtilityConstants.class.getClassLoader();
 
-	public static final File ROOT_LOCATION = System.getProperty( "os.name" ).startsWith( "Mac" ) ?
+	public static final File ROOT_LOCATION = System.getProperty( "app.name" ) != null ?
 		new File( new File( System.getProperty( "user.home" ) ).getAbsoluteFile(), "Library/Application Support/KoLmafia" ) :
 		new File( System.getProperty( "user.dir" ) ).getAbsoluteFile();
 
