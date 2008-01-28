2de729422034ede5e2e933bc42b91fca7c6e4db7
holatuwol <shwei@users.sourceforge.net>
2008-01-28 08:42:43 +0000

    Extract custom combat and moods into own panel class
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@5626 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/swingui/panel/CustomCombatPanel.java
@@ -0,0 +1,289 @@
+/**
+ * Copyright (c) 2005-2007, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.swingui.panel;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeModel;
+
+import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.KoLConstants;
+import net.sourceforge.kolmafia.KoLDatabase;
+import net.sourceforge.kolmafia.KoLFrame;
+import net.sourceforge.kolmafia.LogStream;
+import net.sourceforge.kolmafia.StaticEntity;
+import net.sourceforge.kolmafia.ThreadedButton;
+import net.sourceforge.kolmafia.session.CustomCombatManager;
+
+import net.sourceforge.kolmafia.persistence.Preferences;
+
+public class CustomCombatPanel
+	extends JPanel
+{
+	protected JTree combatTree;
+	protected JTextArea combatEditor;
+	protected DefaultTreeModel combatModel;
+	protected CardLayout combatCards;
+
+	public CustomCombatPanel()
+	{
+		this.combatTree = new JTree();
+		this.combatModel = (DefaultTreeModel) this.combatTree.getModel();
+
+		this.combatCards = new CardLayout();
+
+		this.setLayout( this.combatCards );
+		this.add( "tree", new CustomCombatTreePanel() );
+		this.add( "editor", new CustomCombatEditorPanel() );
+
+		CustomCombatManager.loadSettings();
+		this.refreshCombatTree();
+	}
+
+	public void refreshCombatEditor()
+	{
+		try
+		{
+			BufferedReader reader =
+				KoLDatabase.getReader( new File( KoLConstants.CCS_LOCATION, CustomCombatManager.settingsFileName() ) );
+
+			if ( reader == null )
+			{
+				return;
+			}
+
+			StringBuffer buffer = new StringBuffer();
+			String line;
+
+			while ( ( line = reader.readLine() ) != null )
+			{
+				buffer.append( line );
+				buffer.append( '\n' );
+			}
+
+			reader.close();
+			reader = null;
+
+			this.combatEditor.setText( buffer.toString() );
+		}
+		catch ( Exception e )
+		{
+			// This should not happen.  Therefore, print
+			// a stack trace for debug purposes.
+
+			StaticEntity.printStackTrace( e );
+		}
+
+		this.refreshCombatTree();
+	}
+
+	/**
+	 * Internal class used to handle everything related to displaying custom combat.
+	 */
+
+	public void refreshCombatTree()
+	{
+		this.combatModel.setRoot( CustomCombatManager.getRoot() );
+		this.combatTree.setRootVisible( false );
+
+		for ( int i = 0; i < this.combatTree.getRowCount(); ++i )
+		{
+			this.combatTree.expandRow( i );
+		}
+	}
+
+	private class CustomCombatEditorPanel
+		extends ScrollablePanel
+	{
+		public CustomCombatEditorPanel()
+		{
+			super( "Editor", "save", "help", new JTextArea() );
+			CustomCombatPanel.this.combatEditor = (JTextArea) this.scrollComponent;
+			CustomCombatPanel.this.combatEditor.setFont( KoLConstants.DEFAULT_FONT );
+			CustomCombatPanel.this.refreshCombatTree();
+		}
+
+		public void actionConfirmed()
+		{
+			String saveText = CustomCombatPanel.this.combatEditor.getText();
+
+			File location = new File( KoLConstants.CCS_LOCATION, CustomCombatManager.settingsFileName() );
+			LogStream writer = LogStream.openStream( location, true );
+
+			writer.print( saveText );
+			writer.close();
+			writer = null;
+
+			KoLCharacter.battleSkillNames.setSelectedItem( "custom combat script" );
+			Preferences.setString( "battleAction", "custom combat script" );
+
+			// After storing all the data on disk, go ahead
+			// and reload the data inside of the tree.
+
+			CustomCombatManager.loadSettings();
+			CustomCombatPanel.this.refreshCombatTree();
+			CustomCombatPanel.this.combatCards.show( CustomCombatPanel.this, "tree" );
+		}
+
+		public void actionCancelled()
+		{
+			StaticEntity.openSystemBrowser( "http://kolmafia.sourceforge.net/combat.html" );
+		}
+
+		public void setEnabled( final boolean isEnabled )
+		{
+		}
+	}
+
+	public class CustomCombatTreePanel
+		extends ScrollablePanel
+	{
+		public JComboBox availableScripts;
+
+		public CustomCombatTreePanel()
+		{
+			super( "", "edit", "help", CustomCombatPanel.this.combatTree );
+			CustomCombatPanel.this.combatTree.setVisibleRowCount( 8 );
+
+			this.availableScripts = new CombatComboBox();
+			this.centerPanel.add( this.availableScripts, BorderLayout.NORTH );
+
+			JPanel extraButtons = new JPanel( new GridLayout( 2, 1, 5, 5 ) );
+
+			extraButtons.add( new NewScriptButton() );
+			extraButtons.add( new CopyScriptButton() );
+
+			JPanel buttonHolder = new JPanel( new BorderLayout() );
+			buttonHolder.add( extraButtons, BorderLayout.NORTH );
+
+			this.eastPanel.add( buttonHolder, BorderLayout.SOUTH );
+		}
+
+		public void actionConfirmed()
+		{
+			CustomCombatManager.loadSettings();
+			CustomCombatPanel.this.refreshCombatEditor();
+			CustomCombatPanel.this.combatCards.show( CustomCombatPanel.this, "editor" );
+		}
+
+		public void actionCancelled()
+		{
+		}
+
+		public void setEnabled( final boolean isEnabled )
+		{
+		}
+
+		public class CombatComboBox
+			extends JComboBox
+		{
+			public CombatComboBox()
+			{
+				super( CustomCombatManager.getAvailableScripts() );
+				this.setSelectedItem( CustomCombatManager.settingName() );
+				this.addActionListener( new CombatComboBoxListener() );
+			}
+
+			public class CombatComboBoxListener
+				implements ActionListener
+			{
+				public void actionPerformed( final ActionEvent e )
+				{
+					String script = (String) CombatComboBox.this.getSelectedItem();
+					if ( script != null )
+					{
+						CustomCombatManager.setScript( script );
+						CustomCombatPanel.this.refreshCombatTree();
+					}
+				}
+			}
+		}
+
+		public class NewScriptButton
+			extends ThreadedButton
+		{
+			public NewScriptButton()
+			{
+				super( "new" );
+			}
+
+			public void run()
+			{
+				String name = KoLFrame.input( "Give your combat script a name!" );
+				if ( name == null || name.equals( "" ) || name.equals( "default" ) )
+				{
+					return;
+				}
+
+				CustomCombatManager.setScript( name );
+				CustomCombatTreePanel.this.availableScripts.setSelectedItem( CustomCombatManager.settingName() );
+				CustomCombatPanel.this.refreshCombatTree();
+			}
+		}
+
+		public class CopyScriptButton
+			extends ThreadedButton
+		{
+			public CopyScriptButton()
+			{
+				super( "copy" );
+			}
+
+			public void run()
+			{
+				String name = KoLFrame.input( "Make a copy of current script called:" );
+				if ( name == null || name.equals( "" ) || name.equals( "default" ) )
+				{
+					return;
+				}
+
+				CustomCombatManager.copySettings( name );
+				CustomCombatManager.setScript( name );
+				CustomCombatTreePanel.this.availableScripts.setSelectedItem( CustomCombatManager.settingName() );
+				CustomCombatPanel.this.refreshCombatTree();
+			}
+		}
+	}
+}
+
---
+++ src/net/sourceforge/kolmafia/swingui/panel/MoodOptionsPanel.java
@@ -0,0 +1,372 @@
+/**
+ * Copyright (c) 2005-2007, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.swingui.panel;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import net.java.dev.spellcast.utilities.LockableListModel;
+
+import net.sourceforge.kolmafia.KoLFrame;
+import net.sourceforge.kolmafia.ShowDescriptionList;
+import net.sourceforge.kolmafia.ThreadedButton;
+import net.sourceforge.kolmafia.session.MoodManager;
+import net.sourceforge.kolmafia.session.MoodManager.MoodTrigger;
+import net.sourceforge.kolmafia.swingui.widget.AutoFilterComboBox;
+
+import net.sourceforge.kolmafia.persistence.EffectDatabase;
+import net.sourceforge.kolmafia.persistence.Preferences;
+
+public class MoodOptionsPanel
+	extends JPanel
+{
+	protected JList moodList;
+
+	public MoodOptionsPanel()
+	{
+		super( new BorderLayout() );
+
+		this.add( new MoodTriggerListPanel(), BorderLayout.CENTER );
+
+		AddTriggerPanel triggers = new AddTriggerPanel();
+		this.moodList.addListSelectionListener( triggers );
+		this.add( triggers, BorderLayout.NORTH );
+	}
+
+	private class MoodTriggerListPanel
+		extends ScrollablePanel
+	{
+		public JComboBox availableMoods;
+
+		public MoodTriggerListPanel()
+		{
+			super( "", new ShowDescriptionList( MoodManager.getTriggers() ) );
+
+			this.availableMoods = new MoodComboBox();
+
+			this.centerPanel.add( this.availableMoods, BorderLayout.NORTH );
+			MoodOptionsPanel.this.moodList = (JList) this.scrollComponent;
+
+			JPanel extraButtons = new JPanel( new GridLayout( 3, 1, 5, 5 ) );
+
+			extraButtons.add( new NewMoodButton() );
+			extraButtons.add( new DeleteMoodButton() );
+			extraButtons.add( new CopyMoodButton() );
+
+			JPanel buttonHolder = new JPanel( new BorderLayout() );
+			buttonHolder.add( extraButtons, BorderLayout.NORTH );
+
+			this.actualPanel.add( buttonHolder, BorderLayout.EAST );
+		}
+
+		public void actionConfirmed()
+		{
+		}
+
+		public void actionCancelled()
+		{
+		}
+
+		public void setEnabled( final boolean isEnabled )
+		{
+		}
+
+		private class MoodComboBox
+			extends JComboBox
+		{
+			public MoodComboBox()
+			{
+				super( MoodManager.getAvailableMoods() );
+				String mood = Preferences.getString( "currentMood" );
+				this.setSelectedItem( mood );
+				this.addActionListener( new MoodComboBoxListener() );
+			}
+
+			public class MoodComboBoxListener
+				implements ActionListener
+			{
+				public void actionPerformed( final ActionEvent e )
+				{
+					String mood = (String) MoodComboBox.this.getSelectedItem();
+					if ( mood != null )
+					{
+						MoodManager.setMood( mood );
+					}
+				}
+			}
+		}
+
+		private class NewMoodButton
+			extends ThreadedButton
+		{
+			public NewMoodButton()
+			{
+				super( "new list" );
+			}
+
+			public void run()
+			{
+				String name = KoLFrame.input( "Give your list a name!" );
+				if ( name == null )
+				{
+					return;
+				}
+
+				MoodManager.setMood( name );
+				MoodManager.saveSettings();
+			}
+		}
+
+		private class DeleteMoodButton
+			extends ThreadedButton
+		{
+			public DeleteMoodButton()
+			{
+				super( "delete list" );
+			}
+
+			public void run()
+			{
+				MoodManager.deleteCurrentMood();
+				MoodManager.saveSettings();
+			}
+		}
+
+		private class CopyMoodButton
+			extends ThreadedButton
+		{
+			public CopyMoodButton()
+			{
+				super( "copy list" );
+			}
+
+			public void run()
+			{
+				String moodName = KoLFrame.input( "Make a copy of current mood list called:" );
+				if ( moodName == null )
+				{
+					return;
+				}
+
+				if ( moodName.equals( "default" ) )
+				{
+					return;
+				}
+
+				MoodManager.copyTriggers( moodName );
+				MoodManager.setMood( moodName );
+				MoodManager.saveSettings();
+			}
+		}
+	}
+
+	public class AddTriggerPanel
+		extends GenericPanel
+		implements ListSelectionListener
+	{
+		public LockableListModel EMPTY_MODEL = new LockableListModel();
+		public LockableListModel EFFECT_MODEL = new LockableListModel();
+
+		public TypeComboBox typeSelect;
+		public ValueComboBox valueSelect;
+		public JTextField commandField;
+
+		public AddTriggerPanel()
+		{
+			super( "add entry", "auto-fill" );
+
+			this.typeSelect = new TypeComboBox();
+
+			Object[] names = EffectDatabase.values().toArray();
+
+			for ( int i = 0; i < names.length; ++i )
+			{
+				this.EFFECT_MODEL.add( names[ i ].toString() );
+			}
+
+			this.EFFECT_MODEL.sort();
+
+			this.valueSelect = new ValueComboBox();
+			this.commandField = new JTextField();
+
+			VerifiableElement[] elements = new VerifiableElement[ 3 ];
+			elements[ 0 ] = new VerifiableElement( "Trigger On: ", this.typeSelect );
+			elements[ 1 ] = new VerifiableElement( "Check For: ", this.valueSelect );
+			elements[ 2 ] = new VerifiableElement( "Command: ", this.commandField );
+
+			this.setContent( elements );
+		}
+
+		public void valueChanged( final ListSelectionEvent e )
+		{
+			Object selected = MoodOptionsPanel.this.moodList.getSelectedValue();
+			if ( selected == null )
+			{
+				return;
+			}
+
+			MoodTrigger node = (MoodTrigger) selected;
+			String type = node.getType();
+
+			// Update the selected type
+
+			if ( type.equals( "lose_effect" ) )
+			{
+				this.typeSelect.setSelectedIndex( 0 );
+			}
+			else if ( type.equals( "gain_effect" ) )
+			{
+				this.typeSelect.setSelectedIndex( 1 );
+			}
+			else if ( type.equals( "unconditional" ) )
+			{
+				this.typeSelect.setSelectedIndex( 2 );
+			}
+
+			// Update the selected effect
+
+			this.valueSelect.setSelectedItem( node.getName() );
+			this.commandField.setText( node.getAction() );
+		}
+
+		public void actionConfirmed()
+		{
+			String currentMood = Preferences.getString( "currentMood" );
+			if ( currentMood.equals( "apathetic" ) )
+			{
+				KoLFrame.alert( "You cannot add triggers to an apathetic mood." );
+				return;
+			}
+
+			MoodManager.addTrigger(
+				this.typeSelect.getSelectedType(), (String) this.valueSelect.getSelectedItem(),
+				this.commandField.getText() );
+			MoodManager.saveSettings();
+		}
+
+		public void actionCancelled()
+		{
+			String[] autoFillTypes =
+				new String[] { "minimal set (current active buffs)", "maximal set (all castable buffs)" };
+			String desiredType =
+				(String) KoLFrame.input( "Which kind of buff set would you like to use?", autoFillTypes );
+
+			if ( desiredType == null )
+			{
+				return;
+			}
+
+			if ( desiredType == autoFillTypes[ 0 ] )
+			{
+				MoodManager.minimalSet();
+			}
+			else
+			{
+				MoodManager.maximalSet();
+			}
+
+			MoodManager.saveSettings();
+		}
+
+		public void setEnabled( final boolean isEnabled )
+		{
+		}
+
+		public void addStatusLabel()
+		{
+		}
+
+		private class ValueComboBox
+			extends AutoFilterComboBox
+		{
+			public ValueComboBox()
+			{
+				super( AddTriggerPanel.this.EFFECT_MODEL, false );
+			}
+
+			public void setSelectedItem( final Object anObject )
+			{
+				AddTriggerPanel.this.commandField.setText( MoodManager.getDefaultAction(
+					AddTriggerPanel.this.typeSelect.getSelectedType(), (String) anObject ) );
+				super.setSelectedItem( anObject );
+			}
+		}
+
+		private class TypeComboBox
+			extends JComboBox
+		{
+			public TypeComboBox()
+			{
+				this.addItem( "When an effect is lost" );
+				this.addItem( "When an effect is gained" );
+				this.addItem( "Unconditional trigger" );
+
+				this.addActionListener( new TypeComboBoxListener() );
+			}
+
+			public String getSelectedType()
+			{
+				switch ( this.getSelectedIndex() )
+				{
+				case 0:
+					return "lose_effect";
+				case 1:
+					return "gain_effect";
+				case 2:
+					return "unconditional";
+				default:
+					return null;
+				}
+			}
+
+			private class TypeComboBoxListener
+				implements ActionListener
+			{
+				public void actionPerformed( final ActionEvent e )
+				{
+					AddTriggerPanel.this.valueSelect.setModel( TypeComboBox.this.getSelectedIndex() == 2 ? AddTriggerPanel.this.EMPTY_MODEL : AddTriggerPanel.this.EFFECT_MODEL );
+				}
+			}
+		}
+	}
+}
