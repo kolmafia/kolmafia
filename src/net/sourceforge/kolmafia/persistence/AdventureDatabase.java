fd468840d77e1198e30e4ec8eb29a0ee7034fe5f
Veracity <veracity0@users.sourceforge.net>
2008-02-18 04:33:45 +0000

    Lucky Sewer now offers seal-skull helmet, not seal tooth. Clear and recreate form fields each time through Lucky Sewer to allow changing items.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@5776 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/persistence/AdventureDatabase.java
+++ src/net/sourceforge/kolmafia/persistence/AdventureDatabase.java
@@ -293,9 +293,21 @@ public class AdventureDatabase
 
 	// Lucky sewer options
 	public static final ChoiceAdventure LUCKY_SEWER =
-		new ChoiceAdventure(
-			"Town", "luckySewerAdventure", "Sewer Gnomes",
-			new String[] { "seal-clubbing club", "seal tooth", "helmet turtle", "turtle totem", "pasta spoon", "ravioli hat", "saucepan", "disco mask", "disco ball", "stolen accordion", "mariachi pants" } );
+		new ChoiceAdventure( "Town", "luckySewerAdventure",
+				     "Sewer Gnomes",
+				     new String[] {
+					     "seal-clubbing club",
+					     "seal-skull helmet",
+					     "helmet turtle",
+					     "turtle totem",
+					     "pasta spoon",
+					     "ravioli hat",
+					     "saucepan",
+					     "disco mask",
+					     "disco ball",
+					     "stolen accordion",
+					     "mariachi pants"
+				     } );
 
 	public static final ChoiceAdventure[] CHOICE_ADVS =
 	{
