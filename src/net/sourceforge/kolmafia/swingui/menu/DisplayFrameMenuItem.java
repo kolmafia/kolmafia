f21d76f8edbcca471924c051b9f2ca37f73f4c13
holatuwol <shwei@users.sourceforge.net>
2008-03-16 08:30:00 +0000

    Split menu bar into multiple classes
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@5971 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/swingui/menu/DisplayFrameMenuItem.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2005-2008, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.swingui.menu;
+
+import java.lang.ref.WeakReference;
+
+import javax.swing.SwingUtilities;
+
+import net.java.dev.spellcast.utilities.LicenseDisplay;
+
+import net.sourceforge.kolmafia.CreateFrameRunnable;
+import net.sourceforge.kolmafia.KoLDesktop;
+import net.sourceforge.kolmafia.KoLmafiaGUI;
+import net.sourceforge.kolmafia.StaticEntity;
+import net.sourceforge.kolmafia.swingui.ChatFrame;
+import net.sourceforge.kolmafia.swingui.GenericFrame;
+import net.sourceforge.kolmafia.swingui.panel.VersionDataPanel;
+
+import net.sourceforge.kolmafia.persistence.Preferences;
+
+/**
+ * In order to keep the user interface from freezing (or at least appearing to freeze), this internal class is used
+ * to process the request for viewing frames.
+ */
+
+public class DisplayFrameMenuItem
+	extends ThreadedMenuItem
+{
+	private static final String[] LICENSE_FILENAME =
+	{
+		"kolmafia-license.txt",
+		"spellcast-license.txt",
+		"browserlauncher-license.htm",
+		"sungraphics-license.txt",
+		"foxtrot-license.txt",
+		"jsmooth-license.txt",
+		"unlicensed.htm"
+	};
+
+	private static final String[] LICENSE_NAME =
+	{
+		"KoLmafia",
+		"Spellcast",
+		"BrowserLauncher",
+		"Sun Graphics",
+		"Foxtrot",
+		"JSmooth",
+		"Unlicensed"
+	};
+
+	private String frameClass;
+	private WeakReference frameReference;
+
+	public DisplayFrameMenuItem( final String title, final String frameClass )
+	{
+		super( title );
+		this.frameClass = frameClass;
+	}
+
+	public DisplayFrameMenuItem( final GenericFrame frame )
+	{
+		super( frame == null ? "Show All Displays" : frame.toString() );
+
+		this.frameClass = null;
+		this.frameReference = frame == null ? null : new WeakReference( frame );
+	}
+
+	public void run()
+	{
+		if ( this.frameClass == null )
+		{
+			this.openExistingFrame();
+		}
+		else
+		{
+			this.openNewFrame();
+		}
+	}
+
+	private void openNewFrame()
+	{
+		if ( this.frameClass.equals( "LicenseDisplay" ) )
+		{
+			Object[] parameters = new Object[ 4 ];
+			parameters[ 0 ] = "KoLmafia: Copyright Notices";
+			parameters[ 1 ] = new VersionDataPanel();
+			parameters[ 2 ] = LICENSE_FILENAME;
+			parameters[ 3 ] = LICENSE_NAME;
+
+			SwingUtilities.invokeLater( new CreateFrameRunnable( LicenseDisplay.class, parameters ) );
+		}
+		else
+		{
+			KoLmafiaGUI.constructFrame( this.frameClass );
+		}
+	}
+
+	private void openExistingFrame()
+	{
+		if ( this.frameReference == null )
+		{
+			GenericFrame[] frames = StaticEntity.getExistingFrames();
+			String interfaceSetting = Preferences.getString( "initialDesktop" );
+
+			for ( int i = 0; i < frames.length; ++i )
+			{
+				if ( interfaceSetting.indexOf( frames[ i ].getFrameName() ) == -1 )
+				{
+					frames[ i ].setVisible( true );
+				}
+			}
+
+			if ( KoLDesktop.instanceExists() )
+			{
+				KoLDesktop.getInstance().setVisible( true );
+			}
+
+			return;
+		}
+
+		GenericFrame frame = (GenericFrame) this.frameReference.get();
+		if ( frame != null )
+		{
+			boolean appearsInTab =
+				GenericFrame.appearsInTab( frame instanceof ChatFrame ? "ChatManager" : frame.getFrameName() );
+
+			if ( !appearsInTab )
+			{
+				frame.setVisible( true );
+			}
+
+			frame.requestFocus();
+		}
+	}
+}
---
+++ src/net/sourceforge/kolmafia/swingui/menu/InvocationMenuItem.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2005-2008, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.swingui.menu;
+
+import java.lang.reflect.Method;
+
+import net.sourceforge.kolmafia.KoLConstants;
+import net.sourceforge.kolmafia.KoLmafia;
+import net.sourceforge.kolmafia.RequestThread;
+import net.sourceforge.kolmafia.StaticEntity;
+
+/**
+ * Internal class used to invoke the given no-parameter method on the given object. This is used whenever there is
+ * the need to invoke a method and the creation of an additional class is unnecessary.
+ */
+
+public class InvocationMenuItem
+	extends ThreadedMenuItem
+{
+	private Object object;
+	private Method method;
+
+	public InvocationMenuItem( final String title, final Object object, final String methodName )
+	{
+		this( title, object == null ? null : object.getClass(), methodName );
+		this.object = object;
+	}
+
+	public InvocationMenuItem( final String title, final Class c, final String methodName )
+	{
+		super( title );
+
+		if ( c == null )
+		{
+			return;
+		}
+
+		try
+		{
+			this.object = c;
+			this.method = c.getMethod( methodName, KoLConstants.NOPARAMS );
+		}
+		catch ( Exception e )
+		{
+			// This should not happen.  Therefore, print
+			// a stack trace for debug purposes.
+
+			StaticEntity.printStackTrace( e );
+		}
+	}
+
+	public void run()
+	{
+		try
+		{
+			RequestThread.openRequestSequence();
+
+			if ( this.method != null )
+			{
+				this.method.invoke( this.object instanceof KoLmafia ? StaticEntity.getClient() : this.object, null );
+			}
+
+			RequestThread.closeRequestSequence();
+		}
+		catch ( Exception e1 )
+		{
+			// This should not happen.  Therefore, print
+			// a stack trace for debug purposes.
+
+			StaticEntity.printStackTrace( e1 );
+		}
+	}
+}
