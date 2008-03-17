2dd01527faf667e7a653df3f6593ff9106dad5ae
holatuwol <shwei@users.sourceforge.net>
2008-03-17 08:17:45 +0000

    Update version number
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@5983 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -60,8 +60,8 @@ public interface KoLConstants
 	// Rendered in various locations and therefore made public.
 
 	public static final String REVISION = null;
-	public static final String VERSION_NAME = "KoLmafia v12.3";
-	public static final String VERSION_DATE = "Released on March 10, 2008";
+	public static final String VERSION_NAME = "KoLmafia v12.4";
+	public static final String VERSION_DATE = "Released on March 17, 2008";
 
 	// General constants used for calculations and formatting of
 	// strings, as well as for string parsing.
