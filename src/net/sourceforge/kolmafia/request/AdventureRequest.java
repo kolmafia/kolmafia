317bab50b3c98c6fad21800922654312a5e5a5e4
jasonharper <jasonharper@users.sourceforge.net>
2008-11-05 10:48:18 +0000

    Tries to recognize monster names, and to insert spoilers, in haiku.
    Only tested in
    the Haiku Dungeon: I don't
    know how well it will
    
    work with combats with
    the haiku katana (what
    happens with monster
    
    names over seven
    syllables?). blah blah blah blah
    blah blah haiku sux
    
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@6474 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/AdventureRequest.java
+++ src/net/sourceforge/kolmafia/request/AdventureRequest.java
@@ -63,6 +63,8 @@ public class AdventureRequest
 	private static final Pattern CHOICE_PATTERN = Pattern.compile( "whichchoice value=(\\d+)" );
 
 	private static final Pattern MONSTER_NAME = Pattern.compile( "<span id='monname'(>| title=\")([^<\"]*?)(</span>|\">)", Pattern.DOTALL );
+	private static final Pattern HAIKU_MONSTER_NAME = Pattern.compile(
+		"<b>.*<b>([^<]+)<", Pattern.DOTALL );
 
 	private static final GenericRequest ZONE_UNLOCK = new GenericRequest( "" );
 
@@ -516,12 +518,22 @@ public class AdventureRequest
 
 	private static final String parseCombatEncounter( final String responseText )
 	{
+		String name;
 		Matcher matcher = MONSTER_NAME.matcher( responseText );
 		if ( !matcher.find() )
 		{
-			return "";
+			matcher = HAIKU_MONSTER_NAME.matcher( responseText );
+			if ( !matcher.find() )
+			{
+				return "";
+			}
+			name = matcher.group(1);
 		}
-		String name = CustomCombatManager.encounterKey( matcher.group(2), false );
+		else
+		{
+			name = matcher.group(2);
+		}
+		name = CustomCombatManager.encounterKey( name, false );
 		if ( name.equalsIgnoreCase( fromName ) )
 		{
 			name = CustomCombatManager.encounterKey( toName, false );
