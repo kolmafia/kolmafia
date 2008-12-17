adad673c81d5d9c03c29c381d4365670fe2c3c1e
Veracity <veracity0@users.sourceforge.net>
2008-12-17 17:27:12 +0000

    When you get das boot, you lose your damp old boot
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@6632 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -577,6 +577,7 @@ public class ItemPool
 	public static final int GIFTH = 3430;
 	public static final int SPICE_MELANGE = 3433;
 	public static final int BATHYSPHERE = 3470;
+	public static final int DAMP_OLD_BOOT = 3471;
 	public static final int LARP_MEMBERSHIP_CARD = 3506;
 	public static final int STICKER_BOOK = 3507;
 	public static final int STICKER_SWORD = 3508;
