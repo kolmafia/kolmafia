ce942145dc8d17efc7ccaf221d9900104000ac2e
Veracity <veracity0@users.sourceforge.net>
2008-12-19 05:54:10 +0000

    Add new items. (Re)fix fullness for nigiri: KoL erroneously changed it, I reported it, and changed KoLmafia to accomadate the broken data, and now it has been restored... When you buy the damp old boot, gice a link to the Old Man
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@6635 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -599,6 +599,7 @@ public class ItemPool
 	public static final int DAS_BOOT = 3609;
 	public static final int IMITATION_WHETSTONE = 3610;
 	public static final int BURROWGRUB_HIVE = 3629;
+	public static final int JAMFISH_JAM = 3641;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
