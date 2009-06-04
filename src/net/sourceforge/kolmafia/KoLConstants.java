af911c31d9b1d9d99a929e29729cfe550b829e7e
Veracity <veracity0@users.sourceforge.net>
2009-06-04 22:19:48 +0000

    Version 13.3
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@7358 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -60,8 +60,8 @@ public interface KoLConstants
 	// Rendered in various locations and therefore made public.
 
 	public static final String REVISION = null;
-	public static final String VERSION_NAME = "KoLmafia v13.2";
-	public static final String VERSION_DATE = "Released on May 6, 2009";
+	public static final String VERSION_NAME = "KoLmafia v13.3";
+	public static final String VERSION_DATE = "Released on June 4, 2009";
 
 	// General constants used for calculations and formatting of
 	// strings, as well as for string parsing.
