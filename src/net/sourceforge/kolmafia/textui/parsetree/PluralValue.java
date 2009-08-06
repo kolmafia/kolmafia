63957a297ceb4ce39e5a0fa57271dc870aa48d18
jasonharper <jasonharper@users.sourceforge.net>
2009-08-06 21:12:41 +0000

    Oops, added new file that was missing from previous commit.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@7525 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/textui/parsetree/PluralValue.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2005-2009, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION ) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE ) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.textui.parsetree;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+import net.sourceforge.kolmafia.textui.DataTypes;
+import net.sourceforge.kolmafia.textui.Interpreter;
+
+public class PluralValue
+	extends AggregateValue
+{
+	private TreeSet lookup;
+	
+	public PluralValue( final Type type, ArrayList values )
+	{
+		super( new AggregateType( DataTypes.BOOLEAN_TYPE, type ) );
+
+		this.content = values.toArray( new Value[ 0 ] );
+	}
+
+	public Value aref( final Value key, final Interpreter interpreter )
+	{
+		return DataTypes.makeBooleanValue( this.contains( key ) );
+	}
+
+	public void aset( final Value key, final Value val, final Interpreter interpreter )
+	{
+		throw interpreter.runtimeException( "Cannot modify constant value" );
+	}
+
+	public Value remove( final Value key, final Interpreter interpreter )
+	{
+		throw interpreter.runtimeException( "Cannot modify constant value" );
+	}
+
+	public void clear()
+	{
+	}
+
+	public int count()
+	{
+		Value[] array = (Value[]) this.content;
+		return array.length;
+	}
+
+	public boolean contains( final Value key )
+	{
+		if ( this.lookup == null )
+		{
+			this.lookup = new TreeSet();
+			this.lookup.addAll( Arrays.asList( (Value[]) this.content ) );
+		}
+		return this.lookup.contains( key );
+	}
+
+	public Value[] keys()
+	{
+		return (Value[]) this.content;
+	}
+}
