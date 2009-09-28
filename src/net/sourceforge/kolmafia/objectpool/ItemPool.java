c9d976b2d7f9085de1bb13926bb9453e03e918f4
jasonharper <jasonharper@users.sourceforge.net>
2009-09-28 06:48:04 +0000

    Recognize when an opera mask is used up when casting an AT buff, and replace it with the previously-equipped hat.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@7656 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -428,6 +428,7 @@ public class ItemPool
 	public static final int SCRAP_OF_PAPER = 1959;
 	public static final int EVIL_SCROLL = 1960;
 	public static final int DANCE_CARD = 1963;
+	public static final int OPERA_MASK = 1964;
 	public static final int PUMPKIN_BUCKET = 1971;
 	public static final int STUFFED_COCOABO = 1974;
 	public static final int MACGUFFIN_DIARY = 2044;
