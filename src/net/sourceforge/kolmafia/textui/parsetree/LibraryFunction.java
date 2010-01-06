f047370d913697cb7ec151613ab6d16186a96e96
fronobulax <jaadams5@users.sourceforge.net>
2010-01-06 01:27:16 +0000

    Explict cast to java.lang.Object to make complier happier.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@7958 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/parsetree/LibraryFunction.java
+++ src/net/sourceforge/kolmafia/textui/parsetree/LibraryFunction.java
@@ -116,7 +116,7 @@ public class LibraryFunction
 		{
 			// Invoke the method
 			LibraryFunction.interpreter = interpreter;
-			return (Value) this.method.invoke( this, this.values );
+			return (Value) this.method.invoke( this, (java.lang.Object) this.values );
 		}
 		catch ( InvocationTargetException e )
 		{
