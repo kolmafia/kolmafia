f5e136743fc4a818efd5cba285f2cab2c1eead88
Veracity <veracity0@users.sourceforge.net>
2010-03-05 01:08:57 +0000

    Add more Nemesis and Rabbit Hole items
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8221 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -1044,6 +1044,7 @@ public class ItemPool
 	public static final int STINKY_CHEESE_STAFF = 4403;
 	public static final int ZU_MANNKASE_DIENEN = 4409;
 	public static final int INIGO_BOOK = 4411;
+	public static final int BLACK_HYMNAL = 4426;
 	public static final int GLOWSTICK_ON_A_STRING = 4428;
 	public static final int CANDY_NECKLACE = 4429;
 	public static final int TEDDYBEAR_BACKPACK = 4430;
