a007d585baf303c1f51b3ed9319979ca203a47e0
jasonharper <jasonharper@users.sourceforge.net>
2010-03-19 10:27:22 +0000

    Be more specific when checking if your guild store is open - it could be fooled by a reference to managestore.php in the page's JavaScript, if you had mallselling enabled as a right-click option.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8300 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/GuildRequest.java
+++ src/net/sourceforge/kolmafia/request/GuildRequest.java
@@ -272,7 +272,7 @@ public class GuildRequest
 			return;
 		}
 		
-		KoLCharacter.setGuildStoreOpen( responseText.indexOf( "store.php" ) != -1 );
+		KoLCharacter.setGuildStoreOpen( responseText.indexOf( "\"store.php" ) != -1 );
 
 		Matcher matcher = PLACE_PATTERN.matcher( urlString );
 		String place = matcher.find() ? matcher.group( 1 ) : null;
