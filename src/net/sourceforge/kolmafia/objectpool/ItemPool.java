1b1f355ca775b04ce24c233a77461b90cc494326
jasonharper <jasonharper@users.sourceforge.net>
2010-04-08 13:56:42 +0000

    Added "_legendaryBeat" boolean pref, and a Daily Deed to use that item.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8365 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -1080,6 +1080,7 @@ public class ItemPool
 	public static final int ITTAH_BITTAH_HOOKAH = 4519;
 	public static final int JACKING_MAP = 4560;
 	public static final int TINY_FLY_GLASSES = 4566;
+	public static final int LEGENDARY_BEAT = 4573;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
