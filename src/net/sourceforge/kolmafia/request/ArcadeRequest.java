684e326b6f891e53dd07e9bf937d420139348650
Veracity <veracity0@users.sourceforge.net>
2010-06-28 14:43:22 +0000

    Server side change to Dungeon Fist! made each choice look like an "Encounter" and log as such. Ignore "Encounters" in arcade games.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8517 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/ArcadeRequest.java
+++ src/net/sourceforge/kolmafia/request/ArcadeRequest.java
@@ -258,6 +258,31 @@ public class ArcadeRequest
 		return true;
 	}
 
+	public static final boolean arcadeChoice( final int choice )
+	{
+		// Do not look for "Encounters" inside arcade choices
+		switch ( choice )
+		{
+		case 460: case 461: case 462: case 463: case 464:
+		case 465:	    case 467: case 468: case 469:
+		case 470:	    case 472: case 473: case 474:
+		case 475: case 476: case 477: case 478: case 479:
+		case 480: case 481: case 482: case 483: case 484:
+			// Space Trip
+		case 471:
+			// DemonStar
+		case 485:
+			// Fighters Of Fighting
+		case 486:
+			// Dungeon Fist!
+		case 488: case 489: case 490:
+			// Meteoid
+			return true;
+		}
+
+		return false;
+	}
+
 	/*
 	 * Support for individual games
 	 */
