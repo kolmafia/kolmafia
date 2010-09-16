e6d97624a9a9253907b9eba23f0fcf23b6ad3f0a
Veracity <veracity0@users.sourceforge.net>
2010-09-16 18:56:29 +0000

    Bump version number to 14.2
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8625 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -60,8 +60,8 @@ public interface KoLConstants
 	// Version information for the current version of KoLmafia.
 	// Rendered in various locations and therefore made public.
 
-	public static final String VERSION_NAME = "KoLmafia v14.1";
-	public static final String VERSION_DATE = "Released on June 16, 2010";
+	public static final String VERSION_NAME = "KoLmafia v14.2";
+	public static final String VERSION_DATE = "Released on September 16, 2010";
 	public static final boolean RELEASED = true;
 	public static final String REVISION = null;
 
