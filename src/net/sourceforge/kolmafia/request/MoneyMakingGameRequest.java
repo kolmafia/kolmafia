c804f476ce1080881e9081f928366b65b16303bf
Veracity <veracity0@users.sourceforge.net>
2010-11-14 19:20:19 +0000

    KoL allows any old crap in the "how much to bet" field of the MMG. It strips out non-numeric characters and treats the rest as an integer. Unfortunately, the "any old crap" appears in the URL that we parse to figure out how much the poor wretch ... uhh, lucky winner ... bet. Interpret it the way KoL does.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@8771 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/MoneyMakingGameRequest.java
+++ src/net/sourceforge/kolmafia/request/MoneyMakingGameRequest.java
@@ -47,6 +47,7 @@ import net.sourceforge.kolmafia.webui.MoneyMakingGameDecorator;
 public class MoneyMakingGameRequest
 	extends GenericRequest
 {
+	public static final Pattern HOWMUCH_PATTERN = Pattern.compile( "howmuch=([^&]*)" );
 	public static final Pattern FROM_PATTERN = Pattern.compile( "from=(\\d*)" );
 	public static final Pattern WHICHBET_PATTERN = Pattern.compile( "whichbet=(\\d*)" );
 	public static final Pattern BETID_PATTERN = Pattern.compile( "betid=(\\d*)" );
@@ -84,7 +85,22 @@ public class MoneyMakingGameRequest
 
 	public static final int getHowMuch( final String urlString )
 	{
-		return MoneyMakingGameRequest.parseInteger( GenericRequest.HOWMUCH_PATTERN, urlString );
+		Matcher matcher = HOWMUCH_PATTERN.matcher( urlString );
+		if ( matcher.find() )
+		{
+			// KoL allows any old crap in the input field. It
+			// strips out non-numeric characters and treats the
+			// rest as an integer.
+			String field = GenericRequest.decodeURL( matcher.group(1) );
+			try
+			{
+				return StringUtilities.parseIntInternal2( field );
+			}
+			catch ( NumberFormatException e )
+			{
+			}
+		}
+		return -1;
 	}
 
 	public static final int getWhichBet( final String urlString )
