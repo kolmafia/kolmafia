947f75b6fd4852acc01e57d0382bd0eb01c970a9
slyz <slyz@users.sourceforge.net>
2011-05-30 17:03:17 +0000

    Don't try to record chat requests to session, as it can get in the way of properly recording redirects while adventuring.
    Cobb's Knob oven is hot.
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9372 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/AdventureRequest.java
+++ src/net/sourceforge/kolmafia/request/AdventureRequest.java
@@ -428,7 +428,7 @@ public class AdventureRequest
 		// Otherwise, if KoLAdventure didn't log the location, there
 		// can't be an encounter for us to log.
 
-		if ( GenericRequest.itemMonster == null && !KoLAdventure.recordToSession( urlString, responseText ) )
+		if ( !request.isChatRequest && GenericRequest.itemMonster == null && !KoLAdventure.recordToSession( urlString, responseText ) )
 		{
 			return "";
 		}
