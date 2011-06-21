deffc061beece831666fa3c1c357d054839d74e9
Veracity <veracity0@users.sourceforge.net>
2011-06-21 07:09:01 +0000

    In Beecore, make sure you have 6 tropical orchids before climbing the tower via the built-in quest script. This will use a packet of orchid seeds to make them, if necessary.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9492 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -575,6 +575,7 @@ public class ItemPool
 	public static final int ODOR_EXTRACTOR = 2462;
 	public static final int OLFACTION_BOOK = 2463;
 	public static final int SHIRT_KIT = 2491;
+	public static final int TROPICAL_ORCHID = 2492;
 	public static final int MOLYBDENUM_MAGNET = 2497;
 	public static final int MOLYBDENUM_HAMMER = 2498;
 	public static final int MOLYBDENUM_SCREWDRIVER = 2499;
