b236e318dcf239a5c3203eebdb6548a171af9ddc
holatuwol <shwei@users.sourceforge.net>
2011-08-18 04:49:30 +0000

    Add a dummy CLI command to represent requests from the GUI
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9718 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/textui/command/InternalRequestCommand.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2005-2011, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION ) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE ) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.textui.command;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.sourceforge.kolmafia.request.GenericRequest;
+
+public class InternalRequestCommand
+	extends AbstractCommand
+{
+	private boolean isRunning = false;
+	private List requests = new LinkedList();
+
+	public void addRequest( GenericRequest request )
+	{
+		if ( this.isRunning )
+		{
+			return;
+		}
+
+		this.requests.add( request );
+	}
+
+	public void addRequests( Object [] requests )
+	{
+		if ( this.isRunning )
+		{
+			return;
+		}
+
+		this.requests.addAll( Arrays.asList( requests ) );
+	}
+
+	public void run( final String cmd, final String parameters )
+	{
+		if ( this.isRunning )
+		{
+			return;
+		}
+
+		this.isRunning = true;
+
+		Iterator requestIterator = this.requests.iterator();
+
+		while ( requestIterator.hasNext() )
+		{
+			GenericRequest request = (GenericRequest) requestIterator.next();
+
+			request.run();
+		}
+	}
+}
