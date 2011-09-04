5d1c1f773e5028e5a19d699928b282bd68062101
grotfang <grotfang@users.sourceforge.net>
2011-09-04 22:02:42 +0000

    Merging channels happens at message receipt, allowing chatbot scripts to see hobopolis and the slimetube. Dungeon is the sender for dungeon message. If Dungeon (#917625) a) logs on, and b) joins a clan I may need to change this. I have tested this and it works as I expected, but I could use feedback from people who regularly merge channels. Have I changed anything you used to like?
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9804 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/chat/ChatParser.java
+++ src/net/sourceforge/kolmafia/chat/ChatParser.java
@@ -279,6 +279,14 @@ public class ChatParser
 			channel = ChatManager.getCurrentChannel();
 		}
 
+		if ( Preferences.getBoolean( "mergeHobopolisChat" ) )
+		{
+			if ( channel.equals( "/hobopolis" ) || channel.equals( "/slimetube" ) )
+			{
+				channel = "/clan";
+			}
+		}
+
 		boolean isAction = false;
 
 		int italicIndex = content.indexOf( "<i>" );
