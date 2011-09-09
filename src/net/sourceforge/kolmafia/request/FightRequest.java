8ba91d4705b4cac91c7306be7b68289bb8fb08c7
slyz <slyz@users.sourceforge.net>
2011-09-09 09:31:05 +0000

    Recognize Stinkpalm damage. Treat strings containing only whitespaces passed to to_item() the same as empty strings.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9816 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/FightRequest.java
+++ src/net/sourceforge/kolmafia/request/FightRequest.java
@@ -3092,7 +3092,7 @@ public class FightRequest
 		Pattern.compile( "(your blood, to the tune of|stabs you for|sown|You lose|You gain|strain your neck|approximately|)\\s*#?(\\d[\\d,]*) (\\([^.]*\\) |)((?:[^\\s]+ ){0,3})(?:\"?damage|points?|bullets|hollow|notch(?:es)?|to your opponent|force damage|tiny holes|like this, it's bound)" );
 
 	private static final Pattern ELEMENTAL_PATTERN =
-		Pattern.compile( "(sown|) \\+?([\\d,]+) (\\([^.]*\\) |)(?:(?:slimy, (?:clammy|gross) |hotsy-totsy |)damage|points|HP worth)" );
+		Pattern.compile( "(sown|) \\+?([\\d,]+) (\\([^.]*\\) |)(?:months worth of concentrated palm sweat|(?:slimy, (?:clammy|gross) |hotsy-totsy |)damage|points|HP worth)" );
 
 	private static final Pattern SECONDARY_PATTERN = Pattern.compile( "\\+([\\d,]+)" );
 
