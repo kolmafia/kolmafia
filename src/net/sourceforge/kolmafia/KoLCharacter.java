9782917cbbd850743904b521169024cb1149b8fd
Veracity <veracity0@users.sourceforge.net>
2011-10-16 20:13:17 +0000

    Refreshing concoctions might have to make a request to get gender. Making a request refreshes concoctions. Uh-oh.
    Add items dropped by sexy sorority monsters
    
    Reorganize zones: Clan Basement holds Hobopolis (which holds other zones), The
    Slime Tube, and the Haunted Sorority House
    
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9891 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLCharacter.java
+++ src/net/sourceforge/kolmafia/KoLCharacter.java
@@ -869,12 +869,14 @@ public abstract class KoLCharacter
 		if ( KoLCharacter.gender == 0 &&
 		     !GenericRequest.passwordHash.equals( "" ) )
 		{	// Can't tell?	Look at their vinyl boots!
+			ConcoctionDatabase.deferRefresh( true );
 			GenericRequest req = new GenericRequest(
 				"desc_item.php?whichitem=809051828" );
 			RequestThread.postRequest( req );
 			KoLCharacter.setGender( req.responseText != null &&
 				req.responseText.indexOf( "+15%" ) != -1 ?
 					KoLCharacter.FEMALE : KoLCharacter.MALE );
+			ConcoctionDatabase.deferRefresh( false );
 		}
 		return KoLCharacter.gender;
 	}
