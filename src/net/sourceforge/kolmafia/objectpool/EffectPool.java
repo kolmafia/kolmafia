bb06e5f38e677deaa1af0a5f8cf76ecf2dd5f112
slyz <slyz@users.sourceforge.net>
2011-10-31 22:37:08 +0000

    Haunting Looks is effect number 937, not 938.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9954 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/EffectPool.java
+++ src/net/sourceforge/kolmafia/objectpool/EffectPool.java
@@ -99,7 +99,7 @@ public class EffectPool
 	public static final int CORSICAN_BLESSING = 460;
 	public static final int FORM_OF_BIRD_ID = 511;
 	public static final int COVERED_IN_SLIME_ID = 633;
-	public static final int HAUNTING_LOOKS_ID = 938;
+	public static final int HAUNTING_LOOKS_ID = 937;
 	public static final int DEAD_SEXY_ID = 938;
 	public static final int VAMPIN_ID = 939;
 	public static final int YIFFABLE_YOU_ID = 940;
