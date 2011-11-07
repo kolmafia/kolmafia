120dedbeeb650b2807a336112974eb2a5282b698
holatuwol <shwei@users.sourceforge.net>
2011-11-07 09:12:36 +0000

    Fix internal chat links resulting in runnables in the event dispatch thread.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@9981 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/swingui/listener/HyperlinkAdapter.java
+++ src/net/sourceforge/kolmafia/swingui/listener/HyperlinkAdapter.java
@@ -39,6 +39,7 @@ import java.util.regex.Pattern;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 
+import net.sourceforge.kolmafia.RequestThread;
 import net.sourceforge.kolmafia.swingui.widget.RequestPane;
 
 import net.sourceforge.kolmafia.utilities.InputFieldUtilities;
@@ -70,103 +71,127 @@ public class HyperlinkAdapter
 
 	public void hyperlinkUpdate( final HyperlinkEvent e )
 	{
-		if ( e.getEventType() == HyperlinkEvent.EventType.ACTIVATED )
+		if ( e.getEventType() != HyperlinkEvent.EventType.ACTIVATED )
 		{
-			String location = e.getDescription();
+			return;
+		}
 
+		RequestPane requestPane = (RequestPane) e.getSource();
+		String location = e.getDescription();
+
+		RequestThread.runInParallel( new HyperlinkUpdateRunnable( requestPane, location ) );
+	}
+
+	private class HyperlinkUpdateRunnable
+		implements Runnable
+	{
+		private RequestPane requestPane;
+		private String location;
+
+		private HyperlinkUpdateRunnable( RequestPane requestPane, String location )
+		{
+			this.requestPane = requestPane;
+			this.location = location;
+		}
+
+		public void run()
+		{
 			if ( location.indexOf( "pics.communityofloathing.com" ) != -1 )
 			{
-				this.handleInternalLink( location );
+				HyperlinkAdapter.this.handleInternalLink( location );
+				return;
 			}
 			else if ( location.startsWith( "http://" ) || location.startsWith( "https://" ) )
 			{
 				RelayLoader.openSystemBrowser( location );
+				return;
 			}
 			else if ( location.startsWith( "javascript:" ) && ( location.indexOf( "submit()" ) == -1 || location.indexOf( "messageform" ) != -1 ) )
 			{
 				InputFieldUtilities.alert( "Ironically, Java does not support Javascript." );
+				return;
 			}
 			else if ( location.indexOf( "submit()" ) == -1 )
 			{
-				this.handleInternalLink( location );
+				HyperlinkAdapter.this.handleInternalLink( location );
+				return;
 			}
-			else
+
+			// If it's an attempt to submit an adventure form,
+			// examine the location string to see which form is
+			// being submitted and submit it manually.
+
+			String[] locationSplit = location.split( "\\." );
+			String formId = "\"" + locationSplit[ locationSplit.length - 2 ] + "\"";
+
+			String editorText = requestPane.getText();
+			int formIndex = editorText.indexOf( formId );
+
+			String locationText =
+				editorText.substring(
+					editorText.lastIndexOf( "<form", formIndex ), editorText.toLowerCase().indexOf(
+						"</form>", formIndex ) );
+
+			Matcher inputMatcher = Pattern.compile( "<input.*?>" ).matcher( locationText );
+
+			String lastInput;
+			int patternIndex;
+			Matcher actionMatcher, nameMatcher, valueMatcher;
+			StringBuffer inputString = new StringBuffer();
+
+			// Determine the action associated with the
+			// form -- this is used for the URL.
+
+			patternIndex = 0;
+			do
 			{
-				// If it's an attempt to submit an adventure form,
-				// examine the location string to see which form is
-				// being submitted and submit it manually.
+				actionMatcher = HyperlinkAdapter.ACTION_PATTERNS[ patternIndex ].matcher( locationText );
+			}
+			while ( !actionMatcher.find() && ++patternIndex < 3 );
 
-				String[] locationSplit = location.split( "\\." );
-				String formId = "\"" + locationSplit[ locationSplit.length - 2 ] + "\"";
+			// Figure out which inputs need to be submitted.
+			// This is determined through the existing HTML,
+			// looking at preset values only.
 
-				String editorText = ( (RequestPane) e.getSource() ).getText();
-				int formIndex = editorText.indexOf( formId );
+			while ( inputMatcher.find() )
+			{
+				lastInput = inputMatcher.group();
 
-				String locationText =
-					editorText.substring(
-						editorText.lastIndexOf( "<form", formIndex ), editorText.toLowerCase().indexOf(
-							"</form>", formIndex ) );
-
-				Matcher inputMatcher = Pattern.compile( "<input.*?>" ).matcher( locationText );
-
-				String lastInput;
-				int patternIndex;
-				Matcher actionMatcher, nameMatcher, valueMatcher;
-				StringBuffer inputString = new StringBuffer();
-
-				// Determine the action associated with the
-				// form -- this is used for the URL.
+				// Each input has a name associated with it.
+				// This should be determined first.
 
 				patternIndex = 0;
 				do
 				{
-					actionMatcher = HyperlinkAdapter.ACTION_PATTERNS[ patternIndex ].matcher( locationText );
+					nameMatcher = HyperlinkAdapter.NAME_PATTERNS[ patternIndex ].matcher( lastInput );
 				}
-				while ( !actionMatcher.find() && ++patternIndex < 3 );
+				while ( !nameMatcher.find() && ++patternIndex < 3 );
 
-				// Figure out which inputs need to be submitted.
-				// This is determined through the existing HTML,
-				// looking at preset values only.
+				// Each input has a name associated with it.
+				// This should be determined next.
 
-				while ( inputMatcher.find() )
+				patternIndex = 0;
+				do
 				{
-					lastInput = inputMatcher.group();
-
-					// Each input has a name associated with it.
-					// This should be determined first.
-
-					patternIndex = 0;
-					do
-					{
-						nameMatcher = HyperlinkAdapter.NAME_PATTERNS[ patternIndex ].matcher( lastInput );
-					}
-					while ( !nameMatcher.find() && ++patternIndex < 3 );
-
-					// Each input has a name associated with it.
-					// This should be determined next.
-
-					patternIndex = 0;
-					do
-					{
-						valueMatcher = HyperlinkAdapter.VALUE_PATTERNS[ patternIndex ].matcher( lastInput );
-					}
-					while ( !valueMatcher.find() && ++patternIndex < 3 );
-
-					// Append the latest input's name and value to
-					// the complete input string.
-
-					inputString.append( inputString.length() == 0 ? '?' : '&' );
-
-					inputString.append( StringUtilities.getURLEncode( nameMatcher.group( 1 ) ) );
-					inputString.append( '=' );
-					inputString.append( StringUtilities.getURLEncode( valueMatcher.group( 1 ) ) );
+					valueMatcher = HyperlinkAdapter.VALUE_PATTERNS[ patternIndex ].matcher( lastInput );
 				}
+				while ( !valueMatcher.find() && ++patternIndex < 3 );
 
-				// Now that the entire form string is known, handle
-				// the appropriate internal link.
+				// Append the latest input's name and value to
+				// the complete input string.
 
-				this.handleInternalLink( actionMatcher.group( 1 ) + inputString.toString() );
+				inputString.append( inputString.length() == 0 ? '?' : '&' );
+
+				inputString.append( StringUtilities.getURLEncode( nameMatcher.group( 1 ) ) );
+				inputString.append( '=' );
+				inputString.append( StringUtilities.getURLEncode( valueMatcher.group( 1 ) ) );
 			}
+
+			// Now that the entire form string is known, handle
+			// the appropriate internal link.
+
+			String targetLocation = actionMatcher.group( 1 ) + inputString.toString();
+			HyperlinkAdapter.this.handleInternalLink( targetLocation );
 		}
 	}
 
