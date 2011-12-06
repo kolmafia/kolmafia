2fd20c9fd8a00914050852e5fa122987f1704f5e
holatuwol <shwei@users.sourceforge.net>
2011-12-06 05:25:23 +0000

    If you're debugging the Foxtrot removal, add additional functionality to the ESC key to print out what currently has focus.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@10071 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/swingui/listener/WorldPeaceListener.java
+++ src/net/sourceforge/kolmafia/swingui/listener/WorldPeaceListener.java
@@ -33,13 +33,48 @@
 
 package net.sourceforge.kolmafia.swingui.listener;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.KeyboardFocusManager;
+
+import net.sourceforge.kolmafia.RequestLogger;
 import net.sourceforge.kolmafia.RequestThread;
+import net.sourceforge.kolmafia.preferences.Preferences;
 
 public class WorldPeaceListener
 	extends ThreadedListener
 {
 	protected void execute()
 	{
+		if ( Preferences.getBoolean( "debugFoxtrotRemoval" ) )
+		{
+			KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+
+			Component owner = manager.getFocusOwner();
+
+			if ( owner == null )
+			{
+				owner = manager.getFocusedWindow();
+			}
+
+			if ( owner != null )
+			{
+				RequestLogger.printLine( owner.getClass().getName() + " has focus" );
+
+				Container parent = owner.getParent();
+
+				while ( parent != null )
+				{
+					RequestLogger.printLine( " -> " + parent.getClass().getName() );
+					parent = parent.getParent();
+				}
+			}
+			else
+			{
+				RequestLogger.printLine( "No component has focus" );
+			}
+		}
+
 		RequestThread.declareWorldPeace();
 	}
 }
