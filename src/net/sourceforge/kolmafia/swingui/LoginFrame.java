0e59d06c19d374245ca5c8fbf735edf284b0c2e9
holatuwol <shwei@users.sourceforge.net>
2012-03-06 21:06:32 +0000

    connectViaAddress works when we use a naive secure socket layer
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@10737 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/swingui/LoginFrame.java
+++ src/net/sourceforge/kolmafia/swingui/LoginFrame.java
@@ -435,6 +435,7 @@ public class LoginFrame
 		{
 			{ "proxySet", "KoLmafia needs to connect through a proxy server" },
 			{ "useSecureLogin", "Switch to HTTPS for login (development in progress)" },
+			{ "connectViaAddress", "Use IP address to connect instead of host name" },
 			{ "useNaiveSecureLogin", "Do not have Java try to validate SSL certificates" },
 			{ "allowSocketTimeout", "Improve handling of semi-random lag spikes" },
 		};
