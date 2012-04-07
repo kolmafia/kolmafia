34788ef7cc29d46d0a086ff171cd2c15a697a99f
roippi <roippi@users.sourceforge.net>
2012-04-07 15:48:52 +0000

    questM03Bugbear live tracking
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@10892 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/KnollRequest.java
+++ src/net/sourceforge/kolmafia/request/KnollRequest.java
@@ -39,6 +39,7 @@ import java.util.regex.Pattern;
 import net.sourceforge.kolmafia.RequestLogger;
 
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.persistence.QuestDatabase;
 
 import net.sourceforge.kolmafia.session.ResultProcessor;
 
@@ -120,6 +121,39 @@ public class KnollRequest
 			{
 				ResultProcessor.processItem( ItemPool.STINKY_MUSHROOM, -1 );
 			}
+			
+			// Quest handling from here down to the return;
+			
+			// Ah, you must be our newest Citizen! It is fortunate that you have arrived, for dire times are
+			// upon us, and we require assistance.
+
+			// As you may know, we train bugbears as pets and guards. Lately, though, something is causing
+			// our bugbears to become vicious, and to attack their handlers. I would be grateful if you
+			// would investigate this for me. We keep our bugbears in a pen near the Spooky Forest.
+			if ( responseText.indexOf( "It is fortunate that you have arrived" ) != -1 )
+			{
+				QuestDatabase.setQuestIfBetter( QuestDatabase.BUGBEAR, QuestDatabase.STARTED );
+			}
+			// Mayor Zapruder looks at the tiny pitchfork you've brought him. 
+
+			// "Spooky Gravy Fairies! I should've known. 
+			else if ( responseText.indexOf( "Mayor Zapruder looks at the tiny pitchfork" ) != -1 )
+			{
+				QuestDatabase.setQuestIfBetter( QuestDatabase.BUGBEAR, "step1" );
+			}
+			// "Excellent, Adventurer. Please, hand me the mushroom..."
+			else if ( responseText.indexOf( "Please, hand me the mushroom" ) != -1 )
+			{
+				QuestDatabase.setQuestIfBetter( QuestDatabase.BUGBEAR, "step2" );
+			}
+			// "You've done it! The bugbears have finally returned to a state of normalcy.
+			// Without their Queen to lead them, the spooky gravy fairies won't cause us any more problems.
+			// And now, for your reward."
+			else if ( responseText.indexOf( "The bugbears have finally returned to a state of normalcy" ) != -1 )
+			{
+				QuestDatabase.setQuestIfBetter( QuestDatabase.BUGBEAR, QuestDatabase.FINISHED );
+			}
+			
 			return;
 		}
 	}
