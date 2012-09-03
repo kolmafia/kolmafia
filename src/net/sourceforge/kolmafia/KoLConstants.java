a03e1f00f7a3bac1c10ee7922e1f6831ced77c77
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2012-09-03 11:15:33 +0000

    Bump version to 15.5
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@11467 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -67,8 +67,8 @@ public interface KoLConstants
 	// Version information for the current version of KoLmafia.
 	// Rendered in various locations and therefore made public.
 
-	public static final String VERSION_NAME = "KoLmafia v15.4";
-	public static final String VERSION_DATE = "Released on August 5, 2012";
+	public static final String VERSION_NAME = "KoLmafia v15.5";
+	public static final String VERSION_DATE = "Released on September 3, 2012";
 	public static final boolean RELEASED = true;
 	public static final String REVISION = null;
 
