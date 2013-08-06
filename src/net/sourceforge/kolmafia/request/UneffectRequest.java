af99974c73c268aa3c9fcfb9a4eea16882676c84
Veracity <veracity0@users.sourceforge.net>
2013-08-06 23:31:11 +0000

    Revert that
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@12416 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/UneffectRequest.java
+++ src/net/sourceforge/kolmafia/request/UneffectRequest.java
@@ -361,7 +361,6 @@ public class UneffectRequest
 		removableEffects.add( "Affronted Decency" );
 		removableEffects.add( "Apathy" );
 		removableEffects.add( "Confused" );
-		removableEffects.add( "Consumed by Fear" );
 		removableEffects.add( "Cunctatitis" );
 		removableEffects.add( "Embarrassed" );
 		removableEffects.add( "Easily Embarrassed" );
