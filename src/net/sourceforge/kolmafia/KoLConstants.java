b0e1db2340de98cb270bbcd03d26ce773ffef2a0
Veracity <veracity0@users.sourceforge.net>
2013-09-06 19:25:49 +0000

    Fix version number of items.txt. Bump version number of npcstores.txt (after the fact; I added a field to it a while ago.)
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@12572 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -396,7 +396,7 @@ public interface KoLConstants
 	public static final int MODIFIERS_VERSION = 2;
 	public static final int MONSTERS_VERSION = 5;
 	public static final int NONFILLING_VERSION = 1;
-	public static final int NPCSTORES_VERSION = 1;
+	public static final int NPCSTORES_VERSION = 2;
 	public static final int OUTFITS_VERSION = 1;
 	public static final int PACKAGES_VERSION = 1;
 	public static final int PULVERIZE_VERSION = 2;
