68d5272b8416400ed7f1c8a5553c8f2c38421898
Darzil <darzil@users.sourceforge.net>
2013-09-12 13:08:36 +0000

    A couple of fixes for Hidden Office spoilers and quest tracking.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@12622 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/session/QuestManager.java
+++ src/net/sourceforge/kolmafia/session/QuestManager.java
@@ -598,7 +598,7 @@ public class QuestManager
 		{
 			// If you don't have McClusky File (complete), or McClusky File 5, and accountant doesn't drop file, you must have unlocked office boss
 			if ( InventoryManager.getCount( ItemPool.MCCLUSKY_FILE ) == 0 && InventoryManager.getCount( ItemPool.MCCLUSKY_FILE_PAGE5 ) == 0 &&
-				Preferences.getInteger( "hiddenOfficeProgress" ) < 6 && responseText.indexOf( "<b>McClusky file</b>" ) == -1 )
+				Preferences.getInteger( "hiddenOfficeProgress" ) < 6 && responseText.indexOf( "McClusky file" ) == -1 )
 			{
 				Preferences.setInteger( "hiddenOfficeProgress", 6 );
 			}
