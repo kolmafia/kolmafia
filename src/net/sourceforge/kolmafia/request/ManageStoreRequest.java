bc774e5963009e321b9feab7d6e17e5c5fe0fc44
Veracity <veracity0@users.sourceforge.net>
2013-10-16 13:52:33 +0000

    Register adding and removing items from store via backoffice.php. When remove items from store, tell how many were removed.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@12899 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/ManageStoreRequest.java
+++ src/net/sourceforge/kolmafia/request/ManageStoreRequest.java
@@ -41,6 +41,7 @@ import java.util.regex.Pattern;
 import net.sourceforge.kolmafia.AdventureResult;
 import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.KoLmafia;
+import net.sourceforge.kolmafia.RequestLogger;
 
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 
@@ -59,6 +60,7 @@ public class ManageStoreRequest
 	private static Pattern ITEMID_PATTERN = Pattern.compile( "itemid=(h)?(\\d+)" );
 	private static Pattern PRICE_PATTERN = Pattern.compile( "price=(\\d+)?" );
 	private static Pattern QUANTITY_PATTERN = Pattern.compile( "quantity=(\\d+|\\*|)" );
+	private static Pattern QTY_PATTERN = Pattern.compile( "qty=(\\d+)" );
 	private static Pattern LIMIT_PATTERN = Pattern.compile( "limit=(\\d+)?" );
 
 	// (2) breath mints stocked for 999,999,999 meat each.
@@ -69,7 +71,7 @@ public class ManageStoreRequest
 	private static final int PRICE_MANAGEMENT = 3;
 	private static final int VIEW_STORE_LOG = 4;
 
-	private int takenItemId;
+	private AdventureResult item;
 	private final int requestType;
 
 	public ManageStoreRequest()
@@ -106,7 +108,7 @@ public class ManageStoreRequest
 		this.addFormField( "ajax", "1" );
 
 		this.requestType = ManageStoreRequest.ITEM_REMOVAL;
-		this.takenItemId = itemId;
+		this.item = ItemPool.get( itemId, qty );
 	}
 
 	public ManageStoreRequest( final int[] itemId, final int[] prices, final int[] limits )
@@ -156,26 +158,20 @@ public class ManageStoreRequest
 
 	private void addItem()
 	{
-		// AdventureResult takenItem = new AdventureResult( this.takenItemId, 0 );
-		// String name = takenItem.getName();
+		String name = this.item.getName();
 
-		// KoLmafia.updateDisplay( "Removing " + name + " from store..." );
+		KoLmafia.updateDisplay( "Adding " + name + " to store..." );
 		super.run();
-		// KoLmafia.updateDisplay( takenItem.getCount() + " " + name + " removed from your store." );
-
-		ManageStoreRequest.parseResponse( this.getURLString(), this.responseText );
+		KoLmafia.updateDisplay( this.item.getCount() + " " + name + " added to your store." );
 	}
 
 	private void removeItem()
 	{
-		AdventureResult takenItem = new AdventureResult( this.takenItemId, 0 );
-		String name = takenItem.getName();
+		String name = this.item.getName();
 
 		KoLmafia.updateDisplay( "Removing " + name + " from store..." );
 		super.run();
-		KoLmafia.updateDisplay( takenItem.getCount() + " " + name + " removed from your store." );
-
-		ManageStoreRequest.parseResponse( this.getURLString(), this.responseText );
+		KoLmafia.updateDisplay( this.item.getCount() + " " + name + " removed from your store." );
 	}
 
 	private void managePrices()
@@ -209,6 +205,7 @@ public class ManageStoreRequest
 	@Override
 	public void processResults()
 	{
+		ManageStoreRequest.parseResponse( this.getURLString(), this.responseText );
 	}
 
 	public static final void parseResponse( final String urlString, final String responseText )
@@ -304,13 +301,112 @@ public class ManageStoreRequest
 
 	public static boolean registerRequest( final String urlString )
 	{
-		// backoffice.php?itemid=h362&price=180&quantity=1&limit=&pwd&action=additem&ajax=1
-		// backoffice.php?itemid=362&price=180&quantity=1&limit=&pwd&action=additem&ajax=1
-		// backoffice.php?qty=1&pwd&action=removeitem&itemid=362&ajax=1
-
-		if ( urlString.startsWith( "backoffice.php" ) )
+		if ( !urlString.startsWith( "backoffice.php" ) )
 		{
 			return false;
+		}
+
+		String action = GenericRequest.getAction( urlString );
+		if ( action == null )
+		{
+			return false;
+		}
+
+		if ( action.equals( "additem" ) )
+		{
+			// backoffice.php?itemid=h362&price=180&quantity=1&limit=&pwd&action=additem&ajax=1
+			// backoffice.php?itemid=362&price=180&quantity=1&limit=&pwd&action=additem&ajax=1
+
+			// get the item ID - and whether it is from Hagnk's - from the URL submitted.
+			Matcher itemMatcher = ManageStoreRequest.ITEMID_PATTERN.matcher( urlString );
+			if ( !itemMatcher.find() )
+			{
+				return false;
+			}
+			boolean storage = itemMatcher.group( 1 ) != null;
+			int itemId = StringUtilities.parseInt( itemMatcher.group( 2 ) );
+
+			Matcher quantityMatcher = ManageStoreRequest.QUANTITY_PATTERN.matcher( urlString );
+			if ( !quantityMatcher.find() )
+			{
+				return false;
+			}
+			String quantityString = quantityMatcher.group( 1 );
+			String quantity =
+				quantityString.equals( "" ) ? "1" :
+				quantityString.equals( "*" ) ? "all" :
+				quantityString;
+
+			Matcher priceMatcher = ManageStoreRequest.PRICE_PATTERN.matcher( urlString );
+			if ( !priceMatcher.find() )
+			{
+				return false;
+			}
+			int price = priceMatcher.group( 1 ) == null ? 999999999 : StringUtilities.parseInt( priceMatcher.group( 1 ) );
+
+			Matcher limitMatcher = ManageStoreRequest.LIMIT_PATTERN.matcher( urlString );
+			if ( !limitMatcher.find() )
+			{
+				return false;
+			}
+			int limit = limitMatcher.group( 1 ) == null ? 0 : StringUtilities.parseInt( limitMatcher.group( 1 ) );
+
+			StringBuilder buffer = new StringBuilder();
+
+			buffer.append( "Adding " );
+			buffer.append( quantity );
+			buffer.append( " " );
+			buffer.append( ItemDatabase.getItemName( itemId ) );
+			buffer.append( " to store from " );
+			buffer.append( storage ? "storage" : "inventory" );
+			buffer.append( " for " );
+			buffer.append( KoLConstants.COMMA_FORMAT.format( price ) );
+			buffer.append( " Meat" );
+			if ( limit > 0 )
+			{
+				buffer.append( ", limited to " );
+				buffer.append( String.valueOf( limit ) );
+				buffer.append( "/day" );
+			}
+			else
+			{
+				buffer.append( " with no limit" );
+			}
+
+			RequestLogger.updateSessionLog( buffer.toString() );
+
+			return true;
+		}
+
+		if ( action.equals( "removeitem" ) )
+		{
+			// backoffice.php?qty=1&pwd&action=removeitem&itemid=362&ajax=1
+
+			Matcher itemMatcher = ManageStoreRequest.ITEMID_PATTERN.matcher( urlString );
+			if ( !itemMatcher.find() )
+			{
+				return false;
+			}
+			int itemId = StringUtilities.parseInt( itemMatcher.group( 2 ) );
+
+			Matcher qtyMatcher = ManageStoreRequest.QTY_PATTERN.matcher( urlString );
+			if ( !qtyMatcher.find() )
+			{
+				return false;
+			}
+			int qty = StringUtilities.parseInt( qtyMatcher.group( 1 ) );
+
+			StringBuilder buffer = new StringBuilder();
+
+			buffer.append( "Removing " );
+			buffer.append( qty );
+			buffer.append( " " );
+			buffer.append( ItemDatabase.getItemName( itemId ) );
+			buffer.append( " from store" );
+
+			RequestLogger.updateSessionLog( buffer.toString() );
+
+			return true;
 		}
 
 		return false;
