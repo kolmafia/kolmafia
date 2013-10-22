f8d0ce5c25410215637d2ac12d718ecbda893182
Darzil <darzil@users.sourceforge.net>
2013-10-22 23:21:07 +0000

    Brought Disco Nap removals in line with old Disco Power Nap (not poisons), added sharpened spoon.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@12939 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/UneffectRequest.java
+++ src/net/sourceforge/kolmafia/request/UneffectRequest.java
@@ -404,21 +404,16 @@ public class UneffectRequest
 		if ( KoLCharacter.hasSkill( "Adventurer of Leisure" ) )
 		{
 			removableEffects.add( "Affronted Decency" );
-			removableEffects.add( "A Little Bit Poisoned" );
 			removableEffects.add( "Apathy" );
 			removableEffects.add( "Consumed by Fear" );
 			removableEffects.add( "Cunctatitis" );
 			removableEffects.add( "Easily Embarrassed" );
 			removableEffects.add( "Existential Torment" );
-			removableEffects.add( "Hardly Poisoned at All" );
 			removableEffects.add( "Light-Headed" );
-			removableEffects.add( "Majorly Poisoned" );
 			removableEffects.add( "N-Spatial vision" );
 			removableEffects.add( "Prestidigysfunction" );
 			removableEffects.add( "Rainy Soul Miasma" );
-			removableEffects.add( "Really Quite Poisoned" );
 			removableEffects.add( "Socialismydia" );
-			removableEffects.add( "Somewhat Poisoned" );
 			removableEffects.add( "Tenuous Grip on Reality" );
 			removableEffects.add( "Tetanus" );
 			removableEffects.add( "The Colors..." );
