9a016a5b4b1752a835b65f5fff6caa9edf294775
Darzil <darzil@users.sourceforge.net>
2013-11-07 10:31:37 +0000

    Add Pantsgiving crumb item drop support, as _pantsgivingCrumbs. Remove initial Black Day support - I'd rather have mafia overestimate the cost of one item and give someone a surprise when something is cheaper than underestimate it for many items. Maybe someone else will find supporting this worth doing, lots of code for niche benefit, especially as you'd also have to refactor other stuff to buy most expensive piece of a concoction first.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@13091 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/SkillPool.java
+++ src/net/sourceforge/kolmafia/objectpool/SkillPool.java
@@ -150,6 +150,7 @@ public class SkillPool
 	public static final int TEAR_WAVE = 7145;
 	public static final int WINK = 7168;
 	public static final int TALK_ABOUT_POLITICS = 7169;
+	public static final int POCKET_CRUMBS = 7170;
 	public static final int SNOWCONE = 8000;
 	public static final int STICKER = 8001;
 	public static final int SUGAR = 8002;
