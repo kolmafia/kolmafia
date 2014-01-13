59bb2a560d5aa4c098eda026ffd740f31e40f48a
Veracity <veracity0@users.sourceforge.net>
2014-01-13 17:51:49 +0000

    Fix regexp to pick up current familiar, too.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@13522 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/FamiliarData.java
+++ src/net/sourceforge/kolmafia/FamiliarData.java
@@ -70,7 +70,7 @@ public class FamiliarData
 	public static final FamiliarData NO_FAMILIAR = new FamiliarData( -1 );
 
 	private static final Pattern REGISTER_PATTERN =
-		Pattern.compile( "<img(?<!In Your Crown of Thrones:</td><td><img) src=\"http://images\\.kingdomofloathing\\.com/itemimages/([^\"]*?)\" class=\"hand fam\" onClick='fam\\((\\d+)\\)'>.*?<b>(.*?)</b>.*?\\d+-pound (.*?) \\(([\\d,]+) (?:exp|experience|candy|candies)?, .*? kills?\\)(.*?)<(?:/tr|form)" );
+		Pattern.compile( "<img(?<!In Your Crown of Thrones:</td><td><img) src=\"http://images\\.kingdomofloathing\\.com/itemimages/([^\"]*?)\" class=(?:\"hand fam\"|hand) onClick='fam\\((\\d+)\\)'>.*?<b>(.*?)</b>.*?\\d+-pound (.*?) \\(([\\d,]+) (?:exp|experience|candy|candies)?, .*? kills?\\)(.*?)<(?:/tr|form)" );
 
 	private static final Pattern DESCID_PATTERN = Pattern.compile( "descitem\\((.*?)\\)" );
 
