68efd8d179424adde7e38e294f75790dbcaff145
fronobulax <jaadams5@users.sourceforge.net>
2014-02-15 19:41:36 +0000

    And check in the right new files...
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@13719 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/textui/command/TowerToShadowQuestCommand.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2005-2014, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION ) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE ) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.textui.command;
+
+import net.sourceforge.kolmafia.KoLmafia;
+
+import net.sourceforge.kolmafia.session.SorceressLairManager;
+
+public class TowerToShadowQuestCommand
+	extends AbstractCommand
+{
+	public TowerToShadowQuestCommand()
+	{
+		this.usage = " - complete the Tower to the shadow.";
+	}
+
+	@Override
+	public void run( final String command, final String parameters )
+	{
+
+		if ( command.equals( "partialtower" ) )
+		{
+			SorceressLairManager.fightMostTowerGuardians();
+			return;
+		}
+
+		KoLmafia.updateDisplay( "What... is your quest?  [internal error]" );
+	}
+}
