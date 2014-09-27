8305c29a0446badf9806d2bc8c2b09efd9b4552a
Darzil <darzil@users.sourceforge.net>
2014-09-27 22:24:23 +0000

    Some Twitch stuff
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@14741 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/AdventurePool.java
+++ src/net/sourceforge/kolmafia/objectpool/AdventurePool.java
@@ -190,4 +190,5 @@ public class AdventurePool
 	public static final String MIDDLE_CHAMBER_ID = "407";
 	public static final String LOWER_CHAMBER_ID = "pyramid_state";
 	public static final String SUMMONING_CHAMBER_ID = "manor4_chamber";
+	public static final int POST_MALL = 418;
 }
