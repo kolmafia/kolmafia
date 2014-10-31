bdaf1ec9e9e4808447f2b80cfd13386257299082
Darzil <darzil@users.sourceforge.net>
2014-10-31 15:08:44 +0000

    Pick Frost Flowers automatically on days 4+ (if set to harvest during breakfast).
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@14914 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/CampgroundRequest.java
+++ src/net/sourceforge/kolmafia/request/CampgroundRequest.java
@@ -647,10 +647,10 @@ public class CampgroundRequest
 		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden1.gif", ItemPool.ICE_HARVEST, 3 );
 		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden2.gif", ItemPool.ICE_HARVEST, 6 );
 		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden3.gif", ItemPool.FROST_FLOWER, 1 );
-		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden4.gif", ItemPool.ICE_HARVEST, 12 );
-		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden5.gif", ItemPool.ICE_HARVEST, 15 );
-		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden6.gif", ItemPool.ICE_HARVEST, 18 );
-		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden7.gif", ItemPool.ICE_HARVEST, 21 );
+		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden4.gif", ItemPool.FROST_FLOWER, 1 );
+		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden5.gif", ItemPool.FROST_FLOWER, 1 );
+		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden6.gif", ItemPool.FROST_FLOWER, 1 );
+		if ( !gardenFound ) gardenFound = findImage( responseText, "wintergarden7.gif", ItemPool.FROST_FLOWER, 1 );
 
 		Matcher jungMatcher = JUNG_PATTERN.matcher( responseText );
 		if ( jungMatcher.find() )
