ee0b9016a8b6c15bf154eb6ddbffc5a85e6a2e07
Veracity <veracity0@users.sourceforge.net>
2015-01-09 13:52:32 +0000

    Add Aggressive Snowman Kit, Summon Carrot, Aggressive Carrot, and so on. 3 choice adventures in the new lair have only a single choice.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@15154 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/SkillPool.java
+++ src/net/sourceforge/kolmafia/objectpool/SkillPool.java
@@ -89,6 +89,7 @@ public class SkillPool
 	public static final int HOLLOW_LEG = 116;
 	public static final int PIRATE_BELLOW = 118;
 	public static final int HOLIDAY_FUN = 121;
+	public static final int SUMMON_CARROT = 128;
 	public static final int WALRUS_TONGUE = 1010;
 	public static final int BATTER_UP = 1014;
 	public static final int CLOBBER = 1022;
