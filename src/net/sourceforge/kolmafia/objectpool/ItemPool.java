7828225eb8640489e9038f577cf1e7e300ce01c2
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2015-02-28 07:50:27 +0000

    Using a disposable instant camera doesn't count for upgrading the Yearbook Club Camera
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@15499 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2041,6 +2041,7 @@ public class ItemPool
 	public static final int PHOTOGRAPH_OF_DOG = 7263;
 	public static final int PHOTOGRAPH_OF_RED_NUGGET = 7264;
 	public static final int PHOTOGRAPH_OF_OSTRICH_EGG = 7265;
+	public static final int DISPOSABLE_CAMERA = 7266;
 	public static final int PETE_JACKET_COLLAR = 7267;
 	public static final int PROFESSOR_WHAT_GARMENT = 7269;
 	public static final int PALINDROME_BOOK_2 = 7270;
