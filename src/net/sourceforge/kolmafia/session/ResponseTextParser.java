3dea592f69b7afed5e46858ff323f08c65ed0108
Veracity <veracity0@users.sourceforge.net>
2015-06-25 14:29:48 +0000

    Add a few Madness Bakery recipes. Add a recipe-learning pattern which should match recipes learned in that venue.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@15983 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/session/ResponseTextParser.java
+++ src/net/sourceforge/kolmafia/session/ResponseTextParser.java
@@ -882,6 +882,7 @@ public class ResponseTextParser
 	private static final Pattern [] RECIPE_PATTERNS =
 	{
 		Pattern.compile( "You learn to .*?craft.*? a new item:.*?<b>(.*?)</b>" ),
+		Pattern.compile( "<b>(.*?)</b>.*?You'll have to try that one sometime.*?You (?:have|just) .*?discovered.*? a new recipe" ),
 		Pattern.compile( "You (?:have|just) .*?discovered.*? a new recipe.*?<b>(.*?)</b>" )
 	};
 
