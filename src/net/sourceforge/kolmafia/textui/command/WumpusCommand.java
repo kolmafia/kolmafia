c5160514e9f7eae06822a7304585927c317279f3
Veracity <veracity0@users.sourceforge.net>
2015-07-04 07:17:53 +0000

    When you explore the Wumpus cave, in addition to the exits, log the sounds you hear from (some) adjacent rooms. Add "wumpus replay FILE" command to process the session log of a wumpus exploration and make deductions, etc. Improve deductions such that if you DON'T hear a sound in adjacent rooms, we know that the specified hazard cannot be in those rooms.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@16014 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/command/WumpusCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/WumpusCommand.java
@@ -33,9 +33,17 @@
 
 package net.sourceforge.kolmafia.textui.command;
 
+import java.io.File;
+
+import net.sourceforge.kolmafia.KoLConstants;
+import net.sourceforge.kolmafia.KoLConstants.MafiaState;
+import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.RequestLogger;
 
 import net.sourceforge.kolmafia.session.WumpusManager;
+
+import net.sourceforge.kolmafia.utilities.ByteBufferUtilities;
+import net.sourceforge.kolmafia.utilities.StringUtilities;
 
 public class WumpusCommand
 	extends AbstractCommand
@@ -67,6 +75,87 @@ public class WumpusCommand
 			String code = WumpusManager.getWumpinatorCode();
 			RequestLogger.printLine( code );
 			return;
+		}
+
+		if ( option.equals( "replay" ) )
+		{
+			if ( tokens.length < 2 )
+			{
+				KoLmafia.updateDisplay( MafiaState.ERROR, "Replay from what file?" );
+				return;
+			}
+
+			String fileName = tokens[ 1 ];
+			File file = new File( KoLConstants.DATA_LOCATION, fileName );
+
+			if ( !file.exists() )
+			{
+				KoLmafia.updateDisplay( MafiaState.ERROR, "File " + file + " does not exist" );
+				return;
+			}
+			
+			byte[] bytes = ByteBufferUtilities.read( file );
+			String text = StringUtilities.getEncodedString( bytes, "UTF-8" );
+
+			KoLmafia.updateDisplay( "Read " + KoLConstants.COMMA_FORMAT.format( bytes.length ) +
+						" bytes into a " + KoLConstants.COMMA_FORMAT.format( text.length() ) +
+						" character string" );
+
+			String[] lines = text.split( "\\n" );
+
+			// Start with fresh cave data
+			WumpusManager.reset();
+
+			int index = 0;
+			while ( index < lines.length )
+			{
+				String encounter = null, exits = null, sounds = null;
+				String line = lines[ index++ ];
+
+				// Find the next encounter
+				if ( !line.startsWith( "Encounter:" ) )
+				{
+					continue;
+				}
+
+				encounter = line;
+
+				// Look at all lines up to next encounter, collecting exits and sounds
+				while ( index < lines.length )
+				{
+					line = lines[ index];
+					if ( line.startsWith( "Encounter:" ) )
+					{
+						break;
+					}
+
+					index += 1;
+					
+					if ( line.startsWith( "Exits:" ) )
+					{
+						exits = line;
+					}
+					else if ( line.startsWith( "Sounds:" ) )
+					{
+						sounds = line;
+					}
+				}
+
+				RequestLogger.printLine( encounter );
+				String responseText = WumpusManager.reconstructResponseText( encounter, exits, sounds );
+				if ( responseText != null )
+				{
+					WumpusManager.visitChoice( responseText );
+					StringBuilder buffer = new StringBuilder();
+					buffer.append( "Wumpinator: " );
+					buffer.append( "<a href=\"" ) ;
+					buffer.append( WumpusManager.getWumpinatorURL() );
+					buffer.append( "\">" );
+					buffer.append( "&lt;click here&gt;" );
+					buffer.append( "</a>" );
+					RequestLogger.printLine( buffer.toString() );
+				}
+			}
 		}
 	}
 }
