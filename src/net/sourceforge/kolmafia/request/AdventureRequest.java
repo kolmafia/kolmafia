kolmafia/kolmafia

commit 0585bb58ef0c344ca2b2445d49c1b60e9809c42d

Monsters from Ed Challenge path. Fix capitalization of a few monsters identified by theier image. Add a Spelunky monster.
git-svn-id: http://svn.code.sf.net/p/kolmafia/code@15486 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/request/AdventureRequest.java
@@ -572,6 +572,9 @@ public class AdventureRequest
 			image = monster.group( 1 );
 		}
 
+		// You'd think that the following could/should be:
+		// - in MonsterDatabase
+		// - a Map lookup
 		if ( image != null )
 		{
 			// Always-available monsters are listed above obsolete monsters
@@ -631,7 +634,7 @@ public class AdventureRequest
 				image.startsWith( "spookyhobo" ) ? "Spooky Hobo" :
 				image.startsWith( "slhobo" ) ? "Sleaze Hobo" :
 				// Slime Tube
-				image.startsWith( "slime" ) ? "Slime" + image.charAt( 5 ) :
+				image.startsWith( "slime" ) ? image.substring( 0, 5 ) :
 				// GamePro Bosses
 				image.startsWith( "faq_boss" ) ? "Video Game Boss" :
 				image.startsWith( "faq_miniboss" ) ? "Video Game Miniboss" :
@@ -642,17 +645,17 @@ public class AdventureRequest
 				image.startsWith( "paulblart" ) ? "suburban security civilian" :
 				image.startsWith( "tooold" ) ? "kid who is too old to be Trick-or-Treating" :
 				// Bugbear Invasion
-				image.startsWith( "bb_caveman" ) ? "Angry Cavebugbear" :
+				image.startsWith( "bb_caveman" ) ? "angry cavebugbear" :
 				// Crimbo 2012 wandering elves
-				image.startsWith( "tacoelf_sign" ) ? "Sign-Twirling Crimbo Elf" :
-				image.startsWith( "tacoelf_taco" ) ? "Taco-Clad Crimbo Elf" :
-				image.startsWith( "tacoelf_cart" ) ? "Tacobuilding Crimbo Elf" :
+				image.startsWith( "tacoelf_sign" ) ? "sign-twirling Crimbo elf" :
+				image.startsWith( "tacoelf_taco" ) ? "taco-clad Crimbo elf" :
+				image.startsWith( "tacoelf_cart" ) ? "tacobuilding Crimbo elf" :
 				// Crimbobokutown Toy Factory
-				image.startsWith( "animelf1" ) ? "Tiny-Screwing Animelf" :
-				image.startsWith( "animelf2" ) ? "Plastic-Extruding Animelf" :
-				image.startsWith( "animelf3" ) ? "Circuit-Soldering Animelf" :
-				image.startsWith( "animelf4" ) ? "Quality Control Animelf" :
-				image.startsWith( "animelf5" ) ? "Toy Assembling Animelf" :
+				image.startsWith( "animelf1" ) ? "tiny-screwing animelf" :
+				image.startsWith( "animelf2" ) ? "plastic-extruding animelf" :
+				image.startsWith( "animelf3" ) ? "circuit-soldering animelf" :
+				image.startsWith( "animelf4" ) ? "quality control animelf" :
+				image.startsWith( "animelf5" ) ? "toy assembling animelf" :
 				// Elf Alley
 				image.startsWith( "elfhobo" ) ? "Hobelf" :
 				// Haunted Sorority House
@@ -681,6 +684,7 @@ public class AdventureRequest
 				image.startsWith( "outlawboss" ) ? "outlaw leader" :
 				// Spelunky
 				image.startsWith( "spelunkbeeq" ) ? "queen bee (spelunky)" :
+				image.startsWith( "spelunkghost" ) ? "ghost (spelunky)" :
 				null;
 		}
 

commit 181e6ebeadb6eb0dd1fab7babc959361499f2872

Fix Slime Tube monster disambiguation broken by r15486.
git-svn-id: http://svn.code.sf.net/p/kolmafia/code@16099 b29ace70-8910-0410-8dcc-aa2fc6433167

src/net/sourceforge/kolmafia/request/AdventureRequest.java
@@ -90,7 +90,7 @@ public class AdventureRequest
 	extends GenericRequest
 {
 	public static final String NOT_IN_A_FIGHT = "Not in a Fight";
-	
+
 	private static final Pattern AREA_PATTERN = Pattern.compile( "(adv|snarfblat)=(\\d*)", Pattern.DOTALL );
 
 	// <img id='monpic' src="http://images.kingdomofloathing.com/adventureimages/ssd_sundae.gif" width=100 height=100>
@@ -678,7 +678,7 @@ public class AdventureRequest
 				image.startsWith( "spookyhobo" ) ? "Spooky Hobo" :
 				image.startsWith( "slhobo" ) ? "Sleaze Hobo" :
 				// Slime Tube
-				image.startsWith( "slime" ) ? image.substring( 0, 5 ) :
+				image.startsWith( "slime" ) ? image.substring( 0, 6 ) :
 				// GamePro Bosses
 				image.startsWith( "faq_boss" ) ? "Video Game Boss" :
 				image.startsWith( "faq_miniboss" ) ? "Video Game Miniboss" :
@@ -782,7 +782,7 @@ public class AdventureRequest
 			return DeckOfEveryCardRequest.parseCardEncounter( responseText );
 
 		case 535:	// Deep Inside Ronald, Baby
-		case 536:	// Deep Inside Grimace, Bow Chick-a Bow Bow	
+		case 536:	// Deep Inside Grimace, Bow Chick-a Bow Bow
 		case 585:	// Screwing Around!
 		case 595:	// Fire! I... have made... fire!
 		case 807:	// Breaker Breaker!
@@ -1286,7 +1286,7 @@ public class AdventureRequest
 			StaticEntity.printStackTrace( e );
 			return monsterName;
 		}
-		
+
 		Object[] result;
 		try
 		{
@@ -1296,7 +1296,7 @@ public class AdventureRequest
 		{
 			return monsterName;
 		}
-		
+
 		String text = "";
 		for ( Object result1 : result )
 		{
@@ -1329,7 +1329,7 @@ public class AdventureRequest
 		}
 
 		// Ditto for "a " to recognize the aliases for Your Shadow
-		
+
 		if ( monsterName.startsWith( "a " ) )
 		{
 			monsterName = monsterName.substring( 2 );