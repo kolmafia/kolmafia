8ca66ad327f2855acee791469508c01c5289d78c
Veracity <veracity0@users.sourceforge.net>
2015-08-06 02:31:54 +0000

    Volcano skills
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@16124 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2407,6 +2407,9 @@ public class ItemPool
 	public static final int CRYSTALLINE_LIGHT_BULB = 8456;
 	public static final int SUPERDUPERHEATED_METAL = 8522;
 	public static final int SUPERHEATED_METAL = 8524;
+	public static final int LAVA_MINERS_DAUGHTER = 8482;
+	public static final int PSYCHO_FROM_THE_HEAT = 8483;
+	public static final int THE_FIREGATE_TAPES = 8484;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
