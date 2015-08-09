d8bdb0766d84ff09d65eebb7b695ffd51501bd0e
Veracity <veracity0@users.sourceforge.net>
2015-08-09 14:46:51 +0000

    Certain WLF bounties require that you turn in multiple items
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@16147 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2405,11 +2405,13 @@ public class ItemPool
 	public static final int HEAT_RESISTANT_SHEET_METAL = 8453;
 	public static final int GLOWING_NEW_AGE_HEALING_CRYSTAL = 8455;
 	public static final int CRYSTALLINE_LIGHT_BULB = 8456;
+	public static final int GOOEY_LAVA_GLOBS = 8470;
 	public static final int SUPERDUPERHEATED_METAL = 8522;
 	public static final int SUPERHEATED_METAL = 8524;
 	public static final int LAVA_MINERS_DAUGHTER = 8482;
 	public static final int PSYCHO_FROM_THE_HEAT = 8483;
 	public static final int THE_FIREGATE_TAPES = 8484;
+	public static final int SMOOCH_BRACERS = 8517;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
