6de7cec3efb6b3882f345b3a71c0d7f210d1c6c7
heeheehee <heeheehee@users.sourceforge.net>
2016-04-09 16:01:54 +0000

    Fishin' hat and high-test fishing line can only be purchased if you have a fishin' pole.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@16868 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2591,6 +2591,9 @@ public class ItemPool
 	public static final int SNAKE_OILING_TALES = 8957;
 	public static final int SNAKE_OIL = 8971;
 	public static final int WITCHESS_SET = 8989;
+	public static final int FISHING_LINE = 9007;
+	public static final int FISHING_POLE = 9010;
+	public static final int FISHING_HAT = 9011;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
