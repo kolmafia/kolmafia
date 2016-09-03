c7c24125630607ed0738b7701da6b773f25be986
Darzil <darzil@users.sourceforge.net>
2016-09-03 14:04:16 +0000

    Reassured is shruggable, Time spinner is reusable in combat too.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@17165 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/EffectPool.java
+++ src/net/sourceforge/kolmafia/objectpool/EffectPool.java
@@ -286,6 +286,7 @@ public class EffectPool
 	public static final int BLOOD_SUGAR_SAUCE_MAGIC_LITE = 1457;
 	public static final int BLOOD_SUGAR_SAUCE_MAGIC = 1458;
 	public static final int SOULERSKATES = 1465;
+	public static final int REASSURED = 1492;
 	public static final int UNMUFFLED = 1545;
 	public static final int MUFFLED = 1546;
 	public static final int BORED_WITH_EXPLOSIONS = 1557;
