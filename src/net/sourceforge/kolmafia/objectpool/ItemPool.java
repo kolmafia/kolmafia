d29c36983dbd5d1169cd8260a091ddabd30e499a
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2016-11-09 22:27:25 +0000

    Track turkey blaster uses
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@17362 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2685,6 +2685,7 @@ public class ItemPool
 	public static final int TRICK_TOT_EYEBALL = 9144;
 	public static final int TRICK_TOT_LIBERTY = 9145;
 	public static final int HOARDED_CANDY_WAD = 9146;
+	public static final int TURKEY_BLASTER = 9166;
 	public static final int CORNUCOPIA = 9183;
 	public static final int MEGACOPIA = 9184;
 	public static final int GIANT_PILGRIM_HAT = 9185;
