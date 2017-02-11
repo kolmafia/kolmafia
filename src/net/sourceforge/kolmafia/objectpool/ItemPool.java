3a1dd796b8243ed81c5c9af5d6e525500480cbe6
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2017-02-11 14:42:49 +0000

    Track LOV Extraterrestrial Chocolate uses in _loveChocolatesUsed
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@17793 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2744,6 +2744,7 @@ public class ItemPool
 	public static final int WAX_BOOZE = 9309;
 	public static final int WAX_GLOB = 9310;
 	public static final int HEART_SHAPED_CRATE = 9316;
+	public static final int LOVE_CHOCOLATE = 9325;
 	public static final int ELDRITCH_ICHOR = 9333;
 	public static final int ELDRITCH_TINCTURE = 9335;
 	public static final int ELDRITCH_TINCTURE_DEPLETED = 9336;
