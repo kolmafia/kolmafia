394acc4923efa09d481ea15e84beff30ebb15a53
Darzil <darzil@users.sourceforge.net>
2017-06-08 14:41:09 +0000

    Eternal Flame
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@18094 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2836,6 +2836,8 @@ public class ItemPool
 	public static final int PORTABLE_SPACEGATE = 9465;
 	public static final int NEW_YOU_CLUB_MEMBERSHIP_FORM = 9478;
 	public static final int KREMLIN_BRIEFCASE = 9493;
+	public static final int CELCIUS_233 = 9504;
+	public static final int CELCIUS_233_SINGED = 9505;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
