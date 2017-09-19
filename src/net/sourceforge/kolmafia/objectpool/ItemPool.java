eb2842b3e09298d1d8418bb4ccbaa61c16da325e
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2017-09-19 21:55:19 +0000

    Don't load pocket wish inline
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@18217 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2854,6 +2854,7 @@ public class ItemPool
 	public static final int PERFECTLY_FAIR_COIN = 9526;
 	public static final int CORKED_GENIE_BOTTLE = 9528;
 	public static final int GENIE_BOTTLE = 9529;
+	public static final int POCKET_WISH = 9537;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
