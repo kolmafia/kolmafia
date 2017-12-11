e21e0a55af726419ce97ce20e02fc365fd15b441
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2017-12-11 17:01:32 +0000

    Add some familiar attributes (more to come later).  Add attributes proxy field for familiars.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@18294 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -356,7 +356,7 @@ public interface KoLConstants
 	public static final int DEFAULTS_VERSION = 1;
 	public static final int ENCOUNTERS_VERSION = 1;
 	public static final int EQUIPMENT_VERSION = 2;
-	public static final int FAMILIARS_VERSION = 3;
+	public static final int FAMILIARS_VERSION = 4;
 	public static final int FAXBOTS_VERSION = 1;
 	public static final int FOLDGROUPS_VERSION = 1;
 	public static final int FULLNESS_VERSION = 2;
