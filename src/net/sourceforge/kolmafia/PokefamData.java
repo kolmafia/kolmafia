b92944b1ad0c4475a1452e43ce9ba9d3340ff86f
Veracity <veracity0@users.sourceforge.net>
2018-03-21 00:13:21 +0000

    power goes to 4 and hp goes to 5
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@18526 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/PokefamData.java
+++ src/net/sourceforge/kolmafia/PokefamData.java
@@ -193,6 +193,8 @@ public class PokefamData
 				return 2;
 			case '3':
 				return 3;
+			case '4':
+				return 4;
 			}
 		}
 		return 0;
@@ -210,6 +212,10 @@ public class PokefamData
 				return 2;
 			case '3':
 				return 3;
+			case '4':
+				return 4;
+			case '5':
+				return 5;
 			}
 		}
 		return 0;
