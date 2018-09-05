550f36d08f6d3a34358d855768c60a5d3abcc07d
Darzil <darzil@users.sourceforge.net>
2018-09-05 07:42:33 +0000

    Doh !
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@18852 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/net/sourceforge/kolmafia/request/FantasyRealmRequest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2005-2018, KoLmafia development team
+ * http://kolmafia.sourceforge.net/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  [1] Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  [2] Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *  [3] Neither the name "KoLmafia" nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.sourceforge.kolmafia.request;
+
+import net.sourceforge.kolmafia.KoLConstants.MafiaState;
+import net.sourceforge.kolmafia.KoLmafia;
+import net.sourceforge.kolmafia.RequestThread;
+
+import net.sourceforge.kolmafia.objectpool.Concoction;
+import net.sourceforge.kolmafia.objectpool.ItemPool;
+
+import net.sourceforge.kolmafia.persistence.ConcoctionDatabase;
+
+import net.sourceforge.kolmafia.session.ResultProcessor;
+
+public class FantasyRealmRequest
+	extends CreateItemRequest
+{
+	public FantasyRealmRequest( final Concoction conc )
+	{
+		super( "choice.php", conc );
+	}
+
+	private static String itemIdToOption( final int itemId )
+	{
+		return  itemId == ItemPool.FR_WARRIOR_HELM ? "1" :
+			itemId == ItemPool.FR_MAGE_HAT ? "2" :
+			itemId == ItemPool.FR_ROGUE_MASK ? "3" :
+			"6";
+	}
+
+	@Override
+	public void run()
+	{
+		String name = this.getName();
+
+		KoLmafia.updateDisplay( "Creating 1 " + name + "..." );
+
+		GenericRequest request = new GenericRequest( "place.php?whichplace=realm_fantasy&action=fr_initcenter" );
+		RequestThread.postRequest( request );
+		request.constructURLString( "choice.php?whichchoice=1280&option=" + FantasyRealmRequest.itemIdToOption( this.getItemId() ) );
+		RequestThread.postRequest( request );
+		ConcoctionDatabase.refreshConcoctions( false );
+	}
+
+	public static final boolean registerRequest( final String urlString )
+	{
+		if ( !urlString.startsWith( "choice.php" ) || !urlString.contains( "whichchoice=1280" ) )
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
