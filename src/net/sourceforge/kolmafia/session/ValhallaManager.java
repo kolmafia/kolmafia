290b99f8924215c5323084d2bfeb267e95c88288
lostcalpolydude <lostcalpolydude@users.sourceforge.net>
2019-03-17 17:56:18 +0000

    Track progress toward Blech House
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19139 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/session/ValhallaManager.java
+++ src/net/sourceforge/kolmafia/session/ValhallaManager.java
@@ -661,6 +661,7 @@ public class ValhallaManager
 		Preferences.setString( "latteModifier", "" );
 		Preferences.resetToDefault( "latteUnlocks" );
 		Preferences.setInteger( "highTopPumped", 0 );
+		Preferences.setInteger( "smutOrcNoncombatProgress", 0 );
 		// Copied monsters
 		Preferences.setString( "cameraMonster", "" );
 		Preferences.setString( "crappyCameraMonster", "" );
