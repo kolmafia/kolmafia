cf297ac00b657d25ca4b29ad88053d273546be95
Veracity <veracity0@users.sourceforge.net>
2019-04-29 04:34:56 +0000

    Scurvy and Sobriety Prevention grants a skill
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19196 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -3098,6 +3098,7 @@ public class ItemPool
 	public static final int PR_MEMBER = 10187;
 	public static final int PR_GUEST = 10188;
 	public static final int PIRATE_REALM_FUN_LOG = 10225;
+	public static final int SCURVY_AND_SOBRIETY_PREVENTION = 10228;
 
 	public static final AdventureResult get( String itemName, int count )
 	{
