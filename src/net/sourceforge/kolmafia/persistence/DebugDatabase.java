ceb819b854efb066e7ae58e31f22857a77da0671
Veracity <veracity0@users.sourceforge.net>
2019-08-20 21:49:30 +0000

    A fix for parsing KGB regen broke parsing KGB prismatic damage
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19498 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/persistence/DebugDatabase.java
+++ src/net/sourceforge/kolmafia/persistence/DebugDatabase.java
@@ -1465,7 +1465,7 @@ public class DebugDatabase
 		StringUtilities.globalStringDelete(
 			enchantments,
 			"<b>NOTE:</b> Items that reduce the MP cost of skills will not do so by more than 3 points, in total." );
-		StringUtilities.globalStringReplace( enchantments, "</font><br>", "<br></font>" );
+		// StringUtilities.globalStringReplace( enchantments, "</font><br>", "<br></font>" );
 		StringUtilities.globalStringReplace( enchantments, "<br>", "\n" );
 		StringUtilities.globalStringReplace( enchantments, "<Br>", "\n" );
 		// Following from bogus HTML in Two Crazy Random Summer
