acff65ccb47a6b15c788a2ba25b4fc2e8f7848ff
Veracity <veracity0@users.sourceforge.net>
2019-12-25 17:31:12 +0000

    New Crimbo item and skill. Soup up checkpowers to move items from storage to closet, if needed. Add powers for four items that the souped up checkpowers found for me.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19663 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/UseItemRequest.java
+++ src/net/sourceforge/kolmafia/request/UseItemRequest.java
@@ -3457,6 +3457,7 @@ public class UseItemRequest
 		case ItemPool.ISLAND_DRINKIN:
 		case ItemPool.SCURVY_AND_SOBRIETY_PREVENTION:
 		case ItemPool.THE_IMPLODED_WORLD:
+		case ItemPool.THE_SPIRIT_OF_GIVING:
 		{
 			// You insert the ROM in to your... ROM receptacle and
 			// absorb the knowledge of optimality. You suspect you
