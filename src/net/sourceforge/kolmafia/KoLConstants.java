38220fbd4a8e714e0005ee85ba18c2b97e83d12c
Veracity <veracity0@users.sourceforge.net>
2020-02-09 21:26:19 +0000

    Bump version to 20.2
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19728 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/KoLConstants.java
+++ src/net/sourceforge/kolmafia/KoLConstants.java
@@ -71,8 +71,8 @@ public interface KoLConstants
 	// Version information for the current version of KoLmafia.
 	// Rendered in various locations and therefore made public.
 
-	public static final String VERSION_NAME = "KoLmafia v19.12";
-	public static final String VERSION_DATE = "Released on December 30, 2019";
+	public static final String VERSION_NAME = "KoLmafia v20.2";
+	public static final String VERSION_DATE = "Released on February 9, 2020";
 	public static final boolean RELEASED = true;
 	public static final String REVISION = null;
 
