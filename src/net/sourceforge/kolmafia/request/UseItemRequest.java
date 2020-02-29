86da50f418a68b60c1693825547ffb99df06cfd1
Veracity <veracity0@users.sourceforge.net>
2020-02-29 18:31:51 +0000

    [glitch season reward name] is now reusable, rather than message display. We will still disply the use message if it is different than the previous implementation level.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19789 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/UseItemRequest.java
+++ src/net/sourceforge/kolmafia/request/UseItemRequest.java
@@ -2404,32 +2404,6 @@ public class UseItemRequest
 			return;
 
 		case KoLConstants.MESSAGE_DISPLAY:
-			// If these items cease to be MESSAGE_DISPLAY, move
-			// case block to regular items below
-			switch (itemId )
-			{
-			case ItemPool.GLITCH_ITEM:
-			{
-				// [This needs implementation.
-				// [This needs more implementation.]
-				// [This needs some more implementation.]
-				// [This needs a lot more implementation.]
-				// [This needs a ton more implementation.]
-				// [This needs even more than a ton more implementation.]
-				// [Whoa .]
-				int glitchLevel =
-					responseText.contains( "Whoa" ) ? 7 :
-					responseText.contains( "needs even more than a ton more implementation" ) ? 6 :
-					responseText.contains( "needs a ton more implementation" ) ? 5 :
-					responseText.contains( "needs a lot more implementation" ) ? 4 :
-					responseText.contains( "needs some more implementation" ) ? 3 :
-					responseText.contains( "needs more implementation" ) ? 2 :
-					responseText.contains( "needs implementation" ) ? 1 :
-					0;
-				Preferences.setInteger( "glitchItemImplementationLevel", glitchLevel );
-				break;
-			}
-			}
 			UseItemRequest.showItemUsage( showHTML, responseText );
 			return;
 		}
@@ -6412,6 +6386,33 @@ public class UseItemRequest
 			}
 
 			break;
+		}
+
+		case ItemPool.GLITCH_ITEM:
+		{
+			// [This needs implementation.
+			// [This needs more implementation.]
+			// [This needs some more implementation.]
+			// [This needs a lot more implementation.]
+			// [This needs a ton more implementation.]
+			// [This needs even more than a ton more implementation.]
+			// [Whoa .]
+			int newGlitchLevel =
+				responseText.contains( "Whoa" ) ? 7 :
+				responseText.contains( "needs even more than a ton more implementation" ) ? 6 :
+				responseText.contains( "needs a ton more implementation" ) ? 5 :
+				responseText.contains( "needs a lot more implementation" ) ? 4 :
+				responseText.contains( "needs some more implementation" ) ? 3 :
+				responseText.contains( "needs more implementation" ) ? 2 :
+				responseText.contains( "needs implementation" ) ? 1 :
+				0;
+			int previousGlitchLevel = Preferences.getInteger( "glitchItemImplementationLevel" );
+			if ( previousGlitchLevel != newGlitchLevel )
+			{
+				Preferences.setInteger( "glitchItemImplementationLevel", newGlitchLevel );
+				UseItemRequest.showItemUsage( showHTML, responseText );
+			}
+			return;
 		}
 		}
 
