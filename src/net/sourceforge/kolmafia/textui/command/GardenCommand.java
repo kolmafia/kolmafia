d8a8f26da877ec41f7043e4a3144cd80b5f57427
Veracity <veracity0@users.sourceforge.net>
2020-03-22 20:04:09 +0000

    The "garden pick" and "garden fertilize" commands now tell you why you cannot go into your mushroom garden: already dealt with it today, no turns left, falling down drunk
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@19898 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/command/GardenCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/GardenCommand.java
@@ -39,6 +39,8 @@ import net.sourceforge.kolmafia.KoLmafia;
 
 import net.sourceforge.kolmafia.persistence.ItemDatabase;
 
+import net.sourceforge.kolmafia.preferences.Preferences;
+
 import net.sourceforge.kolmafia.request.CampgroundRequest;
 import net.sourceforge.kolmafia.request.CampgroundRequest.CropType;
 
@@ -50,6 +52,31 @@ public class GardenCommand
 	public GardenCommand()
 	{
 		this.usage = " [pick] - get status of garden, or harvest it.";
+	}
+
+	private boolean checkMushroomGarden( CropType cropType )
+	{
+		if ( cropType != CropType.MUSHROOM )
+		{
+			KoLmafia.updateDisplay( "You don't have a mushroom garden." );
+			return false;
+		}
+		if ( Preferences.getBoolean( "_mushroomGardenVisited" ) )
+		{
+			KoLmafia.updateDisplay( "You've already dealt with your mushroom garden today." );
+			return false;
+		}
+		if ( KoLCharacter.isFallingDown() )
+		{
+			KoLmafia.updateDisplay( "You are too drunk to enter your mushroom garden." );
+			return false;
+		}
+		if ( KoLCharacter.getAdventuresLeft() <= 0 )
+		{
+			KoLmafia.updateDisplay( "You need an available turn to fight through piranha plants." );
+			return false;
+		}
+		return true;
 	}
 
 	@Override
@@ -82,20 +109,17 @@ public class GardenCommand
 		if ( parameters.equals( "fertilize" ) )
 		{
 			// Mushroom garden only
-			if ( cropType != CropType.MUSHROOM )
+			if ( checkMushroomGarden( cropType ) )
 			{
-				KoLmafia.updateDisplay( "You don't have a mushroom garden." );
-				return;
+				CampgroundRequest.harvestMushrooms( false );
 			}
-
-			CampgroundRequest.harvestMushrooms( false );
 			return;
 		}
 
 		if ( parameters.equals( "pick" ) )
 		{
 			// Mushroom garden only
-			if ( cropType == CropType.MUSHROOM )
+			if ( cropType == CropType.MUSHROOM && checkMushroomGarden( cropType ) )
 			{
 				CampgroundRequest.harvestMushrooms( true );
 				return;
