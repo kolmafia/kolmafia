16611005633807c392aae0e9c3fa295965fd2471
Veracity <veracity0@users.sourceforge.net>
2020-04-15 07:47:35 +0000

    More drippy items
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20002 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -3175,6 +3175,7 @@ public class ItemPool
 	public static final int TEMPURA_GREEN_AND_RED_BEAN = 10425;
 	public static final int BIRD_A_DAY_CALENDAR = 10434;
 	public static final int POWERFUL_GLOVE = 10438;
+	public static final int DRIPLET = 10443;
 	public static final int DRIPPY_NUGGET = 10445;
 	public static final int DRIPPY_WINE = 10446;
 	public static final int DRIPPY_CAVIAR = 10447;
