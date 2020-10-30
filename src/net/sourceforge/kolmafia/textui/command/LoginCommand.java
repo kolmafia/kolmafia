cea0cd783697e211571b368aef05eb3d968fa6c7
MCroft <madcarew@users.sourceforge.net>
2020-10-30 15:19:14 +0000

    gCLI login command error handling
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20485 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/command/LoginCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/LoginCommand.java
@@ -33,6 +33,8 @@
 
 package net.sourceforge.kolmafia.textui.command;
 
+import net.sourceforge.kolmafia.KoLConstants;
+import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.session.LogoutManager;
 
 public class LoginCommand
@@ -46,6 +48,19 @@ public class LoginCommand
 	@Override
 	public void run( final String cmd, final String parameters )
 	{
+		if ( parameters == null || parameters.isEmpty() ) {
+			// Print usage
+			KoLmafia.updateDisplay( KoLConstants.MafiaState.ERROR, "login <i>username</i>" + this.usage );
+			KoLmafia.updateDisplay( KoLConstants.MafiaState.ERROR, "Please provide username to login." );
+			return;
+		}
+		if  ( KoLmafia.getSaveState( parameters ) == null || KoLmafia.getSaveState( parameters ).isEmpty() )
+		{
+			// Error, must have stored password for user.
+			KoLmafia.updateDisplay( KoLConstants.MafiaState.ERROR, "No stored password for user: " + parameters );
+			KoLmafia.updateDisplay( KoLConstants.MafiaState.ERROR, "Logout and enter credentials manually." );
+			return;
+		}
 		LogoutManager.logout();
 		this.CLI.attemptLogin( parameters );
 	}
