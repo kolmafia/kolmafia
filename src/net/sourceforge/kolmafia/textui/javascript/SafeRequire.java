94d1bfa9015038eb263795627e8d3fbfd43cf06b
gausie <innerdreams@users.sourceforge.net>
2020-11-28 23:57:34 +0000

    Fixes some bugs with argument inversion on library functions, and some type conversion issues. It also reworks some of the proxy record code to work in a more consistent way. Thanks @ikzann
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20528 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/javascript/SafeRequire.java
+++ src/net/sourceforge/kolmafia/textui/javascript/SafeRequire.java
@@ -30,7 +30,7 @@ public class SafeRequire
 
 	public SafeRequire( Context cx, Scriptable nativeScope )
 	{
-		super( cx, nativeScope, new SoftCachingModuleScriptProvider( new UrlModuleSourceProvider( Arrays.asList( KoLConstants.SCRIPT_LOCATION.toURI(), KoLConstants.RELAY_LOCATION.toURI() ), null ) ), null, null, true );
+		super( cx, nativeScope, new SoftCachingModuleScriptProvider( new UrlModuleSourceProvider( Arrays.asList( KoLConstants.SCRIPT_LOCATION.toURI(), KoLConstants.RELAY_LOCATION.toURI() ), null ) ), null, new MainWarningScript(), true );
 	}
 
 	@Override
