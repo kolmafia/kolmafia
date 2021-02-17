8fea04213b26dd14d7617f874506c4a5eba7c20c
MCroft <madcarew@users.sourceforge.net>
2021-02-17 06:41:15 +0000

    https fix for ircm_extend solves 2nd space in search element issue for inventory (h/t Philmasterplus
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20633 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ src/relay/ircm_extend.3.js
@@ -0,0 +1,25 @@
+(function(window) {
+	var original_function = window.pop_ircm_contents;
+
+	window.pop_ircm_contents = function(i, some) {
+		var original = original_function(i, some);
+		var name = $("[rel^='id=" + i.id + "&'] b").eq(0).text();
+
+		if (name) {
+			var encodedName = encodeURIComponent(name);
+			var wikiUrl = "https://kol.coldfront.net/thekolwiki/index.php/Special:Search?go=Go&search=" + encodedName;
+
+			var html = '<div style="width:100%; padding-bottom: 3px;"><b>Search:</b> ';
+			// mallable == !quest && !gift && tradeable
+			if (i.q == 0 && i.g == 0 && i.t == 1) {
+				html += '<a href="/mall.php?pudnuggler=' + encodedName + '" class="small">[mall]</a>&nbsp;';
+			}
+			html += '<a target="_blank" href="' + wikiUrl + '" class="small">[wiki]</a></div>';
+
+			original[0] += html;
+			original[1]++;
+		}
+
+		return original;
+	}
+})(this);
