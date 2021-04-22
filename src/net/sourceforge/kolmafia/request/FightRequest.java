bf479afd66f5d30bfeaa7137d9323aaf79847a2a
gausie <innerdreams@users.sourceforge.net>
2021-04-22 11:29:05 +0000

    Correctly parse damage from an unusual spelunky shotgun hit message
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20699 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/FightRequest.java
+++ src/net/sourceforge/kolmafia/request/FightRequest.java
@@ -5530,7 +5530,7 @@ public class FightRequest
 	// and this is not the case, then correct the use of this Pattern below.
 
 	private static final Pattern PHYSICAL_PATTERN =
-		Pattern.compile( "(your blood, to the tune of|stabs you for|sown|You lose|You gain|strain your neck|approximately|roughly|)\\s*#?(\\d[\\d,]*) (\\([^.]*\\) |)((?:[^\\s]+ ){0,3})(?:\"?damage|points?|bullets|hollow|notch(?:es)?|to your opponent|to the foul demon|force damage|tiny holes|like this, it's bound|from the power)" );
+		Pattern.compile( "(your blood, to the tune of|stabs you for|sown|You lose|You gain|strain your neck|approximately|roughly|)\\s*#?(\\d[\\d,]*) (\\([^.]*\\) |)((?:[^\\s]+ ){0,3})(?:\"?damage|points?|bullets|hollow|notch(?:es)?|to your opponent|to the foul demon|force damage|tiny holes|like this, it's bound|from the power|tiny lead pellets? that do(?:es)? 1 point of damage)" );
 
 	private static final Pattern ELEMENTAL_PATTERN =
 		Pattern.compile( "(sown|) \\+?([\\d,]+) (\\([^.]*\\) |)(?:months worth of concentrated palm sweat|(?:slimy, (?:clammy|gross) |hotsy-totsy |)damage|points|HP worth)" );
