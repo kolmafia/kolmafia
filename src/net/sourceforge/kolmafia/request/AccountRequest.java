33dbc54823ca935c8c7cbb7aaf0e262b8011218c
Rinn <kirchoffjosephp@users.sourceforge.net>
2021-06-27 21:09:33 +0000

    Fix misplaced space typo
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20779 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/AccountRequest.java
+++ src/net/sourceforge/kolmafia/request/AccountRequest.java
@@ -621,7 +621,7 @@ public class AccountRequest
 		boolean hardcore = JSON.getInt( "hardcore" ) == 1 || sign.equals( "Bad Moon" );
 		KoLCharacter.setHardcore( hardcore );
 
-		boolean casual = JSON.getInt( "casual ") == 1;
+		boolean casual = JSON.getInt( "casual" ) == 1;
 		KoLCharacter.setCasual( casual );
 
 		// This isn't safe in Ed after defeating adventurer, but if we're Ed we haven't freed ralph!
