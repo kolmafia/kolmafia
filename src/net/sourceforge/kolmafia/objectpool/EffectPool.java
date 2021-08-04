72f29d6d9a5b4d8904f1b0257ef3cd660475b637
gausie <innerdreams@users.sourceforge.net>
2021-08-04 12:55:30 +0000

    Consider Ew the Humanity and A Beastly Odor in combat data calculations
    This is assuming it adds 2 copies of the phylum like the snapper. If that's wrong we just need to change the number
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20808 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/objectpool/EffectPool.java
+++ src/net/sourceforge/kolmafia/objectpool/EffectPool.java
@@ -359,6 +359,8 @@ public class EffectPool
 	public static final int CARTOGRAPHICALLY_CHARGED = 2600;
 	public static final int CARTOGRAPHICALLY_AWARE = 2601;
 	public static final int CARTOGRAPHICALLY_ROOTED = 2602;
+	public static final int EW_THE_HUMANITY = 2647;
+	public static final int A_BEASTLY_ODOR = 2648;
 
 	public static final AdventureResult CURSE1_EFFECT = EffectPool.get( EffectPool.ONCE_CURSED );
 	public static final AdventureResult CURSE2_EFFECT = EffectPool.get( EffectPool.TWICE_CURSED );
