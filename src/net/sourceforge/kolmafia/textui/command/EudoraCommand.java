e650356310a2df36a9bd283ad000c2d54ae24593
gausie <innerdreams@users.sourceforge.net>
2021-08-04 09:15:09 +0000

    Support for Our Daily Candles
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20807 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/textui/command/EudoraCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/EudoraCommand.java
@@ -49,7 +49,8 @@ public class EudoraCommand
 		PENPAL( 1, "Pen Pal", "My Own Pen Pal kit" ),
 		GAME( 2, "GameInformPowerDailyPro Magazine", "GameInformPowerDailyPro subscription card" ),
 		XI( 3, "Xi Receiver Unit" , "Xi Receiver Unit" ),
-		NEWYOU( 4, "New-You Club", "New-You Club Membership Form" );
+		NEWYOU( 4, "New-You Club", "New-You Club Membership Form" ),
+		CANDLE( 5, "Our Daily Candles", "Our Daily Candles™ order form" );
 
 		private final int id;
 		private final String name;
@@ -71,8 +72,8 @@ public class EudoraCommand
 		{
 			for ( Correspondent correspondent : Correspondent.values() )
 			{
-				String name = correspondent.getName();
-				if ( name.contains( query ) || name.replaceAll( "[- ]", "" ).contains( query ) )
+				String name = correspondent.getName().toLowerCase();
+				if ( name.contains( query ) || name.replaceAll( "[- ]", "" ).contains( query.toLowerCase() ) )
 				{
 					return correspondent;
 				}
@@ -118,7 +119,7 @@ public class EudoraCommand
 
 	public EudoraCommand()
 	{
-		this.usage = " penpal|game|xi|newyou - switch to the specified correspondent";
+		this.usage = " penpal|game|xi|newyou|candle - switch to the specified correspondent";
 	}
 
 	public static boolean switchTo( final Correspondent correspondent )
@@ -141,7 +142,7 @@ public class EudoraCommand
 		}
 		else
 		{
-			KoLmafia.updateDisplay( MafiaState.ERROR, "Cannot switch to" + correspondent.getName() );
+			KoLmafia.updateDisplay( MafiaState.ERROR, "Cannot switch to " + correspondent.getName() );
 			return false;
 		}
 	}
