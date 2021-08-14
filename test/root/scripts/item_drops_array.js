26ab81e05da8c9d50f4e925c3051088249e8e86c
heeheehee <heeheehee@users.sourceforge.net>
2021-08-14 22:43:12 +0000

    Only coerce map keys for Javascript, not values.
    JS object keys are only representable with certain types, but their
    values are not.
    
    There will continue to be some ASH types that can't be represented,
    such as item[item]. This should be viable if we use JS Maps instead,
    but I haven't tried that out..
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20827 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ test/root/scripts/item_drops_array.js
@@ -0,0 +1,9 @@
+const { print, toJson, itemDropsArray } = require('kolmafia');
+
+let monster = Monster.get("beanbat");
+let drops = itemDropsArray(monster);
+
+// Rhino doesn't support template literal strings. Alas.
+print(monster + " has " + drops.length + " drops.");
+print("The first drop is " + drops[0].drop + " with a rate of " + drops[0].rate + ".");
+print("Its image is " + drops[0].drop.image + ".");
