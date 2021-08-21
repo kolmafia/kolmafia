b0bb81d0874353724fb743286911f836a7c5f98f
fronobulax <jaadams5@users.sourceforge.net>
2021-08-21 20:02:30 +0000

    Revert JSON change until it won't break Batfellow etc.
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20847 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/request/CharPaneRequest.java
+++ src/net/sourceforge/kolmafia/request/CharPaneRequest.java
@@ -1799,7 +1799,7 @@ public class CharPaneRequest
 			ResultProcessor.processAdventuresUsed( turnsThisRun - mafiaTurnsThisRun );
 		}
 
-		KoLCharacter.setLimitmode( Integer.toString(JSON.getInt( "limitmode" ) ));
+		KoLCharacter.setLimitmode( JSON.getString( "limitmode" ) );
 
 		JSONObject lastadv = JSON.getJSONObject( "lastadv" );
 		String adventureId = lastadv.getString( "id" );
