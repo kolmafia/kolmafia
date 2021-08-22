0432f1c8be9a37406e95ee993c18c2279eef5215
fronobulax <jaadams5@users.sourceforge.net>
2021-08-22 19:04:11 +0000

    coverage++
    CharacterEntities had some null checks added and some "lint" addressed.  The included test was passed both before and after these changes.
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20849 b29ace70-8910-0410-8dcc-aa2fc6433167

---
+++ test/net/sourceforge/kolmafia/utilities/CharacterEntitiesTest.java
@@ -0,0 +1,55 @@
+package net.sourceforge.kolmafia.utilities;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * CharacterEntities is pretty well tested by other tests.  This one is mostly to increase the Clover
+ * coverage by filling in things that are not exercised elsewhere.
+ */
+
+public class CharacterEntitiesTest {
+        @Test
+        public void itShouldEscape() {
+                String a = "This is mixed and matched &pound; &curren;";
+                String ea = CharacterEntities.escape(a);
+                String uea = CharacterEntities.unescape(ea);
+                assertEquals(a, uea);
+                String b = "This is mixed and matched \u00a3 \u00a4";
+                String eb = CharacterEntities.escape(b);
+                String ueb = CharacterEntities.unescape(eb);
+                assertEquals(b, ueb);
+        }
+        @Test
+        @Ignore("Failing in Windows but not IDE")
+        public void itShouldUnescape() {
+                String a = "This is mixed and matched &pound; &curren;";
+                String ua = CharacterEntities.unescape(a);
+                String eua = CharacterEntities.escape(ua);
+                assertEquals(a, eua);
+                String b = "This is mixed and matched \u00a3 \u00a4";
+                String ub = CharacterEntities.unescape(b);
+                String eub = CharacterEntities.escape(ub);
+                assertEquals(b, eub);
+        }
+        @Test
+        public void itShouldHandleSomeInterestingCases() {
+                String a = null;
+                String ea = CharacterEntities.escape(a);
+                String ua = CharacterEntities.unescape(a);
+                assertEquals(ea, "");
+                assertEquals(ua, "");
+                a = "This & that";
+                ea = CharacterEntities.escape(a);
+                ua = CharacterEntities.unescape(a);
+                assertEquals(ea, "This &amp; that");
+                assertEquals(a, ua);
+                a = "&xyzzy;";
+                ea = CharacterEntities.escape(a);
+                ua = CharacterEntities.unescape(a);
+                assertEquals(ea,"&amp;xyzzy;");
+                assertEquals(a, ua);
+        }
+}
