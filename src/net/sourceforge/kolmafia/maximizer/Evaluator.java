9786adec83a96dedaad373800d52f2b084f0e1b3
heeheehee <heeheehee@users.sourceforge.net>
2021-09-16 20:39:55 +0000

    Fix "always be equipping" maximizer bug.
    In particular, Maximizer would always prefer equipping something that
    had no effect, over leaving nothing equipped.
    
    This turned out to be two bugs:
    
    1. We expected new CheckedItem( 0, ... ) to equal EquipmentRequest.UNEQUIP,
       which is populated by ItemPool.get( "(none)", 1 ). This function creates an
       object with item id -1 if no matches are found. Thus, we never considered
       equipping nothing as "simpler" than equipping something.
    
    2. We use InventoryManager.getCount() and getAccessibleCount() to determine how
       many of the object we have on hand. This hurts us twice:
    
       a. First, we fail the tiebreaker in MaximizerSpeculation.compareTo() -- we
          don't have any copies of (none) on-hand, so we prefer anything else.
    
       b. Second, Evaluator has some additional logic to make sure we don't try to
          equip more copies than we can acquire. Since we claim we can acquire 0
          copies...
    
    git-svn-id: http://svn.code.sf.net/p/kolmafia/code@20934 b29ace70-8910-0410-8dcc-aa2fc6433167

--- src/net/sourceforge/kolmafia/maximizer/Evaluator.java
+++ src/net/sourceforge/kolmafia/maximizer/Evaluator.java
@@ -2039,7 +2039,7 @@ public class Evaluator
 			// If we currently have nothing equipped, also consider leaving nothing equipped
 			if ( EquipmentManager.getEquipment( Evaluator.toUseSlot( slot ) ) == EquipmentRequest.UNEQUIP )
 			{
-				ranked[ slot ].add( new CheckedItem( 0, equipScope, maxPrice, priceLevel ) );
+				ranked[ slot ].add( new CheckedItem( -1, equipScope, maxPrice, priceLevel ) );
 			}
 
 			for ( CheckedItem item : checkedItemList )
@@ -2657,10 +2657,10 @@ public class Evaluator
 				}
 			}
 
-			// Blunt object fix for only having a foldable that might be neede elsewhere
+			// Blunt object fix for only having a foldable that might be needed elsewhere
 			if ( automatic[ slot ].size() == 1 && ItemDatabase.getFoldGroup( automatic[ slot ].get( 0 ).getName() ) != null )
 			{
-				automatic[ slot ].add( new CheckedItem( 0, equipScope, maxPrice, priceLevel ) );
+				automatic[ slot ].add( new CheckedItem( -1, equipScope, maxPrice, priceLevel ) );
 			}
 
 			if ( this.dump > 0 )
