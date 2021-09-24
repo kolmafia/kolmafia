4c510d0f9e4a0f46130ae564951ff2d19d040998
fredg1 <gagnonfrederic9@gmail.com>
2021-09-23 20:44:24 -0400

    CompositeType documentation

--- src/net/sourceforge/kolmafia/textui/parsetree/CompositeType.java
+++ src/net/sourceforge/kolmafia/textui/parsetree/CompositeType.java
@@ -42,10 +42,23 @@ public abstract class CompositeType
 		this.primitive = false;
 	}
 
+	/**
+	 * Returns the Type of the keys used in this composite type.
+	 */
 	public abstract Type getIndexType();
 
+	/**
+	 * For {@link AggregateType}, returns the type of the values of this composite type.
+	 * 
+	 * <p>For {@link RecordType}, use {@link CompositeType#getDataType(Object)}.
+	 */
 	public abstract Type getDataType();
 
+	/**
+	 * For {@link RecordType}, returns the type of the value associated with this key (if any).
+	 * 
+	 * <p>For {@link AggregateType}, use {@link CompositeType#getDataType()}.
+	 */
 	public abstract Type getDataType( final Object key );
 
 	public abstract Value getKey( final Value key );
