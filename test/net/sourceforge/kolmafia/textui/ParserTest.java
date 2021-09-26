c218ce9d045784b0d624bc43075ffc6120950cf6
fredg1 <gagnonfrederic9@gmail.com>
2021-09-26 19:16:11 -0400

    manual reset of https://github.com/kolmafia/kolmafia/pull/10

--- test/net/sourceforge/kolmafia/textui/ParserTest.java
+++ test/net/sourceforge/kolmafia/textui/ParserTest.java
@@ -2268,6 +2268,12 @@ public class ParserTest
 				null,
 			},
 			{
+				"Illegal record creation",
+				"void f( record foo {int a; int b;} bar )",
+				"Existing type expected for function parameter",
+				null,
+			},
+			{
 				"array of record",
 				"record {int a;}[] r;",
 				null,
