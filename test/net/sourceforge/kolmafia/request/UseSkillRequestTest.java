b927a0bc84a9f42b16ff7d49222b6b6813e45030
heeheehee-kolmafia <83351434+heeheehee-kolmafia@users.noreply.github.com>
2021-10-01 08:37:48 -0400

    Only increment skill usage for successes. (#56)
    * Only increment skill usage for successes.
    
    Thanks to phreddrickk @ kolmafia.us for reporting this issue.
    
    This adds an example of how one might use Mockito to inject a custom
    response. It's not super-well-documented, but I promise all the bits
    are necessary.
    
    In particular: we need to set session ID or else GenericRequest.run()
    will short-circuit, and we need to set max / current MP and "learn"
    the skill so UseSkillRequest.run() won't reject it before even passing
    it on to GenericRequest.
    
    These test cases follow the standard arrange / act / assert model.
    
    * Add a RequestTestBase for common setup / helpers.
    
    Right now, this provides some testing the happy path: you specify some
    response text for a spy, and we inject that data when you execute
    spy.run().
    
    Important: in order for everything to work end-to-end, you need to
    provide a spy, not a regular mock. This allows us to exercise as much
    real behavior as possible.

---
+++ test/net/sourceforge/kolmafia/request/UseSkillRequestTest.java
@@ -0,0 +1,53 @@
+package net.sourceforge.kolmafia.request;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.persistence.SkillDatabase;
+import net.sourceforge.kolmafia.session.ContactManager;
+import org.junit.jupiter.api.Test;
+
+class UseSkillRequestTest extends RequestTestBase {
+
+  private static int EXPERIENCE_SAFARI = SkillDatabase.getSkillId("Experience Safari");
+
+  @Test
+  void errorDoesNotIncrementSkillUses() {
+    ContactManager.registerPlayerId("targetPlayer", "123");
+    KoLCharacter.setMP(1000, 1000, 1000);
+    KoLCharacter.addAvailableSkill(EXPERIENCE_SAFARI);
+    int startingCasts = SkillDatabase.getCasts(EXPERIENCE_SAFARI);
+
+    // We only want to mock network activity, not the rest of UseSkillRequest behavior.
+    UseSkillRequest req = spy(UseSkillRequest.getInstance(EXPERIENCE_SAFARI, "targetPlayer", 1));
+
+    expectSuccess(req, "You don't have enough mana to cast that skill.");
+
+    req.run();
+
+    verify(req).externalExecute();
+    assertEquals("Not enough mana to cast Experience Safari.", UseSkillRequest.lastUpdate);
+    assertEquals(startingCasts, SkillDatabase.getCasts(EXPERIENCE_SAFARI));
+  }
+
+  @Test
+  void successIncrementsSkillUses() {
+    ContactManager.registerPlayerId("targetPlayer", "123");
+    KoLCharacter.setMP(1000, 1000, 1000);
+    KoLCharacter.addAvailableSkill(EXPERIENCE_SAFARI);
+    int startingCasts = SkillDatabase.getCasts(EXPERIENCE_SAFARI);
+
+    UseSkillRequest req = spy(UseSkillRequest.getInstance(EXPERIENCE_SAFARI, "targetPlayer", 1));
+    expectSuccess(
+        req,
+        "You bless your friend, targetPlayer, with the ability to experience a safari adventure.");
+
+    req.run();
+
+    verify(req).externalExecute();
+    assertEquals("", UseSkillRequest.lastUpdate);
+    assertEquals(startingCasts + 1, SkillDatabase.getCasts(EXPERIENCE_SAFARI));
+  }
+}
