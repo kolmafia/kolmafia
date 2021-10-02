bcc5274aed0055b9d10d7726a50a38225d48b96c
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-02 11:04:56 -0400

    Handle implicit narrowing
    Create test for before/after comparison.  Change code for easier access from test and then replace eventually incorrect calculation using Pow with something that is correct and won't overflow.

--- test/net/sourceforge/kolmafia/persistence/SkillDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/SkillDatabaseTest.java
@@ -1,23 +1,24 @@
 package net.sourceforge.kolmafia.persistence;
 
-import net.sourceforge.kolmafia.preferences.Preferences;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SkillDatabaseTest {
-    //This is just one simple test to verify before and after behavior for an implicit narrowing cast
-    //that was replaced by an alternative calculation.
-    @Test
-    public void itShouldCalculateCostCorrectlyAsAFunctionOfCasts() {
-        Preferences.setInteger("_stackLumpsUses", -10);
-        assertEquals(SkillDatabase.stackLumpsCost(), 1);
-        Preferences.setInteger("_stackLumpsUses", 0);
-        assertEquals(SkillDatabase.stackLumpsCost(), 11);
-        Preferences.setInteger("_stackLumpsUses", 1);
-        assertEquals(SkillDatabase.stackLumpsCost(), 111);
-        Preferences.setInteger("_stackLumpsUses", 2);
-        assertEquals(SkillDatabase.stackLumpsCost(), 1111);
-    }
+  // This is just one simple test to verify before and after behavior for an implicit narrowing cast
+  // that was replaced by an alternative calculation.
 
+  @Test
+  public void itShouldCalculateCostCorrectlyAsAFunctionOfCasts() {
+    assertEquals(SkillDatabase.stackLumpsCost(-10), 1);
+    assertEquals(SkillDatabase.stackLumpsCost(0), 11);
+    assertEquals(SkillDatabase.stackLumpsCost(1), 111);
+    assertEquals(SkillDatabase.stackLumpsCost(2), 1111);
+    assertEquals(SkillDatabase.stackLumpsCost(14), 1111111111111111L);
+    // The previous calculation using Pow actually gives incorrect results for more than 14 casts
+    // More than 17 casts overflows a long
+    assertEquals(SkillDatabase.stackLumpsCost(15), 11111111111111111L);
+    assertEquals(SkillDatabase.stackLumpsCost(17), 1111111111111111111L);
+    assertEquals(SkillDatabase.stackLumpsCost(18), Long.MAX_VALUE);
+  }
 }
