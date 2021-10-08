445c70c98d7a72553c6d1b07e7272ab2ff1f2294
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-07 22:05:47 -0400

    More test

--- test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
+++ test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
@@ -18,16 +18,26 @@ public class AshInteropTest {
     // Note that we won't do any actual lookups, since ChatManager.isLiterate is false.
     ContactManager.registerPlayerId("heeheehee", "354981");
 
-    var js = new JavascriptRuntime("getPlayerId('heeheehee')");
+    var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
     assertEquals("", KoLmafia.lastMessage);
     assertEquals("354981", js.execute(null, null, true).toString());
+  }
+
+  @Test
+  void getPlayerNameReturnsString() {
+    ContactManager.registerPlayerId("heeheehee", "354981");
+
+    var js = new JavascriptRuntime("getPlayerName(354981)");
+    assertEquals("", KoLmafia.lastMessage);
+    assertEquals("heeheehee", js.execute(null, null, true).toString());
+
   }
 
   @Test
   void getPlayerIdInvertsGetPlayerName() {
     ContactManager.registerPlayerId("heeheehee", "354981");
 
-    var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId('heeheehee')))");
+    var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
     assertEquals("", KoLmafia.lastMessage);
     assertEquals("heeheehee", js.execute(null, null, true).toString());
   }
