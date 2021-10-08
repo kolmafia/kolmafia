ac26281746c866ce0cddc4f0bb5dbff1a58fb3a7
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-08 12:04:07 -0400

    Update AshInteropTest.java

--- test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
+++ test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.session.ContactManager;
+import net.sourceforge.kolmafia.textui.parsetree.Value;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -20,7 +21,9 @@ public class AshInteropTest {
 
     var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
     assertEquals("", KoLmafia.lastMessage);
-    assertEquals("354981", js.execute(null, null, true).toString());
+    Value ret = js.execute(null, null, true);
+    String retS = ret.toString();
+    assertEquals("354981", retS);
   }
 
   @Test
@@ -29,7 +32,9 @@ public class AshInteropTest {
 
     var js = new JavascriptRuntime("getPlayerName(354981)");
     assertEquals("", KoLmafia.lastMessage);
-    assertEquals("heeheehee", js.execute(null, null, true).toString());
+    Value ret = js.execute(null, null, true);
+    String retS = ret.toString();
+    assertEquals("heeheehee", retS);
   }
 
   @Test
@@ -38,6 +43,31 @@ public class AshInteropTest {
 
     var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
     assertEquals("", KoLmafia.lastMessage);
-    assertEquals("heeheehee", js.execute(null, null, true).toString());
+    Value ret = js.execute(null, null, true);
+    String retS = ret.toString();
+    //currently fails rith retS 354981.0
+    assertEquals("heeheehee", retS);
+  }
+
+  @Test
+  void getPlayerIdNameCoercesArgumentToInt() {
+    ContactManager.registerPlayerId("heeheehee", "354981");
+
+    var js = new JavascriptRuntime("getPlayerName(354981.0)");
+    assertEquals("", KoLmafia.lastMessage);
+    assertEquals(new Value("heeheehee"), js.execute(null, null, true));
+
+  }
+
+  @Test
+  void getPlayerNameInvertsGetPlayerId() {
+    ContactManager.registerPlayerId("heeheehee", "354981");
+
+    var js = new JavascriptRuntime("getPlayerId(parseInt(getPlayerName(354981)))");
+    assertEquals("", KoLmafia.lastMessage);
+    Value ret = js.execute(null, null, true);
+    String retS = ret.toString();
+    //currently fails with retS NaN
+    assertEquals("heeheehee", retS);
   }
 }
