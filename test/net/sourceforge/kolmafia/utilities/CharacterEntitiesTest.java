469034ae4c2acb98d7d8e730df73d75b60905cc0
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-17 19:47:36 -0400

    Update CharacterEntitiesTest.java
    Forgot spotless...

--- test/net/sourceforge/kolmafia/utilities/CharacterEntitiesTest.java
+++ test/net/sourceforge/kolmafia/utilities/CharacterEntitiesTest.java
@@ -29,12 +29,12 @@ public class CharacterEntitiesTest {
     String eua = CharacterEntities.escape(ua);
     assertEquals(a, eua);
     String b = "This is mixed and matched \u00a3 \u00a4";
-    String ub = CharacterEntities.unescape(b);  //nothing to unescape so should be equal
+    String ub = CharacterEntities.unescape(b); // nothing to unescape so should be equal
     assertEquals(b, ub);
-    String eub = CharacterEntities.escape(ub);  //there should be a replacement
-    assertNotEquals(ub, eub); //so not equal
-    assertNotEquals(b, eub); //and not inverse
-    assertEquals(a, eub); //true because of data choice.
+    String eub = CharacterEntities.escape(ub); // there should be a replacement
+    assertNotEquals(ub, eub); // so not equal
+    assertNotEquals(b, eub); // and not inverse
+    assertEquals(a, eub); // true because of data choice.
   }
 
   @Test
