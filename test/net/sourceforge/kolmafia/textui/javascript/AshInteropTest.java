17c8a16b2cf286a8b2c774e7125b7b4f5e7b9b9a
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-18 19:12:51 -0400

    Update AshInteropTest.java
    Trying to isolate failure

--- test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
+++ test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
@@ -2,15 +2,31 @@ package net.sourceforge.kolmafia.textui.javascript;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
 import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.session.ContactManager;
 import net.sourceforge.kolmafia.textui.parsetree.Value;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class AshInteropTest {
-  @BeforeAll
-  static void clearStatus() {
+
+  @Test
+  void getPlayerIdReturnsInt() {
+    ContactManager.registerPlayerId("heeheehee", "354981");
+    var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
+    assertNotNull(js, "JavascriptRuntime returned as null.");
+    Value ret = js.execute(null, null, true);
+    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
+    String retS = ret.toString();
+    assertEquals("354981", retS);
+  }
+
+  /*
+  @BeforeEach
+  void clearStatus() {
     KoLmafia.lastMessage = "";
   }
 
@@ -22,6 +38,7 @@ public class AshInteropTest {
     var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
     assertEquals("", KoLmafia.lastMessage);
     Value ret = js.execute(null, null, true);
+    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
     assertEquals("354981", retS);
   }
@@ -32,7 +49,9 @@ public class AshInteropTest {
 
     var js = new JavascriptRuntime("getPlayerName(354981)");
     assertEquals("", KoLmafia.lastMessage);
+    assertNotNull(js, "Javascript runtime is null, preventing test.");
     Value ret = js.execute(null, null, true);
+    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
     assertEquals("heeheehee", retS);
   }
@@ -43,9 +62,11 @@ public class AshInteropTest {
 
     var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
     assertEquals("", KoLmafia.lastMessage);
+
     Value ret = js.execute(null, null, true);
+    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
-    // currently fails rith retS 354981.0
+    // currently fails with retS 354981.0
     assertEquals("heeheehee", retS);
   }
 
@@ -68,4 +89,5 @@ public class AshInteropTest {
     String retS = ret.toString();
     assertEquals("354981", retS);
   }
+   */
 }
