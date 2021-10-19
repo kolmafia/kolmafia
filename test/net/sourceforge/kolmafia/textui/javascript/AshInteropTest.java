0a327914aafbe01c5887e2b0af33a7599fe2782d
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-18 20:50:25 -0400

    Update AshInteropTest.java
    Works in IntelliJ but not Gradle

--- test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
+++ test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
@@ -2,13 +2,8 @@ package net.sourceforge.kolmafia.textui.javascript;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import net.bytebuddy.implementation.bind.annotation.IgnoreForBinding;
-import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.session.ContactManager;
 import net.sourceforge.kolmafia.textui.parsetree.Value;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class AshInteropTest {
@@ -24,32 +19,11 @@ public class AshInteropTest {
     assertEquals("354981", retS);
   }
 
-  /*
-  @BeforeEach
-  void clearStatus() {
-    KoLmafia.lastMessage = "";
-  }
-
-  @Test
-  void getPlayerIdReturnsInt() {
-    // Note that we won't do any actual lookups, since ChatManager.isLiterate is false.
-    ContactManager.registerPlayerId("heeheehee", "354981");
-
-    var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
-    assertEquals("", KoLmafia.lastMessage);
-    Value ret = js.execute(null, null, true);
-    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
-    String retS = ret.toString();
-    assertEquals("354981", retS);
-  }
-
   @Test
   void getPlayerNameReturnsString() {
     ContactManager.registerPlayerId("heeheehee", "354981");
-
     var js = new JavascriptRuntime("getPlayerName(354981)");
-    assertEquals("", KoLmafia.lastMessage);
-    assertNotNull(js, "Javascript runtime is null, preventing test.");
+    assertNotNull(js, "JavascriptRuntime returned as null.");
     Value ret = js.execute(null, null, true);
     assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
@@ -59,35 +33,22 @@ public class AshInteropTest {
   @Test
   void getPlayerIdInvertsGetPlayerName() {
     ContactManager.registerPlayerId("heeheehee", "354981");
-
-    var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
-    assertEquals("", KoLmafia.lastMessage);
-
+    var js = new JavascriptRuntime("getPlayerId(getPlayerName(354981))");
+    assertNotNull(js, "JavascriptRuntime returned as null.");
     Value ret = js.execute(null, null, true);
     assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
-    // currently fails with retS 354981.0
-    assertEquals("heeheehee", retS);
-  }
-
-  @Test
-  void getPlayerIdNameCoercesArgumentToInt() {
-    ContactManager.registerPlayerId("heeheehee", "354981");
-
-    var js = new JavascriptRuntime("getPlayerName(354981.0)");
-    assertEquals("", KoLmafia.lastMessage);
-    assertEquals(new Value("heeheehee"), js.execute(null, null, true));
+    assertEquals("354981", retS);
   }
 
   @Test
   void getPlayerNameInvertsGetPlayerId() {
     ContactManager.registerPlayerId("heeheehee", "354981");
-
-    var js = new JavascriptRuntime("getPlayerId(getPlayerName(354981).toString())");
-    assertEquals("", KoLmafia.lastMessage);
+    var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
+    assertNotNull(js, "JavascriptRuntime returned as null.");
     Value ret = js.execute(null, null, true);
+    assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
-    assertEquals("354981", retS);
+    assertEquals("heeheehee", retS);
   }
-   */
 }
