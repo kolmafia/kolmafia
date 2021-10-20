55e431fcfab6b2f79fff484c8f374e7302f60eee
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-19 20:34:52 -0400

    Update AshInteropTest.java
    These seem to behave locally.  Push and see...

--- test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
+++ test/net/sourceforge/kolmafia/textui/javascript/AshInteropTest.java
@@ -2,26 +2,36 @@ package net.sourceforge.kolmafia.textui.javascript;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.session.ContactManager;
 import net.sourceforge.kolmafia.textui.parsetree.Value;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AshInteropTest {
+
+  @BeforeEach
+  void clearMessage() {
+    KoLmafia.lastMessage = "";
+  }
 
   @Test
   void getPlayerIdReturnsInt() {
     ContactManager.registerPlayerId("heeheehee", "354981");
     var js = new JavascriptRuntime("getPlayerId(\"heeheehee\")");
+    // before clearing lastMessage before the test, it wasn't empty
+    String x = KoLmafia.getLastMessage();
+    assertEquals(x, "", "Last message not empty getting runtime.");
     assertNotNull(js, "JavascriptRuntime returned as null.");
     Value ret = js.execute(null, null, true);
+    x = KoLmafia.getLastMessage();
+    assertEquals(x, "", "Last message not empty executing.");
     assertNotNull(ret, "Javascript execute returns null instead of a result to be tested.");
     String retS = ret.toString();
     assertEquals("354981", retS);
   }
 
   @Test
-  @Disabled
   void getPlayerNameReturnsString() {
     ContactManager.registerPlayerId("heeheehee", "354981");
     var js = new JavascriptRuntime("getPlayerName(354981)");
@@ -33,7 +43,6 @@ public class AshInteropTest {
   }
 
   @Test
-  @Disabled
   void getPlayerIdInvertsGetPlayerName() {
     ContactManager.registerPlayerId("heeheehee", "354981");
     var js = new JavascriptRuntime("getPlayerId(getPlayerName(354981))");
@@ -45,7 +54,6 @@ public class AshInteropTest {
   }
 
   @Test
-  @Disabled
   void getPlayerNameInvertsGetPlayerId() {
     ContactManager.registerPlayerId("heeheehee", "354981");
     var js = new JavascriptRuntime("getPlayerName(parseInt(getPlayerId(\"heeheehee\")))");
