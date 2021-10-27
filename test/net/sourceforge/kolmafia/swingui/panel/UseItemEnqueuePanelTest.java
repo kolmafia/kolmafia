77db63f1547d138e474dfa69a639b940da58527e
DamianDominoDavis <38517230+DamianDominoDavis@users.noreply.github.com>
2021-10-27 05:08:43 -0700

    item manager food panel button for universal seasoning (#123)
    * item manager food panel button for universal seasoning
    
    * Rename UniversalListener to UniversalSeasoningListener
    
    * Add tests for universal seasoning in UseItemEnqueuePanel
    
    Co-authored-by: Damian Domino Davis <(#2602681)>
    Co-authored-by: Samuel Gaus <sam@gaus.co.uk>

---
+++ test/net/sourceforge/kolmafia/swingui/panel/UseItemEnqueuePanelTest.java
@@ -0,0 +1,84 @@
+package net.sourceforge.kolmafia.swingui.panel;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Arrays;
+import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.preferences.Preferences;
+import net.sourceforge.kolmafia.session.InventoryManager;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class UseItemEnqueuePanelTest {
+  @BeforeAll
+  private static void injectPreferences() {
+    KoLCharacter.reset("fakeUserName");
+
+    // Now that we have a username set, we can edit preferences and have per-user defaults.
+    // But first, make sure we don't persist anything.
+    Preferences.setBoolean("saveSettingsOnSet", false);
+  }
+
+  @AfterAll
+  private static void cleanupSession() {
+    KoLCharacter.reset("");
+  }
+
+  private void loadInventory(String jsonInventory) {
+    try {
+      InventoryManager.parseInventory(new JSONObject(jsonInventory));
+    } catch (JSONException e) {
+      fail("Inventory parsing failed.");
+    }
+  }
+
+  @Test
+  public void universalSeasoningEnabledWhenAvailable() {
+    Preferences.setBoolean("_universalSeasoningUsed", false);
+    loadInventory("{\"10640\": \"1\"}");
+
+    var panel = new UseItemEnqueuePanel(true, false, false, null);
+    var buttonSearch =
+        Arrays.stream(panel.buttons)
+            .filter(b -> b.getText().equals("universal seasoning"))
+            .findFirst();
+    assertTrue(buttonSearch.isPresent());
+    var button = buttonSearch.get();
+    assertTrue(button.isEnabled());
+  }
+
+  @Test
+  public void universalSeasoningDisabledWhenUnavailable() {
+    Preferences.setBoolean("_universalSeasoningUsed", false);
+    loadInventory("{\"10640\": \"0\"}");
+
+    var panel = new UseItemEnqueuePanel(true, false, false, null);
+    var buttonSearch =
+        Arrays.stream(panel.buttons)
+            .filter(b -> b.getText().equals("universal seasoning"))
+            .findFirst();
+    assertTrue(buttonSearch.isPresent());
+    var button = buttonSearch.get();
+    assertFalse(button.isEnabled());
+  }
+
+  @Test
+  public void universalSeasoningDisabledWhenUsed() {
+    Preferences.setBoolean("_universalSeasoningUsed", true);
+    loadInventory("{\"10640\": \"1\"}");
+
+    var panel = new UseItemEnqueuePanel(true, false, false, null);
+    var buttonSearch =
+        Arrays.stream(panel.buttons)
+            .filter(b -> b.getText().equals("universal seasoning"))
+            .findFirst();
+    assertTrue(buttonSearch.isPresent());
+    var button = buttonSearch.get();
+    assertFalse(button.isEnabled());
+  }
+}
