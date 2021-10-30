5557eba246f68e76afd9a2ce153cc166234b9595
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-10-30 15:46:00 -0400

    Fix CodeQL security scan warnings
    Test exercises code to be changed.

---
+++ test/net/sourceforge/kolmafia/request/RelayRequestTest.java
@@ -0,0 +1,21 @@
+package net.sourceforge.kolmafia.request;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import org.junit.jupiter.api.Test;
+
+public class RelayRequestTest {
+
+  @Test
+  public void findVariousRelayFilesOnNot() {
+    File f;
+    f = RelayRequest.findRelayFile("thisIsNotAPipe");
+    assertNotNull(f, "Allowed to find a new file.");
+    assertFalse(f.exists(), "Found file is not supposed to exist.");
+    // f = RelayRequest.findRelayFile("thisIs..NotAPipe");
+    // assertNull(f, "Not supposed to find file with dots in name.");
+    f = RelayRequest.findRelayFile("barrel_sounds.js");
+    assertTrue(f.exists(), "Supposed to find file that exists.");
+  }
+}
