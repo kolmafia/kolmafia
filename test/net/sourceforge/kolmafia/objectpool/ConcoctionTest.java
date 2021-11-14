e44a7c633dae9cc3a2098e57576836571dbf049c
Jamie Adams <82782908+jaadams5@users.noreply.github.com>
2021-11-14 13:13:45 -0500

    Concoctions Contract (#207)
    * Maybe Faster
    
    * Typos
    
    * Update ConcoctionTest.java
    
    * Update ConcoctionTest.java
    
    * Change Structure
    
    * Fix Test
    
    * Lint
    
    Analyze suggested changes.  Trust the compiler to enforce type via signature
    
    * Prefix
    
    * Make local out of heap go away
    
    * Lint and Spotless
    
    * Link, spotless, first hook
    
    * Works
    
    * Probably have a data problem
    
    Name same but itemID not.
    
    * Moved to checkconcoctions
    
    * Update DebugDatabase.java
    
    * Update DebugDatabase.java
    
    * better
    
    * Typo
    
    * size issue fixed

--- test/net/sourceforge/kolmafia/objectpool/ConcoctionTest.java
+++ test/net/sourceforge/kolmafia/objectpool/ConcoctionTest.java
@@ -2,6 +2,9 @@ package net.sourceforge.kolmafia.objectpool;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import net.java.dev.spellcast.utilities.LockableListModel;
 import net.sourceforge.kolmafia.persistence.ConcoctionDatabase;
 import org.junit.jupiter.api.Test;
@@ -21,5 +24,35 @@ public class ConcoctionTest {
     int thing = usableList.getSize();
     usableList.sort();
     assertEquals(usableList.size(), thing);
+  }
+
+  @Test
+  public void exerciseSameNames() {
+    // Exercise compareTo and equals for Concoctions with the same name
+    // Lazy way to get two Concoctions with the same name
+    LockableListModel<Concoction> usables = ConcoctionDatabase.getUsables();
+    List<Concoction> Eds = new ArrayList<>();
+    Iterator<Concoction> ui = usables.iterator();
+    while (ui.hasNext()) {
+      Concoction x = ui.next();
+      if (x.toString().contains("Eye of Ed")) {
+        Eds.add(x);
+      }
+    }
+    assertEquals(2, Eds.size());
+    Concoction e1 = Eds.get(0);
+    Concoction e2 = Eds.get(1);
+    int c1 = e1.compareTo(e2);
+    int c2 = e2.compareTo(e1);
+    boolean b1 = e1.equals(e2);
+    boolean b2 = e2.equals(e1);
+    // they should not be equal by compareTo
+    assertNotEquals(0, c1);
+    // but they should be quasi symmetric
+    assertEquals(c1, -c2);
+    // they should not be equal by equals
+    assertFalse(b1);
+    assertEquals(b1, b2);
+    assertFalse(e1 == e2);
   }
 }
