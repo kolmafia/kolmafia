70ba6ac0c7c34ebef03987faebcfa8244f1f2255
Frederic Gagnon <gagnonfrederic9@gmail.com>
2021-11-14 18:12:30 -0500

    Language Server - Locations - Try (#209)
    * ValidScriptDataWithLocationTests
    
    * helper method
    
    * include xtext.xbase.lib during tests
    
    not needed for the tests to succeed.
    HOWEVER, if a test including a Location/Range/Position FAILS, it needs
    xtext/xbase/lib/util/ToStringBuilder to print the error,
    otherwise the error is a NoClassDefFoundError, making the failed test incomprehensible.
    
    * give a toString to ScriptData
    
    * tweak assertLocationEquals
    Range.equals(Object) checks for the exact class, so we need to make sure
    we don't submit it a Token
    
    * spotless...
    
    * Also prepare Evaluable
    
    This PR is very much ready and waiting for review.
    It's just that I'm making further PRs on my side in the meantime,
    and am sending content shared between many of them this way.
    
    * Spotless
    
    * Try locations
    
    * Description of the location in the test
    
    Co-authored-by: Jamie Adams <82782908+jaadams5@users.noreply.github.com>

--- test/net/sourceforge/kolmafia/textui/parsetree/TryTest.java
+++ test/net/sourceforge/kolmafia/textui/parsetree/TryTest.java
@@ -2,8 +2,10 @@ package net.sourceforge.kolmafia.textui.parsetree;
 
 import static net.sourceforge.kolmafia.textui.ScriptData.invalid;
 import static net.sourceforge.kolmafia.textui.ScriptData.valid;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 import net.sourceforge.kolmafia.textui.ParserTest;
 import net.sourceforge.kolmafia.textui.ScriptData;
@@ -32,7 +34,14 @@ public class TryTest {
             "try-finally",
             "try {} finally {}",
             Arrays.asList("try", "{", "}", "finally", "{", "}"),
-            Arrays.asList("1-1", "1-5", "1-6", "1-8", "1-16", "1-17")));
+            Arrays.asList("1-1", "1-5", "1-6", "1-8", "1-16", "1-17"),
+            scope -> {
+              List<Command> commands = scope.getCommandList();
+
+              Try tryCommand = assertInstanceOf(Try.class, commands.get(0));
+              // From the "try" up to the last token of the "finally"'s command
+              ParserTest.assertLocationEquals(1, 1, 1, 18, tryCommand.getLocation());
+            }));
   }
 
   @ParameterizedTest
