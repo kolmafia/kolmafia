cec434f9fa464a5d19de8ad360f9d79dc8796841
Frederic Gagnon <gagnonfrederic9@gmail.com>
2021-11-16 06:09:09 -0500

    ScriptState locations (#217)

--- test/net/sourceforge/kolmafia/textui/parsetree/LoopContinueTest.java
+++ test/net/sourceforge/kolmafia/textui/parsetree/LoopContinueTest.java
@@ -2,8 +2,10 @@ package net.sourceforge.kolmafia.textui.parsetree;
 
 import static net.sourceforge.kolmafia.textui.ScriptData.invalid;
 import static net.sourceforge.kolmafia.textui.ScriptData.valid;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 import net.sourceforge.kolmafia.textui.ParserTest;
 import net.sourceforge.kolmafia.textui.ScriptData;
@@ -18,7 +20,16 @@ public class LoopContinueTest {
             "continue inside while-loop",
             "while (true) continue;",
             Arrays.asList("while", "(", "true", ")", "continue", ";"),
-            Arrays.asList("1-1", "1-7", "1-8", "1-12", "1-14", "1-22")),
+            Arrays.asList("1-1", "1-7", "1-8", "1-12", "1-14", "1-22"),
+            scope -> {
+              List<Command> commands = scope.getCommandList();
+
+              WhileLoop whileLoop = assertInstanceOf(WhileLoop.class, commands.get(0));
+              Scope whileScope = whileLoop.getScope();
+              LoopContinue loopContinue =
+                  assertInstanceOf(LoopContinue.class, whileScope.getCommandList().get(0));
+              ParserTest.assertLocationEquals(1, 14, 1, 22, loopContinue.getLocation());
+            }),
         invalid(
             "continue inside switch",
             "switch (true) { default: continue; }",
