c3d362a7dc375bb71576af341a63b9deb9be5a84
Frederic Gagnon <gagnonfrederic9@gmail.com>
2021-11-30 13:33:33 -0500

    CompositeReference error locations (#287)

--- src/net/sourceforge/kolmafia/textui/Parser.java
+++ src/net/sourceforge/kolmafia/textui/Parser.java
@@ -3722,21 +3722,28 @@ public class Parser {
         parseAggregate = true;
 
         if (!(type instanceof AggregateType)) {
+          Location location = Parser.makeLocation(current.getLocation(), this.peekPreviousToken());
+          String message;
           if (indices.isEmpty()) {
-            throw this.parseException("Variable '" + var.getName() + "' cannot be indexed");
+            message = "Variable '" + var.getName() + "' cannot be indexed";
           } else {
-            throw this.parseException("Too many keys for '" + var.getName() + "'");
+            message = "Too many keys for '" + var.getName() + "'";
           }
+          throw this.parseException(location, message);
         }
 
         AggregateType atype = (AggregateType) type;
         index = this.parseExpression(scope);
         if (index == null) {
-          throw this.parseException("Index for '" + current.getName() + "' expected");
+          Location errorLocation = this.makeLocation(this.currentToken());
+
+          throw this.parseException(
+              errorLocation, "Index for '" + current.getName() + "' expected");
         }
 
         if (!index.getType().getBaseType().equals(atype.getIndexType().getBaseType())) {
           throw this.parseException(
+              index.getLocation(),
               "Index for '"
                   + current.getName()
                   + "' has wrong data type "
@@ -3759,7 +3766,7 @@ public class Parser {
 
         type = type.asProxy();
         if (!(type instanceof RecordType)) {
-          throw this.parseException("Record expected");
+          throw this.parseException(current.getLocation(), "Record expected");
         }
 
         RecordType rtype = (RecordType) type;
@@ -3768,14 +3775,14 @@ public class Parser {
         if (this.parseIdentifier(field.content)) {
           this.readToken(); // read name
         } else {
-          throw this.parseException("Field name expected");
+          throw this.parseException(field, "Field name expected");
         }
 
         index = Value.locate(this.makeLocation(field), rtype.getFieldIndex(field.content));
         if (index != null) {
           type = rtype.getDataType(index);
         } else {
-          throw this.parseException("Invalid field name '" + field + "'");
+          throw this.parseException(field, "Invalid field name '" + field + "'");
         }
       }
 
