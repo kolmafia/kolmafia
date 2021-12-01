d2f92ac2ee8c05deb256e118de1a3e8a7ea38736
fredg1 <gagnonfrederic9@gmail.com>
2021-12-01 12:43:43 -0500

    FunctionReturn error locations

--- test/net/sourceforge/kolmafia/textui/parsetree/FunctionReturnTest.java
+++ test/net/sourceforge/kolmafia/textui/parsetree/FunctionReturnTest.java
@@ -22,10 +22,11 @@ public class FunctionReturnTest {
             // This has been permitted historically...
             "return outside function",
             "return;",
-            // "Return needs null value"
-            "Cannot return when outside of a function"
-            // Arrays.asList( "return", ";" )
-            ),
+            // "Return needs null value",
+            "Cannot return when outside of a function",
+            "char 1 to char 7"),
+        // Arrays.asList("return", ";"),
+        // Arrays.asList("1-1", "1-7")),
         valid(
             "return void from void function",
             "void f() { return; }",
@@ -70,16 +71,26 @@ public class FunctionReturnTest {
             "int f() {}",
             "Missing return value",
             "char 5 to char 8"),
-        invalid("return void from int function", "int f() { return; }", "Return needs int value"),
+        invalid(
+            "return void from int function",
+            "int f() { return; }",
+            "Return needs int value",
+            "char 11 to char 17"),
         invalid(
             "return string from int function",
             "int f() { return 'str'; }",
-            "Cannot return string value from int function"),
+            "Cannot return string value from int function",
+            "char 18 to char 23"),
         invalid(
             "return int from void function",
             "void f() { return 1; }",
-            "Cannot return a value from a void function"),
-        invalid("non-expression return", "int f() { return (); }", "Expression expected"));
+            "Cannot return a value from a void function",
+            "char 19 to char 20"),
+        invalid(
+            "non-expression return",
+            "int f() { return ?; }",
+            "Expression expected",
+            "char 18 to char 19"));
   }
 
   @ParameterizedTest
