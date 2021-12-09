3e6b9e4c5263ad9d53a5567d7c9e18dd021ac78b
Samuel Gaus <sam@gaus.co.uk>
2021-12-09 21:48:54 +0000

    Fix reagnimated gnome stealing the valour for extra adventures from other sources (#323)
    Also add tests

--- test/net/sourceforge/kolmafia/request/FightRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FightRequestTest.java
@@ -2,13 +2,41 @@ package net.sourceforge.kolmafia.request;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import net.sourceforge.kolmafia.FamiliarData;
+import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.MonsterData;
+import net.sourceforge.kolmafia.objectpool.FamiliarPool;
+import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.persistence.MonsterDatabase;
+import net.sourceforge.kolmafia.preferences.Preferences;
+import net.sourceforge.kolmafia.session.EquipmentManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /** Coverage driven collection of tests for FightRequest. */
 public class FightRequestTest {
   private final FightRequest fr = FightRequest.INSTANCE;
+
+  @BeforeEach
+  public void beforeEach() {
+    KoLCharacter.reset("Test Character");
+  }
+
+  private void parseCombatData(String path, String location, String encounter) throws IOException {
+    String html = Files.readString(Paths.get(path));
+    FightRequest.updateCombatData(location, encounter, html);
+  }
+
+  private void parseCombatData(String path, String location) throws IOException {
+    parseCombatData(path, location, null);
+  }
+
+  private void parseCombatData(String path) throws IOException {
+    parseCombatData(path, null);
+  }
 
   @Test
   public void theProvidedInstanceShouldExistAndReturnSomeThings() {
@@ -50,5 +78,26 @@ public class FightRequestTest {
     assertFalse(FightRequest.inMultiFight);
     // Twiddle
     FightRequest.updateCombatData(null, null, "You twiddle your thumbs.");
+  }
+
+  @Test
+  public void gnomeAdv() throws IOException {
+    var familiar = FamiliarData.registerFamiliar(FamiliarPool.REAGNIMATED_GNOME, 1);
+    KoLCharacter.setFamiliar(familiar);
+    EquipmentManager.setEquipment(EquipmentManager.FAMILIAR, ItemPool.get(ItemPool.GNOMISH_KNEE));
+
+    assertEquals(0, Preferences.getInteger("_gnomeAdv"));
+    parseCombatData("request/test_fight_gnome_adv.html");
+    assertEquals(1, Preferences.getInteger("_gnomeAdv"));
+  }
+
+  @Test
+  public void mafiaThumbRingAdvs() throws IOException {
+    assertEquals(0, Preferences.getInteger("_mafiaThumbRingAdvs"));
+    parseCombatData("request/test_fight_mafia_thumb_ring.html");
+    assertEquals(1, Preferences.getInteger("_mafiaThumbRingAdvs"));
+
+    // Regression test for thumb ring adventures being picked up as Reagnimated Gnome adventures
+    assertEquals(0, Preferences.getInteger("_gnomeAdv"));
   }
 }
