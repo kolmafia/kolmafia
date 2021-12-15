93ebdb23476502fffe49e1641c636ff3cfba42ea
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-12-15 14:38:14 -0500

    Update MoodManagerTest.java

--- test/net/sourceforge/kolmafia/moods/MoodManagerTest.java
+++ test/net/sourceforge/kolmafia/moods/MoodManagerTest.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.KoLConstants;
@@ -97,6 +98,7 @@ class MoodManagerTest {
     MoodManager.setMood("xyzzy");
     assertTrue(MoodManager.getTriggers("xyzzy").isEmpty(), "Triggers unexpectedly defined");
     assertTrue(MoodManager.getTriggers().isEmpty(), "Triggers unexpectedly defined");
+    assertEquals(0L, MoodManager.getMaintenanceCost(), "Unexpected cost");
   }
 
   @Test
@@ -110,15 +112,18 @@ class MoodManagerTest {
       }
     }
     MoodManager.deleteCurrentMood();
-    MoodManager.setMood("xyzzy extends default, nothing");
-    moods = MoodManager.getAvailableMoods();
-    for (Mood moo : moods) {
-      if (moo.getName().contains("xyzzy")) {
-        assertTrue(moo.getParentNames().contains("default"), "Extend did not set parent");
-        assertTrue(moo.getParentNames().contains("nothing"), "Extend did not set parent");
+    // Do this twice because the second time is a different path because mood already there
+    for (int i = 0; i < 2; i++) {
+      MoodManager.setMood("xyzzy extends default, nothing");
+      moods = MoodManager.getAvailableMoods();
+      for (Mood moo : moods) {
+        if (moo.getName().contains("xyzzy")) {
+          assertTrue(moo.getParentNames().contains("default"), "Extend did not set parent");
+          assertTrue(moo.getParentNames().contains("nothing"), "Extend did not set parent");
+        }
       }
     }
-  }
+   }
 
   @Test
   public void itShouldLoadFromFile() {
@@ -135,5 +140,27 @@ class MoodManagerTest {
     KoLCharacter.addAvailableSkill(SkillDatabase.getSkillId("singer's faithful ocelot"));
     // Get the cost of maintaining the current mood
     assertEquals(60L, MoodManager.getMaintenanceCost(), "Unexpected cost");
+  }
+
+  @Test
+  public void itShouldAddAndRemoveTriggers() {
+    resetCharAndPrefs();
+    assertTrue(copyMoodFile(), "Could not copy file");
+    MoodManager.loadSettings();
+    Preferences.setString("currentMood", "meatdrop");
+    List<MoodTrigger> before = MoodManager.getTriggers();
+    assertEquals(8, before.size(), "Unexpected triggers");
+    // Make and add a new trigger
+    MoodTrigger newTrigger = MoodManager.addTrigger("gain_effect", "beaten up", "abort");
+    List<MoodTrigger> after = MoodManager.getTriggers();
+    assertEquals(9, after.size(), "Trigger not added");
+    assertFalse(before.contains(newTrigger), "Unexpected duplication of triggers");
+    assertTrue(after.contains(newTrigger), "Unexpected duplication of triggers");
+    Collection<MoodTrigger> collection = new ArrayList<MoodTrigger>();
+    collection.add(newTrigger);
+    MoodManager.removeTriggers(collection);
+    after = MoodManager.getTriggers();
+    assertEquals(8, after.size(), "Unexpected triggers");
+    assertFalse(after.contains(newTrigger), "Unexpected duplication of triggers");
   }
 }
