917a22c7d41c91dcefdcd6a17ef130e8560c120e
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2021-12-16 16:06:26 -0500

    working

--- test/net/sourceforge/kolmafia/moods/MoodManagerTest.java
+++ test/net/sourceforge/kolmafia/moods/MoodManagerTest.java
@@ -3,7 +3,9 @@ package net.sourceforge.kolmafia.moods;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -19,6 +21,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class MoodManagerTest {
 
@@ -52,6 +55,68 @@ class MoodManagerTest {
     } catch (Exception e) {
       return false;
     }
+  }
+
+  public BufferedReader mockedReader() throws IOException {
+    BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
+    Mockito.when(bufferedReader.readLine())
+        .thenReturn(
+            "[ apathetic ]",
+            "",
+            "[ default ]",
+            "gain_effect beaten up => uneffect Beaten Up",
+            "lose_effect empathy => cast 1 empathy of the newt",
+            "lose_effect leash of linguini => cast 1 leash of linguini",
+            "lose_effect singer's faithful ocelot => cast 1 singer's faithful ocelot",
+            "",
+            "[ meatdrop ]",
+            "unconditional => MercenaryMood.ash",
+            "gain_effect just the best anapests => uneffect Just the Best Anapests",
+            "lose_effect disco leer => cast 1 disco leer",
+            "lose_effect empathy => cast 1 empathy of the newt",
+            "lose_effect fat leon's phat loot lyric => cast 1 fat leon's phat loot lyric",
+            "lose_effect leash of linguini => cast 1 leash of linguini",
+            "lose_effect polka of plenty => cast 1 the polka of plenty",
+            "lose_effect singer's faithful ocelot => cast 1 singer's faithful ocelot",
+            "",
+            "[ nuts ]",
+            "unconditional => MercenaryMood.ash",
+            "gain_effect beaten up => abort",
+            "gain_effect just the best anapests => uneffect Just the Best Anapests",
+            "lose_effect mixed nutrients => use 1 tiny handful of mixed nuts",
+            "",
+            "[ omniquest ]",
+            "unconditional => MercenaryMood.ash",
+            "gain_effect beaten up => uneffect Beaten Up",
+            "gain_effect just the best anapests => uneffect Just the Best Anapests",
+            "lose_effect disco fever => cast 1 disco fever",
+            "",
+            "[ oq_hp ]",
+            "unconditional => MercenaryMood.ash",
+            "lose_effect big => cast 1 get big",
+            "lose_effect mariachi mood => cast 1 moxie of the mariachi",
+            "lose_effect patience of the tortoise => cast 1 patience of the tortoise",
+            "lose_effect phorcefullness => use either 1 philter of phorce, 1 evil philter of phorce",
+            "lose_effect power ballad of the arrowsmith => cast 1 the power ballad of the arrowsmith",
+            "lose_effect rage of the reindeer => cast 1 rage of the reindeer",
+            "lose_effect reptilian fortitude => cast 1 reptilian fortitude",
+            "lose_effect saucemastery => cast 1 sauce contemplation",
+            "lose_effect seal clubbing frenzy => cast 1 seal clubbing frenzy",
+            "lose_effect stevedave's shanty of superiority => cast 1 stevedave's shanty of superiority",
+            "",
+            "[ stannius_spaaace ]",
+            "gain_effect beaten up => abort",
+            "gain_effect just the best anapests => uneffect Just the Best Anapests",
+            "lose_effect empathy => cast 1 empathy of the newt",
+            "lose_effect fresh scent => use 1 chunk of rock salt",
+            "lose_effect leash of linguini => cast 1 leash of linguini",
+            "lose_effect smooth movements => cast 1 smooth movement",
+            "lose_effect springy fusilli => cast 1 springy fusilli",
+            "lose_effect the sonata of sneakiness => cast 1 the sonata of sneakiness",
+            "lose_effect transpondent => use 1 transporter transponder",
+            "",
+            null);
+    return bufferedReader;
   }
 
   @Test
@@ -127,12 +192,12 @@ class MoodManagerTest {
   }
 
   @Test
-  public void itShouldLoadFromFile() {
+  public void itShouldLoadFromFile() throws IOException {
     // Copy file from data to settings
-    assertTrue(copyMoodFile(), "Could not copy file");
+    // assertTrue(copyMoodFile(), "Could not copy file");
     // Load settings and set mood preference
     Preferences.setString("currentMood", "meatdrop");
-    MoodManager.loadSettings();
+    MoodManager.loadSettings(mockedReader());
     // Give Character skills
     KoLCharacter.addAvailableSkill(SkillDatabase.getSkillId("empathy of the newt"));
     KoLCharacter.addAvailableSkill(SkillDatabase.getSkillId("fat leon's phat loot lyric"));
@@ -144,10 +209,10 @@ class MoodManagerTest {
   }
 
   @Test
-  public void itShouldAddAndRemoveTriggers() {
+  public void itShouldAddAndRemoveTriggers() throws IOException {
     initializeCharPrefs();
-    assertTrue(copyMoodFile(), "Could not copy file");
-    MoodManager.loadSettings();
+    // assertTrue(copyMoodFile(), "Could not copy file");
+    MoodManager.loadSettings(mockedReader());
     MoodManager.setMood("meatdrop");
     Preferences.setString("currentMood", "meatdrop");
     List<MoodTrigger> before = MoodManager.getTriggers("meatdrop");
