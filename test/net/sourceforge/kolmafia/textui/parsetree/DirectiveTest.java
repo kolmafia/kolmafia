e54a1ef32f485d96e7b9d84b2b315f47d693cb0a
fredg1 <gagnonfrederic9@gmail.com>
2021-12-17 01:27:53 -0500

    use BeforeAll instead of BeforeEach

--- test/net/sourceforge/kolmafia/textui/parsetree/DirectiveTest.java
+++ test/net/sourceforge/kolmafia/textui/parsetree/DirectiveTest.java
@@ -13,8 +13,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class DirectiveTest {
-  @BeforeEach
-  public void setRevision() {
+  @BeforeAll
+  public static void setRevision() {
     StaticEntity.overrideRevision(10000);
   }
 
