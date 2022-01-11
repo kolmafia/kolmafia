82cf231fc4c623b0488e8d0152ddea38c1556ff5
horrible little slime <69secret69email69@gmail.com>
2022-01-11 12:56:35 -0500

    gradlew

--- src/net/sourceforge/kolmafia/session/DaylightShavingsHelmetManager.java
+++ src/net/sourceforge/kolmafia/session/DaylightShavingsHelmetManager.java
@@ -2,7 +2,6 @@ package net.sourceforge.kolmafia.session;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import net.sourceforge.kolmafia.persistence.EffectDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
 
