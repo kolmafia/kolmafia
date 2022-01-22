9cf8b3573330be54eec375c7c73403f806fe351d
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2022-01-22 17:49:20 -0500

    Park

--- test/net/sourceforge/kolmafia/KoLAdventureTest.java
+++ test/net/sourceforge/kolmafia/KoLAdventureTest.java
@@ -1,8 +1,20 @@
 package net.sourceforge.kolmafia;
 
+import static internal.helpers.CompareContractValidator.checkForContractViolations;
 import static org.junit.jupiter.api.Assertions.*;
 
-class KoLAdventureTest {
-    
+import internal.helpers.CompareContractValidator;
+import java.util.List;
+import net.java.dev.spellcast.utilities.LockableListModel;
+import net.sourceforge.kolmafia.persistence.AdventureDatabase;
+import org.junit.jupiter.api.Test;
 
+class KoLAdventureTest {
+
+  @Test
+  public void comparableShouldHonorContract() {
+    LockableListModel<KoLAdventure> kolAdventures = AdventureDatabase.getAsLockableListModel();
+    List<CompareContractValidator.Violator> badOnes = checkForContractViolations(kolAdventures);
+    assertEquals(0, badOnes.size(), "Contract not met.");
+  }
 }
