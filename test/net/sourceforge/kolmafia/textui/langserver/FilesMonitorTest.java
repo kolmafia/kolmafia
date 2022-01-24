ce686f3a3c6696bab9599bf69da9d79602a73716
fredg1 <gagnonfrederic9@gmail.com>
2022-01-23 19:47:26 -0500

    new tests

--- test/net/sourceforge/kolmafia/textui/langserver/FilesMonitorTest.java
+++ test/net/sourceforge/kolmafia/textui/langserver/FilesMonitorTest.java
@@ -5,10 +5,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collections;
 import net.sourceforge.kolmafia.KoLConstants;
 import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.TextDocumentContentChangeEvent;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.eclipse.lsp4j.VersionedTextDocumentIdentifier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.ThrowingSupplier;
@@ -71,6 +79,142 @@ public class FilesMonitorTest extends AshLanguageServerTest {
                         && params.getDiagnostics().stream()
                             .noneMatch(
                                 diagnostic -> diagnostic.getSeverity() == DiagnosticSeverity.Error);
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+
+    // ---------- didOpen() test ----------
+
+    // Reset invocations...
+    Mockito.clearInvocations(client);
+
+    DidOpenTextDocumentParams openParams =
+        new DidOpenTextDocumentParams(
+            new TextDocumentItem(badScript.toURI().toString(), "ash", 1, "unknown_function()"));
+    proxyServer.getTextDocumentService().didOpen(openParams);
+
+    // Wait for diagnostics...
+    pauser.pause(3000);
+
+    Mockito.verify(client)
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(badScript)
+                        && params.getDiagnostics().stream()
+                            .anyMatch(
+                                diagnostic ->
+                                    diagnostic.getSeverity() == DiagnosticSeverity.Error
+                                        && diagnostic
+                                            .getMessage()
+                                            .startsWith(
+                                                "Function 'unknown_function( )' undefined."));
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+    // goodScript was not changed, and so was not updated.
+    Mockito.verify(client, Mockito.never())
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(goodScript);
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+
+    // ---------- didChange() test ----------
+
+    // Reset invocations...
+    Mockito.clearInvocations(client);
+
+    DidChangeTextDocumentParams changeParams =
+        new DidChangeTextDocumentParams(
+            new VersionedTextDocumentIdentifier(badScript.toURI().toString(), 2),
+            Collections.singletonList(
+                new TextDocumentContentChangeEvent("import " + goodScript.getName())));
+    proxyServer.getTextDocumentService().didChange(changeParams);
+
+    // Wait for diagnostics...
+    pauser.pause(3000);
+
+    Mockito.verify(client)
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(badScript);
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+    // goodScript was *imported* by the new badScript, and so *was* updated.
+    Mockito.verify(client)
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(goodScript);
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+
+    // ---------- didChange() test, obsolete version ----------
+
+    // Reset invocations...
+    Mockito.clearInvocations(client);
+
+    // Note how we're reverting to version 1
+    changeParams =
+        new DidChangeTextDocumentParams(
+            new VersionedTextDocumentIdentifier(badScript.toURI().toString(), 1),
+            Collections.singletonList(new TextDocumentContentChangeEvent("unknown_function()")));
+    proxyServer.getTextDocumentService().didChange(changeParams);
+
+    // Wait for diagnostics...
+    pauser.pause(3000);
+
+    // The version was lower than the one in memory, and so was ignored
+    Mockito.verify(client, Mockito.never()).publishDiagnostics(ArgumentMatchers.any());
+
+    // ---------- didClose() test ----------
+
+    // Reset invocations...
+    Mockito.clearInvocations(client);
+
+    DidCloseTextDocumentParams closeParams =
+        new DidCloseTextDocumentParams(new TextDocumentIdentifier(badScript.toURI().toString()));
+    proxyServer.getTextDocumentService().didClose(closeParams);
+
+    // Wait for diagnostics...
+    pauser.pause(3000);
+
+    // badScript should have returned to its pre-opened state.
+    // goodScript, no longer imported by our version of badScript, should also have been updated.
+    Mockito.verify(client)
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(badScript)
+                        && params.getDiagnostics().stream()
+                            .anyMatch(
+                                diagnostic -> diagnostic.getSeverity() == DiagnosticSeverity.Error);
+                  } catch (URISyntaxException e) {
+                    return false;
+                  }
+                }));
+    Mockito.verify(client)
+        .publishDiagnostics(
+            ArgumentMatchers.argThat(
+                params -> {
+                  try {
+                    return new File(new URI(params.getUri())).equals(goodScript);
                   } catch (URISyntaxException e) {
                     return false;
                   }
