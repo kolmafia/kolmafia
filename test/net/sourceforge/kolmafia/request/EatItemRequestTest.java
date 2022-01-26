70a67840b6a9e4ce10ed57a4935cf73868dc5aa1
Sam Gaus <sam@gaus.co.uk>
2022-01-26 21:59:55 +0000

    Reorganise EatItemRequestTest

--- test/net/sourceforge/kolmafia/request/EatItemRequestTest.java
+++ test/net/sourceforge/kolmafia/request/EatItemRequestTest.java
@@ -1,57 +1,57 @@
 package net.sourceforge.kolmafia.request;
 
+import static internal.helpers.Player.addItem;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.spy;
 
-import net.sourceforge.kolmafia.AdventureResult;
 import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.preferences.Preferences;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-class EatItemRequestTest extends RequestTestBase {
-
-  // We don't use @BeforeEach here because it's specific to milk-related tests.
-  private void milkSetup() {
+class EatItemRequestTest {
+  @BeforeEach
+  private void beforeEach() {
     // Simulate logging out and back in again.
-    KoLCharacter.reset("");
-    KoLCharacter.reset("milk user");
-    // Reset preferences to defaults.
-    AdventureResult.addResultToList(
-        KoLConstants.inventory, ItemPool.get(ItemPool.MILK_OF_MAGNESIUM));
-    // A food item that we can eat.
-    AdventureResult.addResultToList(KoLConstants.inventory, ItemPool.get(ItemPool.TOMATO));
+    KoLCharacter.reset("EatItemRequest");
+    Preferences.reset("EatItemRequest");
   }
 
-  @Test
-  void skipMilkNagIfAlreadyUsedToday() {
-    milkSetup();
-    assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
-    assertTrue(EatItemRequest.askAboutMilk("tomato", 1));
-  }
+  @Nested
+  class MilkOfMagnesium extends RequestTestBase {
+    @BeforeEach
+    private void milkSetup() {
+      addItem(ItemPool.MILK_OF_MAGNESIUM);
+      addItem(ItemPool.TOMATO);
+    }
 
-  @Test
-  void skipMilkNagIfAlreadyActive() {
-    milkSetup();
-    Preferences.setBoolean("milkOfMagnesiumActive", true);
-    assertTrue(EatItemRequest.askAboutMilk("tomato", 1));
-  }
+    @Test
+    void skipMilkNagIfAlreadyUsedToday() {
+      assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
+      assertTrue(EatItemRequest.askAboutMilk("tomato", 1));
+    }
 
-  @Test
-  void milkResponseSetsPreference() {
-    milkSetup();
+    @Test
+    void skipMilkNagIfAlreadyActive() {
+      Preferences.setBoolean("milkOfMagnesiumActive", true);
+      assertTrue(EatItemRequest.askAboutMilk("tomato", 1));
+    }
 
-    assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
-    Preferences.setBoolean("milkOfMagnesiumActive", true);
+    @Test
+    void milkResponseSetsPreference() {
+      assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
+      Preferences.setBoolean("milkOfMagnesiumActive", true);
 
-    EatItemRequest req = spy(new EatItemRequest(ItemPool.get(ItemPool.TOMATO)));
-    // Wiki claims that this message is indeed "You stomach ..."
-    expectSuccess(req, "Satisfied, you let loose a nasty magnesium-flavored belch.");
-    req.run();
+      EatItemRequest req = spy(new EatItemRequest(ItemPool.get(ItemPool.TOMATO)));
+      // Wiki claims that this message is indeed "You stomach ..."
+      expectSuccess(req, "Satisfied, you let loose a nasty magnesium-flavored belch.");
+      req.run();
 
-    assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
-    assertFalse(Preferences.getBoolean("milkOfMagnesiumActive"));
+      assertFalse(Preferences.getBoolean("_milkOfMagnesiumUsed"));
+      assertFalse(Preferences.getBoolean("milkOfMagnesiumActive"));
+    }
   }
 }
