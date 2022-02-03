88656ebdd96d1a34da6105faa0bef118025acbe7
thedictatorkol <77397661+thedictatorkol@users.noreply.github.com>
2022-02-03 23:31:24 +0100

    Update CursedMagnifyingGlassManager.java

--- src/net/sourceforge/kolmafia/session/CursedMagnifyingGlassManager.java
+++ src/net/sourceforge/kolmafia/session/CursedMagnifyingGlassManager.java
@@ -17,7 +17,7 @@ public class CursedMagnifyingGlassManager {
           Map.entry("Nine ravens burst from a nearby tree and take to the sky.", 5),
           Map.entry("Eight rats scurry out from behind a nearby bush, startling you.", 6),
           Map.entry("To your left, seven stray dogs fight over a scrap of carrion.", 7),
-          Map.entry("A creepy-looking little girl walks up and whispers in your ear.  &quot;Six.&quot;", 8)
+          Map.entry("A creepy-looking little girl walks up and whispers in your ear.  &quot;Six.&quot;", 8),
           Map.entry(
               "You look at your left hand and notice, to your horror, that you have five fingers.  Oh, wait, that's the normal number.  Never mind.",
               9),
