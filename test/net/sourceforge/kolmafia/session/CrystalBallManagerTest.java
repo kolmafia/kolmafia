f4d76257f5629bcea817fa5ece688364d7530253
Sam Gaus <sam@gaus.co.uk>
2022-02-04 11:11:48 +0000

    Fix testing for orb change

--- test/net/sourceforge/kolmafia/session/CrystalBallManagerTest.java
+++ test/net/sourceforge/kolmafia/session/CrystalBallManagerTest.java
@@ -1,20 +1,26 @@
 package net.sourceforge.kolmafia.session;
 
+import static internal.helpers.Player.equip;
+import static internal.helpers.Player.setFamiliar;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import internal.helpers.Cleanups;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.MonsterData;
 import net.sourceforge.kolmafia.combat.MonsterStatusTracker;
+import net.sourceforge.kolmafia.objectpool.FamiliarPool;
 import net.sourceforge.kolmafia.persistence.MonsterDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CrystalBallManagerTest {
   static final MonsterData NAILER = MonsterDatabase.findMonster("smut orc nailer");
@@ -43,30 +49,50 @@ public class CrystalBallManagerTest {
     assertFalse(CrystalBallManager.isCrystalBallZone("The Defiled Niche"));
   }
 
-  @Test
-  public void crystalBallMonsterTestString() {
-    assertTrue(
-        CrystalBallManager.isCrystalBallMonster("smut orc nailer", "The Smut Orc Logging Camp"));
-    assertTrue(CrystalBallManager.isCrystalBallMonster("party skelteon", "The Defiled Nook"));
-    assertFalse(CrystalBallManager.isCrystalBallMonster("spiny skelelton", "The Defiled Niche"));
+  @ParameterizedTest
+  @CsvSource({
+    "smut orc nailer, The Smut Orc Logging Camp, true",
+    "party skelteon, The Defiled Nook, true",
+    "spiny skelelton, The Defiled Niche, false"
+  })
+  public void canIdentifyPredictedMonster(String monsterName, String locationName, String result) {
+    var cleanups =
+        new Cleanups(
+            setFamiliar(FamiliarPool.BADGER),
+            equip(EquipmentManager.FAMILIAR, "miniature crystal ball"));
+    try (cleanups) {
+      // From String
+      assertEquals(
+          Boolean.parseBoolean(result),
+          CrystalBallManager.isCrystalBallMonster(monsterName, locationName));
+      // Or from MonsterData
+      var monster = MonsterDatabase.findMonster(monsterName);
+      assertEquals(
+          Boolean.parseBoolean(result),
+          CrystalBallManager.isCrystalBallMonster(monster, locationName));
+    }
   }
 
   @Test
-  public void crystalBallMonsterTestMonsterData() {
-    assertTrue(CrystalBallManager.isCrystalBallMonster(NAILER, "The Smut Orc Logging Camp"));
-    assertTrue(CrystalBallManager.isCrystalBallMonster(SKELTEON, "The Defiled Nook"));
-    assertFalse(CrystalBallManager.isCrystalBallMonster(SKELELTON, "The Defiled Niche"));
+  public void checkMonsterIsNotPredictedWithoutEquippingOrb() {
+    assertFalse(CrystalBallManager.isCrystalBallMonster(NAILER, "The Smut Orc Logging Camp"));
   }
 
   @Test
   public void crystalBallMonsterTestNextEncounter() {
-    assertFalse(CrystalBallManager.isCrystalBallMonster());
+    var cleanups =
+        new Cleanups(
+            setFamiliar(FamiliarPool.BADGER),
+            equip(EquipmentManager.FAMILIAR, "miniature crystal ball"));
+    try (cleanups) {
+      assertFalse(CrystalBallManager.isCrystalBallMonster());
 
-    MonsterStatusTracker.setNextMonster(SKELTEON);
-    assertFalse(CrystalBallManager.isCrystalBallMonster());
+      MonsterStatusTracker.setNextMonster(SKELTEON);
+      assertFalse(CrystalBallManager.isCrystalBallMonster());
 
-    Preferences.setString("nextAdventure", "The Defiled Nook");
-    assertTrue(CrystalBallManager.isCrystalBallMonster());
+      Preferences.setString("nextAdventure", "The Defiled Nook");
+      assertTrue(CrystalBallManager.isCrystalBallMonster());
+    }
   }
 
   @Test
