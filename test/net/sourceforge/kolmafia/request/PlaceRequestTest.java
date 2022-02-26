ac637c0f86e086a427b73710d0077dd0b1f72e16
BadHorseMonkey <58645293+BadHorseMonkey@users.noreply.github.com>
2022-02-26 01:25:38 -0600

    spotless

--- test/net/sourceforge/kolmafia/request/PlaceRequestTest.java
+++ test/net/sourceforge/kolmafia/request/PlaceRequestTest.java
@@ -5,36 +5,36 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import net.sourceforge.kolmafia.KoLCharacter;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import net.sourceforge.kolmafia.AdventureResult;
+import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.Modifiers;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.preferences.Preferences;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PlaceRequestTest {
 
-        // These need to be before and after each because leakage has been observed between tests
-        // in this class.
-        @BeforeEach
-        public void initializeCharPrefs() {
-                KoLCharacter.reset( "fakePrefUser");
-                KoLCharacter.reset(true);
-                Preferences.saveSettingsToFile = false;
-        }
+  // These need to be before and after each because leakage has been observed between tests
+  // in this class.
+  @BeforeEach
+  public void initializeCharPrefs() {
+    KoLCharacter.reset("fakePrefUser");
+    KoLCharacter.reset(true);
+    Preferences.saveSettingsToFile = false;
+  }
 
-        @AfterEach
-        public void resetCharAndPrefs() {
-                KoLCharacter.reset("");
-                KoLCharacter.reset(true);
-                KoLCharacter.setUserId(0);
-                Preferences.saveSettingsToFile = false;
-        }
+  @AfterEach
+  public void resetCharAndPrefs() {
+    KoLCharacter.reset("");
+    KoLCharacter.reset(true);
+    KoLCharacter.setUserId(0);
+    Preferences.saveSettingsToFile = false;
+  }
+
   @Test
   void itShouldSetTheToolbeltAsAFreePullInTTT() {
     // setup environment for test...
@@ -47,26 +47,34 @@ class PlaceRequestTest {
     AdventureResult.addResultToList(KoLConstants.storage, toolbelt);
 
     // check baseline condition
-    assertNotEquals(-1, storage.indexOf(toolbelt), "toolbelt should be in storage before TTT is available");
-    assertEquals(-1, freePulls.indexOf(toolbelt), "toolbelt should not be in Free Pulls before TTT is available");
+    assertNotEquals(
+        -1, storage.indexOf(toolbelt), "toolbelt should be in storage before TTT is available");
+    assertEquals(
+        -1,
+        freePulls.indexOf(toolbelt),
+        "toolbelt should not be in Free Pulls before TTT is available");
 
     PlaceRequest.parseResponse("http://server.fakepath/place=twitch", "tower");
 
-    assertTrue(Preferences.getBoolean("timeTowerAvailable"),"TTT is available");
+    assertTrue(Preferences.getBoolean("timeTowerAvailable"), "TTT is available");
 
-    // time-twitching toolbelt is a free pull if the time tower is available. Verify it is in correct storage list.
-    assertEquals(-1, storage.indexOf(toolbelt), "toolbelt should not be in storage when TTT is available");
-    assertNotEquals(-1, freePulls.indexOf(toolbelt), "toolbelt should be in Free Pulls when TTT is available");
+    // time-twitching toolbelt is a free pull if the time tower is available. Verify it is in
+    // correct storage list.
+    assertEquals(
+        -1, storage.indexOf(toolbelt), "toolbelt should not be in storage when TTT is available");
+    assertNotEquals(
+        -1, freePulls.indexOf(toolbelt), "toolbelt should be in Free Pulls when TTT is available");
 
     // reset the TTT
     PlaceRequest.parseResponse("http://server.fakepath/place=twitch", "temporal ether");
 
-    assertFalse(Preferences.getBoolean("timeTowerAvailable"),"TTT should not be available");
+    assertFalse(Preferences.getBoolean("timeTowerAvailable"), "TTT should not be available");
 
     // check after TTT disappears into the temporal ether
-    assertNotEquals(-1, storage.indexOf(toolbelt), "toolbelt should not be in storage after TTT fades");
-    assertEquals(-1, freePulls.indexOf(toolbelt), "toolbelt should not be in Free Pulls after TTT fades");
-
+    assertNotEquals(
+        -1, storage.indexOf(toolbelt), "toolbelt should not be in storage after TTT fades");
+    assertEquals(
+        -1, freePulls.indexOf(toolbelt), "toolbelt should not be in Free Pulls after TTT fades");
   }
 }
 
