c8c7a38935738538ee7026e58a4f394c92412584
horrible little slime <69secret69email69@gmail.com>
2022-04-19 13:45:38 -0400

    attempt to write tests

--- test/net/sourceforge/kolmafia/request/AdventureRequestTest.java
+++ test/net/sourceforge/kolmafia/request/AdventureRequestTest.java
@@ -1,9 +1,16 @@
 package net.sourceforge.kolmafia.request;
 
 import static internal.helpers.Player.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import net.sourceforge.kolmafia.persistence.AdventureQueueDatabase;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.KoLAdventure;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +38,28 @@ public class AdventureRequestTest {
     addEffect("Fishy");
     KoLCharacter.recalculateAdjustments();
     assertEquals(1, request.getAdventuresUsed());
+  }
+
+  @Test
+  void enqueueGregariousMonsters() throws IOException {
+    String html = Files.readString(Path.of("request/test_fight_gregarious_monster.html"));
+    AdventureRequest request = new AdventureRequest("Noob Cave", "adventure.php", "240");
+    request.responseText = html;
+    AdventureRequest.registerEncounter(request);
+    assertThat(
+        AdventureQueueDatabase.getZoneQueue(KoLAdventure.lastVisitedLocation()),
+        contains("Knob Goblin Embezzler"));
+  }
+
+  @Test
+  void dontEnqueueNongregariousSpecialMonsters() throws IOException {
+    String html = Files.readString(Path.of("request/test_fight_digitized_monster.html"));
+    AdventureRequest request = new AdventureRequest("Noob Cave", "adventure.php", "240");
+    request.responseText = html;
+    AdventureRequest.registerEncounter(request);
+    assertThat(
+        AdventureQueueDatabase.getZoneQueue(KoLAdventure.lastVisitedLocation())
+            .contains("Knob Goblin Embezzler"),
+        equalTo(false));
   }
 }
