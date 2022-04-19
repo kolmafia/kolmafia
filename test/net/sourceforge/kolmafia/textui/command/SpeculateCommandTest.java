312d31edef891e2ca4a8b8b692576310a78de504
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-04-19 17:58:16 +0100

    fix: speculate command loops

---
+++ test/net/sourceforge/kolmafia/textui/command/SpeculateCommandTest.java
@@ -0,0 +1,22 @@
+package net.sourceforge.kolmafia.textui.command;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+
+public class SpeculateCommandTest extends AbstractCommandTestBase {
+
+  public SpeculateCommandTest() {
+    this.command = "speculate";
+  }
+
+  @Test
+  public void mentionsChangedModifiers() {
+    var spec = execute("equip disco mask");
+
+    assertThat(spec, containsString("Moxie"));
+    assertThat(spec, containsString("Buffed Moxie"));
+  }
+
+}
