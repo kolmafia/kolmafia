defd1686285bbc147a099336e8e3823f7288767b
horrible little slime <69secret69email69@gmail.com>
2022-04-21 01:11:59 -0400

    spotless

--- test/net/sourceforge/kolmafia/request/CampgroundRequestTest.java
+++ test/net/sourceforge/kolmafia/request/CampgroundRequestTest.java
@@ -1,28 +1,28 @@
-import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.objectpool.ItemPool;
-import net.sourceforge.kolmafia.preferences.Preferences;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.preferences.Preferences;
+import net.sourceforge.kolmafia.request.CampgroundRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import net.sourceforge.kolmafia.request.CampgroundRequest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CampgroundRequestTest {
-    @BeforeEach
-    public void beforeEach() {
-      KoLCharacter.reset("CampgroundRequest");
-      Preferences.reset("CampgroundRequest");
-    }
+  @BeforeEach
+  public void beforeEach() {
+    KoLCharacter.reset("CampgroundRequest");
+    Preferences.reset("CampgroundRequest");
+  }
 
-    @Test
-    void canDetectExhaustedMedicineCabinet() throws IOException {
-        String html = Files.readString(Path.of("request/test_campground_medicine_cabinet_out_of_consults.html"));
-        CampgroundRequest.parseResponse("campground.php?action=workshed", html);
-        assertEquals(CampgroundRequest.getCurrentWorkshedItem().getItemId(), ItemPool.COLD_MEDICINE_CABINET);
-    }
-  
+  @Test
+  void canDetectExhaustedMedicineCabinet() throws IOException {
+    String html =
+        Files.readString(Path.of("request/test_campground_medicine_cabinet_out_of_consults.html"));
+    CampgroundRequest.parseResponse("campground.php?action=workshed", html);
+    assertEquals(
+        CampgroundRequest.getCurrentWorkshedItem().getItemId(), ItemPool.COLD_MEDICINE_CABINET);
+  }
 }
