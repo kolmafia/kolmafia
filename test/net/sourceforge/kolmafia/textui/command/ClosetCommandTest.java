c705eacd188b43405b83be76cebe3e4d3d89dee0
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-04-29 08:21:03 +0100

    feat: make zap command aware of counts (#732)
    * refactor: share fake client setup code
    
    * feat: allow using count with zap command
    
    * chore: spotless, lint

--- test/net/sourceforge/kolmafia/textui/command/ClosetCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/ClosetCommandTest.java
@@ -1,31 +1,26 @@
 package net.sourceforge.kolmafia.textui.command;
 
+import static internal.helpers.HttpClientWrapper.getRequests;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 
 import internal.helpers.Cleanups;
+import internal.helpers.HttpClientWrapper;
 import internal.helpers.Player;
 import internal.listeners.FakeListener;
-import internal.network.FakeHttpClientBuilder;
 import internal.network.RequestBodyReader;
-import java.net.http.HttpRequest;
-import java.util.List;
 import net.sourceforge.kolmafia.KoLConstants.MafiaState;
 import net.sourceforge.kolmafia.StaticEntity;
 import net.sourceforge.kolmafia.listener.PreferenceListenerRegistry;
-import net.sourceforge.kolmafia.request.GenericRequest;
-import net.sourceforge.kolmafia.utilities.HttpUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ClosetCommandTest extends AbstractCommandTestBase {
-
-  private final FakeHttpClientBuilder fakeClientBuilder = new FakeHttpClientBuilder();
-
-  private List<HttpRequest> getRequests() {
-    return fakeClientBuilder.client.getRequests();
-  }
 
   public ClosetCommandTest() {
     this.command = "closet";
@@ -33,10 +28,7 @@ public class ClosetCommandTest extends AbstractCommandTestBase {
 
   @BeforeEach
   public void initializeState() {
-    GenericRequest.sessionId = "closet"; // do "send" requests
-    HttpUtilities.setClientBuilder(() -> fakeClientBuilder);
-    GenericRequest.resetClient();
-    fakeClientBuilder.client.clear();
+    HttpClientWrapper.setupFakeClient();
     StaticEntity.setContinuationState(MafiaState.CONTINUE);
   }
 
--- test/net/sourceforge/kolmafia/textui/command/RefreshStatusCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/RefreshStatusCommandTest.java
@@ -1,32 +1,20 @@
 package net.sourceforge.kolmafia.textui.command;
 
+import static internal.helpers.HttpClientWrapper.getLastRequest;
+import static internal.helpers.HttpClientWrapper.getRequests;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
-import internal.network.FakeHttpClientBuilder;
-import java.net.http.HttpRequest;
-import java.util.List;
+import internal.helpers.HttpClientWrapper;
 import net.sourceforge.kolmafia.KoLConstants.MafiaState;
 import net.sourceforge.kolmafia.StaticEntity;
-import net.sourceforge.kolmafia.request.GenericRequest;
-import net.sourceforge.kolmafia.utilities.HttpUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RefreshStatusCommandTest extends AbstractCommandTestBase {
-
-  private final FakeHttpClientBuilder fakeClientBuilder = new FakeHttpClientBuilder();
-
-  private List<HttpRequest> getRequests() {
-    return fakeClientBuilder.client.getRequests();
-  }
-
-  private HttpRequest getLastRequest() {
-    return fakeClientBuilder.client.getLastRequest();
-  }
 
   public RefreshStatusCommandTest() {
     this.command = "refresh";
@@ -34,10 +22,7 @@ public class RefreshStatusCommandTest extends AbstractCommandTestBase {
 
   @BeforeEach
   public void initializeState() {
-    GenericRequest.sessionId = "refresh"; // do "send" requests
-    HttpUtilities.setClientBuilder(() -> fakeClientBuilder);
-    GenericRequest.resetClient();
-    fakeClientBuilder.client.clear();
+    HttpClientWrapper.setupFakeClient();
     StaticEntity.setContinuationState(MafiaState.CONTINUE);
   }
 
