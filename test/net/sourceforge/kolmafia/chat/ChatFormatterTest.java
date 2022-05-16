242900606dcc9c9387a5b0f73fd66909b89b8674
libraryaddict <github@lib.co.nz>
2022-05-16 12:55:20 +1200

    Chat Highlighting cleanup & simple test Highlighting could be loaded multiple times, yet would not be unloaded which could create one issue. It was also possible to highlight the same string multiple times. The code was confusing to read, and has been cleaned up a little.

---
+++ test/net/sourceforge/kolmafia/chat/ChatFormatterTest.java
@@ -0,0 +1,44 @@
+package net.sourceforge.kolmafia.chat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.Color;
+import net.sourceforge.kolmafia.preferences.Preferences;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ChatFormatterTest {
+  @BeforeEach
+  public void resetHighlights() {
+    Preferences.setString("highlightList", "");
+    StyledChatBuffer.initializeHighlights();
+  }
+
+  @Test
+  public void addHightlightTest() {
+    Color color = Color.BLACK;
+
+    ChatFormatter.addHighlighting("john of arc", color);
+    ChatFormatter.addHighlighting("john", color);
+    ChatFormatter.addHighlighting("prince john and the three stooges", color);
+
+    String expected =
+        "john of arc\n#000000\njohn\n#000000\nprince john and the three stooges\n#000000";
+
+    assertEquals(expected, Preferences.getString("highlightList"));
+  }
+
+  @Test
+  public void removeHighlightTest() {
+    Color color = Color.BLACK;
+
+    ChatFormatter.addHighlighting("john of arc", color);
+    ChatFormatter.addHighlighting("john", color);
+    ChatFormatter.addHighlighting("prince john and the three stooges", color);
+
+    ChatFormatter.removeHightlighting("john");
+    String expected = "john of arc\n#000000\nprince john and the three stooges\n#000000";
+
+    assertEquals(expected, Preferences.getString("highlightList"));
+  }
+}
