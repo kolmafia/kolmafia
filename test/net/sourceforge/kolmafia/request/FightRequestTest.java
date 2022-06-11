66e84f7e5a9c2933e622915c9190f3ab7a079af7
libraryaddict <libraryaddict@users.noreply.github.com>
2022-06-12 01:32:58 +1200

    Add _bellydancerPickpockets (#811)
    Co-authored-by: Chris Midgley <chris.midgley@dunecomputers.co.uk>

--- test/net/sourceforge/kolmafia/request/FightRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FightRequestTest.java
@@ -607,4 +607,18 @@ public class FightRequestTest {
     assertEquals(Preferences.getInteger("_juneCleaverSleaze"), 2);
     assertEquals(Preferences.getInteger("_juneCleaverFightsLeft"), 0);
   }
+
+  @Test
+  public void canTrackBellydancerPickpocket() throws IOException {
+    assertEquals(0, Preferences.getInteger("_bellydancerPickpockets"));
+
+    parseCombatData("request/test_fight_bellydancing_pickpocket_1.html");
+    assertEquals(1, Preferences.getInteger("_bellydancerPickpockets"));
+
+    parseCombatData("request/test_fight_bellydancing_pickpocket_2.html");
+    assertEquals(2, Preferences.getInteger("_bellydancerPickpockets"));
+
+    parseCombatData("request/test_fight_bellydancing_pickpocket_3.html");
+    assertEquals(3, Preferences.getInteger("_bellydancerPickpockets"));
+  }
 }
