b0b8a283dbac4213f8be3edecc5f76cf26d424bc
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-06-20 19:55:47 +0100

    refactor: remove Object[][] from BarrelPrayerCommand

--- src/net/sourceforge/kolmafia/textui/command/BarrelPrayerCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/BarrelPrayerCommand.java
@@ -19,20 +19,20 @@ public class BarrelPrayerCommand extends AbstractCommand {
   public static final int VIGOR = 3;
   public static final int BUFF = 4;
 
-  public static final Object[][] PRAYER =
-      new Object[][] {
-        {"protection", "barrel lid", PROTECTION},
-        {"glamour", "barrel hoop earring", GLAMOUR},
-        {"vigor", "bankruptcy barrel", VIGOR},
-        {"buff", "class buff", BUFF},
+  private record Prayer(String name, String reward, int id) {}
+
+  public static final Prayer[] PRAYER =
+      new Prayer[] {
+        new Prayer("protection", "barrel lid", PROTECTION),
+        new Prayer("glamour", "barrel hoop earring", GLAMOUR),
+        new Prayer("vigor", "bankruptcy barrel", VIGOR),
+        new Prayer("buff", "class buff", BUFF),
       };
 
   public static final int findPrayer(final String name) {
-    for (int i = 0; i < PRAYER.length; ++i) {
-      if (name.equalsIgnoreCase((String) PRAYER[i][0])
-          || name.equalsIgnoreCase((String) PRAYER[i][1])) {
-        Integer index = (Integer) PRAYER[i][2];
-        return index.intValue();
+    for (Prayer prayer : PRAYER) {
+      if (name.equalsIgnoreCase(prayer.name) || name.equalsIgnoreCase(prayer.reward)) {
+        return prayer.id;
       }
     }
 
