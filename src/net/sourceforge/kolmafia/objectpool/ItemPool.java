ccc695bfa3b50baa60defaa9be42e5e99d663c67
Samuel Gaus <sam@gaus.co.uk>
2022-07-14 21:50:03 +0100

    Track Homemade robot weight (#883)
    * Refactor UseItemRequestTest
    
    * Add tests for homemade robot
    
    * Make tests pass :)
    
    * Remove errant newline
    
    * Update test for fidoxene on unusual base weight fam
    
    Co-authored-by: Chris Midgley <chris.midgley@dunecomputers.co.uk>

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -1846,6 +1846,7 @@ public class ItemPool {
   public static final int LED_CLOCK = 6072;
   public static final int HAGGIS_SOCKS = 6080;
   public static final int CLASSY_MONKEY = 6097;
+  public static final int PILE_OF_USELESS_ROBOT_PARTS = 6100;
   public static final int CEO_OFFICE_CARD = 6116;
   public static final int STRANGE_GOGGLES = 6118;
   public static final int BONSAI_TREE = 6120;
