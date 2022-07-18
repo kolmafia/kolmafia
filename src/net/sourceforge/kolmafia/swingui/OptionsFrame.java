0d86b455850b30a482c2773ddfa3e68809cafd7c
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-07-18 12:55:08 -0400

    Add PreferenceButtonGroup for a set of radio buttons controlled by a setting

--- src/net/sourceforge/kolmafia/swingui/OptionsFrame.java
+++ src/net/sourceforge/kolmafia/swingui/OptionsFrame.java
@@ -1613,6 +1613,50 @@ public class OptionsFrame extends GenericFrame {
     public void focusGained(final FocusEvent e) {}
   }
 
+  public static class PreferenceButtonGroup extends JPanel implements Listener {
+    private final String pref;
+    private final SmartButtonGroup group;
+
+    public PreferenceButtonGroup(String pref, String message, String... buttons) {
+      this.pref = pref;
+      this.group = new SmartButtonGroup(this);
+
+      configure();
+      makeLayout(message, buttons);
+    }
+
+    private void configure() {
+      this.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
+      PreferenceListenerRegistry.registerPreferenceListener(pref, this);
+      this.group.setActionListener(
+          e -> Preferences.setInteger(pref, this.group.getSelectedIndex()));
+    }
+
+    private void makeLayout(String message, String... buttons) {
+      JLabel label = new JLabel(message);
+      this.add(label);
+      for (String button : buttons) {
+        this.group.add(new JRadioButton(button));
+      }
+
+      update();
+    }
+
+    public SmartButtonGroup getButtonGroup() {
+      return this.group;
+    }
+
+    @Override
+    public void update() {
+      this.group.setSelectedIndex(Preferences.getInteger(pref));
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+      return this.getPreferredSize();
+    }
+  }
+
   private static void addToolTip(JComponent component, String tooltip) {
     component.add(Box.createHorizontalStrut(3));
     JLabel label = new JLabel("[");
