a3039f9f578b608bb4177dbd845184041c0e4034
Sam Gaus <sam@gaus.co.uk>
2022-07-19 20:19:52 +0100

    Fix BasementRequesTests once and for all

--- test/net/sourceforge/kolmafia/request/BasementRequestTest.java
+++ test/net/sourceforge/kolmafia/request/BasementRequestTest.java
@@ -1,10 +1,12 @@
 package net.sourceforge.kolmafia.request;
 
 import static internal.helpers.Player.setupFakeResponse;
+import static internal.helpers.Player.withBasementLevel;
+import static internal.helpers.Player.withContinuationState;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import internal.helpers.Cleanups;
 import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.KoLmafia;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,8 +15,7 @@ class BasementRequestTest {
 
   @BeforeAll
   protected static void injectPreferences() {
-    // Set a username so we can edit preferences and have per-user defaults.
-    KoLCharacter.reset("fakeUserName");
+    KoLCharacter.reset("BasementRequestTest");
   }
 
   @ParameterizedTest
@@ -28,14 +29,19 @@ class BasementRequestTest {
     "Collapse That Waveform!, X-dimensional horror"
   })
   void matchesMonsterFightFromResponse(String encounter, String monster) {
-    var req = new BasementRequest("Fernswarthy's Basement, Level 499");
+    var cleanups =
+        new Cleanups(
+            setupFakeResponse(200, "Fernswarthy's Basement, Level 499: " + encounter),
+            withBasementLevel(),
+            withContinuationState());
 
-    // Monster fights don't actually have stat requirements, so BasementRequest won't bail before
-    // trying to enter the fight if we use run().
-    req.responseText = "Fernswarthy's Basement, Level 499: " + encounter;
-    req.processResults();
-
-    assertEquals(monster, BasementRequest.basementMonster);
+    try (cleanups) {
+      var req = new BasementRequest("Fernswarthy's Basement, Level 499");
+      // Monster fights don't actually have stat requirements, so BasementRequest won't bail before
+      // trying to enter the fight if we use run().
+      req.run();
+      assertEquals(monster, BasementRequest.basementMonster);
+    }
   }
 
   @ParameterizedTest
@@ -51,19 +57,20 @@ class BasementRequestTest {
     "Do away with the 'doo, Buffed Mysticality Test"
   })
   void matchesImpassableStatTestFromResponse(String encounter, String summary) {
-    var req = new BasementRequest("Fernswarthy's Basement, Level 499");
-
-    var cleanups = setupFakeResponse(200, "Fernswarthy's Basement, Level 499: " + encounter);
+    var cleanups =
+        new Cleanups(
+            setupFakeResponse(200, "Fernswarthy's Basement, Level 499: " + encounter),
+            withBasementLevel(),
+            withContinuationState());
 
     try (cleanups) {
+      var req = new BasementRequest("Fernswarthy's Basement, Level 499");
       req.run();
+
+      assertEquals(499, BasementRequest.getBasementLevel());
+      assertEquals(6470, BasementRequest.getBasementTestValue());
+      assertEquals(
+          summary + ": 0 current, 6,470 needed", BasementRequest.getBasementLevelSummary());
     }
-
-    // Clear the error state, since we can't pass any of these tests.
-    KoLmafia.forceContinue();
-
-    assertEquals(499, BasementRequest.getBasementLevel());
-    assertEquals(6470, BasementRequest.getBasementTestValue());
-    assertEquals(summary + ": 0 current, 6,470 needed", BasementRequest.getBasementLevelSummary());
   }
 }
