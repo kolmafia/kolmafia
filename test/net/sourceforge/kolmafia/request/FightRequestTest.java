4ce14feb997e57ea7b4e926cf5ea030488920d40
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-07-21 09:05:01 -0400

    19 lovebug properties are per ascension. 7 are also dailies

--- test/net/sourceforge/kolmafia/request/FightRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FightRequestTest.java
@@ -772,39 +772,44 @@ public class FightRequestTest {
     @ParameterizedTest
     @CsvSource({
       // Meat or Item Drop
-      "request/test_fight_lovebug_grub.html, _lovebugsMeatDrop, 1",
-      "request/test_fight_lovebug_cricket.html, _lovebugsItemDrop, 1",
+      "request/test_fight_lovebug_grub.html, lovebugsMeatDrop, 1, false",
+      "request/test_fight_lovebug_cricket.html, lovebugsItemDrop, 1, false",
       // Stat gain
-      "request/test_fight_lovebug_muscle.html, _lovebugsMuscle, 5",
-      "request/test_fight_lovebug_mysticality.html, _lovebugsMysticality, 4",
-      "request/test_fight_lovebug_moxie.html, _lovebugsMoxie, 6",
+      "request/test_fight_lovebug_muscle.html, lovebugsMuscle, 5, false",
+      "request/test_fight_lovebug_mysticality.html, lovebugsMysticality, 4, false",
+      "request/test_fight_lovebug_moxie.html, lovebugsMoxie, 6, false",
       // Meat
-      "request/test_fight_lovebug_meat1.html, _lovebugsMeat, 81",
-      "request/test_fight_lovebug_meat2.html, _lovebugsMeat, 30",
-      "request/test_fight_lovebug_meat3.html, _lovebugsMeat, 26",
-      "request/test_fight_lovebug_meat4.html, _lovebugsMeat, 29",
+      "request/test_fight_lovebug_meat1.html, lovebugsMeat, 81, false",
+      "request/test_fight_lovebug_meat2.html, lovebugsMeat, 30, false",
+      "request/test_fight_lovebug_meat3.html, lovebugsMeat, 26, false",
+      "request/test_fight_lovebug_meat4.html, lovebugsMeat, 29, false",
       // Items
-      "request/test_fight_lovebug_booze.html, _lovebugsBooze, 1",
-      "request/test_fight_lovebug_powder.html, _lovebugsPowder, 1",
+      "request/test_fight_lovebug_booze.html, lovebugsBooze, 1, false",
+      "request/test_fight_lovebug_powder.html, lovebugsPowder, 1, false",
       // Quests
-      "request/test_fight_lovebug_ant.html, _lovebugsOrcChasm, 1",
-      "request/test_fight_lovebug_desert.html, _lovebugsAridDesert, 1",
-      "request/test_fight_lovebug_oil.html, _lovebugsOilPeak, 1",
+      "request/test_fight_lovebug_ant.html, lovebugsOrcChasm, 1, false",
+      "request/test_fight_lovebug_desert.html, lovebugsAridDesert, 1, false",
+      "request/test_fight_lovebug_oil.html, lovebugsOilPeak, 1, false",
       // Currency
-      "request/test_fight_lovebug_beach_buck.html, _lovebugsBeachBuck, 1",
-      "request/test_fight_lovebug_coinspiracy.html, _lovebugsCoinspiracy, 1",
-      "request/test_fight_lovebug_freddy.html, _lovebugsFreddy, 1",
-      "request/test_fight_lovebug_funfunds.html, _lovebugsFunFunds, 1",
-      "request/test_fight_lovebug_hobo_nickel.html, _lovebugsHoboNickel, 1",
-      "request/test_fight_lovebug_walmart.html, _lovebugsWalmart, 1"
+      "request/test_fight_lovebug_beach_buck.html, lovebugsBeachBuck, 1, true",
+      "request/test_fight_lovebug_coinspiracy.html, lovebugsCoinspiracy, 1, true",
+      "request/test_fight_lovebug_freddy.html, lovebugsFreddy, 1, true",
+      "request/test_fight_lovebug_funfunds.html, lovebugsFunFunds, 1, true",
+      "request/test_fight_lovebug_hobo_nickel.html, lovebugsHoboNickel, 1, true",
+      "request/test_fight_lovebug_walmart.html, lovebugsWalmart, 1, true"
     })
-    public void canTrackLoveBugDrops(String responseHtml, String property, int delta) {
-      var cleanups = new Cleanups(setProperty(property, 0));
+    public void canTrackLoveBugDrops(
+        String responseHtml, String property, int delta, boolean daily) {
+      var cleanups = new Cleanups(setProperty("lovebugsUnlocked", true), setProperty(property, 0));
 
       try (cleanups) {
         parseCombatData(responseHtml);
         // assertEquals(delta, Preferences.getInteger(property));
         assertEquals(0, Preferences.getInteger(property));
+        if (daily) {
+          // assertEquals(delta, Preferences.getInteger("_" + property));
+          assertEquals(0, Preferences.getInteger("_" + property));
+        }
       }
     }
   }
