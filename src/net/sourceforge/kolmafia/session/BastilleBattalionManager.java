d40d79f13148677a4b99d282915443bc8681e6d3
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-07-24 19:19:28 -0400

    Collected stats now include active turns of potions

--- src/net/sourceforge/kolmafia/session/BastilleBattalionManager.java
+++ src/net/sourceforge/kolmafia/session/BastilleBattalionManager.java
@@ -69,9 +69,10 @@ public abstract class BastilleBattalionManager {
   //   yield is linear, with randomizing fuzz. Look at the x-intercepts?
   // - Is there a randomizing factor in player stats per game? One hopes not.
   // - How do the potions affect your stats during battles? They do not
-  //   register on the "needles" as stat bonuses, and they do not affect
-  //   cheese yields that scale by stat. But you can definitely win against
-  //   tougher castles if you have them in effect than if you do not.
+  //   register on the "needles" as stat bonuses, and they do not seem to
+  //   affect cheese yields that scale by stat. But you can definitely win
+  //   against tougher castles if you have them in effect than if you do not.
+  // - Do multiple turns of a potion effect stack?
   //
   // Castles:
   //
@@ -244,7 +245,7 @@ public abstract class BastilleBattalionManager {
   // *** Boosts
 
   // Three potions give you 1 turn of increased (by some amount) attack and
-  // defense for one of military, castle, or psychological
+  // defense for one of military, castle, or psychological.
 
   private static AdventureResult SHARK_TOOTH_GRIN = EffectPool.get(EffectPool.SHARK_TOOTH_GRIN);
   private static AdventureResult BOILING_DETERMINATION =
@@ -254,19 +255,46 @@ public abstract class BastilleBattalionManager {
 
   public static class Boosts {
     private final String boosts;
+    private final AdventureResult military;
+    private final AdventureResult castle;
+    private final AdventureResult psychological;
 
     public Boosts() {
+      this.military = this.getBoost(SHARK_TOOTH_GRIN);
+      this.castle = this.getBoost(BOILING_DETERMINATION);
+      this.psychological = this.getBoost(ENHANCED_INTERROGATION);
+      this.boosts = this.makeBoostString();
+    }
+
+    private AdventureResult getBoost(AdventureResult effect) {
+      int index = KoLConstants.activeEffects.indexOf(effect);
+      return index >= 0 ? KoLConstants.activeEffects.get(index) : null;
+    }
+
+    private String makeBoostString() {
       StringBuilder buf = new StringBuilder();
-      if (KoLConstants.activeEffects.contains(SHARK_TOOTH_GRIN)) {
+      if (this.military != null) {
+        int count = this.military.getCount();
+        if (count > 1) {
+          buf.append(String.valueOf(count));
+        }
         buf.append('M');
       }
-      if (KoLConstants.activeEffects.contains(BOILING_DETERMINATION)) {
+      if (this.castle != null) {
+        int count = this.castle.getCount();
+        if (count > 1) {
+          buf.append(String.valueOf(count));
+        }
         buf.append('C');
       }
-      if (KoLConstants.activeEffects.contains(ENHANCED_INTERROGATION)) {
+      if (this.psychological != null) {
+        int count = this.psychological.getCount();
+        if (count > 1) {
+          buf.append(String.valueOf(count));
+        }
         buf.append('P');
       }
-      this.boosts = buf.toString();
+      return buf.toString();
     }
 
     public void log() {
@@ -285,15 +313,30 @@ public abstract class BastilleBattalionManager {
       switch (stat) {
         case MA:
         case MD:
-          return (boosts.contains("M"));
+          return this.military != null;
         case CA:
         case CD:
-          return (boosts.contains("C"));
+          return this.castle != null;
         case PA:
         case PD:
-          return (boosts.contains("P"));
+          return this.psychological != null;
       }
       return false;
+    }
+
+    public int boostedBy(Stat stat) {
+      switch (stat) {
+        case MA:
+        case MD:
+          return this.military == null ? 0 : this.military.getCount();
+        case CA:
+        case CD:
+          return this.castle == null ? 0 : this.castle.getCount();
+        case PA:
+        case PD:
+          return this.psychological == null ? 0 : this.psychological.getCount();
+      }
+      return 0;
     }
 
     @Override
@@ -706,7 +749,7 @@ public abstract class BastilleBattalionManager {
     // Derived
     public final Stat stat;
     public final int statBonus;
-    public final boolean potion;
+    public final int potion;
 
     // This is constructed when we have collected cheese.
     public Cheese(int turn, String encounterName, int cheese) {
@@ -715,7 +758,7 @@ public abstract class BastilleBattalionManager {
       this.encounter = cheeseEncounters.getOrDefault(encounterName, UNKNOWN_ENCOUNTER);
       this.stat = this.encounter.stat;
       this.statBonus = getCurrentStat(this.stat);
-      this.potion = (stat != Stat.NONE) && new Boosts().boosted(this.stat);
+      this.potion = (stat == Stat.NONE) ? 0 : new Boosts().boostedBy(this.stat);
     }
 
     // This is constructed when we have defeated another castle
@@ -725,7 +768,7 @@ public abstract class BastilleBattalionManager {
       this.encounter = new CheeseEncounter(castle);
       this.stat = Stat.NONE;
       this.statBonus = turn / 3;
-      this.potion = false;
+      this.potion = 0;
     }
   }
 
@@ -1657,7 +1700,7 @@ public abstract class BastilleBattalionManager {
   //     string name;           // Name of the cheese-granting encounter
   //     string stat_name;      // Name of relevant stat
   //     int stat_value;        // Value of relevant stat
-  //     boolean potion;        // true if appropriate potion in effect
+  //     int potion;		// Turns of appropriate potions in effect
   //     int cheese;            // How much cheese you looted
   // };
   //
