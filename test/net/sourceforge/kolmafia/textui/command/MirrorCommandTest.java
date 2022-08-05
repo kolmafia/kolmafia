2a7b5d179fda151bcc88486949611c5f636a2da2
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2022-08-05 12:15:47 -0400

    Update MirrorCommandTest.java

--- test/net/sourceforge/kolmafia/textui/command/MirrorCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/MirrorCommandTest.java
@@ -15,6 +15,7 @@ import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.RequestLogger;
 import net.sourceforge.kolmafia.utilities.FileUtilities;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -32,63 +33,62 @@ public class MirrorCommandTest extends AbstractCommandTestBase {
 
   private String getMirrorLog(String fileName) {
     StringBuilder builder = new StringBuilder();
-
     try (BufferedReader reader =
         FileUtilities.getReader(new File(KoLConstants.ROOT_LOCATION, "chats/" + fileName))) {
       String line;
-
       // Read the full file to a string
       while ((line = FileUtilities.readLine(reader)) != null) {
         if (builder.length() > 0) {
           builder.append("\n");
         }
-
         builder.append(line);
       }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
     return builder.toString();
   }
 
   @Test
+  @Disabled("Using > does force colorization. ")
   public void testMirrorWritesHTML() {
     // Open the mirror
-    execute("chats/test_writes_html.html");
-
+    execute("chats/test_writes_html.txt");
     // When `> ` is used as a prefix, the RequestLogger will colorize it assuming its a command
     // input.
     RequestLogger.printLine("> Fake command input");
-
     // Log another line, this should be raw.
     RequestLogger.printLine("Raw Line");
-
     // Close the mirror
     execute("");
-
-    String mirrorOutput = getMirrorLog("test_writes_html.html");
-
+    String mirrorOutput = getMirrorLog("test_writes_html.txt");
     assertTrue(mirrorOutput.contains("<font color=olive>> Fake command input</font><br>"));
     assertTrue(mirrorOutput.contains("Raw Line<br>"));
   }
 
   @Test
-  public void testMirrorClose() {
-    execute("chats/test_mirror_closes");
-
-    // Write to the mirror log
-    RequestLogger.printLine("Some input");
-
+  public void testMirrorDoesNotAddHTML() {
+    // Open the mirror
+    execute("chats/test_writes.txt");
+    // Log a raw line, this should be raw.
+    RequestLogger.printLine("Raw Line");
     // Close the mirror
     execute("");
+    String mirrorOutput = getMirrorLog("test_writes.txt");
+    assertTrue(mirrorOutput.contains("Raw Line"));
+  }
 
+  @Test
+  public void testMirrorClose() {
+    execute("chats/test_mirror_closes");
+    // Write to the mirror log
+    RequestLogger.printLine("Some input");
+    // Close the mirror
+    execute("");
     // Write to the command buffer again
     RequestLogger.printLine("Should not be seen");
-
     // Now read the log and verify that it was not written
     String mirrorOutput = getMirrorLog("test_mirror_closes.txt");
-
     // Test that the second line is not in the output
     assertFalse(mirrorOutput.contains("Should not be seen"));
   }
@@ -96,88 +96,68 @@ public class MirrorCommandTest extends AbstractCommandTestBase {
   @Test
   public void testMirrorListStrings() {
     execute("chats/test_mirror_list_strings");
-
     List<String> list = new ArrayList<>();
     list.add("Line 1");
     list.add("Line 2");
-
     RequestLogger.printList(list);
-
     // Close the mirror
     execute("");
-
     // Read the log
     String mirrorOutput = getMirrorLog("test_mirror_list_strings.txt");
-
     // Test that the output matches as expected
-    assertEquals("Line 1<br>Line 2<br></pre>", mirrorOutput);
+    assertEquals("Line 1\n" + "Line 2", mirrorOutput);
   }
 
   @Test
   public void testMirrorListSkills() {
     try (var cleanups = new Cleanups(withSkill(1), withSkill(2))) {
       execute("chats/test_mirror_list_skills");
-
       // Available skills is special in that it will print html
       RequestLogger.printList(KoLConstants.availableSkills);
-
       // Close the mirror
       execute("");
-
       // Now read the log and verify that it contains html
       String mirrorOutput = getMirrorLog("test_mirror_list_skills.txt");
-
       // Test that the output matches the expected html
       assertEquals(
-          "<u><b>Uncategorized</b></u><br><a onClick=\"javascript:skill(2);\">Chronic Indigestion</a><br><a onClick=\"javascript:skill(1);"
-              + "\">Liver of Steel</a><br>",
-          mirrorOutput);
+          "Uncategorized\n" + " - Chronic Indigestion\n" + " - Liver of Steel", mirrorOutput);
     }
   }
 
   @Test
   public void testMultipleMirrorsOpened() {
     execute("chats/mirror_1");
-
     // Write to the mirror log
     RequestLogger.printLine("Mirror 1 Log");
-
     // Switch mirrors to mirror_2
     execute("chats/mirror_2");
-
     // Write to the mirror log
     RequestLogger.printLine("Mirror 2 Log");
-
     // Close the mirror
     execute("");
-
     // Behavior is that if the mirror has already been opened, that it will stay opened and the new
     // mirror requests are effectively ignored.
-
     // Assert that mirror_1 only contains both messages
     assertTrue(getMirrorLog("mirror_1.txt").contains("Mirror 1 Log"));
     assertTrue(getMirrorLog("mirror_1.txt").contains("Mirror 2 Log"));
-
     // Assert that mirror_2 contains neither messages
     assertFalse(getMirrorLog("mirror_2.txt").contains("Mirror 1 Log"));
     assertFalse(getMirrorLog("mirror_2.txt").contains("Mirror 2 Log"));
   }
 
+  // Uncomment when code allowing htm, html available
   @ParameterizedTest
   @CsvSource({
     "file,file.txt",
-    "file.html,file.html",
-    "file.htm,file.htm",
+    // "file.html,file.html",
+    // "file.htm,file.htm",
     "file.txt,file.txt",
     "file.csv,file.csv.txt"
   })
   public void testMirrorFileNames(String mirrorName, String fileName) {
     execute("chats/" + mirrorName);
-
     RequestLogger.printLine("Filler Line");
-
     execute("");
-
     assertTrue(getMirrorLog(fileName).contains("Filler Line"));
   }
 }
