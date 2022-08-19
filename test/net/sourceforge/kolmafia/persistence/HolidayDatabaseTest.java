697188260b44233df4498884f805f77ec7ea9d98
Sam Gaus <sam@gaus.co.uk>
2022-08-19 18:52:16 +0100

    Add more testing

--- test/net/sourceforge/kolmafia/persistence/HolidayDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/HolidayDatabaseTest.java
@@ -1,13 +1,19 @@
 package net.sourceforge.kolmafia.persistence;
 
+import static internal.helpers.Player.withDay;
 import static net.sourceforge.kolmafia.persistence.HolidayDatabase.ROLLOVER;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import java.time.Month;
 import java.time.ZonedDateTime;
+
+import internal.helpers.Cleanups;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class HolidayDatabaseTest {
   @Nested
@@ -26,6 +32,57 @@ class HolidayDatabaseTest {
       var position = HolidayDatabase.getHamburglarPosition(date);
 
       assertThat(position, equalTo(4));
+    }
+  }
+
+  @Nested
+  class Monday {
+    @ParameterizedTest
+    @CsvSource({
+            "22, true",
+            "23, false"
+    })
+    void canDetectTodayIsMonday(final int date, final boolean isMonday) {
+      var cleanups = new Cleanups(withDay(2022, Month.AUGUST, date));
+
+      try (cleanups) {
+        assertThat(HolidayDatabase.isMonday(), equalTo(isMonday));
+      }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "22, true",
+            "23, false"
+    })
+    void canDetectADayIsMonday(final int date, final boolean isMonday) {
+      var dateTime = ZonedDateTime.of(2022, Month.AUGUST.getValue(), date, 0, 0, 0, 0, ROLLOVER);
+      assertThat(HolidayDatabase.isMonday(dateTime), equalTo(isMonday));
+    }
+  }
+
+  @Nested
+  class RealLifeHolidays {
+    @ParameterizedTest
+    @CsvSource({
+            "0101,Festival of Jarlsberg",
+            "0214,Valentine's Day",
+            "0317,St. Sneaky Pete's Day",
+            "0704,Dependence Day",
+            "0417,Oyster Egg Day",
+            "1124,Feast of Boris",
+            "1031,Halloween",
+            "0202,Groundhog Day",
+            "0401,April Fool's Day",
+            "0919,Talk Like a Pirate Day",
+            "1225,Crimbo",
+            "1022,Holatuwol's Birthday",
+            "0923,Veracity's Birthday",
+            "0217,Gausie's Birthday",
+            "0102,"
+    })
+    void canIdentifyRealLifeHolidays(final String date, final String holiday) {
+      assertThat(HolidayDatabase.getRealLifeHoliday("2022" + date), equalTo(holiday));
     }
   }
 }
