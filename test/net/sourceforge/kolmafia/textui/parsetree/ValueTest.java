90cde9010639bfe7342312a0364f6c48c6584f4a
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2022-09-10 15:37:37 -0700

    test typo

--- test/net/sourceforge/kolmafia/textui/parsetree/ValueTest.java
+++ test/net/sourceforge/kolmafia/textui/parsetree/ValueTest.java
@@ -309,7 +309,7 @@ class ValueTest {
       var value = new Value("150");
       assertThat("\"150\" < 200", value.compareTo(new Value(200)), hasSign(NEGATIVE));
       assertThat("\"150\" == 150", value.compareTo(new Value(150)), hasSign(ZERO));
-      assertThat("\"150\" > 100", value.compareTo(new Value(true)), hasSign(POSITIVE));
+      assertThat("\"150\" > 100", value.compareTo(new Value(100)), hasSign(POSITIVE));
     }
 
     @Test
@@ -317,7 +317,7 @@ class ValueTest {
       var value = new Value("5.2");
       assertThat("\"5.2\" > 4.69", value.compareTo(new Value(4.69)), hasSign(POSITIVE));
       assertThat("\"5.2\" == 5.2", value.compareTo(new Value(5.2)), hasSign(ZERO));
-      assertThat("\"5.2\" < 6.9", value.compareTo(new Value(true)), hasSign(NEGATIVE));
+      assertThat("\"5.2\" < 6.9", value.compareTo(new Value(6.9)), hasSign(NEGATIVE));
     }
 
     @Test
