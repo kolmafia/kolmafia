99f83491382ad67c73bc34fc0a6498057a787eea
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2022-09-20 12:35:48 -0400

    It wasn't spotless that did it...

--- test/net/sourceforge/kolmafia/persistence/ItemFinderTest.java
+++ test/net/sourceforge/kolmafia/persistence/ItemFinderTest.java
@@ -21,7 +21,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ItemFinderTest {
 
