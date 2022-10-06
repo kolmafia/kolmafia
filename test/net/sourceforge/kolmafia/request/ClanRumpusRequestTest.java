3616550feae334d98408a13aa8e16932ffd5feb5
Samuel Gaus <sam@gaus.co.uk>
2022-10-06 13:37:42 +0100

    Add test for chips being tracked in Clan Rumpus Room (#1116)
    * Add testing for Chip machine in clan rumpus room
    
    * Simplify cleanups for clan rumpus room
    
    Co-authored-by: Jamie Adams <82782908+jaadams5@users.noreply.github.com>

--- test/net/sourceforge/kolmafia/request/ClanRumpusRequestTest.java
+++ test/net/sourceforge/kolmafia/request/ClanRumpusRequestTest.java
@@ -1,12 +1,17 @@
 package net.sourceforge.kolmafia.request;
 
+import static internal.helpers.Networking.assertPostRequest;
 import static internal.helpers.Networking.html;
+import static internal.helpers.Player.withClanFurniture;
+import static internal.helpers.Player.withHttpClientBuilder;
 import static internal.helpers.Player.withNextResponse;
 import static internal.helpers.Player.withProperty;
 import static internal.matchers.Preference.isSetTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 import internal.helpers.Cleanups;
+import internal.network.FakeHttpClientBuilder;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import net.sourceforge.kolmafia.request.ClanRumpusRequest.RequestType;
@@ -26,11 +31,42 @@ public class ClanRumpusRequestTest {
     var cleanups =
         new Cleanups(
             withNextResponse(200, html("request/test_clan_ballpit.html")),
-            withProperty("_ballpit", ""));
+            withProperty("_ballpit", false));
 
     try (cleanups) {
       new ClanRumpusRequest(ClanRumpusRequest.RequestType.BALLS).run();
-      assertThat("_ballpit", isSetTo("true"));
+      assertThat("_ballpit", isSetTo(true));
+    }
+  }
+
+  @Test
+  void canTrackChipMachineFinished() {
+    var builder = new FakeHttpClientBuilder();
+
+    builder.client.addResponse(200, html("request/test_clan_rumpus_chips_used.html"));
+
+    var cleanups =
+        new Cleanups(
+            withHttpClientBuilder(builder),
+            // This includes additional furniture to match the test fixture and ensure appropriate
+            // tidy-up afterwards
+            // We only actually care about the snack machine
+            withClanFurniture(
+                "Girls of Loathing Calendar",
+                "Collection of Self-Help Books",
+                "Mr. Klaw \"Skill\" Crane Game",
+                "Inspirational Desk Calendar",
+                "Wrestling Mat",
+                "Snack Machine"),
+            withProperty("_chipBags", 0));
+
+    try (cleanups) {
+      new ClanRumpusRequest(ClanRumpusRequest.RequestType.CHIPS).run();
+      var requests = builder.client.getRequests();
+
+      assertThat(requests, hasSize(1));
+      assertPostRequest(requests.get(0), "/clan_rumpus.php", "preaction=buychips&whichbag=0");
+      assertThat("_chipBags", isSetTo(3));
     }
   }
 
