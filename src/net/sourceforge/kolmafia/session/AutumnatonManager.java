5c8251ac56d200b43e53dfe0cf116d2aa21337d6
horrible little slime <69secret69email69@gmail.com>
2022-10-09 14:05:39 -0400

    Spotless

--- src/net/sourceforge/kolmafia/session/AutumnatonManager.java
+++ src/net/sourceforge/kolmafia/session/AutumnatonManager.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.persistence.AdventureDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
