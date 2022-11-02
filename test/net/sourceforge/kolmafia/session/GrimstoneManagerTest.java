7ee031526ce90292e08894d766a9cdefbc84bb28
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-11-01 20:06:39 -0400

    I'm stumped.

--- test/net/sourceforge/kolmafia/session/GrimstoneManagerTest.java
+++ test/net/sourceforge/kolmafia/session/GrimstoneManagerTest.java
@@ -7,7 +7,8 @@ import static internal.helpers.Player.withGender;
 import static internal.helpers.Player.withHttpClientBuilder;
 import static internal.helpers.Player.withPasswordHash;
 import static internal.helpers.Player.withProperty;
-import static internal.helpers.Player.withTurnsPlayed;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import internal.helpers.Cleanups;
@@ -17,7 +18,6 @@ import java.util.Map;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import net.sourceforge.kolmafia.request.GenericRequest;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -37,12 +37,6 @@ public class GrimstoneManagerTest {
 
   @Nested
   class Wolf {
-    @AfterAll
-    public static void afterAll() {
-      // Because we actually load a charpane.
-      KoLCharacter.reset("");
-    }
-
     @Test
     public void startingHouseRunTakesThreeTurns() {
       var builder = new FakeHttpClientBuilder();
@@ -52,14 +46,12 @@ public class GrimstoneManagerTest {
               withHttpClientBuilder(builder),
               withPasswordHash("grimstone"),
               withGender(KoLCharacter.FEMALE),
-              withProperty("wolfTurnsUsed", 27),
-              withTurnsPlayed(30257));
+              withProperty("wolfTurnsUsed", 27));
       try (cleanups) {
         client.addResponse(
             302, Map.of("location", List.of("fight.php?ireallymeanit=1667327836")), "");
         client.addResponse(200, html("request/test_unleash_inner_wolf.html"));
         client.addResponse(200, ""); // api.php
-        client.addResponse(200, html("request/test_unleash_inner_wolf_charpane.html"));
 
         // I did this in the Relay Browser. We will simulate it being done in
         // via automation - which follows redirects and requests api.php.
@@ -68,17 +60,13 @@ public class GrimstoneManagerTest {
         var request = new GenericRequest(URL);
         request.run();
 
-        // That requested an api.php request. I collected HTML for a chrpane refresh.
-        request.constructURLString("charpane.php", false);
-        request.run();
-
         // A House Run takes 3 turns. This is charged when you initiate it
         // and it redirects to a fight, which is followed by a
         // fight-follows-fight chain.
         assertEquals(30, Preferences.getInteger("wolfTurnsUsed"));
-        assertEquals(30260, KoLCharacter.getTurnsPlayed());
 
         var requests = client.getRequests();
+        assertThat(requests, hasSize(3));
 
         assertPostRequest(
             requests.get(0),
@@ -86,7 +74,6 @@ public class GrimstoneManagerTest {
             "whichplace=ioty2014_wolf&action=wolf_houserun&pwd=grimstone");
         assertGetRequest(requests.get(1), "/fight.php", "ireallymeanit=1667327836");
         assertPostRequest(requests.get(2), "/api.php", "what=status&for=KoLmafia");
-        assertGetRequest(requests.get(3), "/charpane.php", null);
       }
     }
   }
