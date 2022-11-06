d9b311b3f360f7519825fc8545ed9770b67be6d7
Patrick Hulin <patrick.hulin@gmail.com>
2022-11-06 18:25:17 -0500

    Spotless.

--- src/net/sourceforge/kolmafia/swingui/panel/GearChangePanel.java
+++ src/net/sourceforge/kolmafia/swingui/panel/GearChangePanel.java
@@ -19,7 +19,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
 import javax.swing.event.ChangeEvent;
