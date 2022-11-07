cc10902193396db8793e15feeebcf922e53842c0
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2022-11-07 16:50:22 -0500

    Tweaking - No command

--- test/net/sourceforge/kolmafia/request/FightRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FightRequestTest.java
@@ -44,6 +44,7 @@ import net.sourceforge.kolmafia.objectpool.EffectPool;
 import net.sourceforge.kolmafia.objectpool.FamiliarPool;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.objectpool.SkillPool;
+import net.sourceforge.kolmafia.persistence.AdventureDatabase;
 import net.sourceforge.kolmafia.persistence.MonsterDatabase;
 import net.sourceforge.kolmafia.persistence.SkillDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
@@ -53,6 +54,7 @@ import net.sourceforge.kolmafia.session.GreyYouManager;
 import net.sourceforge.kolmafia.session.InventoryManager;
 import net.sourceforge.kolmafia.session.LocketManager;
 import net.sourceforge.kolmafia.textui.command.TestCommand;
+import net.sourceforge.kolmafia.utilities.StringUtilities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
@@ -1087,11 +1089,28 @@ public class FightRequestTest {
             withProperty("questL07Cyrptic", "started"));
     try (cleanups) {
       String html = html("request/evilometer_fight.txt");
-      TestCommand tc = new TestCommand();
-      tc.setContents(html);
-      tc.run("test", "fight 1");
-      assertThat("cyrptNicheEvilness", isSetTo(0));
-    }
+      String[] split = {"fight", "1"};
+      int round = split.length > 1 ? StringUtilities.parseInt(split[1].trim()) : -1;
+      String responseText = html;
+      if (round >= 0) {
+        String adventureName =
+            split.length > 2 ? split[2].trim() : Preferences.getString("nextAdventure");
+        KoLAdventure.setLastAdventure(AdventureDatabase.getAdventure(adventureName));
+        String encounter =
+            split.length > 3
+                ? split[3].trim()
+                : AdventureRequest.parseCombatEncounter(responseText);
+        FightRequest.setCurrentEncounter(encounter);
+        MonsterData monster = AdventureRequest.extractMonster(encounter, responseText);
+        MonsterStatusTracker.setNextMonster(monster);
+        String monsterName = monster.getName();
+        FightRequest.currentRound = round;
+        FightRequest.processResults("fight.php", monsterName, html);
+        // tc.setContents(html);
+        // tc.run("test", "fight 1");
+        assertThat("cyrptNicheEvilness", isSetTo(0));
+      }
+      }
   }
 
   @Test
