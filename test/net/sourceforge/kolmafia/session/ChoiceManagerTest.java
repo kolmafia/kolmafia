2b25dcb8514a727ad8f1a2cccb1d1261766a7667
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-11-08 08:53:55 -0500

    spotless

--- test/net/sourceforge/kolmafia/session/ChoiceManagerTest.java
+++ test/net/sourceforge/kolmafia/session/ChoiceManagerTest.java
@@ -303,8 +303,7 @@ public class ChoiceManagerTest {
         client.addResponse(200, html("request/test_visit_gc_midnight_civic_center.html"));
         client.addResponse(200, ""); // api.php
         client.addResponse(302, Map.of("location", List.of("choice.php")), "");
-        client.addResponse(
-            200, html("request/test_visit_gc_midnight_civic_center_refresh.html"));
+        client.addResponse(200, html("request/test_visit_gc_midnight_civic_center_refresh.html"));
 
         var url = "adventure.php?snarfblat=477";
         var request = new GenericRequest(url);
