5d756a6e339761f253790a06b79fb8e8a8fb3e99
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-11-09 10:04:05 +0000

    tests: use cleanups in guzzlr command test

--- test/net/sourceforge/kolmafia/textui/command/GuzzlrCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/GuzzlrCommandTest.java
@@ -1,37 +1,26 @@
 package net.sourceforge.kolmafia.textui.command;
 
+import static internal.helpers.Player.withItem;
+import static internal.helpers.Player.withProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
-import net.sourceforge.kolmafia.AdventureResult;
+import internal.helpers.Cleanups;
 import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.preferences.Preferences;
-import net.sourceforge.kolmafia.request.GenericRequest;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class GuzzlrCommandTest extends AbstractCommandTestBase {
-  @BeforeEach
-  public void initEach() {
+  @BeforeAll
+  public static void init() {
     KoLCharacter.reset("testUser");
-    KoLCharacter.reset(true);
-    Preferences.resetToDefault("_guzzlrQuestAbandoned");
-    Preferences.resetToDefault("questGuzzlr");
-    Preferences.resetToDefault("guzzlrBronzeDeliveries");
-    Preferences.resetToDefault("guzzlrGoldDeliveries");
-
-    // Stop requests from actually running
-    GenericRequest.sessionId = null;
+    Preferences.reset("testUser");
   }
 
   public GuzzlrCommandTest() {
     this.command = "guzzlr";
-  }
-
-  private static void playerHasGuzzlrTablet() {
-    AdventureResult.addResultToList(KoLConstants.inventory, ItemPool.get(ItemPool.GUZZLR_TABLET));
   }
 
   @Test
@@ -44,109 +33,133 @@ public class GuzzlrCommandTest extends AbstractCommandTestBase {
 
   @Test
   void mustSpecifyParameters() {
-    playerHasGuzzlrTablet();
-    String output = execute("");
+    var cleanups = withItem(ItemPool.GUZZLR_TABLET);
+    try (cleanups) {
+      String output = execute("");
 
-    assertErrorState();
-    assertThat(output, containsString("Use command guzzlr"));
+      assertErrorState();
+      assertThat(output, containsString("Use command guzzlr"));
+    }
   }
 
   @Test
   void abandonMustHaveQuest() {
-    playerHasGuzzlrTablet();
-    Preferences.setString("questGuzzlr", "unstarted");
-    String output = execute("abandon");
+    var cleanups =
+        new Cleanups(withItem(ItemPool.GUZZLR_TABLET), withProperty("questGuzzlr", "unstarted"));
+    try (cleanups) {
+      String output = execute("abandon");
 
-    assertContinueState();
-    assertThat(output, containsString("You don't have a client"));
+      assertContinueState();
+      assertThat(output, containsString("You don't have a client"));
+    }
   }
 
   @Test
   void cannotAbandonTwiceInADay() {
-    playerHasGuzzlrTablet();
-    Preferences.setBoolean("_guzzlrQuestAbandoned", true);
-    Preferences.setString("questGuzzlr", "started");
-    String output = execute("abandon");
+    var cleanups =
+        new Cleanups(
+            withItem(ItemPool.GUZZLR_TABLET),
+            withProperty("_guzzlrQuestAbandoned", true),
+            withProperty("questGuzzlr", "started"));
+    try (cleanups) {
+      String output = execute("abandon");
 
-    assertErrorState();
-    assertThat(output, containsString("already abandoned"));
+      assertErrorState();
+      assertThat(output, containsString("already abandoned"));
+    }
   }
 
   @Test
   void abandonAbandons() {
-    playerHasGuzzlrTablet();
-    Preferences.setString("questGuzzlr", "started");
-    String output = execute("abandon");
+    var cleanups =
+        new Cleanups(withItem(ItemPool.GUZZLR_TABLET), withProperty("questGuzzlr", "started"));
+    try (cleanups) {
+      String output = execute("abandon");
 
-    assertContinueState();
-    assertThat(output, containsString("Abandoning client"));
+      assertContinueState();
+      assertThat(output, containsString("Abandoning client"));
+    }
   }
 
   @Test
   void acceptRequiresNoClient() {
-    playerHasGuzzlrTablet();
-    Preferences.setString("questGuzzlr", "started");
-    String output = execute("accept bronze");
+    var cleanups =
+        new Cleanups(withItem(ItemPool.GUZZLR_TABLET), withProperty("questGuzzlr", "started"));
+    try (cleanups) {
+      String output = execute("accept bronze");
 
-    assertErrorState();
-    assertThat(output, containsString("You already have a client"));
+      assertErrorState();
+      assertThat(output, containsString("You already have a client"));
+    }
   }
 
   @Test
   void acceptRequiresValidClient() {
-    playerHasGuzzlrTablet();
-    String output = execute("accept silver");
+    var cleanups = withItem(ItemPool.GUZZLR_TABLET);
+    try (cleanups) {
+      String output = execute("accept silver");
 
-    assertErrorState();
-    assertThat(output, containsString("Use command 'guzzlr"));
+      assertErrorState();
+      assertThat(output, containsString("Use command 'guzzlr"));
+    }
   }
 
   @Test
   void acceptBronze() {
-    playerHasGuzzlrTablet();
-    String output = execute("accept bronze");
+    var cleanups = withItem(ItemPool.GUZZLR_TABLET);
+    try (cleanups) {
+      String output = execute("accept bronze");
 
-    assertContinueState();
-    assertThat(output, containsString("Accepting a bronze client"));
+      assertContinueState();
+      assertThat(output, containsString("Accepting a bronze client"));
+    }
   }
 
   @Test
   void acceptGoldRequiresFiveBronze() {
-    playerHasGuzzlrTablet();
-    String output = execute("accept gold");
+    var cleanups = withItem(ItemPool.GUZZLR_TABLET);
+    try (cleanups) {
+      String output = execute("accept gold");
 
-    assertErrorState();
-    assertThat(
-        output, containsString("You need to make 5 bronze deliveries to serve gold clients"));
+      assertErrorState();
+      assertThat(
+          output, containsString("You need to make 5 bronze deliveries to serve gold clients"));
+    }
   }
 
   @Test
   void acceptGold() {
-    playerHasGuzzlrTablet();
-    Preferences.setInteger("guzzlrBronzeDeliveries", 5);
-    String output = execute("accept gold");
+    var cleanups =
+        new Cleanups(withItem(ItemPool.GUZZLR_TABLET), withProperty("guzzlrBronzeDeliveries", 5));
+    try (cleanups) {
+      String output = execute("accept gold");
 
-    assertContinueState();
-    assertThat(output, containsString("Accepting a gold client"));
+      assertContinueState();
+      assertThat(output, containsString("Accepting a gold client"));
+    }
   }
 
   @Test
   void acceptPlatinumRequiresFiveGold() {
-    playerHasGuzzlrTablet();
-    String output = execute("accept platinum");
+    var cleanups = withItem(ItemPool.GUZZLR_TABLET);
+    try (cleanups) {
+      String output = execute("accept platinum");
 
-    assertErrorState();
-    assertThat(
-        output, containsString("You need to make 5 gold deliveries to serve platinum clients"));
+      assertErrorState();
+      assertThat(
+          output, containsString("You need to make 5 gold deliveries to serve platinum clients"));
+    }
   }
 
   @Test
   void acceptPlatinum() {
-    playerHasGuzzlrTablet();
-    Preferences.setInteger("guzzlrGoldDeliveries", 5);
-    String output = execute("accept platinum");
+    var cleanups =
+        new Cleanups(withItem(ItemPool.GUZZLR_TABLET), withProperty("guzzlrGoldDeliveries", 5));
+    try (cleanups) {
+      String output = execute("accept platinum");
 
-    assertContinueState();
-    assertThat(output, containsString("Accepting a platinum client"));
+      assertContinueState();
+      assertThat(output, containsString("Accepting a platinum client"));
+    }
   }
 }
