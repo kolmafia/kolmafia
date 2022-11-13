81314c663bc95a43ae85fc8e674e869071da6a17
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2022-11-13 13:12:55 -0500

    test welcomeBackAdv property

--- test/net/sourceforge/kolmafia/textui/command/GongCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/GongCommandTest.java
@@ -10,9 +10,12 @@ import static internal.helpers.Player.withItem;
 import static internal.helpers.Player.withLimitMode;
 import static internal.helpers.Player.withNoEffects;
 import static internal.helpers.Player.withPasswordHash;
+import static internal.helpers.Player.withProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import internal.helpers.Cleanups;
 import internal.network.FakeHttpClientBuilder;
@@ -23,6 +26,7 @@ import net.sourceforge.kolmafia.KoLConstants.MafiaState;
 import net.sourceforge.kolmafia.StaticEntity;
 import net.sourceforge.kolmafia.objectpool.AdventurePool;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.persistence.AdventureDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import net.sourceforge.kolmafia.session.ChoiceManager;
 import net.sourceforge.kolmafia.session.LimitMode;
@@ -30,6 +34,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GongCommandTest extends AbstractCommandTestBase {
 
@@ -79,6 +85,9 @@ public class GongCommandTest extends AbstractCommandTestBase {
         assertThat(output, containsString("Encounter: The Gong Has Been Bung"));
         assertThat(output, containsString("You acquire an effect: Form of...Bird! (15)"));
 
+        assertEquals(LimitMode.BIRD, KoLCharacter.getLimitMode());
+        assertFalse(ChoiceManager.handlingChoice);
+
         var requests = client.getRequests();
         assertThat(requests, hasSize(4));
 
@@ -110,8 +119,9 @@ public class GongCommandTest extends AbstractCommandTestBase {
       }
     }
 
-    @Test
-    public void birdModeAtEndOfJourney() {
+    @ParameterizedTest
+    @ValueSource(ints = {0, AdventurePool.HAUNTED_KITCHEN})
+    public void birdModeAtEndOfJourney(int snarfblat) {
       var builder = new FakeHttpClientBuilder();
       var client = builder.client;
       var cleanups =
@@ -120,6 +130,7 @@ public class GongCommandTest extends AbstractCommandTestBase {
               withItem(ItemPool.GONG),
               withLimitMode(LimitMode.BIRD),
               withNoEffects(),
+              withProperty("welcomeBackAdv", snarfblat),
               withPasswordHash("gong"),
               // If you have a password hash, KoL looks at your vinyl boots
               withGender(KoLCharacter.FEMALE));
@@ -133,22 +144,26 @@ public class GongCommandTest extends AbstractCommandTestBase {
         client.addResponse(200, html("request/test_choose_bird_form.html"));
         client.addResponse(200, ""); // api.php
 
+        if (snarfblat == 0) snarfblat = AdventurePool.NOOB_CAVE;
+        String locationName = AdventureDatabase.getAdventure(snarfblat).getAdventureName();
+
         String output = execute("bird");
         assertThat(output, containsString("Gong path: bird"));
-        assertThat(output, containsString("Noob Cave"));
+        assertThat(output, containsString(locationName));
         assertThat(output, containsString("Welcome Back!"));
         assertThat(output, containsString("You acquire an item: glimmering roc feather"));
         assertThat(output, containsString("Using 1 llama lama gong"));
         assertThat(output, containsString("Encounter: The Gong Has Been Bung"));
         assertThat(output, containsString("You acquire an effect: Form of...Bird! (15)"));
 
+        assertEquals(LimitMode.BIRD, KoLCharacter.getLimitMode());
+        assertFalse(ChoiceManager.handlingChoice);
+
         var requests = client.getRequests();
         assertThat(requests, hasSize(8));
 
         assertPostRequest(
-            requests.get(0),
-            "/adventure.php",
-            "snarfblat=" + AdventurePool.NOOB_CAVE + "&pwd=gong");
+            requests.get(0), "/adventure.php", "snarfblat=" + snarfblat + "&pwd=gong");
         assertGetRequest(requests.get(1), "/choice.php", null);
         assertPostRequest(requests.get(2), "/choice.php", "whichchoice=277&option=1&pwd=gong");
         assertPostRequest(requests.get(3), "/api.php", "what=status&for=KoLmafia");
