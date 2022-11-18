0afb3f8536f762377da4f866b4e31435e335b72f
Sam Gaus <sam@gaus.co.uk>
2022-11-18 08:41:23 +0000

    Handle adding to empty lists

--- src/net/sourceforge/kolmafia/swingui/listener/PopupListener.java
+++ src/net/sourceforge/kolmafia/swingui/listener/PopupListener.java
@@ -1,5 +1,6 @@
 package net.sourceforge.kolmafia.swingui.listener;
 
+import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JComponent;
@@ -25,26 +26,47 @@ public class PopupListener extends MouseAdapter {
   private void maybeShowPopup(MouseEvent e) {
     if (e.isPopupTrigger()) {
       var source = (JComponent) e.getSource();
-
       var point = e.getPoint();
 
       if (source instanceof JList<?> list) {
-        int index = list.locationToIndex(point);
-        if (!list.isSelectedIndex(index)) {
-          list.clearSelection();
-          list.addSelectionInterval(index, index);
-        }
+        select(list, point);
       } else if (source instanceof JTable table) {
-        int row = table.rowAtPoint(point);
-        if (!table.isRowSelected(row)) {
-          table.clearSelection();
-          table.addRowSelectionInterval(row, row);
-        }
+        select(table, point);
       } else {
         return;
       }
 
       popup.show(e.getComponent(), e.getX(), e.getY());
+    }
+  }
+
+  private void select(JList<?> list, Point point) {
+    int index = list.locationToIndex(point);
+
+    if (index < 0) {
+      var size = list.getModel().getSize();
+      if (size == 0) return;
+      index = size - 1;
+    }
+
+    if (!list.isSelectedIndex(index)) {
+      list.clearSelection();
+      list.addSelectionInterval(index, index);
+    }
+  }
+
+  private void select(JTable table, Point point) {
+    int row = table.rowAtPoint(point);
+
+    if (row < 0) {
+      var size = table.getModel().getRowCount();
+      if (size == 0) return;
+      row = size - 1;
+    }
+
+    if (!table.isRowSelected(row)) {
+      table.clearSelection();
+      table.addRowSelectionInterval(row, row);
     }
   }
 }
