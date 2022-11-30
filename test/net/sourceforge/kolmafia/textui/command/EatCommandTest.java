2a76c04f4d0729729e35256a2ede008d154c6a3a
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2022-11-30 14:11:04 +0000

    fix: allow eating a glitch monster after using it (#1317)

--- test/net/sourceforge/kolmafia/textui/command/EatCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/EatCommandTest.java
@@ -4,6 +4,7 @@ import static internal.helpers.HttpClientWrapper.getRequests;
 import static internal.helpers.HttpClientWrapper.setupFakeClient;
 import static internal.helpers.Networking.assertPostRequest;
 import static internal.helpers.Player.withItem;
+import static internal.helpers.Player.withProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
@@ -34,6 +35,23 @@ class EatCommandTest extends AbstractCommandTestBase {
       setupFakeClient();
 
       var cleanups = new Cleanups(withItem(ItemPool.GLITCH_ITEM));
+
+      try (cleanups) {
+        String output = execute("glitch season");
+        var requests = getRequests();
+        assertThat(output, containsString("Eating 1 [glitch season reward name]..."));
+        assertThat(requests, hasSize(1));
+        assertPostRequest(requests.get(0), "/inv_eat.php", "whichitem=10207&ajax=1&quantity=1");
+      }
+    }
+
+    @Test
+    public void canEatGlitchSeasonRewardAfterImplementing() {
+      setupFakeClient();
+
+      var cleanups =
+          new Cleanups(
+              withItem(ItemPool.GLITCH_ITEM), withProperty("_glitchItemImplemented", true));
 
       try (cleanups) {
         String output = execute("glitch season");
