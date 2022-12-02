0321d525e8bf65ae5c923955cd1f8df0ff4b6dd1
Sam Gaus <sam@gaus.co.uk>
2022-12-02 10:43:13 +0000

    Add tests PHPMTRandom, add tests for negative seeds, fix bugs

--- test/net/sourceforge/kolmafia/utilities/PHPMTRandomTest.java
+++ test/net/sourceforge/kolmafia/utilities/PHPMTRandomTest.java
@@ -3,23 +3,23 @@ package net.sourceforge.kolmafia.utilities;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class PHPRandomTest {
+class PHPMTRandomTest {
   // Just some data sourced from PHP 5.3 code to confirm we're right
 
   @ParameterizedTest
   @CsvSource({
-    "1, 1804289383, 846930886, 1681692777, 1714636915",
-    "2147483647, 1065668062, 2142264300, 1066566375, 1064012770",
-    "-8008135, 791676115, 1781863512, 1105079286, 549142576"
+    "1, 1244335972, 15217923, 1546885062, 2002651684",
+    "2147483647, 844801015, 1915574197, 726043576, 780612688"
   })
   void nextInt(
       final int seed, final int first, final int second, final int third, final int fourth) {
-    var rng = new PHPRandom(seed);
+    var rng = new PHPMTRandom(seed);
     assertThat(rng.nextInt(), equalTo(first));
     assertThat(rng.nextInt(), equalTo(second));
     assertThat(rng.nextInt(), equalTo(third));
@@ -27,19 +27,19 @@ class PHPRandomTest {
   }
 
   @Test
-  void nextIntRange() {
-    var rng = new PHPRandom(69420);
-    assertThat(rng.nextInt(5), is(1));
-    assertThat(rng.nextInt(10, 20), is(12));
-    assertThat(rng.nextInt(-5, 100), is(79));
+  void negativeSeed() {
+    var rng = new PHPMTRandom(-8008135);
+    assertThat(rng.nextInt(), equalTo(595078597));
+    assertThat(rng.nextInt(), equalTo(690674654));
+    assertThat(rng.nextInt(), equalTo(1259522838));
+    assertThat(rng.nextInt(), equalTo(277454392));
   }
 
   @Test
-  void array() {
-    var rng = new PHPRandom(6969);
-    var pick = rng.array(5, 3);
-    assertThat(pick[0], is(0));
-    assertThat(pick[1], is(2));
-    assertThat(pick[2], is(3));
+  void nextIntRange() {
+    var rng = new PHPMTRandom(69420);
+    assertThat(rng.nextInt(5), is(2));
+    assertThat(rng.nextInt(10, 20), is(11));
+    assertThat(rng.nextInt(-5, 100), is(32));
   }
 }
