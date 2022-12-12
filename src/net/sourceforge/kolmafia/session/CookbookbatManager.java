7af0ae78f58b8d3ed07e51bb47aa4f0a7354be92
horrible little slime <69secret69email69@gmail.com>
2022-12-12 18:52:21 -0500

    reset quest data on partial match

--- src/net/sourceforge/kolmafia/session/CookbookbatManager.java
+++ src/net/sourceforge/kolmafia/session/CookbookbatManager.java
@@ -8,30 +8,72 @@ import net.sourceforge.kolmafia.preferences.Preferences;
 public class CookbookbatManager {
   private CookbookbatManager() {}
 
-  private static Pattern[] COOKBOOKBAT_QUEST_PATTERNS = {
-    Pattern.compile(
-        "looks off in the distance before speaking, \"As I recall, (?<ingredient>.*) was common in (?<location>.*), back in my day.  Perhaps if you kill (?<monster>), you'll"),
-    Pattern.compile(
-        "My recollection is that (?<ingredient>.*) was often collected from (?<monster>.*)\\. If I recall correctly, you can hunt them in (?<location>.*)\\.\""),
-    Pattern.compile(
-        "If memory serves, (?<ingredient>.*) was very popular in (?<location>.*), during my time\\. Perhaps if you find (?<monster>.*), you'll collect one,"),
+  private record QuestPattern(
+      Pattern pattern, Boolean hasIngredient, Boolean hasMonster, Boolean hasLocation) {}
+  ;
+
+  private static QuestPattern[] COOKBOOKBAT_QUEST_PATTERNS = {
+    new QuestPattern(
+        Pattern.compile(
+            "looks off in the distance before speaking, \"As I recall, (?<ingredient>.*) was common in (?<location>.*), back in my day.  Perhaps if you kill (?<monster>), you'll"),
+        true,
+        true,
+        true),
+    new QuestPattern(
+        Pattern.compile(
+            "My recollection is that (?<ingredient>.*) was often collected from (?<monster>.*)\\. If I recall correctly, you can hunt them in (?<location>.*)\\.\""),
+        true,
+        true,
+        true),
+    new QuestPattern(
+        Pattern.compile(
+            "If memory serves, (?<ingredient>.*) was very popular in (?<location>.*), during my time\\. Perhaps if you find (?<monster>.*), you'll collect one,"),
+        true,
+        true,
+        true),
+    new QuestPattern(
+        Pattern.compile(
+            "rattles, \"If my ancient memory serves, I suggested looking in (?<location>.*)\\."),
+        false,
+        false,
+        true),
+    new QuestPattern(
+        Pattern.compile("croaks, \"If I recall, I suggested that you look for (?<monster>.*)\\.\""),
+        false,
+        true,
+        false),
+    new QuestPattern(
+        Pattern.compile(
+            "According to my memories, (?<monster>.*) at (?<location>.*) may have what"),
+        false,
+        true,
+        true)
   };
 
-  public static void wipeQuest() {
+  private static void wipeQuest() {
     Preferences.resetToDefault("cookbookbatQuestIngredient");
     Preferences.resetToDefault("cookbookbatQuestLocation");
     Preferences.resetToDefault("cookbookbatQuestMonster");
-    Preferences.resetToDefault("cookbookbatQuestTurns");
   }
 
   private static void updateQuestData(String ingredient, String location, String monster) {
-    Preferences.setString("cookbookbatQuestIngredient", ingredient);
-    Preferences.setString("cookbookbatQuestLocation", location);
-    Preferences.setString("cookbookbatQuestMonster", monster);
-    Preferences.resetToDefault("cookbookbatQuestTurns");
+    String currentIngredient = Preferences.getString("cookbookbatQuestIngredient");
+    String currentLocation = Preferences.getString("cookbookbatQuestLocation");
+    String currentMonster = Preferences.getString("cookbookbatQuestMonster");
+    if ((ingredient != null && currentIngredient != "" && ingredient != currentIngredient)
+        || (location != null && currentLocation != "" && location != currentLocation)
+        || (monster != null && currentMonster != "" && monster != currentMonster)) {
+      CookbookbatManager.wipeQuest();
+      Preferences.resetToDefault("cookbookbatQuestTurns");
+    }
+
+    if (ingredient != null) Preferences.setString("cookbookbatQuestIngredient", ingredient);
+    if (location != null) Preferences.setString("cookbookbatQuestLocation", location);
+    if (monster != null) Preferences.setString("cookbookbatQuestMonster", monster);
   }
 
   public static void parseResponse(String responseText) {
+
     Matcher questCompletedMatcher =
         Pattern.compile("As I recall, this is where I told you to look").matcher(responseText);
     if (questCompletedMatcher.find()) {
@@ -39,12 +81,19 @@ public class CookbookbatManager {
       return;
     }
 
-    for (Pattern pattern : COOKBOOKBAT_QUEST_PATTERNS) {
-      Matcher matcher = pattern.matcher(responseText);
+    for (QuestPattern questPattern : COOKBOOKBAT_QUEST_PATTERNS) {
+      Matcher matcher = questPattern.pattern.matcher(responseText);
       if (matcher.find()) {
-        String monsterWithArticle = matcher.group("monster");
-        String monster = MonsterDatabase.findMonster(monsterWithArticle).getName();
-        updateQuestData(matcher.group("ingredient"), matcher.group("location"), monster);
+        String ingredient = null;
+        String location = null;
+        String monster = null;
+
+        if (questPattern.hasIngredient) ingredient = matcher.group("ingredient");
+        if (questPattern.hasLocation) location = matcher.group("location");
+        if (questPattern.hasMonster)
+          monster = MonsterDatabase.findMonster(matcher.group("monster")).getName();
+
+        updateQuestData(ingredient, location, monster);
       }
     }
   }
