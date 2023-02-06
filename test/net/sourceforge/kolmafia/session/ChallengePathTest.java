ca1acb670c31ce816d9dffca6381612d769b36ab
libraryaddict <github@lib.co.nz>
2023-02-06 15:09:50 +1300

    Create failing coat of paint test

--- test/net/sourceforge/kolmafia/session/ChallengePathTest.java
+++ test/net/sourceforge/kolmafia/session/ChallengePathTest.java
@@ -4,15 +4,23 @@ import static internal.helpers.Networking.getPostRequestBody;
 import static internal.helpers.Player.withEquipped;
 import static internal.helpers.Player.withHttpClientBuilder;
 import static internal.helpers.Player.withPasswordHash;
+import static internal.helpers.Player.withProperty;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import internal.helpers.Cleanups;
 import internal.network.FakeHttpClientBuilder;
+import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ChallengePathTest {
+  @BeforeAll
+  public static void beforeAll() {
+    KoLCharacter.reset("ChallengePath");
+  }
+
   @Test
   void testCoatOfPaintCheckedAfterLeavingAvatarPath() {
     var builder = new FakeHttpClientBuilder();
@@ -21,7 +29,10 @@ public class ChallengePathTest {
         new Cleanups(
             withHttpClientBuilder(builder),
             withPasswordHash("test"),
-            withEquipped(ItemPool.COAT_OF_PAINT));
+            withEquipped(ItemPool.COAT_OF_PAINT),
+            withProperty(
+                "_coatOfPaintModifier",
+                "Experience (Muscle): +5, Cold Resistance: +2, Stench Damage: +20, Stench Spell Damage: +20, Familiar Weight: +5"));
 
     try (cleanups) {
       // Perform a reset as if we're leaving an avatar path
