d09ca864af4075cef4a88c0c4719b6b122d40dc9
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-02-08 17:04:13 +0000

    docs: add docs for all slot enumsets

--- src/net/sourceforge/kolmafia/session/EquipmentManager.java
+++ src/net/sourceforge/kolmafia/session/EquipmentManager.java
@@ -124,11 +124,12 @@ public class EquipmentManager {
     }
   }
 
-  // Count of slots visible on equipment page: HAT to FAMILIAR
+  /** "Normal" slots visible on equipment page: HAT to FAMILIAR. */
   public static final EnumSet<Slot> SLOTS = EnumSet.range(Slot.HAT, Slot.FAMILIAR);
+  /** Player slots visible on equipment page: HAT to ACCESSORY3. SLOTS without FAMILIAR. */
   public static final EnumSet<Slot> CORE_EQUIP_SLOTS = EnumSet.range(Slot.HAT, Slot.ACCESSORY3);
 
-  // Count of all equipment slots: HAT to BOOTSPUR
+  /** All slots except NONE and FAKEHAND. HAT to BOOTSPUR. */
   public static final EnumSet<Slot> ALL_SLOTS = EnumSet.range(Slot.HAT, Slot.BOOTSPUR);
 
   /** A list of all possible accessories. Is an interior list of equipmentLists for acc1, 2, 3 */
@@ -142,15 +143,19 @@ public class EquipmentManager {
   /** A list indexed by Slot of equipment we have previously equipped, in order */
   private static final Map<Slot, List<AdventureResult>> historyLists = new EnumMap<>(Slot.class);
 
+  /** All folder slots: 1 to 5. */
   public static final Set<Slot> FOLDER_SLOTS =
       EnumSet.of(Slot.FOLDER1, Slot.FOLDER2, Slot.FOLDER3, Slot.FOLDER4, Slot.FOLDER5);
 
+  /** All folder slots available in aftercore (i.e. not KoLHS): 1 to 3. */
   public static final Set<Slot> FOLDER_SLOTS_AFTERCORE =
       EnumSet.of(Slot.FOLDER1, Slot.FOLDER2, Slot.FOLDER3);
 
+  /** All accessory slots: 1 to 3. */
   public static final Set<Slot> ACCESSORY_SLOTS =
       EnumSet.of(Slot.ACCESSORY1, Slot.ACCESSORY2, Slot.ACCESSORY3);
 
+  /** All sticker slots: 1 to 3. */
   public static final Set<Slot> STICKER_SLOTS =
       EnumSet.of(Slot.STICKER1, Slot.STICKER2, Slot.STICKER3);
 
