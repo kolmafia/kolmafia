9b4be00f3466cac91ed5747b884890369c9b3069
BadHorseMonkey <58645293+BadHorseMonkey@users.noreply.github.com>
2023-02-13 01:44:58 -0600

    Two Crazy Random Summer Ring item parser

--- src/net/sourceforge/kolmafia/persistence/ItemDatabase.java
+++ src/net/sourceforge/kolmafia/persistence/ItemDatabase.java
@@ -31,6 +31,7 @@ import net.sourceforge.kolmafia.RequestThread;
 import net.sourceforge.kolmafia.RestrictedItemType;
 import net.sourceforge.kolmafia.StaticEntity;
 import net.sourceforge.kolmafia.VYKEACompanionData;
+import net.sourceforge.kolmafia.modifiers.ModifierList;
 import net.sourceforge.kolmafia.equipment.Slot;
 import net.sourceforge.kolmafia.modifiers.ModifierList;
 import net.sourceforge.kolmafia.modifiers.StringModifier;
