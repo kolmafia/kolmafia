9d2b4abffe6b6b3a9c6b21390a9e04d755a352e6
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-04-04 14:44:36 +0100

    tests: add test for request editor kit drops

--- test/net/sourceforge/kolmafia/MonsterDataTest.java
+++ test/net/sourceforge/kolmafia/MonsterDataTest.java
@@ -339,11 +339,14 @@ public class MonsterDataTest {
     @ParameterizedTest
     @CsvSource({
       // Test regular drops
+      "fluffy bunny, 'bunny liver (75)'",
       "skeleton with a mop, 'beer-soaked mop (10), ice-cold Willer (30), ice-cold Willer (30)'",
       // Test mix of pp and no pp
       "cheerless mime executive, 'crystalline cheer (100 no pp), crystalline cheer (100 no pp), crystalline cheer (100 no pp), crystalline cheer (100 no pp), crystalline cheer (100 no pp), crystalline cheer (100 no pp), crystalline cheer (100 no pp), warehouse key (0 pp only)'",
       // Test mix of item drops and bounty drops
       "novelty tropical skeleton, 'cherry (0), cherry (0), grapefruit (0), grapefruit (0), orange (0), orange (0), strawberry (0), strawberry (0), lemon (0), lemon (0), novelty fruit hat (0 cond), cherry stem (bounty)'",
+      // Test fractional drops
+      "stench zombie, 'Dreadsylvanian Almanac page (0 no mod), Freddy Kruegerand (0 no mod), muddy skirt (0.1 cond)'"
     })
     void itemDropsAreRenderedProperly(final String monsterName, final String itemDropString) {
       var monster = MonsterDatabase.findMonster(monsterName);
