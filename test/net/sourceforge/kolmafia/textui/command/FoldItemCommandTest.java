eee753450ee5e33f8607f08927243882274c8ca8
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-06-05 17:53:47 +0100

    feat: allow folding with the replica garbage tote

--- test/net/sourceforge/kolmafia/textui/command/FoldItemCommandTest.java
+++ test/net/sourceforge/kolmafia/textui/command/FoldItemCommandTest.java
@@ -1,14 +1,23 @@
 package net.sourceforge.kolmafia.textui.command;
 
+import static internal.helpers.HttpClientWrapper.getRequests;
+import static internal.helpers.Networking.assertPostRequest;
 import static internal.helpers.Player.withHP;
 import static internal.helpers.Player.withItem;
+import static internal.helpers.Player.withPath;
+import static internal.helpers.Player.withSkill;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
 
 import internal.helpers.Cleanups;
+import internal.helpers.HttpClientWrapper;
+import net.sourceforge.kolmafia.AscensionPath.Path;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.objectpool.SkillPool;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class FoldItemCommandTest extends AbstractCommandTestBase {
@@ -68,6 +77,65 @@ public class FoldItemCommandTest extends AbstractCommandTestBase {
       // We didn't give it anything to restore HP with, so we can use that to tell it tried
       assertErrorState();
       assertThat(output, containsString("Autorecovery failed."));
+    }
+  }
+
+  @Nested
+  class GarbageTote {
+    @Test
+    public void checkGarbageToteItem() {
+      var cleanups = new Cleanups(withItem(ItemPool.GARBAGE_TOTE), withSkill(SkillPool.TORSO));
+
+      try (cleanups) {
+        String output = execute("makeshift garbage shirt", true);
+        assertContinueState();
+        assertThat(output, containsString("January's Garbage Tote => makeshift garbage shirt"));
+      }
+    }
+
+    @Test
+    public void foldGarbageToteItem() {
+      HttpClientWrapper.setupFakeClient();
+      var cleanups = new Cleanups(withItem(ItemPool.GARBAGE_TOTE), withSkill(SkillPool.TORSO));
+
+      try (cleanups) {
+        execute("makeshift garbage shirt");
+        assertContinueState();
+
+        var requests = getRequests();
+        assertThat(requests, hasSize(2));
+        assertPostRequest(requests.get(0), "/inv_use.php", "whichitem=9690");
+        assertPostRequest(requests.get(1), "/choice.php", "whichchoice=1275&option=5");
+      }
+    }
+
+    @Test
+    public void checkReplicaGarbageToteItem() {
+      var cleanups =
+          new Cleanups(withItem(ItemPool.REPLICA_GARBAGE_TOTE), withPath(Path.LEGACY_OF_LOATHING));
+
+      try (cleanups) {
+        String output = execute("tinsel tights", true);
+        assertContinueState();
+        assertThat(output, containsString("replica January's Garbage Tote => tinsel tights"));
+      }
+    }
+
+    @Test
+    public void foldReplicaGarbageToteItem() {
+      HttpClientWrapper.setupFakeClient();
+      var cleanups =
+          new Cleanups(withItem(ItemPool.REPLICA_GARBAGE_TOTE), withPath(Path.LEGACY_OF_LOATHING));
+
+      try (cleanups) {
+        execute("tinsel tights");
+        assertContinueState();
+
+        var requests = getRequests();
+        assertThat(requests, hasSize(2));
+        assertPostRequest(requests.get(0), "/inv_use.php", "whichitem=11238");
+        assertPostRequest(requests.get(1), "/choice.php", "whichchoice=1275&option=3");
+      }
     }
   }
 }
