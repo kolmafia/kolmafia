13b5711bfb0dcf4330112e0f6884fe7c5656fb61
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2023-07-05 12:13:18 -0400

    Add a button to clear historical ping data

--- src/net/sourceforge/kolmafia/swingui/panel/PingOptionsPanel.java
+++ src/net/sourceforge/kolmafia/swingui/panel/PingOptionsPanel.java
@@ -1,5 +1,12 @@
 package net.sourceforge.kolmafia.swingui.panel;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import net.sourceforge.kolmafia.preferences.Preferences;
 import net.sourceforge.kolmafia.session.PingManager.PingTest;
 import net.sourceforge.kolmafia.swingui.widget.PreferenceButtonGroup;
 import net.sourceforge.kolmafia.swingui.widget.PreferenceCheckBox;
@@ -13,6 +20,8 @@ public class PingOptionsPanel extends ConfigQueueingPanel {
     super();
 
     this.queue(new PingHistory());
+    this.queue(new PingResetButton());
+    this.queue(this.newSeparator());
     this.queue(
         new PreferenceCheckBox("pingLogin", "Run ping test at login to measure connection lag"));
     this.queue(
@@ -24,6 +33,39 @@ public class PingOptionsPanel extends ConfigQueueingPanel {
             "pingLoginThreshold", 0, "Allowed threshold above minimum historical lag"));
 
     this.makeLayout();
+  }
+
+  private static class PingResetButton extends JPanel {
+    private final JButton button = new JButton("Clear");
+
+    public PingResetButton() {
+      configure();
+      makeLayout();
+    }
+
+    private void configure() {
+      this.setLayout(new FlowLayout(FlowLayout.LEFT, 1, 1));
+      this.addActionListener(
+          e -> {
+            Preferences.setString("pingLongest", "");
+            Preferences.setString("pingShortest", "");
+          });
+    }
+
+    public void addActionListener(ActionListener a) {
+      this.button.addActionListener(a);
+    }
+
+    private void makeLayout() {
+      JLabel label = new JLabel("Reset historical ping times");
+      this.add(label);
+      this.add(this.button);
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+      return this.getPreferredSize();
+    }
   }
 
   private static class PingHistory extends PreferenceTextArea {
