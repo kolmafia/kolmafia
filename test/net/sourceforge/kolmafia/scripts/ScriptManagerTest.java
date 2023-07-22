77eba48a2ad6622791dd63ef3a3c35a78534e542
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-07-22 11:15:20 +0100

    chore: fmt

--- test/net/sourceforge/kolmafia/scripts/ScriptManagerTest.java
+++ test/net/sourceforge/kolmafia/scripts/ScriptManagerTest.java
@@ -8,27 +8,27 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ScriptManagerTest {
-    @Nested
-    class ProjectIdentifier {
-        @Test
-        void setForSourceForge() throws URISyntaxException {
-            URI uri = new URI("https://svn.code.sf.net/p/zlib/code");
-            var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
-            assertEquals("zlib", ident);
-        }
-
-        @Test
-        void setForGitHub() throws URISyntaxException {
-            URI uri = new URI("https://github.com/soolar/CONSUME.ash");
-            var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
-            assertEquals("soolar-CONSUME.ash", ident);
-        }
-
-        @Test
-        void setForFallback() throws URISyntaxException {
-            URI uri = new URI("https://git-codecommit.us-east-2.amazonaws.com/v1/repos/MyDemoRepo");
-            var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
-            assertEquals("v1-repos-MyDemoRepo", ident);
-        }
+  @Nested
+  class ProjectIdentifier {
+    @Test
+    void setForSourceForge() throws URISyntaxException {
+      URI uri = new URI("https://svn.code.sf.net/p/zlib/code");
+      var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
+      assertEquals("zlib", ident);
     }
+
+    @Test
+    void setForGitHub() throws URISyntaxException {
+      URI uri = new URI("https://github.com/soolar/CONSUME.ash");
+      var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
+      assertEquals("soolar-CONSUME.ash", ident);
+    }
+
+    @Test
+    void setForFallback() throws URISyntaxException {
+      URI uri = new URI("https://git-codecommit.us-east-2.amazonaws.com/v1/repos/MyDemoRepo");
+      var ident = ScriptManager.getProjectIdentifier(uri.getHost(), uri.getPath());
+      assertEquals("v1-repos-MyDemoRepo", ident);
+    }
+  }
 }
