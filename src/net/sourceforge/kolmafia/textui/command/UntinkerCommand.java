80881f4b89d8fcb8a8393525633ca7f1146185b6
Sam Gaus <sam@gaus.co.uk>
2023-07-23 11:16:29 +0100

    Use loathing legion screwdriver if you have one (does not fold yet)

--- src/net/sourceforge/kolmafia/textui/command/UntinkerCommand.java
+++ src/net/sourceforge/kolmafia/textui/command/UntinkerCommand.java
@@ -1,10 +1,14 @@
 package net.sourceforge.kolmafia.textui.command;
 
 import net.sourceforge.kolmafia.AdventureResult;
+import net.sourceforge.kolmafia.KoLmafia;
 import net.sourceforge.kolmafia.RequestThread;
+import net.sourceforge.kolmafia.objectpool.ItemPool;
 import net.sourceforge.kolmafia.persistence.ItemFinder;
 import net.sourceforge.kolmafia.persistence.ItemFinder.Match;
+import net.sourceforge.kolmafia.request.GenericRequest;
 import net.sourceforge.kolmafia.request.UntinkerRequest;
+import net.sourceforge.kolmafia.session.InventoryManager;
 
 public class UntinkerCommand extends AbstractCommand {
   public UntinkerCommand() {
@@ -18,11 +22,32 @@ public class UntinkerCommand extends AbstractCommand {
       return;
     }
 
+    var useLoathingLegion =
+        InventoryManager.getAccessibleCount(ItemPool.LOATHING_LEGION_UNIVERSAL_SCREWDRIVER) > 0;
+    if (useLoathingLegion) {
+      InventoryManager.retrieveItem(ItemPool.LOATHING_LEGION_UNIVERSAL_SCREWDRIVER);
+    }
+
     AdventureResult[] itemList =
         ItemFinder.getMatchingItemList(parameters, true, null, Match.UNTINKER);
     for (AdventureResult item : itemList) {
       var count = item.getCount();
-      if (count > 0) {
+      if (count <= 0) continue;
+
+      if (useLoathingLegion) {
+        for (int i = 1; i <= count && KoLmafia.permitsContinue(); ++i) {
+          var req = new GenericRequest("");
+          req.constructURLString(
+              "inv_use.php?pwd="
+                  + GenericRequest.passwordHash
+                  + "&ajax=1&whichitem="
+                  + ItemPool.LOATHING_LEGION_UNIVERSAL_SCREWDRIVER
+                  + "&action=screw&dowhichitem="
+                  + item.getItemId(),
+              false);
+          RequestThread.postRequest(req);
+        }
+      } else {
         RequestThread.postRequest(new UntinkerRequest(item.getItemId(), count));
       }
     }
