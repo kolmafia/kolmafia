0adb71bc4a2f8894e64920c81a25fc3eb5c7ae95
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2023-08-01 14:17:13 -0400

    Put TopMenuStyle detection under a debug flag (#1869)

--- test/net/sourceforge/kolmafia/webui/TopMenuDecoratorTest.java
+++ test/net/sourceforge/kolmafia/webui/TopMenuDecoratorTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 import internal.helpers.Cleanups;
 import internal.helpers.RequestLoggerOutput;
 import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.RequestEditorKit;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import net.sourceforge.kolmafia.request.GenericRequest;
@@ -20,6 +21,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junitpioneer.jupiter.cartesian.CartesianTest;
 import org.junitpioneer.jupiter.cartesian.CartesianTest.Enum;
 import org.junitpioneer.jupiter.cartesian.CartesianTest.Values;
@@ -53,12 +56,13 @@ class TopMenuDecoratorTest {
       };
     }
 
-    @CartesianTest
-    void awesomeMenuIsAwesome(
-        @Values(strings = {"Veracity", "Captain Scotch"}) String player, @Enum TopMenuStyle style) {
+    @ParameterizedTest
+    @ValueSource(strings = {"Veracity", "Captain Scotch"})
+    void awesomeMenuIsAwesome(String player) {
       var cleanups =
           new Cleanups(
-              withTopMenuStyle(style),
+              withTopMenuStyle(TopMenuStyle.FANCY),
+              withProperty("debugTopMenuStyle", true),
               withProperty("relayAddsQuickScripts", true),
               withProperty("scriptlist", "restore hp | restore mp"));
       try (cleanups) {
@@ -66,14 +70,14 @@ class TopMenuDecoratorTest {
         String input = html("request/" + playerToTextFile(player) + ".html");
         String output = RequestEditorKit.getFeatureRichHTML(location, input);
 
-        // We detected that we have an awesomemenu and corrected it
+        // We detected that we have an awesomemenu
         assertEquals(TopMenuStyle.FANCY, GenericRequest.topMenuStyle);
 
         // We inserted a "quick scripts" dropdown
         assertTrue(output.contains("scriptbar"));
 
-        // We do not test for a relay script menu because that looks the
-        // "relay" directory for scripts starting with "relay_".
+        // We do not test for a relay script menu because that looks in
+        // the "relay" directory for scripts starting with "relay_".
 
         // Given that the response text is from awesomemenu.php, we are
         // only doing the RIGHT thing if TopMenuStype is FANCY, but
@@ -86,6 +90,7 @@ class TopMenuDecoratorTest {
       var cleanups =
           new Cleanups(
               withTopMenuStyle(TopMenuStyle.FANCY),
+              withProperty("debugTopMenuStyle", true),
               withProperty("relayAddsQuickScripts", true),
               withProperty("scriptlist", "restore hp | restore mp"));
       try (cleanups) {
@@ -103,11 +108,76 @@ class TopMenuDecoratorTest {
   }
 
   @Nested
+  class CompactMenu {
+    @Test
+    void compactMenuIsCompact() {
+      var cleanups =
+          new Cleanups(
+              withTopMenuStyle(TopMenuStyle.COMPACT),
+              withProperty("relayAddsQuickScripts", true),
+              withProperty("scriptlist", "restore hp | restore mp"));
+      try (cleanups) {
+        String location = "topmenu.php";
+        String input = html("request/test_compact_topmenu.html");
+        String output = RequestEditorKit.getFeatureRichHTML(location, input);
+
+        // We mafiatized the GOTO menu
+        for (String[] item : KoLConstants.GOTO_MENU) {
+          String tag = item[0];
+          assertTrue(output.contains(tag));
+        }
+
+        // We inserted a "quick scripts" dropdown
+        assertTrue(output.contains("scriptbar"));
+
+        // We do not test for a relay script menu because that looks in
+        // the "relay" directory for scripts starting with "relay_".
+
+        // Given that the response text is from awesomemenu.php, we are
+        // only doing the RIGHT thing if TopMenuStype is FANCY, but
+        // there is no way to check that here.
+      }
+    }
+  }
+
+  @Nested
+  class NormalMenu {
+    @Test
+    void normalMenuIsNormal() {
+      var cleanups =
+          new Cleanups(
+              withTopMenuStyle(TopMenuStyle.NORMAL),
+              withProperty("relayAddsQuickScripts", true),
+              withProperty("scriptlist", "restore hp | restore mp"));
+      try (cleanups) {
+        String location = "topmenu.php";
+        String input = html("request/test_normal_topmenu.html");
+        String output = RequestEditorKit.getFeatureRichHTML(location, input);
+
+        // We inserted a "quick scripts" dropdown
+        assertTrue(output.contains("scriptbar"));
+
+        // We do not test for a relay script menu because that looks in
+        // the "relay" directory for scripts starting with "relay_".
+
+        // Given that the response text is from awesomemenu.php, we are
+        // only doing the RIGHT thing if TopMenuStype is FANCY, but
+        // there is no way to check that here.
+      }
+    }
+  }
+
+  @Nested
   class MenuStyle {
     @CartesianTest
     void canDeriveMenuStyle(
         @Values(strings = {"normal", "compact", "fancy"}) String styleName,
         @Enum TopMenuStyle style) {
+
+      // Debug code to check/correct TopMenuStyle every time we decorate
+      // the topmenu found a bug, but is no longer needed. Therefore, it
+      // is now under the "debugTopMenuStyle" property.
+
       String location = "";
       var actualStyle = TopMenuStyle.UNKNOWN;
       switch (styleName) {
@@ -132,6 +202,7 @@ class TopMenuDecoratorTest {
       var cleanups =
           new Cleanups(
               withTopMenuStyle(style),
+              withProperty("debugTopMenuStyle", true),
               withProperty("relayAddsQuickScripts", true),
               withProperty("scriptlist", "restore hp | restore mp"));
       try (cleanups) {
@@ -152,8 +223,8 @@ class TopMenuDecoratorTest {
         // We insert a "quick scripts" dropdown for all three menu styles
         assertTrue(decorated.contains("scriptbar"));
 
-        // We do not test for a relay script menu because that looks the
-        // "relay" directory for scripts starting with "relay_".
+        // We do not test for a relay script menu because that looks in
+        // the "relay" directory for scripts starting with "relay_".
       }
     }
   }
