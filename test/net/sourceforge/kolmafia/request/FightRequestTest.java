bcd42e77d69b5376ac9bbf44caa47f35b1fc307d
Samuel Gaus <sam@gaus.co.uk>
2023-09-08 10:31:56 +0100

    Revert "Changed detect fight when CAB is enabled and adding the script button or not and added test (#1945)" (#1946)
    This reverts commit 8e70e19a61aa926ea5f3468590472bba9787c4a4.

--- test/net/sourceforge/kolmafia/request/FightRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FightRequestTest.java
@@ -217,16 +217,6 @@ public class FightRequestTest {
   }
 
   @Test
-  public void CABandOldForm() {
-    var cleanups = new Cleanups(withProperty("serverAddsCustomCombat", true), withFight());
-    try (cleanups) {
-      parseCombatData("request/test_fight_cab_old_form.html");
-      int fightRound = FightRequest.getCurrentRound();
-      assertEquals(1, fightRound, "Incorrectly retrieved current round");
-    }
-  }
-
-  @Test
   public void hareAdv() {
     var cleanups =
         new Cleanups(
