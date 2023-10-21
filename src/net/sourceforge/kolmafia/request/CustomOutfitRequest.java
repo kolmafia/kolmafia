70ce088df76aa254d9ff020c1b209226eb638d5d
Sam Gaus <sam@gaus.co.uk>
2023-10-21 10:27:05 +0100

    Add chances for outfit treats

--- src/net/sourceforge/kolmafia/request/CustomOutfitRequest.java
+++ src/net/sourceforge/kolmafia/request/CustomOutfitRequest.java
@@ -1,5 +1,7 @@
 package net.sourceforge.kolmafia.request;
 
+import java.util.Arrays;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import net.java.dev.spellcast.utilities.SortedListModel;
@@ -75,15 +77,12 @@ public class CustomOutfitRequest extends GenericRequest {
       SpecialOutfit outfit = new SpecialOutfit(-outfitId, outfitName);
 
       String[] outfitPieces = entryMatcher.group(3).split("<br>");
-      for (int i = 0; i < outfitPieces.length; ++i) {
-        String pieceName = outfitPieces[i];
-        if (pieceName.equals("")) {
-          continue;
-        }
-        int itemId = ItemDatabase.getItemId(pieceName);
-        AdventureResult piece = ItemPool.get(itemId);
-        outfit.addPiece(piece);
-      }
+
+      Arrays.stream(outfitPieces)
+        .filter(Predicate.not(String::isEmpty))
+        .map(ItemDatabase::getItemId)
+        .map(ItemPool::get)
+        .forEach(outfit::addPiece);
 
       outfits.add(outfit);
     }
