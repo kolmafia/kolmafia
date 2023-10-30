904cf6cfee181594074ebc3b8bb32a14df4d03fa
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-10-30 16:08:48 +0000

    chore: refuse to instantiate static class

--- src/net/sourceforge/kolmafia/utilities/Statics.java
+++ src/net/sourceforge/kolmafia/utilities/Statics.java
@@ -3,6 +3,8 @@ package net.sourceforge.kolmafia.utilities;
 import net.sourceforge.kolmafia.persistence.DateTimeManager;
 
 public class Statics {
+  private Statics() {}
+
   public static DateTimeManager DateTimeManager = new DateTimeManager();
 
   static void setDateTimeManager(DateTimeManager dtm) {
