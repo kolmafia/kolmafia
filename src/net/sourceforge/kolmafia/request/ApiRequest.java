a9cadf3eec6fdf1344a609bddb3fe529d3f8b7b1
Sam Gaus <sam@gaus.co.uk>
2023-11-02 17:38:54 +0000

    We got florist friar too

--- src/net/sourceforge/kolmafia/request/ApiRequest.java
+++ src/net/sourceforge/kolmafia/request/ApiRequest.java
@@ -421,7 +421,8 @@ public class ApiRequest extends GenericRequest {
           Map.entry("maydaykit", Map.entry("hasMaydayContract", "")),
           Map.entry("autumnaton", Map.entry("hasAutumnaton", "")),
           Map.entry("tunnelofloveiotm", Map.entry("loveTunnelAvailable", "_loveTunnelToday")),
-          Map.entry("ltt", Map.entry("telegraphOfficeAvailable", "_telegraphOfficeToday")));
+          Map.entry("ltt", Map.entry("telegraphOfficeAvailable", "_telegraphOfficeToday")),
+          Map.entry("floristfriar", Map.entry("ownsFloristFriar", "")));
 
   private static void parseCoolItems(final String coolItems) {
     if (coolItems == null) {
