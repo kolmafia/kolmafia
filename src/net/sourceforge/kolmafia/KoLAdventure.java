8e8199589eadab1dccc6e587dd4d0a2f1bb90f60
Malibu Stacey <50261170+Malibu-Stacey@users.noreply.github.com>
2023-11-22 13:36:37 -0800

    shadow rifts aren't accessible in KoE even if they exist

--- src/net/sourceforge/kolmafia/KoLAdventure.java
+++ src/net/sourceforge/kolmafia/KoLAdventure.java
@@ -886,6 +886,11 @@ public class KoLAdventure implements Comparable<KoLAdventure>, Runnable {
         // need payphone in inventory or to be in ASoL
         return false;
       }
+      
+      if (KoLCharacter.isKingdomOfExploathing()) {
+        // KoE gets no access to it regardless for some reason known only to KoL devs.
+        return false;
+      }
 
       // These are "place.php" visits.
       ShadowRift rift = ShadowRift.findAdventureName(this.adventureName);
