9957d200a34773a3dc724ee95e804eca257e04ce
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2023-12-17 20:44:32 +0000

    feat: easy coinmasters, gunwale whalegun (#2107)
    * feat: candies, gunwale whalegun
    
    * feat: add easy coinmasters

---
+++ src/net/sourceforge/kolmafia/request/Crimbo23PirateFactoryRequest.java
@@ -0,0 +1,74 @@
+package net.sourceforge.kolmafia.request;
+
+import java.util.regex.Pattern;
+import net.sourceforge.kolmafia.AdventureResult;
+import net.sourceforge.kolmafia.CoinmasterData;
+import net.sourceforge.kolmafia.objectpool.ItemPool;
+
+public class Crimbo23PirateFactoryRequest extends CoinMasterRequest {
+  public static final String master = "Crimbuccaneer Foundry";
+
+  private static final Pattern TOKEN_PATTERN =
+      Pattern.compile("([\\d,]+) Crimbuccaneer pieces? of 12");
+  public static final AdventureResult TOKEN = ItemPool.get(ItemPool.CRIMBUCCANEER_PIECE_OF_12, 1);
+
+  public static final CoinmasterData DATA =
+      new CoinmasterData(master, "crimbo23_pirate_factory", Crimbo23PirateFactoryRequest.class)
+          .withToken("Crimbuccaneer piece of 12")
+          .withTokenTest("no Crimbuccaneer pieces of 12")
+          .withTokenPattern(TOKEN_PATTERN)
+          .withItem(TOKEN)
+          .withShopRowFields(master, "crimbo23_pirate_factory")
+          .withNeedsPasswordHash(true);
+
+  public Crimbo23PirateFactoryRequest() {
+    super(DATA);
+  }
+
+  public Crimbo23PirateFactoryRequest(final boolean buying, final AdventureResult[] attachments) {
+    super(DATA, buying, attachments);
+  }
+
+  public Crimbo23PirateFactoryRequest(final boolean buying, final AdventureResult attachment) {
+    super(DATA, buying, attachment);
+  }
+
+  public Crimbo23PirateFactoryRequest(final boolean buying, final int itemId, final int quantity) {
+    super(DATA, buying, itemId, quantity);
+  }
+
+  @Override
+  public void processResults() {
+    parseResponse(this.getURLString(), this.responseText);
+  }
+
+  public static void parseResponse(final String location, final String responseText) {
+    if (!location.contains("whichshop=" + DATA.getNickname())) {
+      return;
+    }
+
+    CoinmasterData data = DATA;
+
+    String action = GenericRequest.getAction(location);
+    if (action != null) {
+      CoinMasterRequest.parseResponse(data, location, responseText);
+      return;
+    }
+
+    // Parse current coin balances
+    CoinMasterRequest.parseBalance(data, responseText);
+  }
+
+  public static String accessible() {
+    return null;
+  }
+
+  public static final boolean registerRequest(final String urlString) {
+    if (!urlString.startsWith("shop.php")
+        || !urlString.contains("whichshop=" + DATA.getNickname())) {
+      return false;
+    }
+
+    return CoinMasterRequest.registerRequest(DATA, urlString, true);
+  }
+}
