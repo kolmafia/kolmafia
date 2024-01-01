69232dec3d7a6bbe4c2209769a0dbf666c450e85
Joshua Dietrich <jdietrich@ascentos.ai>
2024-01-01 06:31:59 -0800

    Added an exit condition if the WitchessRequest didn't get a puzzle

--- src/net/sourceforge/kolmafia/session/WitchessManager.java
+++ src/net/sourceforge/kolmafia/session/WitchessManager.java
@@ -10,12 +10,12 @@ import net.sourceforge.kolmafia.request.WitchessRequest;
 public abstract class WitchessManager {
 	public WitchessManager() {}
 
-	public static final String solveDailyPuzzles() {
+	public static final void solveDailyPuzzles() {
 		var campground = new CampgroundRequest();
 		campground.run();
 		if (!campground.responseText.contains("chesstable.gif")) {
 			KoLmafia.updateDisplay("You don't have a Witchess Set installed in your campground.");
-			return null;
+			return;
 		}
 
 		String[] solvedPuzzles = new String[5];
@@ -24,6 +24,11 @@ public abstract class WitchessManager {
 			WitchessRequest request = new WitchessRequest(String.valueOf(i));
 			request.run();
 			int puzzleId = request.getThisPuzzle();
+
+			if (puzzleId == -1) {
+				// If the puzzleId is -1, we should have already told the user why in the WitchessRequest class, and should simply exit this method now.
+				return;
+			}
 
 			if (request.getIsSolved()) {
 				solvedPuzzles[i - 1] = String.valueOf(puzzleId);
@@ -38,10 +43,13 @@ public abstract class WitchessManager {
 			solRequest.addFormField("number", String.valueOf(puzzleId));
 			solRequest.run();
 
-			solvedPuzzles[i] = String.valueOf(puzzleId);
+			if (solRequest.responseText.startsWith("[true")) {
+				solvedPuzzles[i - 1] = String.valueOf(puzzleId);
+			} else {
+				// The puzzle wasn't solved, we need to handle an abort here?
+			}
 		}
 
 		KoLmafia.updateDisplay("Solved daily Witchess puzzles: " + String.join(", ", solvedPuzzles));
-		return null;
 	}
 }
