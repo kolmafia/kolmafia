a7e515712f26c429e42a174f99e45dcf947d958c
Joshua Dietrich <jdietrich@ascentos.ai>
2024-01-03 15:40:40 -0800

    Ran spotlessApply

--- test/net/sourceforge/kolmafia/request/WitchessRequestTest.java
+++ test/net/sourceforge/kolmafia/request/WitchessRequestTest.java
@@ -6,15 +6,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 import internal.helpers.Cleanups;
+import java.util.stream.Stream;
 import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 public class WitchessRequestTest {
   @BeforeAll
@@ -24,38 +22,37 @@ public class WitchessRequestTest {
   }
 
   private static Stream<Arguments> generateTests() {
-	  return Stream.of(
-			  Arguments.of(23, true),
-			  Arguments.of(23, false),
-			  Arguments.of(56, true),
-			  Arguments.of(56, false),
-			  Arguments.of(63, true),
-			  Arguments.of(63, false),
-			  Arguments.of(66, true),
-			  Arguments.of(66, false),
-			  Arguments.of(67, true),
-			  Arguments.of(67, false),
-			  Arguments.of(68, true),
-			  Arguments.of(68, false),
-			  Arguments.of(70, true),
-			  Arguments.of(70, false),
-			  Arguments.of(71, true),
-			  Arguments.of(71, false),
-			  Arguments.of(78, true),
-			  Arguments.of(78, false),
-			  Arguments.of(79, true),
-			  Arguments.of(79, false),
-			  Arguments.of(82, true),
-			  Arguments.of(82, false),
-			  Arguments.of(84, true),
-			  Arguments.of(84, false),
-			  Arguments.of(87, true),
-			  Arguments.of(87, false),
-			  Arguments.of(89, true),
-			  Arguments.of(89, false),
-			  Arguments.of(93, true),
-			  Arguments.of(93, false)
-	  );
+    return Stream.of(
+        Arguments.of(23, true),
+        Arguments.of(23, false),
+        Arguments.of(56, true),
+        Arguments.of(56, false),
+        Arguments.of(63, true),
+        Arguments.of(63, false),
+        Arguments.of(66, true),
+        Arguments.of(66, false),
+        Arguments.of(67, true),
+        Arguments.of(67, false),
+        Arguments.of(68, true),
+        Arguments.of(68, false),
+        Arguments.of(70, true),
+        Arguments.of(70, false),
+        Arguments.of(71, true),
+        Arguments.of(71, false),
+        Arguments.of(78, true),
+        Arguments.of(78, false),
+        Arguments.of(79, true),
+        Arguments.of(79, false),
+        Arguments.of(82, true),
+        Arguments.of(82, false),
+        Arguments.of(84, true),
+        Arguments.of(84, false),
+        Arguments.of(87, true),
+        Arguments.of(87, false),
+        Arguments.of(89, true),
+        Arguments.of(89, false),
+        Arguments.of(93, true),
+        Arguments.of(93, false));
   }
 
   @ParameterizedTest
