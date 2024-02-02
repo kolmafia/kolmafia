0130815bcdec3d8267774248ee0c3f12441081ed
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2024-02-02 09:19:28 -0600

    Maximizer shouldn't offer grim buffs while standard restricted (#2196)

--- src/net/sourceforge/kolmafia/maximizer/Maximizer.java
+++ src/net/sourceforge/kolmafia/maximizer/Maximizer.java
@@ -1129,6 +1129,9 @@ public class Maximizer {
           duration = 20;
           usesRemaining = (15 - Preferences.getInteger("_deckCardsDrawn")) / 5;
         } else if (cmd.startsWith("grim")) {
+          if (!StandardRequest.isAllowed(RestrictedItemType.FAMILIARS, "Grim Brother")) {
+            continue;
+          }
           var fam = KoLCharacter.ownedFamiliar(FamiliarPool.GRIM_BROTHER);
           if (fam.isEmpty()) {
             if (limitMode.limitFamiliars()) {
