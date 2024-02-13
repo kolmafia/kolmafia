62e1ba3c80e2aa2aab848714ec4817f9d227ad5c
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-02-13 10:22:25 -0500

    spotless

--- test/net/sourceforge/kolmafia/textui/RuntimeLibraryTest.java
+++ test/net/sourceforge/kolmafia/textui/RuntimeLibraryTest.java
@@ -1397,26 +1397,27 @@ public class RuntimeLibraryTest extends AbstractCommandTestBase {
       }
     }
   }
+
   @Test
   public void replaceholder() {
     String maxStr =
-            "5item,meat,0.5initiative,0.1da 1000max,dr,0.5all res,1.5mainstat,-fumble,mox,0.4hp,0.2mp 1000max,3mp regen,0.25spell damage,1.75spell damage percent,2familiar weight,5familiar exp,10exp,5Mysticality experience percent,200combat 20max,+200bonus mafia thumb ring";
+        "5item,meat,0.5initiative,0.1da 1000max,dr,0.5all res,1.5mainstat,-fumble,mox,0.4hp,0.2mp 1000max,3mp regen,0.25spell damage,1.75spell damage percent,2familiar weight,5familiar exp,10exp,5Mysticality experience percent,200combat 20max,+200bonus mafia thumb ring";
     var cleanups =
-            new Cleanups(
-                    withEquippableItem("candy cane sword cane"),
-                    withEquippableItem("pasta spoon"),
-                    withEquippableItem("Rain-Doh violet bo"),
-                    withEquippableItem("Rain-Doh yellow laser gun"),
-                    withEquippableItem("saucepan"),
-                    withEquippableItem("toy accordion"),
-                    withEquippableItem("turtle totem"),
-                    withEquippableItem("psychic's crystal ball"),
-                    withEquippableItem("Rain-Doh green lantern"),
-                    withEquippableItem("stuffed baby gravy fairy"),
-                    withEquippableItem("stuffed key"),
-                    withEquippableItem("unbreakable umbrella (broken)"),
-                    withStats(2, 27, 1),
-                    withSkill(SkillPool.MASTER_OF_THE_SURPRISING_FIST));
+        new Cleanups(
+            withEquippableItem("candy cane sword cane"),
+            withEquippableItem("pasta spoon"),
+            withEquippableItem("Rain-Doh violet bo"),
+            withEquippableItem("Rain-Doh yellow laser gun"),
+            withEquippableItem("saucepan"),
+            withEquippableItem("toy accordion"),
+            withEquippableItem("turtle totem"),
+            withEquippableItem("psychic's crystal ball"),
+            withEquippableItem("Rain-Doh green lantern"),
+            withEquippableItem("stuffed baby gravy fairy"),
+            withEquippableItem("stuffed key"),
+            withEquippableItem("unbreakable umbrella (broken)"),
+            withStats(2, 27, 1),
+            withSkill(SkillPool.MASTER_OF_THE_SURPRISING_FIST));
     String out;
     String cmd;
     cmd = "maximize(\"" + maxStr + "\", false)";
@@ -1428,5 +1429,4 @@ public class RuntimeLibraryTest extends AbstractCommandTestBase {
     assertTrue(out.contains("Folding umbrella"));
     assertTrue(out.contains("Holding unbreakable umbrella..."));
   }
-
 }
