1b8ebbcf26e2b90bfa16f3241afd7ae44b454012
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2024-03-11 13:26:33 -0400

    No Campground in WereProfessor. Improved Fulminate warning

--- src/net/sourceforge/kolmafia/objectpool/ItemPool.java
+++ src/net/sourceforge/kolmafia/objectpool/ItemPool.java
@@ -2391,6 +2391,7 @@ public class ItemPool {
   public static final int DETARTRATED_ANHYDROUS_SUBLICALC = 7489;
   public static final int TRIATOMACEOUS_DUST = 7490;
   public static final int BOTTLE_OF_CHATEAU_DE_VINEGAR = 7491;
+  public static final int BLASTING_SODA = 7492;
   public static final int UNSTABLE_FULMINATE = 7493;
   public static final int WINE_BOMB = 7494;
   public static final int MORTAR_DISSOLVING_RECIPE = 7495;
