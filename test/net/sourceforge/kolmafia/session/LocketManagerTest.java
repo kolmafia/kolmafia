3f5eacc939b5146fbc23ec5e642fe3d22a1415d7
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2024-03-12 14:37:37 -0400

    disambiguate monster names in combat lover's locket (#2234)

--- test/net/sourceforge/kolmafia/session/LocketManagerTest.java
+++ test/net/sourceforge/kolmafia/session/LocketManagerTest.java
@@ -6,6 +6,7 @@ import static internal.matchers.Preference.isSetTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import net.sourceforge.kolmafia.persistence.MonsterDatabase;
 import net.sourceforge.kolmafia.preferences.Preferences;
@@ -61,5 +62,33 @@ public class LocketManagerTest {
         assertThat(LocketManager.getFoughtMonsters(), empty());
       }
     }
+  }
+
+  @Test
+  public void canDecorateAmbiguousMonsters() {
+    String html = html("request/test_choice_reminisce_decorate_gremlins.html");
+    StringBuffer buffer = new StringBuffer(html);
+
+    // <option value="548" >batwinged gremlin</option>
+    // <option value="549" >batwinged gremlin</option>
+    // <option value="546" >erudite gremlin</option>
+    // <option value="547" >erudite gremlin</option>
+    // <option value="552" >spider gremlin</option>
+    // <option value="553" >spider gremlin</option>
+    // <option value="550" >vegetable gremlin</option>
+    // <option value="551" >vegetable gremlin</option>
+
+    assertTrue(buffer.indexOf("<option value=\"549\" >batwinged gremlin</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"547\" >erudite gremlin</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"553\" >spider gremlin</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"551\" >vegetable gremlin</option>") != -1);
+
+    // Ultimately calls LocketManager.decorateMonsterDropdown(buffer);
+    ChoiceAdventures.decorateChoice(1463, buffer, true);
+
+    assertTrue(buffer.indexOf("<option value=\"549\" >batwinged gremlin (tool)</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"547\" >erudite gremlin (tool)</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"553\" >spider gremlin (tool)</option>") != -1);
+    assertTrue(buffer.indexOf("<option value=\"551\" >vegetable gremlin (tool)</option>") != -1);
   }
 }
