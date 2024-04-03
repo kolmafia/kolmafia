ef95f2701022045e39dc186eb582f3b0bc6a6217
Sam Gaus <sam@gaus.co.uk>
2024-04-03 13:34:59 +0100

    Increase coverage

--- test/root/scripts/monster_parts.js
+++ test/root/scripts/monster_parts.js
@@ -1,5 +1,7 @@
 const { print } = require("kolmafia");
 
+print("$monster[none] has no parts: " + (Monster.none.parts.length === 0 ? "true" : "false"));
+
 const actual = Monster.get("Crimbuccaneer retiree").parts;
 const expected = ["armpeg", "headpeg", "legpeg", "torsopeg"];
 
