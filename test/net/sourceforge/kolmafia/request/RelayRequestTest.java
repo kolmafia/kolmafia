7ecb348d8c5d0205356576068ef1d3fa17e18a1b
Jamie Adams <82782908+jaadams5@users.noreply.github.com>
2024-06-26 10:16:19 -0400

    Allow root/relay to be deleted after testing (#2366)
    * Manage relay
    
    * Moved BeforeEach code to test that needed it

--- test/net/sourceforge/kolmafia/request/RelayRequestTest.java
+++ test/net/sourceforge/kolmafia/request/RelayRequestTest.java
@@ -1,15 +1,33 @@
 package net.sourceforge.kolmafia.request;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import net.sourceforge.kolmafia.KoLConstants;
 import net.sourceforge.kolmafia.preferences.Preferences;
+import net.sourceforge.kolmafia.utilities.FileUtilities;
 import org.junit.jupiter.api.Test;
 
 public class RelayRequestTest {
 
+  /**
+   * The global clean up code will delete root/relay since relay is not under Git control. This is
+   * the only test that breaks when that happens. Duplicate the code that loads the relay directory
+   * here so that the directory will be present when the test is run.
+   */
+  public void initializeRelayFileDirectory() {
+    for (int i = 0; i < KoLConstants.RELAY_FILES.length; ++i) {
+      FileUtilities.loadLibrary(
+          KoLConstants.RELAY_LOCATION, KoLConstants.RELAY_DIRECTORY, KoLConstants.RELAY_FILES[i]);
+    }
+  }
+
   @Test
   public void findVariousRelayFilesOrNot() {
+    initializeRelayFileDirectory();
     File f;
     f = RelayRequest.findRelayFile("thisIsNotAPipe");
     assertNotNull(f, "Allowed to find a new file.");
