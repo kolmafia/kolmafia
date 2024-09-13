18811b67fca0dc5fe907edb02b7e4d2e6347d219
Sam Gaus <sam@gaus.co.uk>
2024-09-13 17:47:52 +0100

    Add array of all MafiaClasses to JS stdlib

---
+++ test/root/scripts/mafia_classes.js
@@ -0,0 +1,9 @@
+const { MafiaClasses, print } = require("kolmafia");
+
+print(MafiaClasses.length >= 17);
+
+// Randomly selected subset
+print(MafiaClasses.includes(Coinmaster));
+print(MafiaClasses.includes(Item));
+print(MafiaClasses.includes(Thrall));
+print(MafiaClasses.includes(Vykea));
