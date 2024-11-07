be3e9bb154bb58f872da8d75445a930408ba2e2f
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2024-11-07 23:16:54 +0000

    feat: track split pea soup banish (#2497)
    Also some plurals and consumable data.

--- src/net/sourceforge/kolmafia/request/FightRequest.java
+++ src/net/sourceforge/kolmafia/request/FightRequest.java
@@ -11092,6 +11092,12 @@ public class FightRequest extends GenericRequest {
         }
 
         break;
+
+      case ItemPool.SPLIT_PEA_SOUP:
+        if (responseText.contains("like a pea and split") || itemRunawaySuccess) {
+          BanishManager.banishCurrentMonster(Banisher.SPLIT_PEA_SOUP);
+        }
+        break;
     }
 
     if (itemId != itemId2) {
