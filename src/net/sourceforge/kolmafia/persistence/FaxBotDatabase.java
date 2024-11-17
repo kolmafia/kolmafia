f47c8b5199c522b38bdd6707989e1149b0a1eaf1
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-11-17 18:47:39 -0500

    Suggested changes

--- src/net/sourceforge/kolmafia/persistence/FaxBotDatabase.java
+++ src/net/sourceforge/kolmafia/persistence/FaxBotDatabase.java
@@ -5,12 +5,12 @@ import static net.sourceforge.kolmafia.preferences.Preferences.setBoolean;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HexFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -455,8 +455,8 @@ public class FaxBotDatabase {
       String checksum;
       try {
         byte[] data = Files.readAllBytes(file.toPath());
-        byte[] hash = MessageDigest.getInstance("MD5").digest(data);
-        checksum = new BigInteger(1, hash).toString(16);
+        byte[] hash = MessageDigest.getInstance("SHA-256").digest(data);
+        checksum = HexFormat.of().formatHex(hash);
       } catch (Exception e) {
         checksum = NOHASH;
       }
