716ec45c88b89c37fa85bed2f12110dd3db0a603
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-11-17 20:11:52 -0500

    Park

--- test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
@@ -3,10 +3,12 @@ package net.sourceforge.kolmafia.persistence;
 import static internal.helpers.Player.withNextResponse;
 
 import internal.helpers.Cleanups;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FaxBotDatabaseTest {
 
+  @Disabled
   @Test
   public void configureFax() {
     var cleanups = new Cleanups(withNextResponse(200, "xyzzy"));
