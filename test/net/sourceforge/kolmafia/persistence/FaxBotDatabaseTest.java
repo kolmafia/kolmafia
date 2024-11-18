8b05ce0f11d02f43e6586ab058888a6c9a00041d
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-11-18 15:21:23 -0500

    Park

--- test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
@@ -1,12 +1,27 @@
 package net.sourceforge.kolmafia.persistence;
 
 import static internal.helpers.Player.withNextResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import internal.helpers.Cleanups;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
+
+import java.util.List;
 
 class FaxBotDatabaseTest {
+    @Test
+    public void doThingsForCoverage() {
+      FaxBotDatabase.reconfigure();
+      FaxBotDatabase.FaxBot faxBot = FaxBotDatabase.getFaxbot("OnlyFax");
+      assertEquals(faxBot.getName(), "OnlyFax");
+      assertEquals(faxBot.getPlayerId(), 3690803);
+      List<String> categories = faxBot.getCategories();
+      assertEquals(categories.size(), 13);
+      assertTrue(categories.contains("Standard"));
+    }
 
   @Disabled
   @Test
