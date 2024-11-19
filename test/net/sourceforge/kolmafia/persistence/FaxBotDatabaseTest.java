de3236055df5b7e3229ca9dab32a530f464796d7
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-11-19 12:47:43 -0500

    Not Working

--- test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
@@ -1,5 +1,6 @@
 package net.sourceforge.kolmafia.persistence;
 
+import static internal.helpers.Player.withDataFile;
 import static internal.helpers.Player.withNextResponse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,16 +10,38 @@ import internal.helpers.Cleanups;
 import java.util.List;
 import net.java.dev.spellcast.utilities.LockableListModel;
 import net.sourceforge.kolmafia.MonsterData;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class FaxBotDatabaseTest {
 
-  private static MonsterData valieMonsterData =
-    new MonsterData("angry cavebugbear", 1183,"", "" );
+  public static Cleanups globalCleanup;
+
+  @BeforeAll
+  public static void beforeAll() {
+    globalCleanup =
+        new Cleanups(
+            withDataFile("cheesefax.xml"),
+            withDataFile("easyfax.xml"),
+            withDataFile("onlyfax.xml"));
+    // Configure
+    FaxBotDatabase.reconfigure();
+  }
+
+  @AfterAll
+  public static void afterAll() {
+    globalCleanup.close();
+  }
+
+  private static MonsterData validMonsterData =
+      new MonsterData("angry cavebugbear", 1183, new String[] {"bb_caveman.gif"}, "");
+  private static MonsterData invalidMonsterData =
+      new MonsterData("not a real monster", 1183, new String[] {"bb_caveman.gif"}, "");
+
   @Test
   public void doThingsForCoverage() {
-    FaxBotDatabase.reconfigure();
     FaxBotDatabase.FaxBot faxBot = FaxBotDatabase.getFaxbot("OnlyFax");
     assertEquals(faxBot.getName(), "OnlyFax");
     assertEquals(faxBot.getPlayerId(), 3690803);
@@ -33,27 +56,24 @@ class FaxBotDatabaseTest {
   public void makeARequestWithMonsterData() {
     FaxBotDatabase.reconfigure();
     FaxBotDatabase.FaxBot faxBot = FaxBotDatabase.getFaxbot("OnlyFax");
-    String name = "angry cavebugbear";
-    String attributes = "";
-    int id = 1183;
-    String[] images = {"bb_caveman.gif"};
-    MonsterData monster = new MonsterData(name, id, images, attributes);
     // as currently written faxbot.request is expected to fail in the test environment
     // because the faxbot is not online and the test has not yet provided resposes that
     // would indicate otherwise.
-    boolean result = faxBot.request(monster);
+    boolean result = faxBot.request(validMonsterData);
     assertFalse(result);
   }
 
   @Test
   public void coverageForInvalidFaxbot() {
     FaxBotDatabase.FaxBot faxBot = new FaxBotDatabase.FaxBot(null, "notReal");
-    String name = "angry cavebugbear";
-    String attributes = "";
-    int id = 1183;
-    String[] images = {"bb_caveman.gif"};
-    MonsterData monster = new MonsterData(name, id, images, attributes);
-    boolean result = faxBot.request(monster);
+    boolean result = faxBot.request(validMonsterData);
+    assertFalse(result);
+  }
+
+  @Test
+  public void coverageForInvalidMonster() {
+    FaxBotDatabase.FaxBot faxBot = FaxBotDatabase.getFaxbot("OnlyFax");
+    boolean result = faxBot.request(invalidMonsterData);
     assertFalse(result);
   }
 
