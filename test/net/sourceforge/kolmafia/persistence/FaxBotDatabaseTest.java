b4afa7035f0f3368b0c39c597986584b3c8722c6
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2024-11-18 19:27:32 -0500

    Park but not debugged

--- test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/FaxBotDatabaseTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import internal.helpers.Cleanups;
+import net.java.dev.spellcast.utilities.LockableListModel;
+import net.sourceforge.kolmafia.MonsterData;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 import java.util.List;
+import java.util.Map;
 
 class FaxBotDatabaseTest {
     @Test
@@ -21,6 +24,21 @@ class FaxBotDatabaseTest {
       List<String> categories = faxBot.getCategories();
       assertEquals(categories.size(), 13);
       assertTrue(categories.contains("Standard"));
+      List<LockableListModel<FaxBotDatabase.Monster>> monCat = faxBot.getMonstersByCategory();
+      assertEquals(monCat.size(), 13);
+    }
+
+    @Test
+    public void makeARequestWithMonsterData() {
+      FaxBotDatabase.reconfigure();
+      FaxBotDatabase.FaxBot faxBot = FaxBotDatabase.getFaxbot("OnlyFax");
+      String name = "angry bugbear";
+      String attributes = "";
+      int id = 256;
+      String[] images = {"angbugbear.gif"};
+      MonsterData monster = new MonsterData(name, id, images, attributes);
+      boolean result = faxBot.request(monster);
+      assertTrue(result);
     }
 
   @Disabled
