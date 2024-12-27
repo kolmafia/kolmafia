015da71b2f34246cff50c2a323f263e8c357ddd4
Patrick Hulin <patrick.hulin@gmail.com>
2024-12-27 11:25:42 -0500

    Differentiate this Egg is walkable. (#2586)

--- src/net/sourceforge/kolmafia/session/ChoiceControl.java
+++ src/net/sourceforge/kolmafia/session/ChoiceControl.java
@@ -10189,6 +10189,7 @@ public abstract class ChoiceControl {
       case 1501: // Make a Wish
       case 1509: // Adjust Jill-of-All-Trades Lighting
       case 1510: // Burning Leaves
+      case 1516: // Differentiate this Egg
       case 1517: // Mimic DNA Bank
       case 1518: // Prepare your Meal
       case 1523: // Research Bench
