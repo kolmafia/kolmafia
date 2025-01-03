d340bfb56f370053b5f2929f86a785beaa886bc5
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2025-01-03 10:33:07 -0500

    re-fix test, since registering currencies allows us to reconize old-style coinmasters again

--- test/net/sourceforge/kolmafia/request/CoinMasterRequestTest.java
+++ test/net/sourceforge/kolmafia/request/CoinMasterRequestTest.java
@@ -152,15 +152,9 @@ public class CoinMasterRequestTest {
             text.contains("Elf Guard Armory\tsell\t3\tElf Guard commandeering gloves\tROW1412"));
         assertFalse(
             text.contains("Elf Guard Armory\tsell\t3\tElf Guard officer's sidearm\tROW1413"));
-        // assertTrue(text.contains("Elf Guard Armory\tsell\t3\tKelflar vest\tROW1415"));
-        assertTrue(
-            text.contains("Elf Guard Armory\tROW1415\tElf Army machine parts (3)\tKelflar vest"));
+        assertTrue(text.contains("Elf Guard Armory\tsell\t3\tKelflar vest\tROW1415"));
         assertFalse(text.contains("Elf Guard Armory\tsell\t3\tElf Guard mouthknife\tROW1416"));
-        // assertTrue(text.contains("Elf Guard Armory\tbuy\t200\tElf Guard honor
-        // present\tROW1411"));
-        assertTrue(
-            text.contains(
-                "Elf Guard Armory\tROW1411\tElf Guard honor present\tElf Army machine parts"));
+        assertTrue(text.contains("Elf Guard Armory\tbuy\t200\tElf Guard honor present\tROW1411"));
 
         var requests = client.getRequests();
         assertThat(requests, hasSize(1));
