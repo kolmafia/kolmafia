225aec3ba23c4a40ddea0869936a6c8877efdab1
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2025-01-05 11:45:30 -0500

    test leakage?

--- test/net/sourceforge/kolmafia/session/ChoiceAdventuresTest.java
+++ test/net/sourceforge/kolmafia/session/ChoiceAdventuresTest.java
@@ -11,21 +11,14 @@ import net.sourceforge.kolmafia.KoLCharacter;
 import net.sourceforge.kolmafia.equipment.Slot;
 import net.sourceforge.kolmafia.objectpool.EffectPool;
 import net.sourceforge.kolmafia.objectpool.FamiliarPool;
-import net.sourceforge.kolmafia.preferences.Preferences;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ChoiceAdventuresTest {
-  @BeforeAll
-  public static void beforeAll() {
-    KoLCharacter.reset("ChoiceAdventures");
-  }
-
   @BeforeEach
-  public void beforeEach() {
-    Preferences.reset("ChoiceAdventures");
+  public void beforeAll() {
+    KoLCharacter.reset("ChoiceAdventures");
   }
 
   @Nested
