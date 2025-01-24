e5e125611b46e2a7dcce742f990203156583ad7a
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2025-01-24 16:15:12 -0500

    park

--- test/net/sourceforge/kolmafia/textui/DataTypesTest.java
+++ test/net/sourceforge/kolmafia/textui/DataTypesTest.java
@@ -2,14 +2,15 @@ package net.sourceforge.kolmafia.textui;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class DataTypesTest {
   @Test
   public void itShouldRecognizeJackingAsLocation() {
     String name = "Professor Jacking\'s Huge-A-Ma-Tron";
-    //name = "The Haunted Gallery";
-    //String output = execute("to_location("+name+")");
-    //assertTrue(false);
+    name = "The Haunted Gallery";
     var parsed = DataTypes.parseLocationValue(name, false);
+    String xxx = parsed.toString();
     assertTrue(name.equals(parsed.toString()));
   }
 
