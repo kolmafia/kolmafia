d55f63682bc506a8ca4d1a4a080abfcefb479b3e
Veracity0 <83725298+Veracity0@users.noreply.github.com>
2025-02-04 09:24:00 -0500

    test for quantity=0

--- test/net/sourceforge/kolmafia/request/PixelRequestTest.java
+++ test/net/sourceforge/kolmafia/request/PixelRequestTest.java
@@ -68,4 +68,29 @@ public class PixelRequestTest {
       }
     }
   }
+
+  @Test
+  public void askingForZeroMakesOne() {
+    var cleanups = new Cleanups();
+    // 1 of each of red / green / blue pixel
+    BASE_PIXELS.stream().map(Player::withItem).forEach(cleanups::add);
+
+    try (cleanups) {
+      Concoction whitePixel = ConcoctionPool.get(ItemPool.WHITE_PIXEL);
+      PixelRequest request = new PixelRequest(whitePixel);
+
+      // Check pixel counts.
+      for (int itemId : BASE_PIXELS) {
+        assertThat(ItemPool.get(itemId), isInInventory(1));
+      }
+
+      String url = "shop.php?whichshop=mystic&action=buyitem&whichrow=26&quantity=0";
+      String responseText = html("request/test_white_pixel_0.html");
+      ShopRequest.parseResponse(url, responseText);
+
+      for (int itemId : BASE_PIXELS) {
+        assertThat(ItemPool.get(itemId), isInInventory(0));
+      }
+    }
+  }
 }
