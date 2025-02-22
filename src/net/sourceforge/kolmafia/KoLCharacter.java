609a6c159571097e06ad1ef8f113d43b2a6fb373
Patrick Hulin <patrick.hulin@gmail.com>
2025-02-22 13:01:39 -0500

    Grafted familiars can't be used in Zootomist. (#2769)
    * Grafted familiars can't be used in Zootomist.
    
    * handle comments.
    
    ---------
    
    Co-authored-by: Jamie Adams <82782908+jaadams5@users.noreply.github.com>

--- src/net/sourceforge/kolmafia/KoLCharacter.java
+++ src/net/sourceforge/kolmafia/KoLCharacter.java
@@ -13,6 +13,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import net.java.dev.spellcast.utilities.LockableListModel;
 import net.java.dev.spellcast.utilities.SortedListModel;
 import net.sourceforge.kolmafia.AscensionPath.Path;
@@ -4436,13 +4437,32 @@ public abstract class KoLCharacter {
         .orElse(null);
   }
 
+  private static Stream<Integer> graftedFamiliars() {
+    return Stream.of(
+            "zootGraftedButtCheekLeftFamiliar",
+            "zootGraftedButtCheekRightFamiliar",
+            "zootGraftedFootLeftFamiliar",
+            "zootGraftedFootRightFamiliar",
+            "zootGraftedHandLeftFamiliar",
+            "zootGraftedHandRightFamiliar",
+            "zootGraftedHeadFamiliar",
+            "zootGraftedNippleLeftFamiliar",
+            "zootGraftedNippleRightFamiliar",
+            "zootGraftedShoulderLeftFamiliar",
+            "zootGraftedShoulderRightFamiliar")
+        .map(pref -> Preferences.getInteger(pref))
+        .filter(id -> id > 0);
+  }
+
   private static boolean isUsable(FamiliarData f) {
     if (f == FamiliarData.NO_FAMILIAR) return !KoLCharacter.inQuantum();
 
     return StandardRequest.isAllowed(f)
         && (!KoLCharacter.inZombiecore() || f.isUndead())
         && (!KoLCharacter.inBeecore() || !KoLCharacter.hasBeeosity(f.getRace()))
-        && (!KoLCharacter.inGLover() || KoLCharacter.hasGs(f.getRace()));
+        && (!KoLCharacter.inGLover() || KoLCharacter.hasGs(f.getRace()))
+        && (!KoLCharacter.inZootomist()
+            || !KoLCharacter.graftedFamiliars().anyMatch(id -> id == f.getId()));
   }
 
   /**
