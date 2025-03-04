5f08425a36caffe39cd86845ae2bda372b449d79
Sam Gaus <sam@gaus.co.uk>
2025-03-04 11:25:13 +0000

    Testing

--- test/net/sourceforge/kolmafia/session/LeprecondoManagerTest.java
+++ test/net/sourceforge/kolmafia/session/LeprecondoManagerTest.java
@@ -5,12 +5,22 @@ import static internal.helpers.Player.withItem;
 import static internal.helpers.Player.withProperty;
 import static internal.matchers.Preference.isSetTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import internal.helpers.Cleanups;
+import net.sourceforge.kolmafia.KoLCharacter;
+import net.sourceforge.kolmafia.preferences.Preferences;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LeprecondoManagerTest {
+  @BeforeEach
+  public void beforeEach() {
+    KoLCharacter.reset("LeprecondoManagerTest");
+    Preferences.reset("LeprecondoManagerTest");
+  }
+
   @ParameterizedTest
   @CsvSource(
       value = {
@@ -32,6 +42,17 @@ class LeprecondoManagerTest {
     try (cleanups) {
       LeprecondoManager.processNeedChange(need);
       assertThat("leprecondoNeedOrder", isSetTo(expected));
+    }
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {"The Marinara Trench,sensory deprivation tank", "An Octopus's Garden,"})
+  void getsUndiscoveredFurnitureForLocation(final String zone, final String expected) {
+    var cleanups =
+        new Cleanups(withItem("Leprecondo"), withProperty("leprecondoDiscovered", "1,2,3,4,5,6,7"));
+    try (cleanups) {
+      var furniture = LeprecondoManager.getUndiscoveredFurnitureForLocation(zone);
+      assertThat(furniture, equalTo(expected));
     }
   }
 }
