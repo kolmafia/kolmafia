025ec684ce1f17e60d0d8c0373f9bd722b683ffe
Sam Gaus <sam@gaus.co.uk>
2025-04-24 10:57:18 +0100

    Flatten out loops, do not make calcs until we know we need them

--- src/net/sourceforge/kolmafia/session/InventoryManager.java
+++ src/net/sourceforge/kolmafia/session/InventoryManager.java
@@ -1309,36 +1309,39 @@ public abstract class InventoryManager {
     }
 
     CraftingType method = ConcoctionDatabase.getMixingMethod(item);
+    long price = ConcoctionDatabase.getCreationCost(method);
+    AdventureResult[] ingredients = ConcoctionDatabase.getIngredients(itemId);
+
+    if (ingredients.length == 0) {
+      // This is a concoction with no ingredients, so if creatable == 0,
+      // we can be sure that we cannot concoct it right now.
+      var conc = ConcoctionPool.get(itemId);
+      if (conc == null || conc.creatable == 0) {
+        return Long.MAX_VALUE;
+      }
+      if (price == 0) {
+        return 0;
+      }
+    }
 
     long yield = ConcoctionDatabase.getYield(itemId);
     long madeQuantity = (quantity + yield - 1) / yield;
-    long price = ConcoctionDatabase.getCreationCost(method) * madeQuantity;
+    price *= madeQuantity;
 
-    AdventureResult[] ingredients = ConcoctionDatabase.getIngredients(itemId);
+    for (AdventureResult ingredient : ingredients) {
+      long needed = ingredient.getCount() * madeQuantity;
 
-    // This is a concoction with no ingredients, so if creatable = 0, we can be sure that we cannot
-    // concoct it right now.
-    if (ingredients.length == 0) {
-      var conc = ConcoctionPool.get(itemId);
-      if (conc != null && conc.creatable == 0) {
-        return Long.MAX_VALUE;
+      long ingredientPrice =
+          ingredient.isMeat()
+              ? needed
+              : InventoryManager.priceToAcquire(
+                  ingredient.getInstance(needed), exact, mallPriceOnly, level + 1);
+
+      if (ingredientPrice == Long.MAX_VALUE) {
+        return ingredientPrice;
       }
-    } else {
-      for (AdventureResult ingredient : ingredients) {
-        long needed = ingredient.getCount() * madeQuantity;
 
-        long ingredientPrice =
-            ingredient.isMeat()
-                ? needed
-                : InventoryManager.priceToAcquire(
-                    ingredient.getInstance(needed), exact, mallPriceOnly, level + 1);
-
-        if (ingredientPrice == Long.MAX_VALUE) {
-          return ingredientPrice;
-        }
-
-        price += ingredientPrice;
-      }
+      price += ingredientPrice;
     }
 
     return price * quantity / (yield * madeQuantity);
