fed0f506d4ab5ea4fe0d931fa6249382c908f10c
jaadams5 <82782908+jaadams5@users.noreply.github.com>
2025-04-29 19:34:43 -0400

    Park

--- test/net/sourceforge/kolmafia/maximizer/IterativeTest.java
+++ test/net/sourceforge/kolmafia/maximizer/IterativeTest.java
@@ -1,15 +1,16 @@
 package net.sourceforge.kolmafia.maximizer;
 
+import static internal.helpers.Maximizer.getBoosts;
+import static internal.helpers.Maximizer.maximize;
 import static internal.helpers.Player.withClass;
 import static internal.helpers.Player.withEquippableItem;
 import static internal.helpers.Player.withItem;
 import static internal.helpers.Player.withLevel;
-import static internal.helpers.Player.withStats;
 
 import internal.helpers.Cleanups;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
-
 import net.sourceforge.kolmafia.AdventureResult;
 import net.sourceforge.kolmafia.AscensionClass;
 import net.sourceforge.kolmafia.KoLCharacter;
@@ -32,23 +33,35 @@ public class IterativeTest {
   public void theTest(
       @CartesianTest.Values(ints = {1, 2, 3, 4, 5, 6}) int enumClassId,
       @CartesianTest.Values(ints = {1, 5, 13}) int level,
+      @CartesianTest.Values(strings = {"", "weapon damage", "ranged damage"}) String maxStringPart1,
+      @CartesianTest.Values(strings = {"", "effective"}) String maxStringPart2,
       @CartesianTest.Values(booleans = {true, false}) boolean equippable) {
     var cleanups = new Cleanups();
     AscensionClass thisClass = AscensionClass.find(enumClassId);
     cleanups.add(withClass(thisClass));
-    cleanups.add(withStats(level, level, level));
+    cleanups.add(withLevel(level));
     dumpStats("Before");
     EnumMap<Slot, AdventureResult> equipment = EquipmentManager.allEquipment();
     for (Map.Entry<Slot, AdventureResult> entry : equipment.entrySet()) {
-      if ((entry.getKey() == Slot.WEAPON) || (entry.getKey()== Slot.OFFHAND)) {
+      if ((entry.getKey() == Slot.WEAPON) || (entry.getKey() == Slot.OFFHAND)) {
         AdventureResult thing = entry.getValue();
         if (equippable) {
           cleanups.add(withEquippableItem(thing));
+        } else {
+          cleanups.add(withItem(thing));
         }
-        cleanups.add(withItem(thing));
       }
     }
     dumpStats("After");
+    String maxStr = maxStringPart1 + " " + maxStringPart2;
+    try (cleanups) {
+      maximize(maxStr);
+    }
+    List<Boost> boosts = getBoosts();
+    String boostString = boosts.toString();
+    if (!boostString.contains("nothing useful found")) {
+      System.out.println(boosts);
+    }
   }
 
   private void dumpStats(String before) {
