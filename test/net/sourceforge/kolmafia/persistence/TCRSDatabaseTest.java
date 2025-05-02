05b174d1f1ae30677e282c388046b2416fb1fc63
Samuel Gaus <sam@gaus.co.uk>
2025-05-02 22:19:36 +0100

    Carry over non-randomised TCRS modifiers (#2903)
    * Carry over modifiers
    
    * File linting and formatting
    
    * Test the carry-over code
    
    * Add html fixture
    
    * Fix test

--- test/net/sourceforge/kolmafia/persistence/TCRSDatabaseTest.java
+++ test/net/sourceforge/kolmafia/persistence/TCRSDatabaseTest.java
@@ -1,12 +1,18 @@
 package net.sourceforge.kolmafia.persistence;
 
+import static internal.helpers.Networking.html;
 import static internal.helpers.Player.withClass;
+import static internal.helpers.Player.withHttpClientBuilder;
 import static internal.helpers.Player.withPath;
 import static internal.helpers.Player.withSign;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
 import internal.helpers.Cleanups;
+import internal.network.FakeHttpClientBuilder;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -84,6 +90,31 @@ public class TCRSDatabaseTest {
       TCRSDatabase.loadTCRSData();
       Modifiers mods = ModifierDatabase.getModifiers(ModifierType.ITEM, ItemPool.CHATEAU_SKYLIGHT);
       assertThat(mods.getDouble(DoubleModifier.ADVENTURES), is(3.0));
+    }
+  }
+
+  @Test
+  void someModifiersCarryOver() {
+    var builder = new FakeHttpClientBuilder();
+    var client = builder.client;
+    var cleanups =
+        new Cleanups(
+            withPath(Path.CRAZY_RANDOM_SUMMER_TWO),
+            withClass(AscensionClass.SEAL_CLUBBER),
+            withSign(ZodiacSign.MONGOOSE),
+            withHttpClientBuilder(builder));
+
+    client.addResponse(
+        200, html("request/test_desc_item_tcrs_ring_of_telling_skeletons_what_to_do.html"));
+
+    try (cleanups) {
+      ModifierDatabase.resetModifiers();
+      var ring = TCRSDatabase.deriveItem(ItemPool.RING_OF_TELLING_SKELETONS_WHAT_TO_DO);
+      assertThat(ring, not(nullValue()));
+      assertThat(
+          ring.modifiers,
+          equalTo(
+              "Hot Resistance: +1, Conditional Skill (Equipped): \"Tell a Skeleton What To Do\", Conditional Skill (Equipped): \"Tell This Skeleton What To Do\""));
     }
   }
 }
