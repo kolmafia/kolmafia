6838f3449f89d56641356bc2220c9ae6bec403ed
Samuel Gaus <sam@gaus.co.uk>
2025-05-04 10:27:08 +0100

    Fix further issues supplying a "bandersnatch" value for the Peridot choice (#2907)
    * Fix peridot parsing by widening choice parsing appropriately
    
    * Linting for ChoiceUtilities class
    
    * Formatting
    
    * Quick fix of semi-unrelated issue
    
    * Support filling extra hidden fields with the extra field in choiceAdventureXXX
    
    * Remove accidental whitespace
    
    * Update src/net/sourceforge/kolmafia/utilities/ChoiceUtilities.java
    
    Co-authored-by: Chris Midgley <chris.midgley@dunecomputers.co.uk>
    
    ---------
    
    Co-authored-by: Chris Midgley <chris.midgley@dunecomputers.co.uk>

--- test/net/sourceforge/kolmafia/utilities/ChoiceUtilitiesTest.java
+++ test/net/sourceforge/kolmafia/utilities/ChoiceUtilitiesTest.java
@@ -4,12 +4,42 @@ import static internal.helpers.Networking.html;
 import static internal.helpers.Player.withProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ChoiceUtilitiesTest {
+  @Nested
+  class ValidateChoiceFields {
+    @Test
+    void cannotSupplyRandomExtraValues() {
+      var page = html("request/test_choice_peridot_zone.html");
+      var errors = ChoiceUtilities.validateChoiceFields("1", "author=gausie&bandersnatch=1", page);
+      assertThat(errors, equalTo("Choice option 1557/1 does not require 'author'.\n"));
+    }
+
+    @Test
+    void cannotMissRequiredExtraValues() {
+      var page = html("request/test_choice_peridot_zone.html");
+      var errors = ChoiceUtilities.validateChoiceFields("1", "", page);
+      assertThat(
+          errors, equalTo("Choice option 1557/1 requires 'bandersnatch' but not supplied.\n"));
+    }
+
+    @Test
+    void canMixMultipleErrorMessages() {
+      var page = html("request/test_choice_peridot_zone.html");
+      var errors = ChoiceUtilities.validateChoiceFields("1", "author=gausie", page);
+      assertThat(
+          errors,
+          equalTo(
+              "Choice option 1557/1 requires 'bandersnatch' but not supplied.\nChoice option 1557/1 does not require 'author'.\n"));
+    }
+  }
 
   @Test
   void extractChoiceFromLyleOnDevServer() {
@@ -22,13 +52,23 @@ class ChoiceUtilitiesTest {
     }
   }
 
-  @Test
-  void parseChoicesFromPeridot() {
-    var page = html("request/test_choice_peridot_zone.html");
-    var choices = ChoiceUtilities.parseChoices(page);
-    assertThat(choices, aMapWithSize(2));
-    // The first value for option 1
-    assertThat(choices, hasEntry(1, "a sleeping Knob Goblin Guard"));
-    assertThat(choices, hasEntry(2, "I choose peace"));
+  @Nested
+  class PeridotOfPeril {
+    @Test
+    void parseChoicesFromPeridot() {
+      var page = html("request/test_choice_peridot_zone.html");
+      var choices = ChoiceUtilities.parseChoices(page);
+      assertThat(choices, aMapWithSize(2));
+      // The first value for option 1
+      assertThat(choices, hasEntry(1, "a sleeping Knob Goblin Guard"));
+      assertThat(choices, hasEntry(2, "I choose peace"));
+    }
+
+    @Test
+    void canSupplyBandersnatchToPeridot() {
+      var page = html("request/test_choice_peridot_zone.html");
+      var errors = ChoiceUtilities.validateChoiceFields("1", "bandersnatch=555", page);
+      assertThat(errors, is(nullValue()));
+    }
   }
 }
