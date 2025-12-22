7c1b6550305717acf20c2a849dc88d6d7f4f2640
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2025-12-22 20:24:24 +0000

    feat: convert precinct to new coinmaster, add commented rows

--- src/net/sourceforge/kolmafia/request/coinmaster/shop/PrecinctRequest.java
+++ src/net/sourceforge/kolmafia/request/coinmaster/shop/PrecinctRequest.java
@@ -1,27 +1,21 @@
 package net.sourceforge.kolmafia.request.coinmaster.shop;
 
-import java.util.regex.Pattern;
-import net.sourceforge.kolmafia.AdventureResult;
 import net.sourceforge.kolmafia.CoinmasterData;
-import net.sourceforge.kolmafia.objectpool.ItemPool;
+import net.sourceforge.kolmafia.preferences.Preferences;
 
 public abstract class PrecinctRequest extends CoinMasterShopRequest {
   public static final String master = "Precinct Materiel Division";
   public static final String SHOPID = "detective";
 
-  private static final Pattern TOKEN_PATTERN = Pattern.compile("<td>([\\d,]+) cop dollar");
-  public static final AdventureResult DOLLAR = ItemPool.get(ItemPool.COP_DOLLAR, 1);
-
   public static final CoinmasterData PRECINCT =
       new CoinmasterData(master, "Precinct Materiel Division", PrecinctRequest.class)
-          .withToken("cop dollar")
-          .withTokenPattern(TOKEN_PATTERN)
-          .withItem(DOLLAR)
-          .withShopRowFields(master, SHOPID)
+          .withNewShopRowFields(master, SHOPID)
           .withAccessible(PrecinctRequest::accessible);
 
   public static String accessible() {
-    // *** Finish this.
-    return null;
+    if (Preferences.getBoolean("hasDetectiveSchool")) {
+      return null;
+    }
+    return "You cannot access the Precinct";
   }
 }
