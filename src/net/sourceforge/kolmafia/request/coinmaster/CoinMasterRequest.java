2c488922fa6959e3883edf78d4dfffe5335a46f4
benjaminphillabaum <bphillab@gmail.com>
2026-01-10 13:18:36 -0500

    Missed a spot when restoring

--- src/net/sourceforge/kolmafia/request/coinmaster/CoinMasterRequest.java
+++ src/net/sourceforge/kolmafia/request/coinmaster/CoinMasterRequest.java
@@ -368,7 +368,7 @@ public class CoinMasterRequest extends GenericRequest {
     String buy = data.getBuyAction();
     String sell = data.getSellAction();
 
-    if (buy == null && sell == null && data.getBuyItems() == null) {
+    if (buy == null && sell == null) {
 
       // You can neither buy nor sell from this Coinmaster?
       return;
