787e4cd3899edb70bd502e8681e23d22602172c0
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2026-05-19 10:04:35 +0100

    fix: Effect data was a tree map, keep it a tree map

--- src/net/sourceforge/kolmafia/persistence/EffectDatabase.java
+++ src/net/sourceforge/kolmafia/persistence/EffectDatabase.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -37,7 +36,7 @@ import net.sourceforge.kolmafia.utilities.StringUtilities;
 public class EffectDatabase {
   private static String[] canonicalNames = new String[0];
   private static final Map<String, int[]> effectIdSetByName = new TreeMap<>();
-  private static final Map<Integer, EffectData> effectDataById = new LinkedHashMap<>();
+  private static final Map<Integer, EffectData> effectDataById = new TreeMap<>();
   private static final Map<String, Integer> effectIdByDescription = new HashMap<>();
 
   public static boolean newEffects = false;
