603e78da17d437b766db17053b5da47900cdd2f9
libraryaddict <libraryaddict@users.noreply.github.com>
2026-06-06 11:51:46 +1200

    Change order of operations to have most expensive go last

--- src/net/sourceforge/kolmafia/maximizer/Evaluator.java
+++ src/net/sourceforge/kolmafia/maximizer/Evaluator.java
@@ -1188,9 +1188,9 @@ public class Evaluator {
         }
 
         if (item.getCount() != 0
-            && (this.getScore(familiarMods, Map.of(Slot.FAMILIAR, item)) - nullScore > 0.0
-                || item.automaticFlag
-                || this.posEquip.contains(item))) {
+            && (item.automaticFlag
+                || this.posEquip.contains(item)
+                || this.getScore(familiarMods, Map.of(Slot.FAMILIAR, item)) - nullScore > 0.0)) {
           ranked.get(Slot.FAMILIAR).add(item);
         }
       }
@@ -1226,9 +1226,9 @@ public class Evaluator {
         }
 
         if (item.getCount() != 0
-            && (this.getScore(familiarMods, Map.of(Slot.FAMILIAR, item)) - nullScore > 0.0
-                || item.automaticFlag
-                || this.posEquip.contains(item))) {
+            && (item.automaticFlag
+                || this.posEquip.contains(item)
+                || this.getScore(familiarMods, Map.of(Slot.FAMILIAR, item)) - nullScore > 0.0)) {
           ranked.getFamiliar(f).add(item);
         }
       }
