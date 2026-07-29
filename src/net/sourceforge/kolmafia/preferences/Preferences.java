70caea0b0284ce2fdaa7e40e2c1946ffce51ef0c
Jamie Adams <82782908+jaadams5@users.noreply.github.com>
2026-07-29 16:03:25 -0400

    Park

--- src/net/sourceforge/kolmafia/preferences/Preferences.java
+++ src/net/sourceforge/kolmafia/preferences/Preferences.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.AtomicMoveNotSupportedException;
 import java.nio.file.Files;
