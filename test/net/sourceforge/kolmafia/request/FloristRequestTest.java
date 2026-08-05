780023ab49583c03d5c862fbe474a52219a36d06
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2026-08-05 10:44:25 +0100

    remove floristFriarChecked
    Now that we can rely on ownsFloristFriar being set from api.php coolitems, remove floristFriarChecked and don't check if floristFriar is available unless it is owned

--- test/net/sourceforge/kolmafia/request/FloristRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FloristRequestTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import net.sourceforge.kolmafia.AscensionPath.Path;
 import net.sourceforge.kolmafia.KoLCharacter;
-import net.sourceforge.kolmafia.persistence.QuestDatabase;
-import net.sourceforge.kolmafia.persistence.QuestDatabase.Quest;
 import net.sourceforge.kolmafia.preferences.Preferences;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,40 +18,34 @@ public class FloristRequestTest {
   }
 
   @Test
-  public void checked() {
-    FloristRequest.checkFloristAvailable();
+  public void unowned() {
+    Preferences.setBoolean("ownsFloristFriar", false);
+    FloristRequest.setFloristFriarAvailable(true);
     assertFalse(FloristRequest.haveFlorist());
-    assertFalse(Preferences.getBoolean("floristFriarChecked"));
-
-    QuestDatabase.setQuest(Quest.LARVA, QuestDatabase.STARTED);
-    FloristRequest.checkFloristAvailable();
-    assertFalse(FloristRequest.haveFlorist());
-    assertTrue(Preferences.getBoolean("floristFriarChecked"));
   }
 
   @Test
   public void unavailable() {
+    Preferences.setBoolean("ownsFloristFriar", true);
     String responseText = html("request/test_cant_get_there.html");
     FloristRequest.parseResponse("choice.php?whichchoice=720", responseText);
     assertFalse(FloristRequest.haveFlorist());
-    assertFalse(Preferences.getBoolean("floristFriarChecked"));
   }
 
   @Test
   public void available() {
+    Preferences.setBoolean("ownsFloristFriar", true);
     String responseText = html("request/test_florist_friar.html");
     FloristRequest.parseResponse("choice.php?whichchoice=720", responseText);
     assertTrue(FloristRequest.haveFlorist());
-    assertTrue(Preferences.getBoolean("floristFriarChecked"));
   }
 
   @Test
   public void availableLegacy() {
+    Preferences.setBoolean("ownsFloristFriar", true);
     KoLCharacter.setPath(Path.LEGACY_OF_LOATHING);
     String responseText = html("request/test_florist_friar.html");
     FloristRequest.parseResponse("choice.php?whichchoice=720", responseText);
     assertTrue(FloristRequest.haveFlorist());
-    assertTrue(Preferences.getBoolean("floristFriarChecked"));
-    assertFalse(Preferences.getBoolean("ownsFloristFriar"));
   }
 }
