c62bf73fe52b2f8ee8f2eb1f9a7c46e43a938081
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2026-08-05 11:01:01 +0100

    Revert "Fix test"
    This reverts commit 1252682c5e95a9f9c97d239f39cca00767dc4361.

--- test/net/sourceforge/kolmafia/DebugModifiersTest.java
+++ test/net/sourceforge/kolmafia/DebugModifiersTest.java
@@ -357,9 +357,7 @@ public class DebugModifiersTest {
   @Test
   void listsFlorist() {
     try (var cleanups =
-        new Cleanups(
-            withProperty("ownsFloristFriar", true),
-            withFlorist(AdventurePool.NOOB_CAVE, FloristRequest.Florist.HORN_OF_PLENTY))) {
+        withFlorist(AdventurePool.NOOB_CAVE, FloristRequest.Florist.HORN_OF_PLENTY)) {
       evaluateDebugModifiers(DoubleModifier.ITEMDROP);
       assertThat(output(), containsDebugRow("Florist", "Horn of Plenty", 25.0, 25.0));
     }
