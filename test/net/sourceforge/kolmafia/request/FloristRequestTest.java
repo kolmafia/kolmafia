7ac8e5f8a413ec788767f70a99d3c3025da75c33
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2026-08-17 09:46:47 +0100

    chore: spotless

--- test/net/sourceforge/kolmafia/request/FloristRequestTest.java
+++ test/net/sourceforge/kolmafia/request/FloristRequestTest.java
@@ -60,8 +60,7 @@ public class FloristRequestTest {
             withProperty("ownsFloristFriar", true),
             withPath(Path.STANDARD),
             withRestricted(true),
-            withNotAllowedInStandard(
-                RestrictedItemType.BOOKSHELF_BOOKS, "Florist Friar"))) {
+            withNotAllowedInStandard(RestrictedItemType.BOOKSHELF_BOOKS, "Florist Friar"))) {
       assertThat(FloristRequest.haveFlorist(), is(false));
     }
   }
