0f8d71e202ee43cd2a8d65e5f3fd4547c60bdd14
Mark Duda <markduda@microsoft.com>
2026-09-02 16:36:03 -0400

    Short-circuit UNEQUIP candidate setup

--- src/net/sourceforge/kolmafia/maximizer/CheckedItem.java
+++ src/net/sourceforge/kolmafia/maximizer/CheckedItem.java
@@ -35,17 +35,17 @@ public class CheckedItem extends AdventureResult {
       boolean ignoreStandardRestriction) {
     super(itemId, 1, false);
 
-    this.inventory = InventoryManager.getCount(itemId);
-
-    this.initial = InventoryManager.getAccessibleCount(itemId, true, ignoreStandardRestriction);
-    boolean isCodpieceGem = EquipmentDatabase.isCodpieceGem(itemId);
-
-    // special case used to get a CheckedItem that equals UNEQUIP.
+    // Special case used to get a CheckedItem that equals UNEQUIP.
     if (itemId == -1) {
       this.name = "(none)";
       this.inventory = Integer.MAX_VALUE;
       this.initial = Integer.MAX_VALUE;
+      return;
     }
+
+    this.inventory = InventoryManager.getCount(itemId);
+    this.initial = InventoryManager.getAccessibleCount(itemId, true, ignoreStandardRestriction);
+    boolean isCodpieceGem = EquipmentDatabase.isCodpieceGem(itemId);
 
     String itemName = this.getName();
     int maxUseful = 3;
