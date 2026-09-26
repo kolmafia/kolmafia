a0ea5e80b65f5d7ad734034b2e4071f47cb75483
Samuel Gaus <sam@gaus.co.uk>
2026-09-26 10:29:06 +0100

    Fix spotless wrapping

--- src/net/sourceforge/kolmafia/KoLmafia.java
+++ src/net/sourceforge/kolmafia/KoLmafia.java
@@ -695,8 +695,7 @@ public abstract class KoLmafia {
       boolean shouldResetCounters = false;
       boolean shouldResetGlobalCounters = false;
       // Assume if rollover has changed by an hour, it is a new rollover. Time varies slightly
-      // between
-      // servers by a few seconds.
+      // between servers by a few seconds.
       shouldResetCounters =
           KoLCharacter.getRollover() - Preferences.getLong("lastCounterDay") > 3600;
       shouldResetGlobalCounters =
